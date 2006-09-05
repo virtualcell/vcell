046e30b1c175e5e899e51c1aef2bd5d23ef81445
Jim Schaff <schaff@uchc.edu>
2006-09-05 17:59:25 +0000

    svn path=/VCell5/trunk/; revision=125

---
+++ VCellOpt/org/vcell/optimization/Optimization.java
@@ -0,0 +1,33 @@
+package org.vcell.optimization;
+
+import java.util.Properties;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.vcell.model.analysis.IAnalysisTask;
+import org.vcell.model.analysis.IAnalysisTaskFactory;
+
+import cbit.vcell.modelopt.ParameterEstimationTaskXMLPersistence;
+import cbit.vcell.xml.XMLTags;
+
+public class Optimization implements BundleActivator {
+
+		/*
+		 * (non-Javadoc)
+		 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+		 */
+		public void start(BundleContext context) throws Exception {
+	        System.out.println("Starting org.vcell.optimization.Optimization");
+	        Properties props = new Properties();
+	        props.put("AnalysisTaskType", ParameterEstimationTaskXMLPersistence.ParameterEstimationTaskTag);
+	        context.registerService(IAnalysisTaskFactory.class.getName(), new ParameterEstimationTaskFactoryImpl(), props);
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+		 */
+		public void stop(BundleContext context) throws Exception {
+		}
+
+	}
---
+++ VCellOpt/org/vcell/optimization/ParameterEstimationTaskView.java
@@ -0,0 +1,35 @@
+package org.vcell.optimization;
+
+import javax.swing.JPanel;
+
+import org.vcell.model.analysis.IAnalysisTask;
+import org.vcell.model.analysis.IAnalysisTaskView;
+
+import cbit.vcell.modelopt.ParameterEstimationTask;
+import cbit.vcell.modelopt.gui.OptTestPanel;
+
+public class ParameterEstimationTaskView implements IAnalysisTaskView {
+
+	private OptTestPanel optTestPanel = null;
+	
+	public JPanel getPanel() {
+		if (optTestPanel==null){
+			optTestPanel = new OptTestPanel();
+		}
+		return optTestPanel;
+	}
+
+	public void setAnalysisTask(IAnalysisTask newAnalysisTask) {
+		getPanel();
+		optTestPanel.setParameterEstimationTask((ParameterEstimationTask)newAnalysisTask);
+	}
+	
+	public IAnalysisTask getAnalysisTask(){
+		if (optTestPanel!=null){
+			return optTestPanel.getParameterEstimationTask();
+		}else{
+			return null;
+		}
+	}
+
+}
