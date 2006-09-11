244a8a29830637b082684658907b3384600e13fb
Jim Schaff <schaff@uchc.edu>
2006-09-11 19:08:13 +0000

    svn path=/VCell5/trunk/; revision=207

---
+++ VCell Model/cbit/vcell/geometry/CompartmentSubVolume.java
@@ -0,0 +1,54 @@
+package cbit.vcell.geometry;
+/*©
+ * (C) Copyright University of Connecticut Health Center 2001.
+ * All rights reserved.
+©*/
+import cbit.util.KeyValue;
+import cbit.util.Matchable;
+/**
+ * This type was created in VisualAge.
+ */
+public class CompartmentSubVolume extends SubVolume {
+/**
+ * Compartment constructor comment.
+ * @param name java.lang.String
+ * @param geometry cbit.vcell.geometry.Geometry
+ */
+public CompartmentSubVolume(KeyValue key, int handleValue) {
+	super(key,"Compartment", handleValue);
+	//setHandle(0);
+}
+
+
+/**
+ * This method was created in VisualAge.
+ * @return boolean
+ * @param obj java.lang.Object
+ */
+public boolean compareEqual(Matchable obj) {
+	if (!compareEqual0(obj)){
+		return false;
+	}
+	if (!(obj instanceof CompartmentSubVolume)){
+		return false;
+	}
+
+	return true;
+}
+
+
+/**
+ * getVCML method comment.
+ */
+public String getVCML() {
+	return "";
+}
+
+
+/**
+ * isInside method comment.
+ */
+public boolean isInside(double x, double y, double z, GeometrySpec geometrySpec) {
+	return true;
+}
+}
