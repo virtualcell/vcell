9ee3edde624f2a019ba576996f02f6ff6942f538
Jim Schaff <schaff@uchc.edu>
2006-09-12 00:10:17 +0000

    svn path=/VCell5/trunk/; revision=227

---
+++ VCellManager/src/cbit/vcell/numericstest/SolutionTemplateBeanInfo.java
@@ -0,0 +1,442 @@
+package cbit.vcell.numericstest;
+
+/**
+ * The bean information class for cbit.vcell.numericstest.SolutionTemplate.
+ */
+public class SolutionTemplateBeanInfo extends java.beans.SimpleBeanInfo {
+/**
+ * Gets the constants property descriptor.
+ * @return java.beans.IndexedPropertyDescriptor
+ */
+public java.beans.IndexedPropertyDescriptor constantsPropertyDescriptor() {
+	java.beans.IndexedPropertyDescriptor aDescriptor = null;
+	try {
+		try {
+			/* Using methods via getMethod is the faster way to create the constants property descriptor. */
+			java.lang.reflect.Method aGetMethod = null;
+			try {
+				/* Attempt to find the method using getMethod with parameter types. */
+				java.lang.Class aGetMethodParameterTypes[] = {};
+				aGetMethod = getBeanClass().getMethod("getConstants", aGetMethodParameterTypes);
+			} catch (Throwable exception) {
+				/* Since getMethod failed, call findMethod. */
+				handleException(exception);
+				aGetMethod = findMethod(getBeanClass(), "getConstants", 0);
+			};
+			java.lang.reflect.Method aSetMethod = null;
+			try {
+				/* Attempt to find the method using getMethod with parameter types. */
+				java.lang.Class aSetMethodParameterTypes[] = {
+					cbit.vcell.math.Constant[].class
+				};
+				aSetMethod = getBeanClass().getMethod("setConstants", aSetMethodParameterTypes);
+			} catch (Throwable exception) {
+				/* Since getMethod failed, call findMethod. */
+				handleException(exception);
+				aSetMethod = findMethod(getBeanClass(), "setConstants", 1);
+			};
+			java.lang.reflect.Method aGetIndexedMethod = null;
+			try {
+				/* Attempt to find the method using getMethod with parameter types. */
+				java.lang.Class aGetIndexedMethodParameterTypes[] = {
+					int.class
+				};
+				aGetIndexedMethod = getBeanClass().getMethod("getConstants", aGetIndexedMethodParameterTypes);
+			} catch (Throwable exception) {
+				/* Since getMethod failed, call findMethod. */
+				handleException(exception);
+				aGetIndexedMethod = findMethod(getBeanClass(), "getConstants", 1);
+			};
+			java.lang.reflect.Method aSetIndexedMethod = null;
+			try {
+				/* Attempt to find the method using getMethod with parameter types. */
+				java.lang.Class aSetIndexedMethodParameterTypes[] = {
+					int.class,
+					cbit.vcell.math.Constant.class
+				};
+				aSetIndexedMethod = getBeanClass().getMethod("setConstants", aSetIndexedMethodParameterTypes);
+			} catch (Throwable exception) {
+				/* Since getMethod failed, call findMethod. */
+				handleException(exception);
+				aSetIndexedMethod = findMethod(getBeanClass(), "setConstants", 2);
+			};
+			aDescriptor = new java.beans.IndexedPropertyDescriptor("constants"
+			, aGetMethod, aSetMethod, aGetIndexedMethod, aSetIndexedMethod);
+		} catch (Throwable exception) {
+			/* Since we failed using methods, try creating a default property descriptor. */
+			handleException(exception);
+			aDescriptor = new java.beans.IndexedPropertyDescriptor("constants"
+			, getBeanClass());
+		};
+		aDescriptor.setBound(true);
+		/* aDescriptor.setConstrained(false); */
+		/* aDescriptor.setDisplayName("constants"); */
+		/* aDescriptor.setShortDescription("constants"); */
+		/* aDescriptor.setExpert(false); */
+		/* aDescriptor.setHidden(false); */
+		/* aDescriptor.setValue("preferred", new Boolean(false)); */
+		/* aDescriptor.setValue("ivjDesignTimeProperty", new Boolean(true)); */
+	} catch (Throwable exception) {
+		handleException(exception);
+	};
+	return aDescriptor;
+}
+/**
+ * Gets the domainName property descriptor.
+ * @return java.beans.PropertyDescriptor
+ */
+public java.beans.PropertyDescriptor domainNamePropertyDescriptor() {
+	java.beans.PropertyDescriptor aDescriptor = null;
+	try {
+		try {
+			/* Using methods via getMethod is the faster way to create the domainName property descriptor. */
+			java.lang.reflect.Method aGetMethod = null;
+			try {
+				/* Attempt to find the method using getMethod with parameter types. */
+				java.lang.Class aGetMethodParameterTypes[] = {};
+				aGetMethod = getBeanClass().getMethod("getDomainName", aGetMethodParameterTypes);
+			} catch (Throwable exception) {
+				/* Since getMethod failed, call findMethod. */
+				handleException(exception);
+				aGetMethod = findMethod(getBeanClass(), "getDomainName", 0);
+			};
+			java.lang.reflect.Method aSetMethod = null;
+			aDescriptor = new java.beans.PropertyDescriptor("domainName"
+			, aGetMethod, aSetMethod);
+		} catch (Throwable exception) {
+			/* Since we failed using methods, try creating a default property descriptor. */
+			handleException(exception);
+			aDescriptor = new java.beans.PropertyDescriptor("domainName"
+			, getBeanClass());
+		};
+		/* aDescriptor.setBound(false); */
+		/* aDescriptor.setConstrained(false); */
+		/* aDescriptor.setDisplayName("domainName"); */
+		/* aDescriptor.setShortDescription("domainName"); */
+		/* aDescriptor.setExpert(false); */
+		/* aDescriptor.setHidden(false); */
+		/* aDescriptor.setValue("preferred", new Boolean(false)); */
+		/* aDescriptor.setValue("ivjDesignTimeProperty", new Boolean(true)); */
+	} catch (Throwable exception) {
+		handleException(exception);
+	};
+	return aDescriptor;
+}
+/**
+ * Find the method by comparing (name & parameter size) against the methods in the class.
+ * @return java.lang.reflect.Method
+ * @param aClass java.lang.Class
+ * @param methodName java.lang.String
+ * @param parameterCount int
+ */
+public static java.lang.reflect.Method findMethod(java.lang.Class aClass, java.lang.String methodName, int parameterCount) {
+	try {
+		/* Since this method attempts to find a method by getting all methods from the class,
+	this method should only be called if getMethod cannot find the method. */
+		java.lang.reflect.Method methods[] = aClass.getMethods();
+		for (int index = 0; index < methods.length; index++){
+			java.lang.reflect.Method method = methods[index];
+			if ((method.getParameterTypes().length == parameterCount) && (method.getName().equals(methodName))) {
+				return method;
+			}
+		}
+	} catch (java.lang.Throwable exception) {
+		return null;
+	}
+	return null;
+}
+/**
+ * Returns the BeanInfo of the superclass of this bean to inherit its features.
+ * @return java.beans.BeanInfo[]
+ */
+public java.beans.BeanInfo[] getAdditionalBeanInfo() {
+	java.lang.Class superClass;
+	java.beans.BeanInfo superBeanInfo = null;
+
+	try {
+		superClass = getBeanDescriptor().getBeanClass().getSuperclass();
+	} catch (java.lang.Throwable exception) {
+		return null;
+	}
+
+	try {
+		superBeanInfo = java.beans.Introspector.getBeanInfo(superClass);
+	} catch (java.beans.IntrospectionException ie) {}
+
+	if (superBeanInfo != null) {
+		java.beans.BeanInfo[] ret = new java.beans.BeanInfo[1];
+		ret[0] = superBeanInfo;
+		return ret;
+	}
+	return null;
+}
+/**
+ * Gets the bean class.
+ * @return java.lang.Class
+ */
+public static java.lang.Class getBeanClass() {
+	return cbit.vcell.numericstest.SolutionTemplate.class;
+}
+/**
+ * Gets the bean class name.
+ * @return java.lang.String
+ */
+public static java.lang.String getBeanClassName() {
+	return "cbit.vcell.numericstest.SolutionTemplate";
+}
+public java.beans.BeanDescriptor getBeanDescriptor() {
+	java.beans.BeanDescriptor aDescriptor = null;
+	try {
+		/* Create and return the SolutionTemplateBeanInfo bean descriptor. */
+		aDescriptor = new java.beans.BeanDescriptor(cbit.vcell.numericstest.SolutionTemplate.class);
+		/* aDescriptor.setExpert(false); */
+		/* aDescriptor.setHidden(false); */
+		/* aDescriptor.setValue("hidden-state", Boolean.FALSE); */
+	} catch (Throwable exception) {
+	};
+	return aDescriptor;
+}
+/**
+ * Gets the getConstants(int) method descriptor.
+ * @return java.beans.MethodDescriptor
+ */
+public java.beans.MethodDescriptor getConstants_intMethodDescriptor() {
+	java.beans.MethodDescriptor aDescriptor = null;
+	try {
+		/* Create and return the getConstants(int) method descriptor. */
+		java.lang.reflect.Method aMethod = null;
+		try {
+			/* Attempt to find the method using getMethod with parameter types. */
+			java.lang.Class aParameterTypes[] = {
+				int.class
+			};
+			aMethod = getBeanClass().getMethod("getConstants", aParameterTypes);
+		} catch (Throwable exception) {
+			/* Since getMethod failed, call findMethod. */
+			handleException(exception);
+			aMethod = findMethod(getBeanClass(), "getConstants", 1);
+		};
+		try {
+			/* Try creating the method descriptor with parameter descriptors. */
+			java.beans.ParameterDescriptor aParameterDescriptor1 = new java.beans.ParameterDescriptor();
+			aParameterDescriptor1.setName("arg1");
+			aParameterDescriptor1.setDisplayName("index");
+			java.beans.ParameterDescriptor aParameterDescriptors[] = {
+				aParameterDescriptor1
+			};
+			aDescriptor = new java.beans.MethodDescriptor(aMethod, aParameterDescriptors);
+		} catch (Throwable exception) {
+			/* Try creating the method descriptor without parameter descriptors. */
+			handleException(exception);
+			aDescriptor = new java.beans.MethodDescriptor(aMethod);
+		};
+		/* aDescriptor.setDisplayName("getConstants(int)"); */
+		/* aDescriptor.setShortDescription("getConstants(int)"); */
+		/* aDescriptor.setExpert(false); */
+		/* aDescriptor.setHidden(false); */
+		/* aDescriptor.setValue("preferred", new Boolean(false)); */
+	} catch (Throwable exception) {
+		handleException(exception);
+	};
+	return aDescriptor;
+}
+/**
+ * Return the event set descriptors for this bean.
+ * @return java.beans.EventSetDescriptor[]
+ */
+public java.beans.EventSetDescriptor[] getEventSetDescriptors() {
+	try {
+		java.beans.EventSetDescriptor aDescriptorList[] = {};
+		return aDescriptorList;
+	} catch (Throwable exception) {
+		handleException(exception);
+	};
+	return null;
+}
+/**
+ * Return the method descriptors for this bean.
+ * @return java.beans.MethodDescriptor[]
+ */
+public java.beans.MethodDescriptor[] getMethodDescriptors() {
+	try {
+		java.beans.MethodDescriptor aDescriptorList[] = {
+			getConstants_intMethodDescriptor()
+			,setConstants_int_cbitvcellmathConstantMethodDescriptor()
+		};
+		return aDescriptorList;
+	} catch (Throwable exception) {
+		handleException(exception);
+	};
+	return null;
+}
+/**
+ * Return the property descriptors for this bean.
+ * @return java.beans.PropertyDescriptor[]
+ */
+public java.beans.PropertyDescriptor[] getPropertyDescriptors() {
+	try {
+		java.beans.PropertyDescriptor aDescriptorList[] = {
+			constantsPropertyDescriptor()
+			,domainNamePropertyDescriptor()
+			,templateExpressionPropertyDescriptor()
+			,varNamePropertyDescriptor()
+		};
+		return aDescriptorList;
+	} catch (Throwable exception) {
+		handleException(exception);
+	};
+	return null;
+}
+/**
+ * Called whenever the bean information class throws an exception.
+ * @param exception java.lang.Throwable
+ */
+private void handleException(java.lang.Throwable exception) {
+
+	/* Uncomment the following lines to print uncaught exceptions to stdout */
+	// System.out.println("--------- UNCAUGHT EXCEPTION ---------");
+	// exception.printStackTrace(System.out);
+}
+/**
+ * Gets the setConstants(int, cbit.vcell.math.Constant) method descriptor.
+ * @return java.beans.MethodDescriptor
+ */
+public java.beans.MethodDescriptor setConstants_int_cbitvcellmathConstantMethodDescriptor() {
+	java.beans.MethodDescriptor aDescriptor = null;
+	try {
+		/* Create and return the setConstants(int, cbit.vcell.math.Constant) method descriptor. */
+		java.lang.reflect.Method aMethod = null;
+		try {
+			/* Attempt to find the method using getMethod with parameter types. */
+			java.lang.Class aParameterTypes[] = {
+				int.class,
+				cbit.vcell.math.Constant.class
+			};
+			aMethod = getBeanClass().getMethod("setConstants", aParameterTypes);
+		} catch (Throwable exception) {
+			/* Since getMethod failed, call findMethod. */
+			handleException(exception);
+			aMethod = findMethod(getBeanClass(), "setConstants", 2);
+		};
+		try {
+			/* Try creating the method descriptor with parameter descriptors. */
+			java.beans.ParameterDescriptor aParameterDescriptor1 = new java.beans.ParameterDescriptor();
+			aParameterDescriptor1.setName("arg1");
+			aParameterDescriptor1.setDisplayName("index");
+			java.beans.ParameterDescriptor aParameterDescriptor2 = new java.beans.ParameterDescriptor();
+			aParameterDescriptor2.setName("arg2");
+			aParameterDescriptor2.setDisplayName("constants");
+			java.beans.ParameterDescriptor aParameterDescriptors[] = {
+				aParameterDescriptor1,
+				aParameterDescriptor2
+			};
+			aDescriptor = new java.beans.MethodDescriptor(aMethod, aParameterDescriptors);
+		} catch (Throwable exception) {
+			/* Try creating the method descriptor without parameter descriptors. */
+			handleException(exception);
+			aDescriptor = new java.beans.MethodDescriptor(aMethod);
+		};
+		/* aDescriptor.setDisplayName("setConstants(int, cbit.vcell.math.Constant)"); */
+		/* aDescriptor.setShortDescription("setConstants(int, cbit.vcell.math.Constant)"); */
+		/* aDescriptor.setExpert(false); */
+		/* aDescriptor.setHidden(false); */
+		/* aDescriptor.setValue("preferred", new Boolean(false)); */
+	} catch (Throwable exception) {
+		handleException(exception);
+	};
+	return aDescriptor;
+}
+/**
+ * Gets the templateExpression property descriptor.
+ * @return java.beans.PropertyDescriptor
+ */
+public java.beans.PropertyDescriptor templateExpressionPropertyDescriptor() {
+	java.beans.PropertyDescriptor aDescriptor = null;
+	try {
+		try {
+			/* Using methods via getMethod is the faster way to create the templateExpression property descriptor. */
+			java.lang.reflect.Method aGetMethod = null;
+			try {
+				/* Attempt to find the method using getMethod with parameter types. */
+				java.lang.Class aGetMethodParameterTypes[] = {};
+				aGetMethod = getBeanClass().getMethod("getTemplateExpression", aGetMethodParameterTypes);
+			} catch (Throwable exception) {
+				/* Since getMethod failed, call findMethod. */
+				handleException(exception);
+				aGetMethod = findMethod(getBeanClass(), "getTemplateExpression", 0);
+			};
+			java.lang.reflect.Method aSetMethod = null;
+			try {
+				/* Attempt to find the method using getMethod with parameter types. */
+				java.lang.Class aSetMethodParameterTypes[] = {
+					cbit.vcell.parser.Expression.class
+				};
+				aSetMethod = getBeanClass().getMethod("setTemplateExpression", aSetMethodParameterTypes);
+			} catch (Throwable exception) {
+				/* Since getMethod failed, call findMethod. */
+				handleException(exception);
+				aSetMethod = findMethod(getBeanClass(), "setTemplateExpression", 1);
+			};
+			aDescriptor = new java.beans.PropertyDescriptor("templateExpression"
+			, aGetMethod, aSetMethod);
+		} catch (Throwable exception) {
+			/* Since we failed using methods, try creating a default property descriptor. */
+			handleException(exception);
+			aDescriptor = new java.beans.PropertyDescriptor("templateExpression"
+			, getBeanClass());
+		};
+		aDescriptor.setBound(true);
+		/* aDescriptor.setConstrained(false); */
+		/* aDescriptor.setDisplayName("templateExpression"); */
+		/* aDescriptor.setShortDescription("templateExpression"); */
+		/* aDescriptor.setExpert(false); */
+		/* aDescriptor.setHidden(false); */
+		/* aDescriptor.setValue("preferred", new Boolean(false)); */
+		/* aDescriptor.setValue("ivjDesignTimeProperty", new Boolean(true)); */
+	} catch (Throwable exception) {
+		handleException(exception);
+	};
+	return aDescriptor;
+}
+/**
+ * Gets the varName property descriptor.
+ * @return java.beans.PropertyDescriptor
+ */
+public java.beans.PropertyDescriptor varNamePropertyDescriptor() {
+	java.beans.PropertyDescriptor aDescriptor = null;
+	try {
+		try {
+			/* Using methods via getMethod is the faster way to create the varName property descriptor. */
+			java.lang.reflect.Method aGetMethod = null;
+			try {
+				/* Attempt to find the method using getMethod with parameter types. */
+				java.lang.Class aGetMethodParameterTypes[] = {};
+				aGetMethod = getBeanClass().getMethod("getVarName", aGetMethodParameterTypes);
+			} catch (Throwable exception) {
+				/* Since getMethod failed, call findMethod. */
+				handleException(exception);
+				aGetMethod = findMethod(getBeanClass(), "getVarName", 0);
+			};
+			java.lang.reflect.Method aSetMethod = null;
+			aDescriptor = new java.beans.PropertyDescriptor("varName"
+			, aGetMethod, aSetMethod);
+		} catch (Throwable exception) {
+			/* Since we failed using methods, try creating a default property descriptor. */
+			handleException(exception);
+			aDescriptor = new java.beans.PropertyDescriptor("varName"
+			, getBeanClass());
+		};
+		/* aDescriptor.setBound(false); */
+		/* aDescriptor.setConstrained(false); */
+		/* aDescriptor.setDisplayName("varName"); */
+		/* aDescriptor.setShortDescription("varName"); */
+		/* aDescriptor.setExpert(false); */
+		/* aDescriptor.setHidden(false); */
+		/* aDescriptor.setValue("preferred", new Boolean(false)); */
+		/* aDescriptor.setValue("ivjDesignTimeProperty", new Boolean(true)); */
+	} catch (Throwable exception) {
+		handleException(exception);
+	};
+	return aDescriptor;
+}
+}
