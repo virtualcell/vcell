fa5f49c2fd4e5810502a318a4e16e347facf6f89
Jim Schaff <schaff@uchc.edu>
2006-09-20 16:11:03 +0000

    svn path=/VCell5/trunk/; revision=382

--- VCell_UI/cbit/xml/merge/gui/XmlComparePolicy.java
+++
@@ -1,7 +0,0 @@
-package cbit.xml.merge.gui;
-
-public interface XmlComparePolicy {
-	public String getKeyAttributeName(String elementName);
-	public boolean ignoreElement(String elementName);
-	public boolean ignoreAttribute(String elementName, String attributeName);
-}
