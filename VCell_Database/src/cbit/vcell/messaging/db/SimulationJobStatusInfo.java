eda02885acfa3593ae1c9fc338bb2fbf483b5ea7
Jim Schaff <schaff@uchc.edu>
2006-09-22 20:11:25 +0000

    factor out the database
    svn path=/VCell5/trunk/; revision=675

---
+++ VCell_Database/src/cbit/vcell/messaging/db/SimulationJobStatusInfo.java
@@ -0,0 +1,52 @@
+package cbit.vcell.messaging.db;
+import cbit.util.User;
+import cbit.vcell.solvers.SimulationJobStatus;
+
+/**
+ * Insert the type's description here.
+ * Creation date: (5/9/2006 3:55:48 PM)
+ * @author: Jim Schaff
+ */
+public class SimulationJobStatusInfo implements java.io.Serializable {
+	private SimulationJobStatus simJobStatus = null;
+	private int dimension = 0;
+
+/**
+ * SimulationJobStatusInfo constructor comment.
+ */
+public SimulationJobStatusInfo(SimulationJobStatus sjs, int dim) {
+	super();
+	simJobStatus = sjs;
+	dimension = dim;
+}
+
+
+/**
+ * Insert the method's description here.
+ * Creation date: (5/9/2006 3:57:30 PM)
+ * @return cbit.vcell.messaging.db.SimulationJobStatus
+ */
+public SimulationJobStatus getSimJobStatus() {
+	return simJobStatus;
+}
+
+
+/**
+ * Insert the method's description here.
+ * Creation date: (5/9/2006 3:57:30 PM)
+ * @return User
+ */
+public User getUser() {
+	return simJobStatus.getVCSimulationIdentifier().getOwner();
+}
+
+
+/**
+ * Insert the method's description here.
+ * Creation date: (5/9/2006 3:57:30 PM)
+ * @return boolean
+ */
+public boolean isPDE() {
+	return dimension > 0;
+}
+}
---
+++ VCell_Database/src/cbit/vcell/messaging/db/UpdateSynchronizationException.java
@@ -0,0 +1,22 @@
+package cbit.vcell.messaging.db;
+
+/**
+ * Insert the type's description here.
+ * Creation date: (5/27/2003 11:53:54 AM)
+ * @author: Fei Gao
+ */
+public class UpdateSynchronizationException extends cbit.util.DataAccessException {
+/**
+ * UpdateSynchronizationException constructor comment.
+ */
+public UpdateSynchronizationException() {
+	super();
+}
+/**
+ * UpdateSynchronizationException constructor comment.
+ * @param s java.lang.String
+ */
+public UpdateSynchronizationException(String s) {
+	super(s);
+}
+}
