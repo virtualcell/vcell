a546289cc66e2a0072bbd21f734bcbec6f40095f
Jim Schaff <schaff@uchc.edu>
2006-09-23 23:15:40 +0000

    factor optimization into app and algorithm
    svn path=/VCell5/trunk/; revision=690

---
+++ VCell_Modelapp_physics/src/org/vcell/modelapp/physics/OOModelingTaskActivator.java
@@ -0,0 +1,29 @@
+package org.vcell.modelapp.physics;
+
+import java.util.Properties;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.vcell.modelapp.analysis.IAnalysisTaskFactory;
+
+public class OOModelingTaskActivator implements BundleActivator {
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		System.out.println("starting "+getClass().getName());
+        Properties props = new Properties();
+        props.put("AnalysisTaskType", OOModelingTaskXMLPersistence.OOModelingTaskTag);
+        context.registerService(IAnalysisTaskFactory.class.getName(), new OOModelingTaskFactory(), props);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+	}
+
+}
