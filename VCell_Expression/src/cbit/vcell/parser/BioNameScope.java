c57f1c8bdb2f29317545289b1fba47d25b8a5c54
Jim Schaff <schaff@uchc.edu>
2006-09-26 02:04:12 +0000

    last refactoring before NRCAM renewal
    svn path=/VCell5/trunk/; revision=702

---
+++ VCell_Expression/src/cbit/vcell/parser/BioNameScope.java
@@ -0,0 +1,15 @@
+package cbit.vcell.parser;
+
+/**
+ * Insert the type's description here.
+ * Creation date: (7/31/2003 12:40:03 PM)
+ * @author: Jim Schaff
+ */
+public abstract class BioNameScope extends AbstractNameScope {
+/**
+ * BioNameScope constructor comment.
+ */
+public BioNameScope() {
+	super();
+}
+}
--- VCell_Expression/src/org/vcell/expression/Activator.java
+++ VCell_Expression/src/org/vcell/expression/Activator.java
@@ -10,6 +10,8 @@ public class Activator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
+		System.out.println("starting "+getClass().getName());
+		System.out.println(getClass().getName()+" : "+new cbit.vcell.parser.Expression("hello*goodbye").infix());
 	}
 
 	/*
