67127757f3751f8e5311b26a85865df6aebb1e4b
Jim Schaff <schaff@uchc.edu>
2006-09-26 02:04:41 +0000

    last refactoring before NRCAM renewal
    svn path=/VCell5/trunk/; revision=706

--- VCell_Matrix/src/org/vcell/matrix/Activator.java
+++ VCell_Matrix/src/org/vcell/matrix/Activator.java
@@ -10,6 +10,7 @@ public class Activator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
+		System.out.println("starting "+getClass().getName());
 	}
 
 	/*
