abd40c142abc7e7cf73cc619ef0346f84acc1afd
Jim Schaff <schaff@uchc.edu>
2006-09-27 00:07:27 +0000

    factor out server administration
    svn path=/VCell5/trunk/; revision=822

---
+++ VCell_Server_Admin/src/cbit/vcell/messaging/admin/JobTableModel.java
@@ -0,0 +1,77 @@
+package cbit.vcell.messaging.admin;
+import java.util.Date;
+
+import cbit.vcell.messaging.admin.sorttable.ManageTableModel;
+import cbit.vcell.solvers.SimpleJobStatus;
+
+/**
+ * Insert the type's description here.
+ * Creation date: (8/28/2003 1:38:32 PM)
+ * @author: Fei Gao
+ */
+public class JobTableModel extends ManageTableModel {
+	private final static int columnIndex_UserID = 0;
+	private final static int columnIndex_SimID = 1;
+	private final static int columnIndex_SolverType = 2;
+	private final static int columnIndex_Status = 3;
+	private final static int columnIndex_ComputeHost = 4;
+	private final static int columnIndex_ServerID = 5;
+	private final static int columnIndex_TaskID = 6;
+	private final static int columnIndex_SubmitDate = 7;
+	private final static int columnIndex_StartDate = 8;
+	private final static int columnIndex_EndDate = 9;
+
+/**
+ * JobTableModel constructor comment.
+ */
+public JobTableModel() {
+	super();
+	columns = new String[]{"User ID", "Sim ID", "Solver Type", "Status", "Compute Host", "Server ID", "Task ID", "Submit Date", "Start Date", "End Date"};
+}
+
+
+/**
+ * Insert the method's description here.
+ * Creation date: (8/19/2003 2:14:05 PM)
+ * @return java.lang.Class
+ * @param columnIndex int
+ */
+public Class getColumnClass(int columnIndex) {
+	if (columnIndex == columnIndex_SimID || columnIndex == columnIndex_TaskID) {
+		return Number.class;
+	} else if (columnIndex == columnIndex_SubmitDate || columnIndex == columnIndex_StartDate || columnIndex == columnIndex_EndDate) {
+		return Date.class;
+	} else {		
+		return String.class;
+	}
+}
+
+
+/**
+ * getValueAt method comment.
+ */
+public Object getValueAt(int row, int col) {
+	if (row >= rows.size() || row < 0 || col < 0 || col >= columns.length) {
+		return null;
+	}
+
+	SimpleJobStatus jobStatus = (SimpleJobStatus)rows.get(row);
+	Object[] values = jobStatus.toObjects();
+	return values[col];
+}
+
+
+/**
+ * Insert the method's description here.
+ * Creation date: (8/18/2003 8:24:43 AM)
+ * @param status cbit.vcell.messaging.admin.PerformanceStatus
+ */
+public synchronized void remove(int index) {
+	if (index >= rows.size() || index < 0) {
+		System.out.println("Array index out of bound " + index);		
+	} else {
+		rows.remove(index);
+		fireTableDataChanged();
+	}
+}
+}
---
+++ VCell_Server_Admin/src/cbit/vcell/messaging/admin/SolverTaskDescriptionRenderer.java
@@ -0,0 +1,41 @@
+package cbit.vcell.messaging.admin;
+/**
+ * Insert the type's description here.
+ * Creation date: (7/8/2004 2:26:27 PM)
+ * @author: Fei Gao
+ */
+public class SolverTaskDescriptionRenderer extends javax.swing.table.DefaultTableCellRenderer {
+/**
+ * SolverTaskDescriptionRenderer constructor comment.
+ */
+public SolverTaskDescriptionRenderer() {
+	super();
+}
+
+
+	/**
+	 *  This method is sent to the renderer by the drawing table to
+	 *  configure the renderer appropriately before drawing.  Return
+	 *  the Component used for drawing.
+	 *
+	 * @param	table		the JTable that is asking the renderer to draw.
+	 *				This parameter can be null.
+	 * @param	value		the value of the cell to be rendered.  It is
+	 *				up to the specific renderer to interpret
+	 *				and draw the value.  eg. if value is the
+	 *				String "true", it could be rendered as a
+	 *				string or it could be rendered as a check
+	 *				box that is checked.  null is a valid value.
+	 * @param	isSelected	true is the cell is to be renderer with
+	 *				selection highlighting
+	 * @param	row	        the row index of the cell being drawn.  When
+	 *				drawing the header the rowIndex is -1.
+	 * @param	column	        the column index of the cell being drawn
+	 */
+public java.awt.Component getTableCellRendererComponent(javax.swing.JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+	super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+	setText(value.toString());
+	setToolTipText(value.toString());
+	return this;
+}
+}
