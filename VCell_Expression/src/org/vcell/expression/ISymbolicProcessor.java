5844770d9497e8578b14905725c011e951ce3ae9
Jim Schaff <schaff@uchc.edu>
2006-10-23 20:08:52 +0000

    break apart thirdParty
    svn path=/VCell5/trunk/; revision=926

---
+++ VCell_Expression/src/org/vcell/expression/ISymbolicProcessor.java
@@ -0,0 +1,28 @@
+package org.vcell.expression;
+
+public interface ISymbolicProcessor {
+	
+	/**
+	 * generalized simplification (may be replaced with more task-oriented routine)
+	 * @param expression
+	 * @return
+	 */
+	public abstract IExpression simplify(IExpression expression);
+	
+	/**
+	 * generalized simplification (may be replaced with more task-oriented routine)
+	 * @param expression
+	 * @return
+	 */
+	public abstract IRationalExpression simplify(IRationalExpression expression);
+	
+	/**
+	 * solve() attempts to solve the equation "expression == 0" with respect to "symbol".
+	 * 
+	 * @param expression
+	 * @param symbol
+	 * @return
+	 */
+	public abstract IExpression solve(IExpression expression, String symbol) throws ExpressionException;
+	
+}
