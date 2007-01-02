99d185e912a7836699244214e0e1ec918b999a31
Jim Schaff <schaff@uchc.edu>
2007-01-02 14:55:16 +0000

    Componentizing Expression and Unit modules
    svn path=/VCell5/trunk/; revision=1018

--- VCell_Modelapp_physics/src/cbit/vcell/modelapp/physics/PhysicsMapping.java
+++ VCell_Modelapp_physics/src/cbit/vcell/modelapp/physics/PhysicsMapping.java
@@ -1,6 +1,7 @@
 package cbit.vcell.modelapp.physics;
 
 import java.beans.PropertyVetoException;
+import java.util.ArrayList;
 
 import jscl.plugin.Expression;
 import jscl.plugin.ParseException;
@@ -8,13 +9,30 @@ import jscl.plugin.Variable;
 
 import org.vcell.expression.ExpressionException;
 import org.vcell.expression.ExpressionUtilities;
+import org.vcell.expression.IExpression;
+import org.vcell.physics.component.Capacitor;
+import org.vcell.physics.component.Connection;
+import org.vcell.physics.component.Connector;
+import org.vcell.physics.component.CurrentSource;
+import org.vcell.physics.component.Location;
 import org.vcell.physics.component.LumpedLocation;
+import org.vcell.physics.component.ModelComponent;
 import org.vcell.physics.component.OOModel;
+import org.vcell.physics.component.OnePortElectricalDevice;
+import org.vcell.physics.component.TwoPortElectricalComponent;
+import org.vcell.physics.component.VoltageSource;
 
 import cbit.util.TokenMangler;
 import cbit.vcell.model.Feature;
 import cbit.vcell.model.Membrane;
+import cbit.vcell.model.Model;
+import cbit.vcell.model.ReactionStep;
+import cbit.vcell.model.Structure;
+import cbit.vcell.modelapp.FeatureMapping;
+import cbit.vcell.modelapp.MembraneMapping;
+import cbit.vcell.modelapp.SimulationContext;
 import cbit.vcell.modelapp.StructureMapping;
+import cbit.vcell.units.VCUnitDefinition;
 
 public class PhysicsMapping {
 	/**
@@ -29,9 +47,9 @@ public class PhysicsMapping {
 		// add locations to physicalModel
 		// (names of locations always start with the structure name followed by an underscore ... e.g. "cytosol_0_1")
 		//
-		//addLocations(simContext,physicalModel);
+		addLocations(simContext,physicalModel);
 
-		//addElectricalDevices(simContext,physicalModel);
+		addElectricalDevices(simContext,physicalModel);
 
 		PhysicsMapping.addChemicalDevices(simContext,physicalModel);
 
@@ -42,21 +60,7 @@ public class PhysicsMapping {
 		return physicalModel;
 	}
 
-
-
-	/**
-	 * Insert the method's description here.
-	 * Creation date: (1/12/2004 1:35:34 AM)
-	 * @return ncbc_old.physics.component.PhysicalModel
-	 */
-	public static void addChemicalDevices(cbit.vcell.modelapp.SimulationContext simContext, org.vcell.physics.component.OOModel oOModel) throws ExpressionException, java.beans.PropertyVetoException {
-	
-		cbit.vcell.model.Structure structures[] = simContext.getModel().getStructures();
-	
-		if (simContext.getGeometryContext().getGeometry().getDimension()>0){
-			throw new RuntimeException("doesn't currently support spatial models");
-		}
-	
+	public static void addLocations(SimulationContext simContext, OOModel oOModel){
 		//
 		// add Devices for locations to the physical model
 		//
@@ -73,6 +77,37 @@ public class PhysicsMapping {
 			LumpedLocation lumpedLocation = new LumpedLocation(structureMappings[i].getStructure().getName(),dim);
 			oOModel.addModelComponent(lumpedLocation);
 		}
+		//
+		// Connect Devices (adjacency)
+		//
+		for (int i = 0; i < structureMappings.length; i++){
+			if (structureMappings[i].getStructure() instanceof Membrane){
+				Membrane membrane = ((MembraneMapping)structureMappings[i]).getMembrane();
+				Feature insideFeature = membrane.getInsideFeature();
+				Feature outsideFeature = membrane.getOutsideFeature();
+				LumpedLocation membraneLocation = (LumpedLocation)oOModel.getModelComponent(membrane.getName());
+				LumpedLocation insideLocation = (LumpedLocation)oOModel.getModelComponent(insideFeature.getName());
+				LumpedLocation outsideLocation = (LumpedLocation)oOModel.getModelComponent(outsideFeature.getName());
+				membraneLocation.addAdjacentLocation(insideLocation);
+				insideLocation.addAdjacentLocation(membraneLocation);
+				membraneLocation.addAdjacentLocation(outsideLocation);
+				outsideLocation.addAdjacentLocation(membraneLocation);
+			}
+		}
+	}
+
+	/**
+	 * Insert the method's description here.
+	 * Creation date: (1/12/2004 1:35:34 AM)
+	 * @return ncbc_old.physics.component.PhysicalModel
+	 */
+	public static void addChemicalDevices(cbit.vcell.modelapp.SimulationContext simContext, org.vcell.physics.component.OOModel oOModel) throws ExpressionException, java.beans.PropertyVetoException {
+	
+	
+		if (simContext.getGeometryContext().getGeometry().getDimension()>0){
+			throw new RuntimeException("doesn't currently support spatial models");
+		}
+	
 		//
 		// add Devices for molecular species to the physical model
 		//
@@ -177,6 +212,217 @@ public class PhysicsMapping {
 							}
 						}
 					}
+				}
+			}
+//			//
+//			// add Current sources (membrane reactions that carry current)
+//			//
+//			ReactionStep reactionSteps[] = model.getReactionSteps();
+//			for (int i = 0; i < reactionSteps.length; i++){
+//				if (reactionSteps[i].getStructure() instanceof Membrane){
+//					Membrane membrane = (Membrane)reactionSteps[i].getStructure();
+//					int physicsOptions = reactionSteps[i].getPhysicsOptions();
+//					int valence = (int)reactionSteps[i].getChargeCarrierValence().getExpression().evaluateConstant();
+//					if (valence!=0 && (physicsOptions==ReactionStep.PHYSICS_ELECTRICAL_ONLY || physicsOptions==ReactionStep.PHYSICS_MOLECULAR_AND_ELECTRICAL)){
+//						for (int j = 0; j < locations.length; j++){
+//							if (locations[j] instanceof LumpedLocation && locations[j].getName().startsWith(membrane.getName())){
+//								LumpedLocation surfaceLocation = (LumpedLocation)locations[j];
+//								String name = reactionSteps[i].getName(); // +"_"+surfaceLocation.getName();
+//								IExpression expression = reactionSteps[i].getKinetics().getCurrentParameter().getExpression();
+//								CurrentSource newCurrentSource = new CurrentSource("current_"+name,1.0); // expression.evaluateConstant());
+//								physicalModel.addModelComponent(newCurrentSource);
+//								//
+//								// connect the "positive" electrical connector on this device 
+//								//
+//								Location adjacentLocations[] = surfaceLocation.getAdjacentLocations();
+//								LumpedLocation neighbor1 = (LumpedLocation)adjacentLocations[0];
+//								OnePortElectricalDevice material1 = (OnePortElectricalDevice)physicalModel.getModelComponent("conductor_"+neighbor1.getName());
+//								LumpedLocation neighbor2 = (LumpedLocation)adjacentLocations[1];
+//								OnePortElectricalDevice material2 = (OnePortElectricalDevice)physicalModel.getModelComponent("conductor_"+neighbor2.getName());
+//								if (neighbor1.getName().startsWith(membrane.getOutsideFeature().getName())){
+//									physicalModel.addConnection(new Connection(new Connector[] { newCurrentSource.getConnectors()[0] , material1.getConnectors()[0] } ));
+//								}else if (neighbor2.getName().startsWith(membrane.getOutsideFeature().getName())){
+//									physicalModel.addConnection(new Connection(new Connector[] { newCurrentSource.getConnectors()[0] , material2.getConnectors()[0] } ));
+//								}
+//								if (neighbor1.getName().startsWith(membrane.getInsideFeature().getName())){
+//									physicalModel.addConnection(new Connection(new Connector[] { newCurrentSource.getConnectors(1), material1.getConnectors(0) } ));
+//								}else if (neighbor2.getName().startsWith(membrane.getInsideFeature().getName())){
+//									physicalModel.addConnection(new Connection(new Connector[] { newCurrentSource.getConnectors()[1],material2.getConnectors(0) } ));
+//								}
+//							}
+//						}
+//					}
+//				}
+//			}
+		}
+	}
+	public static Location[] getLocations(OOModel ooModel){
+		ModelComponent[] components = ooModel.getModelComponents();
+		ArrayList<Location> locations = new ArrayList<Location>();
+		for (int i = 0; i < components.length; i++) {
+			if (components[i] instanceof Location){
+				locations.add((Location)components[i]);
+			}
+		}
+		return locations.toArray(new Location[locations.size()]);
+	}
+	
+	public static void addElectricalDevices(cbit.vcell.modelapp.SimulationContext simContext, OOModel physicalModel) throws org.vcell.expression.ExpressionException, java.beans.PropertyVetoException {
+
+		Model model = simContext.getModel();
+		Structure structures[] = model.getStructures();
+		Location[] locations = getLocations(physicalModel);
+		
+		//
+		// add Devices for material properties of lumped 3D locations (unresolved volumes)
+		//
+		for (int i = 0; i < locations.length; i++){
+			if (locations[i] instanceof LumpedLocation){
+				LumpedLocation lumpedLocation = (LumpedLocation)locations[i];
+				if (lumpedLocation.getDimension()==3){
+					OnePortElectricalDevice infiniteConductor = new OnePortElectricalDevice("conductor_"+lumpedLocation.getName());
+					physicalModel.addModelComponent(infiniteConductor);
+				}
+			}else{
+				throw new RuntimeException("unexpected location type '"+locations[i].getClass().getName()+"'");
+			}
+		}
+		//
+		// add Devices for material properties of lumped 2D locations (unresolved membranes)
+		// connect devices for material properties (start with SurfaceElectricalMaterial)
+		//
+		for (int i = 0; i < locations.length; i++){
+			if (locations[i] instanceof LumpedLocation){
+				LumpedLocation lumpedLocation = (LumpedLocation)locations[i];
+				if (lumpedLocation.getDimension()==2){
+					Capacitor membraneCapacitance = new Capacitor("cap_"+lumpedLocation.getName(),1);
+					physicalModel.addModelComponent(membraneCapacitance);
+					Location[] adjacentLocations = lumpedLocation.getAdjacentLocations();
+					if (adjacentLocations.length!=2 || adjacentLocations[0].getDimension()!=3 || adjacentLocations[1].getDimension()!=3){
+						throw new RuntimeException("expecting two 3D locations adjacent to surface location: "+lumpedLocation.getName());
+					}
+					OnePortElectricalDevice conductor0 = (OnePortElectricalDevice)physicalModel.getModelComponent("conductor_"+adjacentLocations[0].getName());
+					OnePortElectricalDevice conductor1 = (OnePortElectricalDevice)physicalModel.getModelComponent("conductor_"+adjacentLocations[1].getName());
+					Connector conductor0_connector = conductor0.getConnectors(0);
+					physicalModel.addConnection(new Connection(new Connector[] { conductor0_connector, membraneCapacitance.getConnectors(0)} ));
+					Connector conductor1_connector = conductor1.getConnectors(0);
+					physicalModel.addConnection(new Connection(new Connector[] { conductor1_connector, membraneCapacitance.getConnectors(1)} ));
+				}
+			}else{
+				throw new RuntimeException("unexpected location type '"+locations[i].getClass().getName()+"'");
+			}
+		}
+		
+		//
+		// add Device for voltage/current clamp electrode(s)
+		//
+		cbit.vcell.modelapp.ElectricalStimulus stimuli[] = simContext.getElectricalStimuli();
+		cbit.vcell.modelapp.Electrode groundElectrode = simContext.getGroundElectrode();
+		for (int i = 0; i < stimuli.length; i++){
+			cbit.vcell.modelapp.ElectricalStimulus stimulus = stimuli[i];
+			//
+			// get electrodes
+			//
+			cbit.vcell.modelapp.Electrode probeElectrode = stimulus.getElectrode();
+			if (probeElectrode == null){
+				throw new RuntimeException("null electrode for electrical stimulus");
+			}
+			if (groundElectrode == null){
+				throw new RuntimeException("null ground electrode for electrical stimulus");
+			}
+
+			Feature negativeFeature = groundElectrode.getFeature();
+			Feature positiveFeature = probeElectrode.getFeature();
+
+			FeatureMapping negativeFeatureMapping = (FeatureMapping)simContext.getGeometryContext().getStructureMapping(negativeFeature);
+			FeatureMapping positiveFeatureMapping = (FeatureMapping)simContext.getGeometryContext().getStructureMapping(positiveFeature);
+			//
+			// right now, only support clamping a single membrane (almost ALWAYS the case).
+			// find the membrane
+			//
+			Membrane clampedMembrane = null;
+			for (int j = 0; j < structures.length; j++){
+				if (structures[j] instanceof Membrane){
+					Membrane membrane = (Membrane)structures[j];
+					if ((membrane.getInsideFeature() == negativeFeature && membrane.getOutsideFeature() == positiveFeature) ||
+						(membrane.getInsideFeature() == positiveFeature && membrane.getOutsideFeature() == negativeFeature)){
+						clampedMembrane = membrane;
+					}
+				}
+			}
+			
+			//
+			// really should check the position of the electrodes to determine which regions are affected
+			//
+			System.out.println("<<< WARNING >>> MappingUtilities.addElectricalDevices(): should check which volume regions are affected by voltage/current clamp electrodes");
+			LumpedLocation surfaceLocation = null;
+			for (int j = 0; j < locations.length; j++){
+				if (locations[j] instanceof LumpedLocation && ((LumpedLocation)locations[j]).getDimension()==2 && locations[j].getName().startsWith(clampedMembrane.getName()+"_")){
+					surfaceLocation = (LumpedLocation)locations[j];
+					LumpedLocation adjacentVolumeLocation1 = (LumpedLocation)surfaceLocation.getAdjacentLocations()[0];
+					LumpedLocation adjacentVolumeLocation2 = (LumpedLocation)surfaceLocation.getAdjacentLocations()[1];
+					//
+					// find location for positive electrode
+					//
+					LumpedLocation positiveVolumeLocation = null;
+					if (adjacentVolumeLocation1.getName().startsWith(positiveFeature.getName())){
+						positiveVolumeLocation = adjacentVolumeLocation1;
+					}else if (adjacentVolumeLocation2.getName().startsWith(positiveFeature.getName())){
+						positiveVolumeLocation = adjacentVolumeLocation2;
+					}else{
+						throw new RuntimeException("could not resolve positive electrode at surface location : "+surfaceLocation.getName());
+					}
+					//
+					// find location for negative electrode
+					//
+					LumpedLocation negativeVolumeLocation = null;
+					if (adjacentVolumeLocation1.getName().startsWith(negativeFeature.getName())){
+						negativeVolumeLocation = adjacentVolumeLocation1;
+					}else if (adjacentVolumeLocation2.getName().startsWith(negativeFeature.getName())){
+						negativeVolumeLocation = adjacentVolumeLocation2;
+					}else{
+						throw new RuntimeException("could not resolve negative electrode at surface location : "+surfaceLocation.getName());
+					}
+
+					//
+					// get VolumeElectricalMaterial to connect positive electrode
+					//
+					OnePortElectricalDevice positiveElectricalMaterial = (OnePortElectricalDevice)physicalModel.getModelComponent("conductor_"+positiveVolumeLocation.getName());
+					if (positiveElectricalMaterial == null){
+						throw new RuntimeException("found no conductor modelComponent in VolumeLocation "+positiveVolumeLocation.getName());
+					}
+					//
+					// get VolumeElectricalMaterial to connect negative electrode
+					//
+					OnePortElectricalDevice negativeElectricalMaterial = (OnePortElectricalDevice)physicalModel.getModelComponent("conductor_"+negativeVolumeLocation.getName());
+					if (negativeElectricalMaterial == null){
+						throw new RuntimeException("found no conductor in VolumeLocation "+negativeVolumeLocation.getName());
+					}
+
+					//
+					// create surface component for voltage/current clamp device
+					//
+					TwoPortElectricalComponent clampDevice = null;
+					if (stimulus instanceof cbit.vcell.modelapp.CurrentClampStimulus){
+						VCUnitDefinition unit = VCUnitDefinition.UNIT_pA;  // current clamp electrodes are always "total current"
+						cbit.vcell.modelapp.CurrentClampStimulus ccStimulus = (cbit.vcell.modelapp.CurrentClampStimulus)stimulus;
+						clampDevice = new CurrentSource("currentClamp",ccStimulus.getCurrentParameter().getExpression().evaluateConstant());
+						
+					}else if (stimulus instanceof cbit.vcell.modelapp.VoltageClampStimulus){
+						VCUnitDefinition unit = VCUnitDefinition.UNIT_mV;
+						cbit.vcell.modelapp.VoltageClampStimulus vcStimulus = (cbit.vcell.modelapp.VoltageClampStimulus)stimulus;
+						clampDevice = new VoltageSource("voltageClamp",vcStimulus.getVoltageParameter().getExpression().evaluateConstant());
+					}
+
+					//
+					// add device to model and add "Connection" from clamp device "Connectors" to appropriate "VolumeElectricalMaterials"
+					//
+					physicalModel.addModelComponent(clampDevice);
+					Connection positiveConnection = new Connection(new Connector[] { clampDevice.getConnectors(0),positiveElectricalMaterial.getConnectors(0) } );
+					physicalModel.addConnection(positiveConnection);
+					Connection negativeConnection = new Connection(new Connector[] { clampDevice.getConnectors(1),negativeElectricalMaterial.getConnectors(0) } );
+					physicalModel.addConnection(negativeConnection);
+					break; // use first region only
 				}
 			}
 		}
