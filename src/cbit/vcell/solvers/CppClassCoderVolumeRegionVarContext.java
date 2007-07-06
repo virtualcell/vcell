c94ed03cc046f0984d82664c29f216c2933d0e12
Fei Gao <fgao@uchc.edu>
2007-07-06 19:27:07 +0000

    Bug 628    standalone finite volume : fourth pass
    svn path=/vcell/trunk/VCell/; revision=1485

--- src/cbit/vcell/solvers/CppClassCoderVolumeRegionVarContext.java
+++ src/cbit/vcell/solvers/CppClassCoderVolumeRegionVarContext.java
@@ -108,7 +108,7 @@ protected Variable[] getRequiredVariables() throws Exception {
  * @param out java.io.PrintWriter
  */
 protected void writeConstructor(java.io.PrintWriter out) throws Exception {
-	out.println(getClassName()+"::"+getClassName()+"(Feature *Afeature,CString AspeciesName)");
+	out.println(getClassName()+"::"+getClassName()+"(Feature *Afeature, string& AspeciesName)");
 	out.println(": "+getParentClassName()+"(Afeature,AspeciesName)");
 	out.println("{");
 	try {
@@ -145,8 +145,8 @@ public void writeDeclaration(java.io.PrintWriter out) throws Exception {
 	out.println("class " + getClassName() + " : public " + getParentClassName());
 	out.println("{");
 	out.println(" public:");
-	out.println("    "+getClassName() + "(Feature *feature, CString speciesName);");
-	out.println("    virtual boolean resolveReferences(Simulation *sim);");
+	out.println("\t"+getClassName() + "(Feature *feature, string& speciesName);");
+	out.println("\tvirtual bool resolveReferences(Simulation *sim);");
 
 	try {
 		Expression ic = getEquation().getInitialExpression();
