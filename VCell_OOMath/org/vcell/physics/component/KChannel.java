e16efeea94004710f5f34d4a1ea66c29ffb09027
Jim Schaff <schaff@uchc.edu>
2008-06-13 18:47:20 +0000

    properly guard H-H model (Na and K channels), model still not working.
    svn path=/VCell5/trunk/; revision=2848

--- VCell_OOMath/org/vcell/physics/component/KChannel.java
+++ VCell_OOMath/org/vcell/physics/component/KChannel.java
@@ -17,7 +17,6 @@ public class KChannel extends IonChannelHH {
 		Variable alpha_n = new Variable("alpha_n(t)",VCUnitDefinition.UNIT_per_s);
 		Variable beta_n = new Variable("beta_n(t)",VCUnitDefinition.UNIT_per_s);
 		Variable alpha_n_exp_arg = new Variable("alpha_n_exp_arg(t)",VCUnitDefinition.UNIT_DIMENSIONLESS);
-		Variable beta_n_exp_arg = new Variable("beta_n_exp_arg(t)",VCUnitDefinition.UNIT_DIMENSIONLESS);
 		Variable V_n = new Variable("V_n(t)",VCUnitDefinition.UNIT_mV);
 		Parameter Vrest_n = new Parameter("Vrest_n",VCUnitDefinition.UNIT_mV);
 		
@@ -29,7 +28,6 @@ public class KChannel extends IonChannelHH {
 		addSymbol(beta_n);
 		addSymbol(V_n);
 		addSymbol(alpha_n_exp_arg);
-		addSymbol(beta_n_exp_arg);
 		addSymbol(Vrest_n);
 
 		addSymbol(gK_max);
@@ -38,10 +36,9 @@ public class KChannel extends IonChannelHH {
 			addEquation(Expression.valueOf("gK_max - (0.36)"));
 			addEquation(Expression.valueOf("d(n_o(t),t) - (alpha_n(t)*(1-n_o(t)) - beta_n(t)*n_o(t))"));
 			addEquation(Expression.valueOf("n_o(0) - ("+n_o_init_value+")"));
-			addEquation(Expression.valueOf("alpha_n(t) - (0.01 * (10.0 - V(t)) / (-1.0 + exp(alpha_n_exp_arg(t))))"));
+			addEquation(Expression.valueOf("alpha_n(t) - (((gt(1000*abs(alpha_n_exp_arg(t)),1))*(0.1*alpha_n_exp_arg(t) / (-1.0 + exp(alpha_n_exp_arg(t))))) + (le(1000*abs(alpha_n_exp_arg(t)),1))*0.1)"));
 			addEquation(Expression.valueOf("alpha_n_exp_arg(t) - (0.1 * (10.0 - V_n(t)))"));
-			addEquation(Expression.valueOf("beta_n(t) - (0.125 * exp(beta_n_exp_arg(t)))"));
-			addEquation(Expression.valueOf("beta_n_exp_arg(t) - (-0.0125 * V_n(t))"));
+			addEquation(Expression.valueOf("beta_n(t) - (0.125 * exp(-0.0125 * V_n(t)))"));
 			addEquation(Expression.valueOf("V_n(t) - (V(t) - Vrest_n)"));
 			addEquation(Expression.valueOf("Vrest_n - (-62)"));
 		} catch (ParseException e) {
--- VCell_OOMath/org/vcell/physics/component/NaChannel.java
+++ VCell_OOMath/org/vcell/physics/component/NaChannel.java
@@ -18,7 +18,6 @@ public class NaChannel extends IonChannelHH {
 		Variable alpha_m = new Variable("alpha_m(t)",VCUnitDefinition.UNIT_per_s);
 		Variable beta_m = new Variable("beta_m(t)",VCUnitDefinition.UNIT_per_s);
 		Variable alpha_m_exp_arg = new Variable("alpha_m_exp_arg(t)",VCUnitDefinition.UNIT_DIMENSIONLESS);
-		Variable beta_m_exp_arg = new Variable("beta_m_exp_arg(t)",VCUnitDefinition.UNIT_DIMENSIONLESS);
 		Variable V_m = new Variable("V_m(t)",VCUnitDefinition.UNIT_mV);
 		Parameter Vrest_m = new Parameter("Vrest_m",VCUnitDefinition.UNIT_mV);
 
@@ -26,8 +25,6 @@ public class NaChannel extends IonChannelHH {
 		Variable h_o_init = new Variable("h_o(0)",VCUnitDefinition.UNIT_DIMENSIONLESS);
 		Variable alpha_h = new Variable("alpha_h(t)",VCUnitDefinition.UNIT_per_s);
 		Variable beta_h = new Variable("beta_h(t)",VCUnitDefinition.UNIT_per_s);
-		Variable alpha_h_exp_arg = new Variable("alpha_h_exp_arg(t)",VCUnitDefinition.UNIT_DIMENSIONLESS);
-		Variable beta_h_exp_arg = new Variable("beta_h_exp_arg(t)",VCUnitDefinition.UNIT_DIMENSIONLESS);
 		Variable V_h = new Variable("V_h(t)",VCUnitDefinition.UNIT_mV);
 		Parameter Vrest_h = new Parameter("Vrest_h",VCUnitDefinition.UNIT_mV);
 		
@@ -38,7 +35,6 @@ public class NaChannel extends IonChannelHH {
 		addSymbol(alpha_m);
 		addSymbol(beta_m);
 		addSymbol(alpha_m_exp_arg);
-		addSymbol(beta_m_exp_arg);
 		addSymbol(V_m);
 		addSymbol(Vrest_m);
 		
@@ -46,8 +42,6 @@ public class NaChannel extends IonChannelHH {
 		addSymbol(h_o_init);
 		addSymbol(alpha_h);
 		addSymbol(beta_h);
-		addSymbol(alpha_h_exp_arg);
-		addSymbol(beta_h_exp_arg);
 		addSymbol(V_h);
 		addSymbol(Vrest_h);
 		
@@ -58,19 +52,16 @@ public class NaChannel extends IonChannelHH {
 			
 			addEquation(Expression.valueOf("d(m_o(t),t) - (alpha_m(t)*(1-m_o(t)) - beta_m(t)*m_o(t))"));
 			addEquation(Expression.valueOf("m_o(0) - ("+m_o_init_value+")"));
-			addEquation(Expression.valueOf("alpha_m(t) - ((0.1 * (25.0 - V_m(t)) / (-1.0 + exp(alpha_m_exp_arg(t)))))"));
+			addEquation(Expression.valueOf("alpha_m(t) - (((gt(1000*abs(alpha_m_exp_arg(t)),1))*(alpha_m_exp_arg(t) / (-1.0 + exp(alpha_m_exp_arg(t)))))+(le(1000*abs(alpha_m_exp_arg(t)),1))*1.0)"));
 			addEquation(Expression.valueOf("alpha_m_exp_arg(t) - ((0.1 * (25.0 - V_m(t))))"));
-			addEquation(Expression.valueOf("beta_m(t) - ((4.0 * exp(beta_m_exp_arg(t))))"));
-			addEquation(Expression.valueOf("beta_m_exp_arg(t) - ( - (0.05555555555555555 * V_m(t)))"));
+			addEquation(Expression.valueOf("beta_m(t) - ((4.0 * exp(- (0.05555555555555555 * V_m(t)))))"));
 			addEquation(Expression.valueOf("V_m(t) - (V(t) - Vrest_m)"));
 			addEquation(Expression.valueOf("Vrest_m - (-62)"));
 			
 			addEquation(Expression.valueOf("d(h_o(t),t) - (alpha_h(t)*(1-h_o(t)) - beta_h(t)*h_o(t))"));
 			addEquation(Expression.valueOf("h_o(0) - ("+h_o_init_value+")"));
-			addEquation(Expression.valueOf("alpha_h(t) - ((0.07 * exp(alpha_h_exp_arg(t))))"));
-			addEquation(Expression.valueOf("alpha_h_exp_arg(t) - (- (0.05 * V_h(t)))"));
-			addEquation(Expression.valueOf("beta_h(t) - (gt(1000*abs(beta_h_exp_arg(t)),1)*((1.0 / (1.0 + exp(beta_h_exp_arg(t))))))"));
-			addEquation(Expression.valueOf("beta_h_exp_arg(t) - ((0.1 * (30.0 - V_h(t))))"));
+			addEquation(Expression.valueOf("alpha_h(t) - ((0.07 * exp(- (0.05 * V_h(t)))))"));
+			addEquation(Expression.valueOf("beta_h(t) - ((1.0 / (1.0 + exp(0.1 * (30.0 - V_h(t))))))"));
 			addEquation(Expression.valueOf("V_h(t) - (V(t) - Vrest_h)"));
 			addEquation(Expression.valueOf("Vrest_h - (-62)"));
 		} catch (ParseException e) {
