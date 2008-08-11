b525731ce0db5a14a2c4ff5fd0d31cd716ca94a9
Frank Morgan <frm@uchc.edu>
2008-08-11 18:13:18 +0000

    Bug 988: 	Make DialogUtils and PopupGenerator EventDispatch safe.
    svn path=/vcell/trunk/VCell/; revision=3224

--- src/cbit/vcell/client/PopupGenerator.java
+++ src/cbit/vcell/client/PopupGenerator.java
@@ -1,9 +1,9 @@
 package cbit.vcell.client;
+import cbit.util.EventDispatchRunWithException;
 import cbit.vcell.client.server.*;
 import javax.swing.*;
 import java.awt.*;
 import cbit.gui.*;
-import java.util.*;
 import cbit.vcell.client.task.UserCancelException;
 /**
  * Insert the type's description here.
@@ -21,67 +21,58 @@ public class PopupGenerator extends DialogUtils{
  * @param preferenceName java.lang.String
  */
 private static String showDialog(final Component requester, final UserPreferences preferences, final UserMessage userMessage, final String replacementText, final int jOptionPaneMessageType) {
-	//
-	// if userMessage is a warning that can be ignored, and the preference is to ignore it, then return default selection.
-	//
-	if (userMessage.getUserPreferenceWarning() > -1 && preferences!=null && !preferences.getShowWarning(userMessage.getUserPreferenceWarning())){
-		return userMessage.getDefaultSelection();
-	}
+	return (String)
+	new EventDispatchRunWithException (){
+		public Object runWithException() throws Exception{
+			//
+			// if userMessage is a warning that can be ignored, and the preference is to ignore it, then return default selection.
+			//
+			if (userMessage.getUserPreferenceWarning() > -1 && preferences!=null && !preferences.getShowWarning(userMessage.getUserPreferenceWarning())){
+				return userMessage.getDefaultSelection();
+			}
 
-	
-	String message = userMessage.getMessage(replacementText);
-	JPanel panel = createMessagePanel(message);
-	JCheckBox checkBox = null;
-	if (userMessage.getUserPreferenceWarning() >= 0){
-		checkBox = new JCheckBox("Do not show this warning again");
-		panel.add(checkBox, BorderLayout.SOUTH);
-	}
-	JOptionPane pane = new JOptionPane(panel, jOptionPaneMessageType, 0, null, userMessage.getOptions(), userMessage.getDefaultSelection());
-	final JDialog dialog = pane.createDialog(requester, "");
-	switch (jOptionPaneMessageType) {
-		case JOptionPane.WARNING_MESSAGE: {
-			dialog.setTitle("WARNING:");
-			break;
-		}
-		case JOptionPane.ERROR_MESSAGE: {
-			dialog.setTitle("ERROR:");
-			break;
-		}
-		case JOptionPane.INFORMATION_MESSAGE: {
-			dialog.setTitle("INFO:");
-			break;
-		}
-	}
-	dialog.setResizable(true);
-	dialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
-	String id = Long.toString(System.currentTimeMillis());
-	Hashtable choices = new Hashtable();
-	try {
-		if (SwingUtilities.isEventDispatchThread()) {
-			ZEnforcer.showModalDialogOnTop(dialog,requester);
-		} else {
-			SwingUtilities.invokeAndWait(new Runnable() {
-				public void run() {
-					ZEnforcer.showModalDialogOnTop(dialog,requester);
+			
+			String message = userMessage.getMessage(replacementText);
+			JPanel panel = createMessagePanel(message);
+			JCheckBox checkBox = null;
+			if (userMessage.getUserPreferenceWarning() >= 0){
+				checkBox = new JCheckBox("Do not show this warning again");
+				panel.add(checkBox, BorderLayout.SOUTH);
+			}
+			JOptionPane pane = new JOptionPane(panel, jOptionPaneMessageType, 0, null, userMessage.getOptions(), userMessage.getDefaultSelection());
+			final JDialog dialog = pane.createDialog(requester, "");
+			switch (jOptionPaneMessageType) {
+				case JOptionPane.WARNING_MESSAGE: {
+					dialog.setTitle("WARNING:");
+					break;
 				}
-			});
+				case JOptionPane.ERROR_MESSAGE: {
+					dialog.setTitle("ERROR:");
+					break;
+				}
+				case JOptionPane.INFORMATION_MESSAGE: {
+					dialog.setTitle("INFO:");
+					break;
+				}
+			}
+			dialog.setResizable(true);
+			dialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+			try {
+				ZEnforcer.showModalDialogOnTop(dialog,requester);
+				if (checkBox!=null){
+					preferences.setShowWarning(userMessage.getUserPreferenceWarning(), ! checkBox.isSelected());
+				}
+				Object selectedValue = pane.getValue();
+				if(selectedValue == null || selectedValue.equals(JOptionPane.UNINITIALIZED_VALUE)) {
+					return UserMessage.OPTION_CANCEL;
+				} else {
+					return (String)selectedValue;
+				}
+			}finally {
+				dialog.dispose();
+			}
 		}
-		choices.put(id, pane.getValue());
-	} catch (Exception exc) {
-		exc.printStackTrace(System.out);
-		throw new RuntimeException(exc.getMessage());
-	} finally {
-		if (checkBox!=null){
-			preferences.setShowWarning(userMessage.getUserPreferenceWarning(), ! checkBox.isSelected());
-		}
-		dialog.dispose();
-		Object selectedValue = choices.get(id);
-		if(selectedValue == null || selectedValue.equals(JOptionPane.UNINITIALIZED_VALUE)) {
-			return UserMessage.OPTION_CANCEL;
-		} else {
-			return (String)selectedValue;
-		}
-	}
+	}.runEventDispatchThreadSafelyWrapRuntime();
 }
 
 
@@ -164,7 +155,7 @@ public static String showInputDialog(TopLevelWindowManager requester, String mes
  * @param preferences cbit.vcell.client.UserPreferences
  * @param preferenceName java.lang.String
  */
-public static String showInputDialog(final Component requester, String message, String initialValue) throws cbit.vcell.client.task.UserCancelException{
+public static String showInputDialog(final Component requester, String message, String initialValue) throws UserCancelException{
 
 	try{
 		return showInputDialog0(requester,message,initialValue);
