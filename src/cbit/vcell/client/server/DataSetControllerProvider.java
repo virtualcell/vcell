ae987131071f369719a2e00c9e2e769cd8f0f13a
Anuradha Lakshminarayana <anu@uchc.edu>
2008-09-15 16:07:07 +0000

    Bug 1059 :    Patch to Beta (4.6) - Wrong err msg while opening model without globals on 4.5 (Rel)
    svn path=/vcell/branches/VCell_4.6/VCell/; revision=3463

--- src/cbit/vcell/client/server/DataSetControllerProvider.java
+++
@@ -1,9 +0,0 @@
-package cbit.vcell.client.server;
-
-import cbit.vcell.server.DataAccessException;
-import cbit.vcell.server.DataSetController;
-
-public interface DataSetControllerProvider {
-
-	DataSetController getDataSetController() throws DataAccessException;
-}
--- src/org/vcell/optimization/ObjectiveFunction.java
+++
@@ -1,5 +0,0 @@
-package org.vcell.optimization;
-
-public interface ObjectiveFunction {
-
-}
