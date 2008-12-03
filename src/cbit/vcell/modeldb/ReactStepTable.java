c100e712d2ac87e69f83bbca49bb3c3ffe40789f
Frank Morgan <frm@uchc.edu>
2008-12-03 18:39:51 +0000

    Bug 1135: 	Increase name field size in ReactStepTable (vc_reactstep) from 64 to 255.
    svn path=/vcell/trunk/VCell/; revision=4060

--- src/cbit/vcell/modeldb/ReactStepTable.java
+++ src/cbit/vcell/modeldb/ReactStepTable.java
@@ -20,7 +20,7 @@ public class ReactStepTable extends cbit.sql.Table {
 	public final Field structRef			= new Field("structRef",	"integer",		"NOT NULL "+StructTable.REF_TYPE);
 	//public final Field kinetics 			= new Field("kinetics",		"long raw",		"NOT NULL");
 	public final Field kinetics 			= new Field("kinetics",		"CLOB",		"");
-	public final Field name	 				= new Field("name",			"varchar(64)",	"");
+	public final Field name	 				= new Field("name",			"varchar2(255)",	"");
 	public final Field chargeValence		= new Field("chargeValence","integer",		"");
 	public final Field physicsOptions		= new Field("physicsOptions","integer",		"");
 	public final Field kineticsLarge		= new Field("kineticsLRG",	"CLOB",				"");
