c37adac6a1f163faef0b4f0039c80219323df71d
Fei Gao <fgao@uchc.edu>
2009-03-26 18:36:51 +0000

    Bug 1221 	  	an unsaved biomodel which has a name can't save as the name
    svn path=/vcell/trunk/VCell/; revision=4562

--- src/cbit/vcell/client/task/NewName.java
+++ src/cbit/vcell/client/task/NewName.java
@@ -38,7 +38,8 @@ public int getTaskType() {
  */
 public void run(Hashtable<String, Object> hashTable) throws java.lang.Exception {
 	DocumentWindowManager documentWindowManager = (DocumentWindowManager)hashTable.get("documentWindowManager");
-	if (documentWindowManager.getVCDocument().getDocumentType() == VCDocument.MATHMODEL_DOC) {
+	VCDocument document = documentWindowManager.getVCDocument();
+	if (document.getDocumentType() == VCDocument.MATHMODEL_DOC) {
 		if (((MathModelWindowManager)documentWindowManager).hasUnappliedChanges()) {
 			String msg = "Changes have been made in VCML Editor, please click \"Apply Changes\" or \"Cancel\" to proceed.";
 			PopupGenerator.showErrorDialog(documentWindowManager, msg);
@@ -47,19 +48,20 @@ public void run(Hashtable<String, Object> hashTable) throws java.lang.Exception 
 	}
 
 	MDIManager mdiManager = (MDIManager)hashTable.get("mdiManager");
-	String oldName = documentWindowManager.getVCDocument().getName();
+	String oldName = document.getName();
+	String oldVersionName = document.getVersion() == null ? "" : oldName;
 	String myself = documentWindowManager.getRequestManager().getConnectionStatus().getUserName();
 	// if the version is null which means this is a new document, so the owner would be myself
-	String owner = documentWindowManager.getVCDocument().getVersion() == null ? myself : documentWindowManager.getVCDocument().getVersion().getOwner().getName();
-	String newName = mdiManager.getDatabaseWindowManager().showSaveDialog(documentWindowManager.getVCDocument().getDocumentType(), (JFrame)hashTable.get("currentDocumentWindow"), oldName);	
+	String owner = document.getVersion() == null ? myself : document.getVersion().getOwner().getName();
+	String newName = mdiManager.getDatabaseWindowManager().showSaveDialog(document.getDocumentType(), (JFrame)hashTable.get("currentDocumentWindow"), oldName);	
 	if (newName == null || newName.trim().length()==0){
 		throw new Exception("A name must be given to save");
 	} else if (newName.contains("'")){
 		throw new Exception("Apostrophe is not allowed in names");
-	} else if (owner.equals(myself) && oldName.equals(newName)) {
+	} else if (owner.equals(myself) && oldVersionName.equals(newName)) {
 		throw new Exception("A model with name '" + newName + "' already exists. Please give a different name.");
 	}
-	documentWindowManager.getVCDocument().setName(newName);
+	document.setName(newName);
 	hashTable.put("newName", newName);
 }
 
