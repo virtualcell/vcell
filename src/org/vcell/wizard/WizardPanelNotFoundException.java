ac740b72d09b4f926c24a37f84184ee63974f3ca
Li Ye <lye@uchc.edu>
2009-07-21 20:56:42 +0000

    Bug 1390 Add wizard package to vcell project at org.vcell.wizard
    svn path=/vcell/trunk/VCell/; revision=5133

---
+++ src/org/vcell/wizard/WizardPanelNotFoundException.java
@@ -0,0 +1,11 @@
+package org.vcell.wizard;
+
+
+public class WizardPanelNotFoundException extends RuntimeException {
+        
+    public WizardPanelNotFoundException() {
+        super();
+    }
+     
+    
+}
