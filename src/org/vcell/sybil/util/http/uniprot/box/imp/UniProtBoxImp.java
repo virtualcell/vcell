deb8c871f19b1ef3b2d64fee0164cb706b65fe39
Anuradha Lakshminarayana <anu@uchc.edu>
2010-01-22 19:53:08 +0000

    Bug 1541 :    Display name for linked pathway commons entity, not just urns/urls.
    svn path=/vcell/trunk/VCell/; revision=5876

--- src/org/vcell/sybil/util/http/uniprot/box/imp/UniProtBoxImp.java
+++ src/org/vcell/sybil/util/http/uniprot/box/imp/UniProtBoxImp.java
@@ -19,9 +19,9 @@ public class UniProtBoxImp implements UniProtBox {
 	protected RelationMap<Entry, Entry> replaceMap = new RelationHashMap<Entry, Entry>();
 	protected Map<Entry, String> recommendedNamesMap = new HashMap<Entry, String>();
 	
-	@Override public Collection<Entry> entries() { return entryDir.values(); }
+	 public Collection<Entry> entries() { return entryDir.values(); }
 
-	@Override public Entry entry(String id) {
+	 public Entry entry(String id) {
 		Entry entry = entryDir.get(id);
 		if(entry == null) {
 			entry = new EntryImp(this, id);
@@ -30,47 +30,47 @@ public class UniProtBoxImp implements UniProtBox {
 		return entry;
 	}
 
-	@Override public void setReplaces(Entry entryReplacer, Entry entryReplaced) {
+	 public void setReplaces(Entry entryReplacer, Entry entryReplaced) {
 		replaceMap.add(entryReplacer, entryReplaced);
 	}
 
-	@Override public boolean replaces(Entry entryReplacer, Entry entryReplaced) {
+	 public boolean replaces(Entry entryReplacer, Entry entryReplaced) {
 		return replaceMap.contains(entryReplacer, entryReplaced);
 	}
 
-	@Override public void remove(Entry entry) {
+	 public void remove(Entry entry) {
 		entryDir.remove(entry.id());
 		replaceMap.removeA(entry);
 		replaceMap.removeB(entry);
 	}
 
-	@Override public void unsetReplaces(Entry entryReplacer, Entry entryReplaced) {
+	 public void unsetReplaces(Entry entryReplacer, Entry entryReplaced) {
 		replaceMap.remove(entryReplacer, entryReplaced);
 	}
 
-	@Override public Set<Entry> entriesReplacing(Entry entryReplaced) {
+	 public Set<Entry> entriesReplacing(Entry entryReplaced) {
 		return replaceMap.getASet(entryReplaced);
 	}
 
-	@Override public void removeEntriesReplacing(Entry entryReplaced) { 
+	 public void removeEntriesReplacing(Entry entryReplaced) { 
 		replaceMap.removeB(entryReplaced);
 	}
 
-	@Override public Set<Entry> entriesReplacedBy(Entry entryReplacer) {
+	 public Set<Entry> entriesReplacedBy(Entry entryReplacer) {
 		return replaceMap.getBSet(entryReplacer);
 	}
 
-	@Override public void removeEntriesReplacedBy(Entry entryReplacer) { 
+	 public void removeEntriesReplacedBy(Entry entryReplacer) { 
 		replaceMap.removeA(entryReplacer);
 	}
 
-	@Override public boolean entryIsObsolete(Entry entry) { return replaceMap.containsB(entry); }
-	@Override public boolean entryIsObsolete(String id) { return entryIsObsolete(this.entry(id)); }
+	 public boolean entryIsObsolete(Entry entry) { return replaceMap.containsB(entry); }
+	 public boolean entryIsObsolete(String id) { return entryIsObsolete(this.entry(id)); }
 
-	@Override public String recommendedName(Entry entry) { return recommendedNamesMap.get(entry); }
-	@Override public void setRecommendedName(Entry entry, String name) { recommendedNamesMap.put(entry, name); }
+	 public String recommendedName(Entry entry) { return recommendedNamesMap.get(entry); }
+	 public void setRecommendedName(Entry entry, String name) { recommendedNamesMap.put(entry, name); }
 
-	@Override public void add(UniProtBox box) {
+	 public void add(UniProtBox box) {
 		for(Entry entry : box.entries()) {
 			Entry entryThis = entry(entry.id());
 			for(Entry entryReplaced : entry.replacedEntries()) {
