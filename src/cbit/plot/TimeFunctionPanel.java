fae7d560d224486add599afab4ea3de81923275e
Frank Morgan <frm@uchc.edu>
2010-01-26 21:13:09 +0000

    Bug 1544: 	Implement Graph Electrical Stimulus function.
    svn path=/vcell/trunk/VCell/; revision=5879

---
+++ src/cbit/plot/TimeFunctionPanel.java
@@ -0,0 +1,193 @@
+package cbit.plot;
+
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.JTextField;
+import java.awt.Insets;
+import javax.swing.JLabel;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.border.LineBorder;
+
+import org.vcell.util.gui.DialogUtils;
+
+import cbit.vcell.math.ReservedVariable;
+import cbit.vcell.parser.Expression;
+
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class TimeFunctionPanel extends JPanel {
+	private PlotPane plotPane;
+	private JTextField funcTextField;
+	private JTextField timeBegTextField;
+	private JTextField timeStepTextField;
+	private JTextField timeStepCountTextField;
+	
+	public TimeFunctionPanel() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0};
+		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 0.0};
+		setLayout(gridBagLayout);
+		
+		plotPane = new PlotPane();
+		plotPane.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		GridBagConstraints gbc_plotPane = new GridBagConstraints();
+		gbc_plotPane.weighty = 1.0;
+		gbc_plotPane.weightx = 1.0;
+		gbc_plotPane.insets = new Insets(4, 4, 5, 4);
+		gbc_plotPane.fill = GridBagConstraints.BOTH;
+		gbc_plotPane.gridx = 0;
+		gbc_plotPane.gridy = 0;
+		add(plotPane, gbc_plotPane);
+		
+		JPanel funcPanel = new JPanel();
+		GridBagConstraints gbc_funcPanel = new GridBagConstraints();
+		gbc_funcPanel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_funcPanel.insets = new Insets(4, 4, 4, 4);
+		gbc_funcPanel.gridx = 0;
+		gbc_funcPanel.gridy = 1;
+		add(funcPanel, gbc_funcPanel);
+		GridBagLayout gbl_funcPanel = new GridBagLayout();
+		gbl_funcPanel.columnWidths = new int[]{0, 0, 0};
+		gbl_funcPanel.rowHeights = new int[]{0, 0};
+		gbl_funcPanel.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_funcPanel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		funcPanel.setLayout(gbl_funcPanel);
+		
+		JLabel lblTimeFunction = new JLabel("Time Function");
+		GridBagConstraints gbc_lblTimeFunction = new GridBagConstraints();
+		gbc_lblTimeFunction.insets = new Insets(0, 0, 0, 4);
+		gbc_lblTimeFunction.anchor = GridBagConstraints.EAST;
+		gbc_lblTimeFunction.gridx = 0;
+		gbc_lblTimeFunction.gridy = 0;
+		funcPanel.add(lblTimeFunction, gbc_lblTimeFunction);
+		
+		funcTextField = new JTextField();
+		GridBagConstraints gbc_funcTextField = new GridBagConstraints();
+		gbc_funcTextField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_funcTextField.gridx = 1;
+		gbc_funcTextField.gridy = 0;
+		funcPanel.add(funcTextField, gbc_funcTextField);
+		funcTextField.setColumns(10);
+		
+		JPanel timePanel = new JPanel();
+		GridBagConstraints gbc_timePanel = new GridBagConstraints();
+		gbc_timePanel.insets = new Insets(4, 4, 5, 4);
+		gbc_timePanel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_timePanel.gridx = 0;
+		gbc_timePanel.gridy = 2;
+		add(timePanel, gbc_timePanel);
+		timePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JLabel lblTimeBegin = new JLabel("Time Begin");
+		timePanel.add(lblTimeBegin);
+		
+		timeBegTextField = new JTextField();
+		timePanel.add(timeBegTextField);
+		timeBegTextField.setColumns(10);
+		
+		JLabel lblStep = new JLabel("Time Step");
+		timePanel.add(lblStep);
+		
+		timeStepTextField = new JTextField();
+		timePanel.add(timeStepTextField);
+		timeStepTextField.setColumns(10);
+		
+		JLabel lblSteps = new JLabel("Step Count");
+		timePanel.add(lblSteps);
+		
+		timeStepCountTextField = new JTextField();
+		timePanel.add(timeStepCountTextField);
+		timeStepCountTextField.setColumns(10);
+		
+		JButton btnPlot = new JButton("Refresh Plot");
+		btnPlot.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				refreshPlot();
+			}
+		});
+		timePanel.add(btnPlot);
+		
+		init();
+	}
+
+	public static void main(String[] args){
+    	TimeFunctionPanel test = new TimeFunctionPanel();
+    	JDialog jdialog = new JDialog(JOptionPane.getRootFrame());
+    	jdialog.setContentPane(test);
+    	jdialog.pack();
+    	jdialog.setVisible(true);
+	}
+	
+	private void init(){
+		funcTextField.setText("t");
+		timeBegTextField.setText("0");
+		timeStepTextField.setText(".1");
+		timeStepCountTextField.setText("100");
+		refreshPlot();
+	}
+	
+	private void refreshPlot(){
+		try {
+			Expression exp = new Expression(funcTextField.getText());
+			exp.flatten();
+			cbit.vcell.parser.SimpleSymbolTable symbolTable =
+				new cbit.vcell.parser.SimpleSymbolTable(new String[] {"t"/*, "x", "y", "z"*/});
+			exp.bindExpression(symbolTable);
+			
+			double valueArray[] = new double[1/*4*/];
+//			valueArray[ReservedVariable.Z.getIndex()] = 0 ;
+//			valueArray[ReservedVariable.Y.getIndex()] = 0 ;
+//			valueArray[ReservedVariable.X.getIndex()] = 0 ;
+			
+			int totalTimePoints = 0;
+			double beginTime = 0;
+			double timeStep = 0;
+			try{
+				totalTimePoints = Integer.parseInt(timeStepCountTextField.getText())+1;
+			}catch (Exception e){
+				throw new Exception("Couldn't evaluate 'Step Count' as integer\n"+e.getMessage());
+			}
+			try{
+				beginTime = Double.parseDouble(timeBegTextField.getText());
+			}catch (Exception e){
+				throw new Exception("Couldn't evaluate 'Time Begin' as number\n"+e.getMessage());
+			}
+			try{
+				timeStep = Double.parseDouble(timeStepTextField.getText());
+			}catch (Exception e){
+				throw new Exception("Couldn't evaluate 'Time Step' as number\n"+e.getMessage());
+			}
+
+			double[] timePoints = new double[totalTimePoints];
+			double[] funcVals = new double[totalTimePoints];
+
+			for (int i = 0; i < totalTimePoints; i++) {
+				valueArray[ReservedVariable.TIME.getIndex()] = beginTime+i*timeStep;
+				timePoints[i] = valueArray[ReservedVariable.TIME.getIndex()];
+				funcVals[i] = exp.evaluateVector(valueArray);
+			}
+
+			PlotData plotData = new PlotData(timePoints, funcVals);
+			Plot2D plot2D = new Plot2D(null, new String[] { "Time Function" },new PlotData[] { plotData },
+					new String[] {"Time Function Value", "Time", "[" + "Time Function" + "]"});
+			plotPane.setPlot2D(	plot2D);
+		} catch (Exception e) {
+			e.printStackTrace();
+			DialogUtils.showErrorDialog(this, "Error refreshing Plot:\n"+e.getMessage());
+			plotPane.setPlot2D(	null);
+		}
+
+	}
+	public void setTimeFunction(String timeFunction){
+		funcTextField.setText(timeFunction);
+		refreshPlot();
+	}
+}
