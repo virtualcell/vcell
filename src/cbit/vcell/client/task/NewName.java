78efed1a397f0574435efcbe57cbae575015dd26
Frank Morgan <frm@uchc.edu>
2010-04-20 17:42:54 +0000

    Bug 1694: 	Allow user to re-enter doc name when saving if new exists.
    svn path=/vcell/trunk/VCell/; revision=6447

--- src/cbit/vcell/client/task/NewName.java
+++ src/cbit/vcell/client/task/NewName.java
@@ -1,12 +1,16 @@
 package cbit.vcell.client.task;
 
+import java.awt.Component;
 import java.util.Hashtable;
-import javax.swing.*;
 
 import org.vcell.util.UserCancelException;
+import org.vcell.util.document.User;
 import org.vcell.util.document.VCDocument;
+import org.vcell.util.document.VCDocumentInfo;
+import org.vcell.util.gui.DialogUtils;
 
 import cbit.vcell.client.*;
+import cbit.vcell.clientdb.DocumentManager;
 /**
  * Insert the type's description here.
  * Creation date: (5/31/2004 6:03:16 PM)
@@ -37,20 +41,55 @@ public void run(Hashtable<String, Object> hashTable) throws java.lang.Exception 
 
 	MDIManager mdiManager = (MDIManager)hashTable.get("mdiManager");
 	String oldName = document.getName();
-	String oldVersionName = document.getVersion() == null ? "" : oldName;
-	String myself = documentWindowManager.getRequestManager().getConnectionStatus().getUserName();
-	// if the version is null which means this is a new document, so the owner would be myself
-	String owner = document.getVersion() == null ? myself : document.getVersion().getOwner().getName();
-	String newName = mdiManager.getDatabaseWindowManager().showSaveDialog(document.getDocumentType(), (JFrame)hashTable.get("currentDocumentWindow"), oldName);	
-	if (newName == null || newName.trim().length()==0){
-		throw new Exception("A name must be given to save");
-//	} else if (newName.contains("'")){
-//		throw new Exception("Apostrophe is not allowed in names");
-	} else if (owner.equals(myself) && oldVersionName.equals(newName)) {
-		throw new Exception("A model with name '" + newName + "' already exists. Please give a different name.");
+	
+	User user = mdiManager.getFocusedWindowManager().getRequestManager().getDocumentManager().getUser();
+	DocumentManager documentManager = mdiManager.getFocusedWindowManager().getRequestManager().getDocumentManager();
+	VCDocumentInfo[] vcDocumentInfos = new VCDocumentInfo[0];
+	String documentTypeDescription = "unknown";
+	if(document.getDocumentType() == VCDocument.MATHMODEL_DOC){
+		documentTypeDescription = "MathModel";
+		vcDocumentInfos = documentManager.getMathModelInfos();
+	}else if(document.getDocumentType() == VCDocument.BIOMODEL_DOC){
+		documentTypeDescription = "BioModel";
+		vcDocumentInfos = documentManager.getBioModelInfos();
+	}else if(document.getDocumentType() == VCDocument.GEOMETRY_DOC){
+		documentTypeDescription = "Geometry";
+		vcDocumentInfos = documentManager.getGeometryInfos();
 	}
-	document.setName(newName);
-	hashTable.put("newName", newName);
+	String newDocumentName = (oldName==null?"New"+documentTypeDescription:oldName);
+	while(true){
+		newDocumentName =
+			mdiManager.getDatabaseWindowManager().showSaveDialog(
+					document.getDocumentType(),
+					(Component)hashTable.get("currentDocumentWindow"),
+					newDocumentName);
+			if (newDocumentName == null || newDocumentName.trim().length()==0){
+				newDocumentName = null;
+				DialogUtils.showWarningDialog(
+						(Component)hashTable.get("currentDocumentWindow"),
+						"New "+documentTypeDescription+" name cannot be empty.");
+				continue;
+			}
+			//Check name conflict
+			boolean bNameConflict = false;
+			for (int i = 0; i < vcDocumentInfos.length; i++) {
+				if(vcDocumentInfos[i].getVersion().getOwner().compareEqual(user)){
+					if(vcDocumentInfos[i].getVersion().getName().equals(newDocumentName)){
+						bNameConflict = true;
+						break;
+					}
+				}
+			}
+			if(bNameConflict){
+				DialogUtils.showWarningDialog((Component)hashTable.get("currentDocumentWindow"),
+				"A "+documentTypeDescription+" with name '"+newDocumentName+"' already exists.  Choose a different name.");
+				continue;
+			}else{
+				break;
+			}
+		}
+	document.setName(newDocumentName);
+	hashTable.put("newName", newDocumentName);
 }
 
 }
