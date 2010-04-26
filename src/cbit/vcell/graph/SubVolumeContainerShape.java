953fa5dd364b4a38fa5d3e715d9a7c24c0b8ea0b
Frank Morgan <frm@uchc.edu>
2010-04-26 14:02:06 +0000

    Bug 1732: patch 	Patch: Threading problem with SubVolumeContainerShape.
    svn path=/vcell/branches/VCell_4.8/VCell/; revision=6513

---
+++ src/cbit/vcell/graph/SubVolumeContainerShape.java
@@ -0,0 +1,209 @@
+package cbit.vcell.graph;
+/*©
+ * (C) Copyright University of Connecticut Health Center 2001.
+ * All rights reserved.
+©*/
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.awt.image.MemoryImageSource;
+
+import cbit.gui.graph.ContainerShape;
+import cbit.gui.graph.GraphModel;
+import cbit.gui.graph.LayoutException;
+import cbit.gui.graph.Shape;
+import cbit.image.VCPixelClass;
+import cbit.vcell.geometry.Geometry;
+import cbit.vcell.geometry.GeometryException;
+import cbit.vcell.geometry.GeometrySpec;
+/**
+ * This type was created in VisualAge.
+ */
+public class SubVolumeContainerShape extends ContainerShape{
+	Geometry geom = null;
+	private static final int SAMPLED_GEOM_SIZE_MAX = 150;
+	private java.awt.image.BufferedImage brightImage = null;
+
+/**
+ * ImageShape constructor comment.
+ * @param graphModel cbit.vcell.graph.GraphModel
+ */
+public SubVolumeContainerShape(Geometry argGeom,GraphModel graphModel) {
+	super(graphModel);
+	geom = argGeom;
+}
+
+/**
+ * getModelObject method comment.
+ */
+public Object getModelObject() {
+	return geom;
+}
+
+
+/**
+ * This method was created by a SmartGuide.
+ * @return int
+ * @param g java.awt.Graphics
+ */
+public Dimension getPreferedSize(java.awt.Graphics2D g) {
+	if(brightImage != null){
+		return new Dimension(brightImage.getWidth(),brightImage.getHeight());
+	}
+	return new Dimension(10,10);
+}
+
+
+/**
+ * This method was created by a SmartGuide.
+ * @return int
+ */
+public Point getSeparatorDeepCount() {	
+	return new Point(0,0);
+}
+
+/**
+ * This method was created in VisualAge.
+ */
+public void layout() throws LayoutException {
+
+	super.layout();
+
+	//for (int i=0;i<childShapeList.size();i++){
+		//SubvolumeShape shape = (SubvolumeShape)childShapeList.elementAt(i);
+		//Point attachPoint = getLocalAttachmentPoint((SubVolume)shape.getModelObject());
+////System.out.println("attachPoint for "+((SubVolume)shape.getModelObject()).getName()+" is "+attachPoint);
+		//shape.screenPos.x = attachPoint.x - shape.screenSize.width/2;
+		//shape.screenPos.y = attachPoint.y - shape.screenSize.height/2;
+		//shape.layout();
+	//}
+}
+
+
+/**
+ * paint method comment.
+ */
+public void paint(java.awt.Graphics2D g, int parentOffsetX, int parentOffsetY) {
+	
+	int absPosX = screenPos.x + parentOffsetX;
+	int absPosY = screenPos.y + parentOffsetY;
+
+	//
+	// draw background image (of handles)
+	//
+	if(brightImage != null){
+		g.drawImage(brightImage,absPosX, absPosY, null);
+	}
+	//
+	// draw attachment points within image (or they can be hidden)
+	//
+	for (int i = 0; i < childShapeList.size(); i++) {
+		Shape child = (Shape) childShapeList.elementAt(i);
+		child.paint(g, absPosX, absPosY);
+	}
+}
+
+public void refreshDisplayImage(BufferedImage newDisplayImage){
+	brightImage = newDisplayImage;
+}
+/**
+ * This method was created in VisualAge.
+ */
+public static BufferedImage CreateDisplayImage(Geometry geom) throws GeometryException{
+	try {		
+		int REAL_SAMPLE_X = 0;
+		int REAL_SAMPLE_Y = 0;
+		if(geom != null){
+			if(geom.getDimension() > 0){
+				//Calc Scaling parameters
+				double srcScaleX = (double)SAMPLED_GEOM_SIZE_MAX/geom.getExtent().getX();
+				double srcScaleY = (double)SAMPLED_GEOM_SIZE_MAX/geom.getExtent().getY();
+				
+				if(srcScaleX < srcScaleY){
+					REAL_SAMPLE_X = SAMPLED_GEOM_SIZE_MAX;
+					REAL_SAMPLE_Y = Math.max((int)(srcScaleX*geom.getExtent().getY()),1);
+				}
+				else{
+					REAL_SAMPLE_Y = SAMPLED_GEOM_SIZE_MAX;
+					REAL_SAMPLE_X = Math.max((int)(srcScaleY*geom.getExtent().getX()),1);		
+				}
+			}
+		}
+
+		GeometrySpec geometrySpec = geom.getGeometrySpec();
+		if (geometrySpec.getDimension() > 0) {
+			//
+			BufferedImage brightImage = new java.awt.image.BufferedImage(REAL_SAMPLE_X,REAL_SAMPLE_Y,java.awt.image.BufferedImage.TYPE_INT_RGB);
+			java.awt.Graphics2D brightG2D = brightImage.createGraphics();
+			brightG2D.setColor(java.awt.Color.white);
+			brightG2D.fillRect(0,0,REAL_SAMPLE_X,REAL_SAMPLE_Y);
+
+			cbit.image.VCImage sampledImage = geometrySpec.getSampledImage();
+			java.awt.image.IndexColorModel handleColorMap = GeometrySpec.getHandleColorMap();
+			byte[] reds = new byte[256];
+			handleColorMap.getReds(reds);
+			byte[] greens = new byte[256];
+			handleColorMap.getGreens(greens);
+			byte[] blues = new byte[256];
+			handleColorMap.getBlues(blues);
+			//Create projections of each subvolume handle
+			VCPixelClass[] pixClassHandles = sampledImage.getPixelClasses();
+			byte[] pixels = sampledImage.getPixels();
+			for(int i = 0;i < pixClassHandles.length;i+= 1){
+				byte[] zBuf = new byte[sampledImage.getNumX()*sampledImage.getNumY()];
+				java.util.Arrays.fill(zBuf,(byte)0);
+				//Project z
+				for(int j =0;j<pixels.length;j+= 1){
+					if(pixels[j] == pixClassHandles[i].getPixel()){
+						zBuf[j%zBuf.length] = (byte)1;
+					}
+				}
+				//Scale X-Y
+				int cmapIndex = (int)(pixClassHandles[i].getPixel()&0xff);
+				byte ired = reds[cmapIndex];
+				byte igrn = greens[cmapIndex];
+				byte iblu = blues[cmapIndex];
+				MemoryImageSource mis1 = new MemoryImageSource(sampledImage.getNumX(), 
+											sampledImage.getNumY(), 
+											new java.awt.image.IndexColorModel(8,2,
+												new byte[]{0,ired},
+												new byte[]{0,igrn},
+												new byte[]{0,iblu},
+												new byte[]{0,(byte)(200)}),
+											zBuf, 
+											0, sampledImage.getNumX());
+				javax.swing.ImageIcon theImageIcon =
+					new javax.swing.ImageIcon(
+						java.awt.Toolkit.getDefaultToolkit().createImage(mis1).
+							getScaledInstance(REAL_SAMPLE_X,REAL_SAMPLE_Y,java.awt.Image.SCALE_AREA_AVERAGING));
+
+				brightG2D.drawImage(theImageIcon.getImage(),0,0,theImageIcon.getImageObserver());
+				
+			}
+			return brightImage;
+		}
+		return null;
+	}catch (GeometryException e){
+		e.printStackTrace(System.out);
+		throw e;
+	} catch (Throwable e) {
+		e.printStackTrace(System.out);
+		throw new RuntimeException("Imageshape refreshImage Error " + e.getMessage());
+	}
+}
+
+
+/**
+ * refreshLabel method comment.
+ */
+public void refreshLabel() {}
+
+
+/**
+ * This method was created by a SmartGuide.
+ * @param newSize java.awt.Dimension
+ */
+public void resize(java.awt.Graphics2D g, Dimension newSize) {
+	return;
+}
+}
