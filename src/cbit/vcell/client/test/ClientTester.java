7cf75425b584c36eb4968991c11107559ac6a2e0
Frank Morgan <frm@uchc.edu>
2010-07-19 15:00:04 +0000

    Bug 1875:re-opened 	Patch:Implement UserLoginInfo saved to database.  Add client and server vcellsoftwareVersion to DB.
    svn path=/vcell/branches/VCell_4.8/VCell/; revision=6868

---
+++ src/cbit/vcell/client/test/ClientTester.java
@@ -0,0 +1,166 @@
+package cbit.vcell.client.test;
+
+import cbit.sql.*;
+/*©
+ * (C) Copyright University of Connecticut Health Center 2001.
+ * All rights reserved.
+©*/
+import java.awt.Frame;
+
+import cbit.vcell.server.UserLoginInfo;
+import cbit.vcell.server.VCellConnection;
+import cbit.vcell.server.VCellConnectionFactory;
+import cbit.vcell.client.server.ClientServerInfo;
+import cbit.vcell.client.server.ClientServerManager;
+/**
+ * This type was created in VisualAge.
+ */
+public class ClientTester {
+/**
+ * ClientTester constructor comment.
+ */
+public ClientTester() {
+	super();
+}
+/**
+ * This method was created in VisualAge.
+ * @return VCellConnection
+ * @param args java.lang.String[]
+ */
+public static cbit.vcell.client.server.ClientServerManager mainInit(String args[], String programName) throws Exception {
+	ClientServerInfo csInfo = null;
+	if (args.length == 3) {
+		if (args[0].equalsIgnoreCase("-local")) {
+			csInfo = ClientServerInfo.createLocalServerInfo(args[1], args[2]);
+		} else {
+			csInfo = ClientServerInfo.createRemoteServerInfo(new String[] {args[0]}, args[1], args[2]);
+		}
+	}else{
+		System.err.println("usage: " + programName + " -local userid password");
+		//System.err.println("usage: " + programName + " -jms userid password");
+		System.err.println("usage: " + programName +" host userid password");
+		throw new Exception("cannot connect");
+	}
+	ClientServerManager clientServerManager = new ClientServerManager();
+	clientServerManager.connect(null, csInfo);
+	
+	return clientServerManager;
+}
+/**
+ * This method was created in VisualAge.
+ * @return VCellConnection
+ * @param args java.lang.String[]
+ */
+public static cbit.vcell.client.server.ClientServerManager mainInit(String args[], String programName, Frame mainWindow) throws Exception {
+	ClientServerInfo csInfo = null;
+	if (args.length == 3) {
+		if (args[0].equalsIgnoreCase("-local")) {
+			csInfo = ClientServerInfo.createLocalServerInfo(args[1], args[2]);
+		} else {
+			csInfo = ClientServerInfo.createRemoteServerInfo(new String[] {args[0]}, args[1], args[2]);
+		}
+	}else{
+		System.err.println("usage: " + programName + " -local userid password");
+		//System.err.println("usage: " + programName + " -jms userid password");
+		System.err.println("usage: " + programName +" host userid password");
+		throw new Exception("cannot connect");
+	}
+	ClientServerManager clientServerManager = new ClientServerManager();
+	clientServerManager.connect(null, csInfo);
+	
+	return clientServerManager;
+}
+/**
+ * This method was created in VisualAge.
+ * @return VCellConnection
+ * @param args java.lang.String[]
+ */
+protected static cbit.vcell.server.VCellBootstrap VCellBootstrapInit(String args[], String programName) throws Exception {
+	if (args.length != 5) {
+		System.err.println("usage: "+programName+" host port AdminUserid AdminUserKey AdminPassword");
+		throw new Exception("cannot connect");
+	}
+	cbit.vcell.server.VCellServerFactory vcServerFactory = null;
+	new org.vcell.util.PropertyLoader();
+	if (!args[0].equalsIgnoreCase("-local")) {
+		try {
+			String SERVICE_NAME = "VCellBootstrapServer";
+			String host = args[0];
+			int port = Integer.parseInt(args[1]);
+			String connectString = "//"+host+":"+port+"/"+SERVICE_NAME;
+			cbit.vcell.server.VCellBootstrap vcellBootstrap = (cbit.vcell.server.VCellBootstrap)java.rmi.Naming.lookup(connectString);
+			return vcellBootstrap;
+		} catch (Throwable e){
+			throw new Exception("cannot contact server: "+e.getMessage());
+		}
+	} else {
+		throw new Exception("must use remote connection");
+	}
+}
+/**
+ * This method was created in VisualAge.
+ * @return VCellConnection
+ * @param args java.lang.String[]
+ */
+protected static cbit.vcell.server.VCellConnectionFactory VCellConnectionFactoryInit(String args[], String programName) throws Exception {
+	if (args.length != 3 && args.length != 7) {
+		System.err.println("usage: " + programName + " -local userid password [driverName connectionURL userid password]");
+		System.err.println("usage: " + programName + " -jms userid password");
+		System.err.println("usage: " + programName +" host userid password");
+		throw new Exception("cannot connect");
+	}
+	cbit.vcell.server.VCellConnectionFactory vcConnFactory = null;
+	new org.vcell.util.PropertyLoader();		
+	UserLoginInfo userLoginInfo = new UserLoginInfo(args[1], args[2]);
+	if (args[0].startsWith("-")) {
+		org.vcell.util.SessionLog log = new org.vcell.util.StdoutSessionLog(userLoginInfo.getUserName());
+		if (args[0].equalsIgnoreCase("-jms")) {
+			vcConnFactory = new cbit.vcell.server.LocalVCellConnectionFactory(userLoginInfo, log, false);
+		} else if (args[0].equalsIgnoreCase("-local")) {
+			vcConnFactory = new cbit.vcell.server.LocalVCellConnectionFactory(userLoginInfo, log, true);
+			if (args.length == 7) {
+				ConnectionFactory conFactory = new cbit.sql.OraclePoolingConnectionFactory(log, args[3], args[4], args[5], args[6]);
+				((cbit.vcell.server.LocalVCellConnectionFactory)vcConnFactory).setConnectionFactory(conFactory);
+			}
+		}
+	} else {
+		String host = args[0];
+		vcConnFactory = new cbit.vcell.server.RMIVCellConnectionFactory(host,userLoginInfo);
+	} 
+	return vcConnFactory;
+}
+/**
+ * This method was created in VisualAge.
+ * @return VCellConnection
+ * @param args java.lang.String[]
+ */
+protected static cbit.vcell.server.VCellServerFactory VCellServerFactoryInit(String args[], String programName) throws Exception {
+	if (args.length != 3 && args.length != 4) {
+		System.err.println("usage: "+programName+" [-local] userid password");
+		System.err.println("usage: "+programName+" host userid userkey password");
+		throw new Exception("cannot connect");
+	}
+	cbit.vcell.server.VCellServerFactory vcServerFactory = null;
+	org.vcell.util.PropertyLoader.loadProperties();
+	org.vcell.util.document.User user = null;
+	if (!args[0].equalsIgnoreCase("-local")) {
+		String host = args[0];
+		String userid = args[1];
+		org.vcell.util.document.KeyValue userKey = new org.vcell.util.document.KeyValue(args[2]);
+		user = new org.vcell.util.document.User(userid, userKey);
+		String password = args[3];
+		System.setSecurityManager(new java.rmi.RMISecurityManager());
+		vcServerFactory = new cbit.vcell.server.RMIVCellServerFactory(host, user, password);
+	} else {
+		String userid = args[1];
+		String password = args[2];
+//		cbit.vcell.server.SessionLog log = new cbit.vcell.server.StdoutSessionLog(userid);
+		org.vcell.util.SessionLog log = new org.vcell.util.NullSessionLog();
+		cbit.sql.ConnectionFactory conFactory = new cbit.sql.OraclePoolingConnectionFactory(log);
+		cbit.sql.KeyFactory keyFactory = new cbit.sql.OracleKeyFactory();
+		cbit.vcell.messaging.JmsConnectionFactory jmsConnFactory = new cbit.vcell.messaging.JmsConnectionFactoryImpl();
+		vcServerFactory = new cbit.vcell.server.LocalVCellServerFactory(userid,password,"<<local>>",jmsConnFactory,conFactory,keyFactory,log);
+	}
+	return vcServerFactory;
+}
+}
--- src/cbit/vcell/server/UserLoginInfo.java
+++ src/cbit/vcell/server/UserLoginInfo.java
@@ -3,6 +3,7 @@ package cbit.vcell.server;
 import java.io.Serializable;
 
 import org.vcell.util.PropertyLoader;
+import org.vcell.util.document.User;
 
 public class UserLoginInfo implements Serializable {
 	private String userName;
@@ -10,7 +11,8 @@ public class UserLoginInfo implements Serializable {
 	private String os_name;//os.name Operating system name 
 	private String os_arch;// os.arch Operating system architecture 
 	private String os_version;// os.version Operating system version
-	private String client;//VCell client logging in from
+	private String vcellSoftwareVersion;//VCell client logging in from
+	private User user;
 	public UserLoginInfo(String userName,String password) {
 		super();
 		this.userName = userName;
@@ -18,7 +20,7 @@ public class UserLoginInfo implements Serializable {
 		os_name = System.getProperty("os.name");
 		os_arch = System.getProperty("os.arch");
 		os_version = System.getProperty("os.version");
-		client = System.getProperty(PropertyLoader.vcellSoftwareVersion);
+		vcellSoftwareVersion = System.getProperty(PropertyLoader.vcellSoftwareVersion);
 	}
 	public String getOs_name() {
 		return os_name;
@@ -35,7 +37,26 @@ public class UserLoginInfo implements Serializable {
 	public String getPassword() {
 		return password;
 	}
-	public String getClient(){
-		return client;
+	public String getVCellSoftwareVersion(){
+		return vcellSoftwareVersion;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) throws Exception{
+		if(user.compareEqual(this.user)){
+			//Happens during 'reconnect'
+			return;
+		}
+		if(this.user != null){
+			//Different user never allowed to be reset
+			//During 'login' or 'change user' a new UserLoginInfo should be created
+			throw new Exception("UserLoginInfo: unexpected 'set' of different user.");
+		}
+		//set 'user' name must be the same as the 'login' name
+		if(!user.getName().equals(this.userName)){
+			throw new Exception("UserLoginInfo: 'set' user "+user.getName()+" does not equal login name "+this.userName);
+		}
+		this.user = user;
 	}
 }
