534c093625097b9a445120bf3f1f107685bab763
Fei Gao <fgao@uchc.edu>
2010-07-30 14:03:55 +0000

    removed an unused import
    svn path=/vcell/trunk/VCell/; revision=6936

--- src/cbit/vcell/client/desktop/DatabaseSearchPanel.java
+++ src/cbit/vcell/client/desktop/DatabaseSearchPanel.java
@@ -9,7 +9,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.security.spec.ECGenParameterSpec;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
