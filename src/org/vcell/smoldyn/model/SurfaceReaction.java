b4a6f71b5f4d846facccdf2e2400be3b9d958b8c
Matt Fenwick <mfenwick100@gmail.com>
2010-08-20 12:38:41 +0000

    Reopen Bug 1918: 	remove @Override's
    svn path=/vcell/trunk/VCell/; revision=7079

--- src/org/vcell/smoldyn/model/SurfaceReaction.java
+++ src/org/vcell/smoldyn/model/SurfaceReaction.java
@@ -1,5 +1,12 @@
 package org.vcell.smoldyn.model;
 
+import java.util.ArrayList;
+
+import org.vcell.smoldyn.model.SpeciesAndState.Product;
+import org.vcell.smoldyn.model.SpeciesAndState.Reactant;
+import org.vcell.smoldyn.model.util.ReactionParticipants;
+import org.vcell.smoldyn.simulation.SimulationUtilities;
+
 
 /** 
  * A reaction that only occurs on a surface.  It is specified by its {@link SurfaceReactionParticipants} (reactants and products), its
@@ -16,34 +23,54 @@ package org.vcell.smoldyn.model;
  */
 public class SurfaceReaction implements SmoldynReaction {
 
+	private final ArrayList<SpeciesAndState> reactants = new ArrayList<SpeciesAndState>(2);
+	private final ArrayList<SpeciesAndState> products = new ArrayList<SpeciesAndState>(2);
+	private final String name;
+	private final Surface surface;
+	private final double rate;
 	
 	
 	/**
-	 * Constructs a new SurfaceReaction.  Either reactants or products may have 0 participants, but not both, as this would be a do-nothing
-	 * reaction.  If reactants has 0 participants, this indicates a 0th order reaction.  If products has 0 participants, this indicates
+	 * Constructs a new reaction which is localized to a surface.
+	 * If reactants has 0 participants, this indicates a 0th order reaction.  If products has 0 participants, this indicates
 	 * a reaction which destroys molecules.  If both have at least 1 participant, this is a 'normal' surface reaction.
-	 * 
-	 * @param reactionname String -- must be non-null
-	 * @param surface Surface -- must be non-null
-	 * @param reactants {@link SurfaceReactionParticipants} -- must be non-null.  also, both reactants and products can not have zero 
-	 * 		participants
-	 * @param products {@link SurfaceReactionParticipants} -- must be non-null
-	 * @param rate double -- must be non-negative
-	 * 
+	 *
+	 * @param reactionname -- not null
+	 * @param surface -- not null
+	 * @param participants -- not null
+	 * @param rate -- not negative
 	 */
-	public SurfaceReaction() {
+	public SurfaceReaction(String reactionname, Surface surface, ReactionParticipants participants, double rate) {
+		SimulationUtilities.checkForNull("argument to surface reaction constructor", reactionname, surface, participants);
+		SimulationUtilities.checkForNonNegative("surface reaction rate", rate);
+		for(Reactant r : participants.getReactants()) {
+			this.reactants.add(r);
+		}
+		for(Product p : participants.getProducts()) {
+			this.products.add(p);
+		}
+		this.rate = rate;
+		this.name = reactionname;
+		this.surface = surface;
 	}
 
-	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.name;
 	}
 
-	@Override
 	public double getRate() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.rate;
 	}
 
+	public Surface getSurface() {
+		return this.surface;
+	}
+	
+	public SpeciesAndState [] getReactants() {
+		return this.reactants.toArray(new SpeciesAndState [this.reactants.size()]);
+	}
+	
+	public SpeciesAndState [] getProducts() {
+		return this.products.toArray(new SpeciesAndState [this.products.size()]);
+	}
 }
