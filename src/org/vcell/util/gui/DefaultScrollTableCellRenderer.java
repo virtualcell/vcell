aade0ea29b58bdc0ba7ac7e158048dd7dbf21a34
Fei Gao <fgao@uchc.edu>
2010-11-19 20:01:00 +0000

    Bug 2025 	  	Change BioModelEditor to tree view -- first pass
    svn path=/vcell/trunk/VCell/; revision=7697

---
+++ src/org/vcell/util/gui/DefaultScrollTableCellRenderer.java
@@ -0,0 +1,97 @@
+package org.vcell.util.gui;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.JComponent;
+import javax.swing.JTable;
+import javax.swing.UIManager;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableCellRenderer;
+
+import org.vcell.util.NumberUtils;
+
+import cbit.gui.ReactionEquation;
+import cbit.vcell.client.desktop.biomodel.BioModelEditor;
+import cbit.vcell.client.desktop.biomodel.BioModelEditorReactionTableModel;
+
+@SuppressWarnings("serial")
+public class DefaultScrollTableCellRenderer extends DefaultTableCellRenderer {
+	private static final Color hoverColor = new Color(0xFDFCDC);
+	public static final Border DEFAULT_GAP = BorderFactory.createEmptyBorder(2,4,2,4);
+	static final Border focusHighlightBorder = UIManager.getBorder("Table.focusCellHighlightBorder");
+	static final Color uneditableForeground = UIManager.getColor("TextField.inactiveForeground");
+	static final Border noFocusBorder = new EmptyBorder(1, 1, 1, 1);
+	static final Color everyOtherRowColor = new Color(0xe8edff);
+	static Font regularFont = null;
+	static Font uneditableFont = null;
+	/**
+	 * DefaultTableCellRendererEnhanced constructor comment.
+	 */
+	public DefaultScrollTableCellRenderer() {
+		super();
+		setOpaque(true);
+		regularFont = getFont();
+		uneditableFont = regularFont.deriveFont(Font.BOLD);
+	}
+	/**
+	 * Insert the method's description here.
+	 * Creation date: (3/27/2001 1:07:02 PM)
+	 * @return java.awt.Component
+	 * @param table javax.swing.JTable
+	 * @param value java.lang.Object
+	 * @param isSelected boolean
+	 * @param hasFocus boolean
+	 * @param row int
+	 * @param column int
+	 */
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+		super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+		setBorder(DEFAULT_GAP);
+		if (isSelected) {
+			setBackground(table.getSelectionBackground());
+			setForeground(table.getSelectionForeground());
+		} else {
+			if (table instanceof ScrollTable && ((ScrollTable)table).getHoverRow() == row) {
+				setBackground(hoverColor);
+			} else {
+				setBackground(row % 2 == 0 ? table.getBackground() : everyOtherRowColor);				
+			}
+			setForeground(table.getForeground());
+		}
+		
+		if (!table.isEnabled() || !table.getModel().isCellEditable(row, column)) {
+			setForeground(uneditableForeground);
+			setFont(uneditableFont);
+		}
+		if (value instanceof Double) {
+			Double doubleValue = (Double)value;
+			if (doubleValue.isNaN() || doubleValue.isInfinite()) {
+				setText(java.text.NumberFormat.getInstance().format(doubleValue.doubleValue()));
+			} else {
+				String formattedDouble = NumberUtils.formatNumber(doubleValue.doubleValue());
+				setText(formattedDouble);
+			}			
+		} else if (value instanceof JComponent) {
+			JComponent jc = (JComponent)value;
+			if (hasFocus) {
+			    jc.setBorder(focusHighlightBorder );
+			} else {
+			    jc.setBorder(noFocusBorder);
+			}
+			return jc;
+		}
+		if (value != null && (value.equals(BioModelEditor.ADD_NEW_HERE_TEXT) 
+				|| value.equals(BioModelEditorReactionTableModel.REACTION_CHOOSE_STRUCTURE_TEXT))) {
+			setText("<html><i>" + value + "</i></html>");
+		} 
+		if (value instanceof ReactionEquation) {
+			ReactionEquation reactionEquation = (ReactionEquation)value;
+			setText("<html>" + reactionEquation.getEquationleftHand() + "<b>" + ReactionEquation.REACTION_GOESTO + "</b>" + reactionEquation.getEquationRightHand());
+		}
+		return this;
+	}
+}
---
+++ src/org/vcell/util/gui/ScrollTable.java
@@ -0,0 +1,281 @@
+package org.vcell.util.gui;
+
+import java.awt.AWTException;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Robot;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.InputEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.border.Border;
+import javax.swing.event.TableModelEvent;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableModel;
+
+import cbit.gui.ReactionEquation;
+import cbit.gui.ScopedExpression;
+import cbit.gui.TableCellEditorAutoCompletion;
+import cbit.vcell.model.gui.ScopedExpressionTableCellRenderer;
+
+/**
+ * ScrollTable extends JTable and has a JScrollPane which encloses the table itself. The default cell renderer
+ * is {@link DefaultScrollTableCellRenderer} which has special background for noneditable table cells. The cell render
+ * for {@link ScopedExpression} is {@link ScopedExpressionTableCellRenderer} and the cell editor for {@link ScopedExpression}
+ * is {@link TableCellEditorAutoCompletion}.
+ * 
+ * The default auto resize mode is AUTO_RESIZE_OFF because most of tables have expression columns. The expression 
+ * columns will be formatted nicely automatically. You need to set auto resize mode if you don't want AUTO_RESIZE_OFF.
+ * 
+ * By default, when editing an expression, it will automatically try to bind expression to find errors. However, 
+ * there are exceptions like in Kinetics Editor, new parameter will be added it is not already defined. Here you 
+ * don't want to validate expression bindings. So you need to call ScrollTable.setValidateExpressionBinding(false).
+ * 
+ * To add ScrollTable to a JPanel, call ScrollTable.getEnclosingScrollPane() and add that to JPanel.
+ * @author fgao
+ *
+ */
+@SuppressWarnings("serial")
+public class ScrollTable extends JTable {
+	private static final Border tableCellHeaderBorder = (UIManager.getBorder("TableHeader.cellBorder"));
+	private static final Color tableHeaderColor = new Color(0xb9c9fe);
+
+	public enum CheckOption {
+		CheckAll("Check All"),
+		CheckSelected("Check Selected"),
+		UncheckAll("Uncheck All"),
+		UncheckSelected("Uncheck Selected");
+		
+		private String text = null;
+		CheckOption(String text) {
+			this.text = text;
+		}
+		public final String getText() {
+			return text;
+		}		
+	}
+	
+	static class ScrollTableBooleanCellRenderer extends JCheckBox implements TableCellRenderer {
+		public ScrollTableBooleanCellRenderer() {
+			super();
+			setHorizontalAlignment(JLabel.CENTER);
+			setBorderPainted(true);
+		}
+
+		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row,  int column) {
+			if (isSelected) {
+				setBackground(table.getSelectionBackground());
+				setForeground(table.getSelectionForeground());
+			} else {
+				setBackground(row % 2 == 0 ? table.getBackground() : DefaultScrollTableCellRenderer.everyOtherRowColor);
+				setForeground(table.getForeground());
+			
+				if (table.isEnabled() && table.getModel().isCellEditable(row, column)) {
+					setEnabled(true);
+				} else {
+					setEnabled(false);
+				}
+			}
+			 
+			setSelected((value != null && ((Boolean) value).booleanValue()));			
+			if (hasFocus) {
+				setBorder(DefaultScrollTableCellRenderer.focusHighlightBorder);
+			} else {
+				setBorder(DefaultScrollTableCellRenderer.noFocusBorder);
+			}
+
+			return this;
+		}
+	}
+	
+	private JScrollPane enclosingScrollPane = null;
+	private boolean bValidateExpressionBinding = true;
+	private ComponentAdapter componentListener = null;
+	protected int hoverRow = -1, hoverColumn = -1;
+	
+	public ScrollTable() {
+		super();		
+		initialize();
+	}
+	
+	private void initialize() {		
+		autoResizeMode = AUTO_RESIZE_OFF;
+		// make it bigger on Mac
+		setRowHeight(getRowHeight() + 4);
+		putClientProperty("terminateEditOnFocusLost", Boolean.TRUE);
+		
+		enclosingScrollPane = new JScrollPane(this);
+		enclosingScrollPane.getViewport().setBackground(Color.WHITE);
+		setPreferredScrollableViewportSize(new Dimension(200,100));
+		setIntercellSpacing(new Dimension(2,2));
+		
+		setDefaultRenderer(Object.class, new DefaultScrollTableCellRenderer());		
+		setDefaultRenderer(Number.class, new DefaultScrollTableCellRenderer());		
+		setDefaultRenderer(Double.class, new DefaultScrollTableCellRenderer());		
+		setDefaultRenderer(Boolean.class, new ScrollTableBooleanCellRenderer());
+		final TableCellRenderer defaultTableCellHeaderRenderer = getTableHeader().getDefaultRenderer();
+		getTableHeader().setDefaultRenderer(new TableCellRenderer() {
+
+			public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+				Component component = defaultTableCellHeaderRenderer.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+				if (component instanceof JLabel){
+					((JLabel)component).setHorizontalAlignment(SwingConstants.CENTER);
+					((JLabel)component).setBackground(tableHeaderColor);
+					((JLabel)component).setPreferredSize(new Dimension(50, 25));
+					((JLabel)component).setBorder(tableCellHeaderBorder);
+				}
+				return component;
+			}			
+		});
+		
+		// to gain focus if being clicked.
+		MouseAdapter mouseListener = new MouseAdapter() {
+			@Override
+			public void mouseClicked(final MouseEvent e) {
+				if (!hasFocus()) {
+					requestFocusInWindow();
+						
+					if (e.getButton() == MouseEvent.BUTTON1) {
+						addFocusListener(new FocusListener() {
+							public void focusLost(FocusEvent e) {
+							}									
+							public void focusGained(FocusEvent e) {
+								removeFocusListener(this);
+								Robot robot;
+								try {
+									robot = new Robot();
+									robot.mousePress(InputEvent.BUTTON1_MASK);
+									robot.mouseRelease(InputEvent.BUTTON1_MASK);
+								} catch (AWTException ex) {
+									ex.printStackTrace();
+								}											
+							}
+						});
+					}
+				}
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				hoverRow = rowAtPoint(e.getPoint());
+				hoverColumn = columnAtPoint(e.getPoint());
+				repaint();
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				hoverRow = -1;
+				hoverColumn = -1;
+				repaint();
+			}			
+		};
+		MouseMotionAdapter mouseMotionListener = new MouseMotionAdapter() {
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				hoverRow = rowAtPoint(e.getPoint());
+				hoverColumn = columnAtPoint(e.getPoint());
+				repaint();
+			}
+			
+		};
+		addMouseListener(mouseListener);
+		addMouseMotionListener(mouseMotionListener);
+	}
+	
+	public final JScrollPane getEnclosingScrollPane() {
+		return enclosingScrollPane;
+	}
+
+	public final void setValidateExpressionBinding(boolean bValidateExpressionBinding) {
+		this.bValidateExpressionBinding = bValidateExpressionBinding;
+		setDefaultEditor(ScopedExpression.class,new TableCellEditorAutoCompletion(this, bValidateExpressionBinding));
+		setDefaultEditor(ReactionEquation.class,new TableCellEditorAutoCompletion(this, bValidateExpressionBinding));
+	}
+
+	@Override
+	public void setVisible(boolean aFlag) {
+		super.setVisible(aFlag);
+		enclosingScrollPane.setVisible(aFlag);
+	}
+
+	@Override
+	public void setAutoResizeMode(int mode) {
+		super.setAutoResizeMode(mode);
+		if (autoResizeMode == AUTO_RESIZE_OFF) {
+			setDefaultRenderer(ScopedExpression.class, new ScopedExpressionTableCellRenderer());
+			
+			if (componentListener == null) {
+				componentListener  = new ComponentAdapter() {
+					public void componentResized(ComponentEvent e) {
+						SwingUtilities.invokeLater(new Runnable() {
+							public void run() {
+								ScopedExpressionTableCellRenderer.formatTableCellSizes(ScrollTable.this);
+							}
+						});	
+					}
+				};
+			}
+			enclosingScrollPane.removeComponentListener(componentListener);
+			enclosingScrollPane.addComponentListener(componentListener);
+		} else {
+			if (componentListener != null) {
+				enclosingScrollPane.removeComponentListener(componentListener);
+			}
+		}
+	}
+
+	@Override
+	public void setModel(TableModel dataModel) {
+		super.setModel(dataModel);
+		
+		// setting up listeners 
+		boolean bHasScopedExpressionColumn = false;
+		for (int i = 0; i < dataModel.getColumnCount(); i ++) {
+			if (getColumnClass(i).equals(ScopedExpression.class)) {
+				bHasScopedExpressionColumn = true;
+				break;
+			} else if (getColumnClass(i).equals(ReactionEquation.class)) {
+				bHasScopedExpressionColumn = true;
+				break;
+			}
+		}
+		if (bHasScopedExpressionColumn) {
+			setValidateExpressionBinding(bValidateExpressionBinding);
+			setAutoResizeMode(autoResizeMode);
+		} else {
+			if (autoResizeMode == AUTO_RESIZE_OFF) {
+				autoResizeMode = AUTO_RESIZE_SUBSEQUENT_COLUMNS;
+			}
+			setAutoResizeMode(autoResizeMode);
+		}
+	}
+
+	@Override
+	public void tableChanged(TableModelEvent e) {
+		super.tableChanged(e);
+		if (autoResizeMode == JTable.AUTO_RESIZE_OFF) {
+			ScopedExpressionTableCellRenderer.formatTableCellSizes(ScrollTable.this);
+		}
+	}
+
+	public final int getHoverRow() {
+		return hoverRow;
+	}
+
+	public final int getHoverColumn() {
+		return hoverColumn;
+	}	
+}
