5a34a407e58a71051cef5f65695d488496fd6455
Oliver Reubenacker <oruebenacker@uchc.edu>
2010-11-29 23:45:17 +0000

    2033 	  	Cleanup Visual Age-generated spaghetti code
    svn path=/vcell/trunk/VCell/; revision=7740

--- src/cbit/vcell/model/gui/ReactionCartoonEditorDialog.java
+++ src/cbit/vcell/model/gui/ReactionCartoonEditorDialog.java
@@ -1,12 +1,16 @@
 package cbit.vcell.model.gui;
 
-/*©
+/*
  * (C) Copyright University of Connecticut Health Center 2001.
  * All rights reserved.
-©*/
+ */
 import java.awt.BorderLayout;
+import java.awt.Insets;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 
 import org.vcell.util.gui.JInternalFrameEnhanced;
 
@@ -15,113 +19,76 @@ import cbit.vcell.graph.ReactionCartoonEditorPanel;
 import cbit.vcell.graph.StructureCartoonTool;
 import cbit.vcell.model.Model;
 import cbit.vcell.model.Structure;
-/**
- * This type was created in VisualAge.
- */
+
+@SuppressWarnings("serial")
 public class ReactionCartoonEditorDialog extends JInternalFrameEnhanced {
-	private javax.swing.JPanel ivjContentsPane = null;
-	private ReactionCartoonEditorPanel ivjReactionCartoonEditorPanel = null;
-	private StructureCartoonTool structureCartoonTool = null;
-/**
- * Constructor
- */
-/* WARNING: THIS METHOD WILL BE REGENERATED. */
-public ReactionCartoonEditorDialog(StructureCartoonTool sct) {
-	super();
-	structureCartoonTool = sct;
-	initialize();
-}
-/**
- * Insert the method's description here.
- * Creation date: (6/14/2005 4:16:19 PM)
- */
-public void cleanupOnClose() {
+	private ReactionCartoonEditorPanel reactionCartoonEditorPanel = null;
 
-	getReactionCartoonEditorPanel().cleanupOnClose();
-}
-/**
- * Return the ReactionCartoonEditorPanel property value.
- * @return cbit.vcell.graph.ReactionCartoonEditorPanel
- */
-/* WARNING: THIS METHOD WILL BE REGENERATED. */
-private ReactionCartoonEditorPanel getReactionCartoonEditorPanel() {
-	if (ivjReactionCartoonEditorPanel == null) {
-		try {
-			ivjReactionCartoonEditorPanel = new ReactionCartoonEditorPanel();
-			ivjReactionCartoonEditorPanel.setName("ReactionCartoonEditorPanel");
-			// user code begin {1}
-			// user code end
-		} catch (java.lang.Throwable ivjExc) {
-			// user code begin {2}
-			// user code end
-			handleException(ivjExc);
+	public ReactionCartoonEditorDialog(StructureCartoonTool sct) {
+		super();
+		initialize();
+	}
+
+	public void cleanupOnClose() {
+		getReactionCartoonEditorPanel().cleanupOnClose();
+	}
+
+	private ReactionCartoonEditorPanel getReactionCartoonEditorPanel() {
+		if (reactionCartoonEditorPanel == null) {
+			try {
+				reactionCartoonEditorPanel = new ReactionCartoonEditorPanel();
+				reactionCartoonEditorPanel.setName("ReactionCartoonEditorPanel");
+			} catch (Throwable throwable) {
+				handleException(throwable);
+			}
 		}
+		return reactionCartoonEditorPanel;
 	}
-	return ivjReactionCartoonEditorPanel;
-}
-/**
- * Called whenever the part throws an exception.
- * @param exception java.lang.Throwable
- */
-private void handleException(java.lang.Throwable exception) {
-
-	/* Uncomment the following lines to print uncaught exceptions to stdout */
-	// System.out.println("--------- UNCAUGHT EXCEPTION ---------");
-	// exception.printStackTrace(System.out);
-}
-/**
- * This method was created in VisualAge.
- * @param model cbit.vcell.model.Model
- * @param structure cbit.vcell.model.Structure
- */
-public void init(Model model, Structure structure, DocumentManager documentManager) {
-	getReactionCartoonEditorPanel().setModel(model);
-	getReactionCartoonEditorPanel().setStructure(structure);
-	getReactionCartoonEditorPanel().setDocumentManager(documentManager);
-	setTitle("Reactions for "+structure.getName());
-}
-/**
- * Initialize the class.
- */
-/* WARNING: THIS METHOD WILL BE REGENERATED. */
-private void initialize() {
-	try {
-		setFrameIcon(new ImageIcon(getClass().getResource("/images/step.gif")));
-		setName("ReactionCartoonEditorDialog");
-		setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-		setClosable(true);
-		setSize(600, 495);
-		setResizable(true);
-		add(getReactionCartoonEditorPanel(), BorderLayout.CENTER);
-	} catch (java.lang.Throwable ivjExc) {
-		handleException(ivjExc);
-	}
-	// user code begin {2}
-	// user code end
-}
-/**
- * main entrypoint - starts the part when it is run as an application
- * @param args java.lang.String[]
- */
-public static void main(java.lang.String[] args) {
-	try {
-		javax.swing.JFrame frame = new javax.swing.JFrame();
-		ReactionCartoonEditorDialog aReactionCartoonEditorDialog;
-		aReactionCartoonEditorDialog = new ReactionCartoonEditorDialog(null);
-		frame.setContentPane(aReactionCartoonEditorDialog);
-		frame.setSize(aReactionCartoonEditorDialog.getSize());
-		frame.addWindowListener(new java.awt.event.WindowAdapter() {
-			public void windowClosing(java.awt.event.WindowEvent e) {
-				System.exit(0);
-			};
-		});
-		java.awt.Insets insets = frame.getInsets();
-		frame.setSize(frame.getWidth() + insets.left + insets.right, frame.getHeight() + insets.top + insets.bottom);
-		frame.setVisible(true);
-	} catch (Throwable exception) {
-		System.err.println("Exception occurred in main() of javax.swing.JInternalFrame");
+	private void handleException(java.lang.Throwable exception) {
+		System.out.println("--------- UNCAUGHT EXCEPTION ---------");
 		exception.printStackTrace(System.out);
 	}
-}
+
+	public void init(Model model, Structure structure, DocumentManager documentManager) {
+		getReactionCartoonEditorPanel().setModel(model);
+		getReactionCartoonEditorPanel().setStructure(structure);
+		getReactionCartoonEditorPanel().setDocumentManager(documentManager);
+		setTitle("Reactions for " + structure.getName());
+	}
+
+	private void initialize() {
+		try {
+			setFrameIcon(new ImageIcon(getClass().getResource("/images/step.gif")));
+			setName("ReactionCartoonEditorDialog");
+			setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+			setClosable(true);
+			setSize(600, 495);
+			setResizable(true);
+			add(getReactionCartoonEditorPanel(), BorderLayout.CENTER);
+		} catch (Throwable throwable) {
+			handleException(throwable);
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			JFrame frame = new JFrame();
+			ReactionCartoonEditorDialog aReactionCartoonEditorDialog;
+			aReactionCartoonEditorDialog = new ReactionCartoonEditorDialog(null);
+			frame.setContentPane(aReactionCartoonEditorDialog);
+			frame.setSize(aReactionCartoonEditorDialog.getSize());
+			frame.addWindowListener(new WindowAdapter() {
+				public void windowClosing(WindowEvent e) {
+					System.exit(0);
+				};
+			});
+			Insets insets = frame.getInsets();
+			frame.setSize(frame.getWidth() + insets.left + insets.right, frame.getHeight() + insets.top + insets.bottom);
+			frame.setVisible(true);
+		} catch (Throwable exception) {
+			System.err.println("Exception occurred in main() of javax.swing.JInternalFrame");
+			exception.printStackTrace(System.out);
+		}
+	}
 
 }
