599ac2be052ed82d3cd8fd5f50f7fe66b9a7b138
Fei Gao <fgao@uchc.edu>
2011-01-14 18:30:47 +0000

    Bug 2104 	  	add missing property listeners
    svn path=/vcell/trunk/VCell/; revision=7928

--- src/cbit/vcell/client/desktop/biomodel/ParameterPropertiesPanel.java
+++ src/cbit/vcell/client/desktop/biomodel/ParameterPropertiesPanel.java
@@ -5,6 +5,8 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.beans.PropertyChangeEvent;
@@ -35,7 +37,7 @@ public class ParameterPropertiesPanel extends DocumentEditorSubPanel {
 	private JTextField nameTextField = null;
 	private JTextField descriptionTextField = null;
 
-	private class EventHandler implements FocusListener, PropertyChangeListener {
+	private class EventHandler implements ActionListener, FocusListener, PropertyChangeListener {
 		public void focusGained(FocusEvent e) {
 		}
 		public void focusLost(FocusEvent e) {
@@ -50,7 +52,12 @@ public class ParameterPropertiesPanel extends DocumentEditorSubPanel {
 				updateInterface();
 			}
 		}
-	};
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == nameTextField) {
+				changeName();
+			}			
+		}
+	}
 
 /**
  * EditSpeciesDialog constructor comment.
@@ -78,6 +85,7 @@ private void initialize() {
 	try {
 		nameTextField = new JTextField();
 		nameTextField.setEditable(false);
+		nameTextField.addActionListener(eventHandler);
 		descriptionTextField = new JTextField();
 		descriptionTextField.setEditable(false);
 		annotationTextArea = new javax.swing.JTextArea("", 1, 30);
