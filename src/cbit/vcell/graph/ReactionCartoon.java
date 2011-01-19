9bd1d104c637d3bbef17167e349cc31252a97a55
Oliver Reubenacker <oruebenacker@uchc.edu>
2011-01-19 21:26:19 +0000

    2113 	  	Refactoring ReactionCartoonTool to make code simpler and more flexible
    svn path=/vcell/trunk/VCell/; revision=7947

--- src/cbit/vcell/graph/ReactionCartoon.java
+++ src/cbit/vcell/graph/ReactionCartoon.java
@@ -7,7 +7,6 @@ package cbit.vcell.graph;
 import java.awt.Graphics2D;
 import java.beans.PropertyChangeEvent;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
