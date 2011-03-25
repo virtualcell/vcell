33d2d4c9d278da0078a8be637f114408b2006c5c
Jim Schaff <schaff@uchc.edu>
2011-03-25 19:04:11 +0000

    Bug 2246    enhancement of BioPaxObject propertyPanel
    svn path=/vcell/trunk/VCell/; revision=8440

--- src/cbit/vcell/client/desktop/biomodel/BioPaxObjectPropertiesPanel.java
+++ src/cbit/vcell/client/desktop/biomodel/BioPaxObjectPropertiesPanel.java
@@ -2,14 +2,32 @@ package cbit.vcell.client.desktop.biomodel;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Comparator;
 
 import org.vcell.pathway.BioPaxObject;
+import org.vcell.pathway.Catalysis;
+import org.vcell.pathway.CellularLocationVocabulary;
 import org.vcell.pathway.Complex;
+import org.vcell.pathway.Dna;
+import org.vcell.pathway.DnaRegion;
 import org.vcell.pathway.Entity;
+import org.vcell.pathway.EntityFeature;
+import org.vcell.pathway.Evidence;
+import org.vcell.pathway.Gene;
 import org.vcell.pathway.Interaction;
+import org.vcell.pathway.InteractionParticipant;
+import org.vcell.pathway.InteractionVocabulary;
 import org.vcell.pathway.PhysicalEntity;
+import org.vcell.pathway.Protein;
+import org.vcell.pathway.Provenance;
+import org.vcell.pathway.PublicationXref;
+import org.vcell.pathway.RelationshipXref;
+import org.vcell.pathway.Rna;
+import org.vcell.pathway.RnaRegion;
+import org.vcell.pathway.SmallMolecule;
+import org.vcell.pathway.UnificationXref;
 import org.vcell.pathway.Xref;
 import org.vcell.util.gui.ScrollTable;
 
@@ -27,10 +45,12 @@ public class BioPaxObjectPropertiesPanel extends DocumentEditorSubPanel {
 	private static class BioPaxObjectProperty {
 		String name;
 		String value;
-		private BioPaxObjectProperty(String name, String value) {
+		BioPaxObject bioPaxObject;
+		private BioPaxObjectProperty(String name, String value, BioPaxObject bioPaxObject) {
 			super();
 			this.name = name;
 			this.value = value;
+			this.bioPaxObject = bioPaxObject;
 		}
 	}
 	private static class BioPaxObjectPropertiesTableModel extends VCellSortTableModel<BioPaxObjectProperty> {
@@ -126,39 +146,118 @@ protected void refreshInterface() {
 	}
 	ArrayList<BioPaxObjectProperty> propertyList = new ArrayList<BioPaxObjectProperty>();
 	
-	if(bioPaxObject instanceof PhysicalEntity){
-		ArrayList<String> name = ((PhysicalEntity)bioPaxObject).getName();
-		if(name != null){
+	if (bioPaxObject instanceof Entity){
+		Entity entity = (Entity)bioPaxObject;
+		// entity::type
+		propertyList.add(new BioPaxObjectProperty("Type", bioPaxObject.getTypeLabel(), null));
+		// entity::name
+		ArrayList<String> name = entity.getName();
+		if (name != null){
 			if (name.size() > 0) {
-				propertyList.add(new BioPaxObjectProperty("name", name.get(0)));
+				propertyList.add(new BioPaxObjectProperty("Name", name.get(0), null));
 			}
-			if(name.size()>1){
-				if(((PhysicalEntity)bioPaxObject).getTypeLabel().equals("Complex")){ 
-					String location  = name.get(1);
+		}
+		// entity::availability (***ignored***)
+		// entity::dataSource (***ignored***)
+		// entity::evidence (***ignored***)
+		
+		if(entity instanceof PhysicalEntity){
+			PhysicalEntity physicalEntity = (PhysicalEntity)entity;
+			// physicalEntity::feature (***ignored***)
+			// physicalEntity::memberPhysicalEntity (***ignored***)
+			// physicalEntity::notFeature (***ignored***)
+			
+			// physicalEntity::cellular location
+			CellularLocationVocabulary cellularLocation = physicalEntity.getCellularLocation();
+			if (cellularLocation!=null){
+				propertyList.add(new BioPaxObjectProperty("Cellular Location", cellularLocation.getTerm().get(0),cellularLocation));
+			}else if (name != null && name.size()>1){
+				String location  = name.get(1);
+				if (location.contains("[") && location.contains("]")){
 					location = location.substring(location.indexOf("[")+1, location.indexOf("]"));
-					propertyList.add(new BioPaxObjectProperty("Cellular Location", location));
+					propertyList.add(new BioPaxObjectProperty("Cellular Location", location, null));
 				}
 			}
-		}
 			
-		propertyList.add(new BioPaxObjectProperty("Type", bioPaxObject.getTypeLabel()));
-		if(((PhysicalEntity)bioPaxObject).getTypeLabel().equals("Complex")){
-			Complex complex = (Complex) bioPaxObject;
-			for(PhysicalEntity pe : complex.getComponents()){
-				propertyList.add(new BioPaxObjectProperty("Component", pe.getName().get(0)));
+			if (entity instanceof Complex){
+				Complex complex = (Complex)entity;
+				// complex::componentStoichiometry (***ignored***)
+
+				// complex::components
+				for(PhysicalEntity pe : complex.getComponents()){
+					propertyList.add(new BioPaxObjectProperty("Component", pe.getName().get(0), pe));
+				}
+				
+			} else if (entity instanceof Protein){
+				Protein protein = (Protein)entity;
+				// protein::entity reference (***ignored***)
+
+			} else if (entity instanceof SmallMolecule){
+				SmallMolecule smallMolecule = (SmallMolecule) entity;
+				// smallMolecule::entityReference (***ignored***)
+
+			} else if (entity instanceof Dna){
+				// dna::entityReference (***ignored***)
+
+			} else if (entity instanceof DnaRegion){
+				// dnaRegion::entityReference (***ignored***)
+				
+			} else if (entity instanceof Rna){
+				// rna::entityReference (***ignored***)
+				
+			} else if (entity instanceof RnaRegion){
+				// rnaRegion::entityReference (***ignored***)
+				
 			}
-		}	
-	}else if(bioPaxObject instanceof Interaction){
-		ArrayList<String> nameList = ((Interaction)bioPaxObject).getName();
-		if(nameList.size() > 0) {
-			propertyList.add(new BioPaxObjectProperty("name", nameList.get(0)));
+		}else if(entity instanceof Interaction){
+			Interaction interaction = (Interaction)entity;
+			// interaction::interactionType
+			for (InteractionVocabulary interactionVocabulary : interaction.getInteractionTypes()){
+				if (interactionVocabulary.getTerm().size()>0){
+					propertyList.add(new BioPaxObjectProperty("Interaction Type", interactionVocabulary.getTerm().get(0), interactionVocabulary));
+				}
+			}
+			// interaction::participants
+			for (InteractionParticipant interactionParticipant : interaction.getParticipants()){
+				PhysicalEntity physicalEntity = interactionParticipant.getPhysicalEntity();
+				String physicalEntityName = physicalEntity.getName().size()>0 ? physicalEntity.getName().get(0) : physicalEntity.getID();
+				propertyList.add(new BioPaxObjectProperty(interactionParticipant.getLevel3PropertyName(), physicalEntityName, physicalEntity));
+			}
+			
+			if (interaction instanceof Catalysis){
+				Catalysis catalysis = (Catalysis)interaction;
+				// catalysis::controlled
+				Interaction controlledInteraction = catalysis.getControlledInteraction();
+				if (controlledInteraction!=null){
+					String controlledName = controlledInteraction.getID();
+					if (controlledInteraction.getName().size()>0){
+						controlledName = controlledInteraction.getName().get(0);
+					}
+					propertyList.add(new BioPaxObjectProperty("Controlled Interaction", controlledName, controlledInteraction));
+				}
+			}
+			
 		}
-		propertyList.add(new BioPaxObjectProperty("Type", bioPaxObject.getTypeLabel()));
-	}
-	if (bioPaxObject instanceof Entity) {
+
 		ArrayList<Xref> xrefList = ((Entity) bioPaxObject).getxRef();
 		for (Xref xref : xrefList) {
-			propertyList.add(new BioPaxObjectProperty("Xref", xref.getDb() + ":" + xref.getId()));
+			if (xref instanceof UnificationXref){
+				propertyList.add(new BioPaxObjectProperty("Xref", xref.getDb() + ":" + xref.getId(), xref));
+			}
+		}
+		for (Xref xref : xrefList) {
+			if (xref instanceof RelationshipXref){
+				propertyList.add(new BioPaxObjectProperty("Xref (related)", xref.getDb() + ":" + xref.getId(), xref));
+			}
+		}
+		for (Xref xref : xrefList) {
+			if (xref instanceof PublicationXref){
+				propertyList.add(new BioPaxObjectProperty("Publication", xref.getDb() + ":" + xref.getId(), xref));
+			}
+		}
+		// entity::comments
+		for (String comment : entity.getComments()){
+			propertyList.add(new BioPaxObjectProperty("Comment", comment, null));
 		}
 	}
 	tableModel.setData(propertyList);
