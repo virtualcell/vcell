07626596197ec24ff5ff82f42b0808a2920701e9
Edward Boyce <boyce@uchc.edu>
2011-03-28 16:23:25 +0000

    Bug 2251:
    Fixing whitespace
    
    svn path=/vcell/trunk/VCell/; revision=8472

--- convertSmoldyn.py
+++ convertSmoldyn.py
@@ -160,10 +160,10 @@ pc=PseudocolorAttributes()
 pc.pointType=4
 pc.pointSizePixels = 8
 if (includeVarNumber==0):
-     pc.max=(len(particleTypeList))
+          pc.max=(len(particleTypeList))
      else:
           pc.max=(len(particleIncludeList))
-          pc.maxFlag=1
+pc.maxFlag=1
           
 SetPlotOptions(pc)
 
