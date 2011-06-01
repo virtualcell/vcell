06c9411d4b3d673c5b17dc5faafe8712442a4426
Li Ye <lye@uchc.edu>
2011-06-01 16:04:18 +0000

    Bug 2387: Added disclaimer info. to Wizard package in VCell.
    svn path=/vcell/trunk/VCell/; revision=8925

--- src/org/vcell/wizard/Wizard.java
+++ src/org/vcell/wizard/Wizard.java
@@ -21,7 +21,20 @@ import javax.swing.border.EmptyBorder;
 
 import org.vcell.util.BeanUtils;
 import org.vcell.util.gui.DialogUtils;
+/**
+ * Copyright © 2008, 2010 Oracle and/or its affiliates. All rights reserved. Use is subject to license terms.
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ 
 
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    * Neither the name of Oracle Corporation nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+ 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+You acknowledge that this software is not designed, licensed or intended for use in the design, construction, operation or maintenance of any nuclear facility. 
+ */
 /**
  * This class implements a basic wizard dialog, where the users can
  * insert one or more Components to act as panels. These panels can be navigated
@@ -29,6 +42,9 @@ import org.vcell.util.gui.DialogUtils;
  * can be closed using the 'Cancel' button. Note that even though the dialog
  * uses a CardLayout manager, the order of the panels is not fixed. Each panel
  * determines at runtime what its next and previous panel will be.
+ */
+/**
+ * Modified by Tracy Li for VCell usage.
  */
 public class Wizard extends WindowAdapter implements PropertyChangeListener {
 
