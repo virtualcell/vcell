1231916b2c45f99319cf888b81bf4f38ca413461
Fei Gao <fgao@uchc.edu>
2011-06-09 16:28:18 +0000

    2409		patch 5.0beta : remove ion messaging.
    svn path=/vcell/branches/VCell_5.0/VCell/; revision=9039

---
+++ src/cbit/vcell/server/VCellServer.java
@@ -0,0 +1,52 @@
+package cbit.vcell.server;
+import java.rmi.RemoteException;
+import java.util.Date;
+
+import org.vcell.util.CacheStatus;
+import org.vcell.util.DataAccessException;
+import org.vcell.util.document.User;
+/**
+ * This type was created in VisualAge.
+ */
+public interface VCellServer extends java.rmi.Remote {
+
+/**
+ * This method was created in VisualAge.
+ * @return cbit.vcell.server.AdminDatabaseServer
+ * @exception java.rmi.RemoteException The exception description.
+ */
+AdminDatabaseServer getAdminDatabaseServer() throws RemoteException;
+
+
+/**
+ * This method was created in VisualAge.
+ * @return CacheStatus
+ */
+CacheStatus getCacheStatus() throws RemoteException;
+
+
+/**
+ * This method was created in VisualAge.
+ * @return java.lang.String[]
+ */
+User[] getConnectedUsers() throws RemoteException;
+
+/**
+ * This method was created in VisualAge.
+ * @return CacheStatus
+ */
+ServerInfo getServerInfo() throws RemoteException;
+
+/**
+ * This method was created by a SmartGuide.
+ * @return cbit.vcell.server.VCellConnection
+ * @exception java.rmi.RemoteException The exception description.
+ */
+public VCellConnection getVCellConnection(User user) throws RemoteException, DataAccessException;
+
+/**
+ * This method was created in VisualAge.
+ * @exception java.rmi.RemoteException The exception description.
+ */
+Date getBootTime() throws RemoteException;
+}
