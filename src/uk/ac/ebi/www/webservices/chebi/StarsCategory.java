8889304e505dfb0a57acd95875eeb554e4044a24
Li Ye <lye@uchc.edu>
2011-06-16 16:07:48 +0000

    Bug 2417: Generated CHEBI webservice client services.
    svn path=/vcell/trunk/VCell/; revision=9102

---
+++ src/uk/ac/ebi/www/webservices/chebi/StarsCategory.java
@@ -0,0 +1,72 @@
+/**
+ * StarsCategory.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package uk.ac.ebi.www.webservices.chebi;
+
+public class StarsCategory implements java.io.Serializable {
+    private java.lang.String _value_;
+    private static java.util.HashMap _table_ = new java.util.HashMap();
+
+    // Constructor
+    protected StarsCategory(java.lang.String value) {
+        _value_ = value;
+        _table_.put(_value_,this);
+    }
+
+    public static final java.lang.String _value1 = "ALL";
+    public static final java.lang.String _value2 = "TWO ONLY";
+    public static final java.lang.String _value3 = "THREE ONLY";
+    public static final StarsCategory value1 = new StarsCategory(_value1);
+    public static final StarsCategory value2 = new StarsCategory(_value2);
+    public static final StarsCategory value3 = new StarsCategory(_value3);
+    public java.lang.String getValue() { return _value_;}
+    public static StarsCategory fromValue(java.lang.String value)
+          throws java.lang.IllegalArgumentException {
+        StarsCategory enumeration = (StarsCategory)
+            _table_.get(value);
+        if (enumeration==null) throw new java.lang.IllegalArgumentException();
+        return enumeration;
+    }
+    public static StarsCategory fromString(java.lang.String value)
+          throws java.lang.IllegalArgumentException {
+        return fromValue(value);
+    }
+    public boolean equals(java.lang.Object obj) {return (obj == this);}
+    public int hashCode() { return toString().hashCode();}
+    public java.lang.String toString() { return _value_;}
+    public java.lang.Object readResolve() throws java.io.ObjectStreamException { return fromValue(_value_);}
+    public static org.apache.axis.encoding.Serializer getSerializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new org.apache.axis.encoding.ser.EnumSerializer(
+            _javaType, _xmlType);
+    }
+    public static org.apache.axis.encoding.Deserializer getDeserializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new org.apache.axis.encoding.ser.EnumDeserializer(
+            _javaType, _xmlType);
+    }
+    // Type metadata
+    private static org.apache.axis.description.TypeDesc typeDesc =
+        new org.apache.axis.description.TypeDesc(StarsCategory.class);
+
+    static {
+        typeDesc.setXmlType(new javax.xml.namespace.QName("http://www.ebi.ac.uk/webservices/chebi", "StarsCategory"));
+    }
+    /**
+     * Return type metadata object
+     */
+    public static org.apache.axis.description.TypeDesc getTypeDesc() {
+        return typeDesc;
+    }
+
+}
