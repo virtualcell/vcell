dfbc013f68f6d1b943a221a01af4f5144e25f77d
Edward Boyce <boyce@uchc.edu>
2011-09-13 18:23:03 +0000

    Bug 2598:    Commit of JVisit2.3.2 sources.
    svn path=/vcell/trunk/VCell/; revision=9724

---
+++ src_jvisit/llnl/visit/Version.java
@@ -0,0 +1,15 @@
+package llnl.visit;
+
+// ****************************************************************************
+// Class: Version
+//
+// Purpose:
+//   This automatically generated class contains VisIt's version.
+//
+// ****************************************************************************
+
+public class Version
+{
+    public final static byte[] version = {'2', '.', '3', '.', '2', '\0', '\0', '\0', '\0', '\0'};
+}
+
