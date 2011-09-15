cad9f547076b1ce14368564b629f14792435bb2c
Edward Boyce <boyce@uchc.edu>
2011-09-15 21:23:13 +0000

    Bug 2601:     Specify URL for 5.0 full licensing info
    svn path=/vcell/trunk/VCell/; revision=9737

--- src/cbit/vcell/client/desktop/DocumentWindow.java
+++ src/cbit/vcell/client/desktop/DocumentWindow.java
@@ -2892,7 +2892,7 @@ public void showTransMADialog()
 							"Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\n" +
 							"The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\n"+
 							"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n\n"+
-							"Please see http://vcell.org for licensing information on third party software included with the Virtual Cell."
+							"Please see http://vcell.org/userdocs/Beta/VCell5.0licensing.txt for licensing information on third party software included with the Virtual Cell."
 							);
 					
 					
