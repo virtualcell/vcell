53ebf3f98750ace4590da3c4bae1f6b1216365f1
Oliver Reubenacker <oruebenacker@uchc.edu>
2011-12-09 21:21:26 +0000

    2845 	  	Updated pathway URLs and samples
    svn path=/vcell/trunk/VCell/; revision=10483

--- src/cbit/vcell/client/desktop/biomodel/BioPaxObjectPropertiesPanel.java
+++ src/cbit/vcell/client/desktop/biomodel/BioPaxObjectPropertiesPanel.java
@@ -43,6 +43,8 @@ import org.vcell.pathway.RnaRegion;
 import org.vcell.pathway.SmallMolecule;
 import org.vcell.pathway.UnificationXref;
 import org.vcell.pathway.Xref;
+import org.vcell.pathway.sbpax.SBEntity;
+import org.vcell.pathway.sbpax.SBVocabulary;
 import org.vcell.relationship.RelationshipObject;
 import org.vcell.util.gui.DefaultScrollTableCellRenderer;
 import org.vcell.util.gui.DialogUtils;
@@ -209,165 +211,175 @@ protected void refreshInterface() {
 		return;
 	}
 	ArrayList<BioPaxObjectProperty> propertyList = new ArrayList<BioPaxObjectProperty>();
-	
-	if (bioPaxObject instanceof Entity){
-		Entity entity = (Entity)bioPaxObject;
-		// entity::type
-		propertyList.add(new BioPaxObjectProperty("Type", bioPaxObject.getTypeLabel(), null));
-		// entity::name
-		ArrayList<String> name = entity.getName();
-		if (name != null){
-			if (name.size() > 0) {
-				propertyList.add(new BioPaxObjectProperty("Name", name.get(0), null));
+
+	if(bioPaxObject instanceof SBEntity) {
+		SBEntity sbEntity = (SBEntity) bioPaxObject;
+		if (sbEntity instanceof Entity){
+			Entity entity = (Entity) sbEntity;
+			// entity::type
+			propertyList.add(new BioPaxObjectProperty("Type", bioPaxObject.getTypeLabel(), null));
+			// entity::name
+			ArrayList<String> name = entity.getName();
+			if (name != null){
+				if (name.size() > 0) {
+					propertyList.add(new BioPaxObjectProperty("Name", name.get(0), null));
+				}
 			}
-		}
-		// entity::availability (***ignored***)
-		// entity::dataSource (***ignored***)
-		// entity::evidence (***ignored***)
-		
-		// entity::Link
-		for(RelationshipObject rObject : bioModel.getRelationshipModel().getRelationshipObjects(bioPaxObject)){
-			BioModelEntityObject beObject = rObject.getBioModelEntityObject();
-			propertyList.add(new BioPaxObjectProperty("Linked physiology object", beObject.getName(), bioPaxObject));
-		}
-		
-		if(entity instanceof PhysicalEntity){
-			PhysicalEntity physicalEntity = (PhysicalEntity)entity;
-			// physicalEntity::feature (***ignored***)
-			// physicalEntity::memberPhysicalEntity (***ignored***)
-			// physicalEntity::notFeature (***ignored***)
-			
-			if(!(physicalEntity instanceof SmallMolecule)){
-				// physicalEntity::cellular location
-				CellularLocationVocabulary cellularLocation = physicalEntity.getCellularLocation();
-				if (cellularLocation!=null && cellularLocation.getTerm() != null && cellularLocation.getTerm().size() > 0){
-					propertyList.add(new BioPaxObjectProperty("Cellular Location", cellularLocation.getTerm().get(0),cellularLocation));
-				}else if (name != null && name.size()>1){
-					String location  = name.get(1);
-					if (location.contains("[") && location.contains("]")){
-						location = location.substring(location.indexOf("[")+1, location.indexOf("]"));
-						propertyList.add(new BioPaxObjectProperty("Cellular Location", location, null));
+			// entity::availability (***ignored***)
+			// entity::dataSource (***ignored***)
+			// entity::evidence (***ignored***)
+
+			// entity::Link
+			for(RelationshipObject rObject : bioModel.getRelationshipModel().getRelationshipObjects(bioPaxObject)){
+				BioModelEntityObject beObject = rObject.getBioModelEntityObject();
+				propertyList.add(new BioPaxObjectProperty("Linked physiology object", beObject.getName(), bioPaxObject));
+			}
+
+			if(entity instanceof PhysicalEntity){
+				PhysicalEntity physicalEntity = (PhysicalEntity)entity;
+				// physicalEntity::feature (***ignored***)
+				// physicalEntity::memberPhysicalEntity (***ignored***)
+				// physicalEntity::notFeature (***ignored***)
+
+				if(!(physicalEntity instanceof SmallMolecule)){
+					// physicalEntity::cellular location
+					CellularLocationVocabulary cellularLocation = physicalEntity.getCellularLocation();
+					if (cellularLocation!=null && cellularLocation.getTerm() != null && cellularLocation.getTerm().size() > 0){
+						propertyList.add(new BioPaxObjectProperty("Cellular Location", cellularLocation.getTerm().get(0),cellularLocation));
+					}else if (name != null && name.size()>1){
+						String location  = name.get(1);
+						if (location.contains("[") && location.contains("]")){
+							location = location.substring(location.indexOf("[")+1, location.indexOf("]"));
+							propertyList.add(new BioPaxObjectProperty("Cellular Location", location, null));
+						}
 					}
 				}
-			}
-			
-			if (entity instanceof Complex){
-				Complex complex = (Complex)entity;
-				// complex::componentStoichiometry (***ignored***)
 
-				// complex::components
-				for(PhysicalEntity pe : complex.getComponents()){
-					propertyList.add(new BioPaxObjectProperty("Component", getEntityName(pe), pe));
-				}
-				
-			} else if (entity instanceof Protein){
-//				Protein protein = (Protein)entity;
-				// protein::entity reference (***ignored***)
+				if (entity instanceof Complex){
+					Complex complex = (Complex)entity;
+					// complex::componentStoichiometry (***ignored***)
 
-			} else if (entity instanceof SmallMolecule){
-//				SmallMolecule smallMolecule = (SmallMolecule) entity;
-				// smallMolecule::entityReference (***ignored***)
-
-			} else if (entity instanceof Dna){
-				// dna::entityReference (***ignored***)
-
-			} else if (entity instanceof DnaRegion){
-				// dnaRegion::entityReference (***ignored***)
-				
-			} else if (entity instanceof Rna){
-				// rna::entityReference (***ignored***)
-				
-			} else if (entity instanceof RnaRegion){
-				// rnaRegion::entityReference (***ignored***)
-				
-			}
-		}else if(entity instanceof Interaction){
-			Interaction interaction = (Interaction)entity;
-			// interaction::interactionType
-			for (InteractionVocabulary interactionVocabulary : interaction.getInteractionTypes()){
-				if (interactionVocabulary.getTerm().size()>0){
-					propertyList.add(new BioPaxObjectProperty("Interaction Type", interactionVocabulary.getTerm().get(0), interactionVocabulary));
-				}
-			}
-			// interaction::participants
-			for (InteractionParticipant interactionParticipant : interaction.getParticipants()){
-				PhysicalEntity physicalEntity = interactionParticipant.getPhysicalEntity();
-				String physicalEntityName = physicalEntity.getName().size()>0 ? physicalEntity.getName().get(0) : physicalEntity.getIDShort();
-				String cellularLocation = "";
-				if(physicalEntity.getCellularLocation() != null){
-					cellularLocation = physicalEntity.getCellularLocation().getTerm().size()>0 ? " ["+physicalEntity.getCellularLocation().getTerm().get(0)+"]" : ""; 
-				}
-				propertyList.add(new BioPaxObjectProperty(interactionParticipant.getLevel3PropertyName(), physicalEntityName+cellularLocation, physicalEntity));
-			}
-			
-			// get the catalysts for interactions
-			Set<String> catalysisList = getCatalysisSet(interaction);
-			if(catalysisList.size() > 0 ){
-				for(String str : catalysisList){
-					propertyList.add(new BioPaxObjectProperty("Catalyzed by", str, interaction));
-				}
-			}
-			
-			// get the controls for interactions
-			Set<String> controlList = getControlSet(interaction);
-			if(controlList.size() > 0 ){
-				for(String str : controlList){
-					propertyList.add(new BioPaxObjectProperty("Controled by", str, interaction));
-				}
-			}
-			
-			if (interaction instanceof Catalysis){
-				Catalysis catalysis = (Catalysis)interaction;
-				// catalysis::controlled
-				Interaction controlledInteraction = catalysis.getControlledInteraction();
-				if (controlledInteraction!=null){
-					String controlledName = controlledInteraction.getIDShort();
-					if (controlledInteraction.getName().size()>0){
-						controlledName = controlledInteraction.getName().get(0);
+					// complex::components
+					for(PhysicalEntity pe : complex.getComponents()){
+						propertyList.add(new BioPaxObjectProperty("Component", getEntityName(pe), pe));
 					}
-					propertyList.add(new BioPaxObjectProperty("Controlled Interaction", controlledName, controlledInteraction));
+
+				} else if (entity instanceof Protein){
+					//				Protein protein = (Protein)entity;
+					// protein::entity reference (***ignored***)
+
+				} else if (entity instanceof SmallMolecule){
+					//				SmallMolecule smallMolecule = (SmallMolecule) entity;
+					// smallMolecule::entityReference (***ignored***)
+
+				} else if (entity instanceof Dna){
+					// dna::entityReference (***ignored***)
+
+				} else if (entity instanceof DnaRegion){
+					// dnaRegion::entityReference (***ignored***)
+
+				} else if (entity instanceof Rna){
+					// rna::entityReference (***ignored***)
+
+				} else if (entity instanceof RnaRegion){
+					// rnaRegion::entityReference (***ignored***)
+
+				}
+			}else if(entity instanceof Interaction){
+				Interaction interaction = (Interaction)entity;
+				// interaction::interactionType
+				for (InteractionVocabulary interactionVocabulary : interaction.getInteractionTypes()){
+					if (interactionVocabulary.getTerm().size()>0){
+						propertyList.add(new BioPaxObjectProperty("Interaction Type", interactionVocabulary.getTerm().get(0), interactionVocabulary));
+					}
+				}
+				// interaction::participants
+				for (InteractionParticipant interactionParticipant : interaction.getParticipants()){
+					PhysicalEntity physicalEntity = interactionParticipant.getPhysicalEntity();
+					String physicalEntityName = physicalEntity.getName().size()>0 ? physicalEntity.getName().get(0) : physicalEntity.getIDShort();
+					String cellularLocation = "";
+					if(physicalEntity.getCellularLocation() != null){
+						cellularLocation = physicalEntity.getCellularLocation().getTerm().size()>0 ? " ["+physicalEntity.getCellularLocation().getTerm().get(0)+"]" : ""; 
+					}
+					propertyList.add(new BioPaxObjectProperty(interactionParticipant.getLevel3PropertyName(), physicalEntityName+cellularLocation, physicalEntity));
+				}
+
+				// get the catalysts for interactions
+				Set<String> catalysisList = getCatalysisSet(interaction);
+				if(catalysisList.size() > 0 ){
+					for(String str : catalysisList){
+						propertyList.add(new BioPaxObjectProperty("Catalyzed by", str, interaction));
+					}
+				}
+
+				// get the controls for interactions
+				Set<String> controlList = getControlSet(interaction);
+				if(controlList.size() > 0 ){
+					for(String str : controlList){
+						propertyList.add(new BioPaxObjectProperty("Controled by", str, interaction));
+					}
+				}
+
+				if (interaction instanceof Catalysis){
+					Catalysis catalysis = (Catalysis)interaction;
+					// catalysis::controlled
+					Interaction controlledInteraction = catalysis.getControlledInteraction();
+					if (controlledInteraction!=null){
+						String controlledName = controlledInteraction.getIDShort();
+						if (controlledInteraction.getName().size()>0){
+							controlledName = controlledInteraction.getName().get(0);
+						}
+						propertyList.add(new BioPaxObjectProperty("Controlled Interaction", controlledName, controlledInteraction));
+					}
+				}
+
+			}else if(entity instanceof GroupObject){
+				GroupObject groupObject = (GroupObject)entity;
+				for(BioPaxObject bpo : groupObject.getGroupedObjects()){
+					propertyList.add(new BioPaxObjectProperty("Element::" + bpo.getTypeLabel(), 
+							getEntityName((Entity)bpo), bpo));
 				}
 			}
-			
-		}else if(entity instanceof GroupObject){
-			GroupObject groupObject = (GroupObject)entity;
-			for(BioPaxObject bpo : groupObject.getGroupedObjects()){
-				propertyList.add(new BioPaxObjectProperty("Element::" + bpo.getTypeLabel(), 
-						getEntityName((Entity)bpo), bpo));
+			//		
+			//		//Neighbor: this function will be removed later
+			//		PathwayGrouping pathwayGrouping = new PathwayGrouping();
+			//		Set<BioPaxObject> neighbors = pathwayGrouping.computeNeighbors(bioModel.getPathwayModel(), (Entity) bioPaxObject);
+			//		if(neighbors != null ){
+			//			for(BioPaxObject bpo : neighbors){
+			//				if(((Entity)bpo).getName() != null && ((Entity)bpo).getName().size()>0)
+			//					propertyList.add(new BioPaxObjectProperty("Neighbors", (((Entity)bpo).getName().get(0)), bpo));
+			//			}
+			//		}
+
+			// entity::xRef
+			ArrayList<Xref> xrefList = ((Entity) bioPaxObject).getxRef();
+			for (Xref xref : xrefList) {
+				if (xref instanceof UnificationXref){
+					propertyList.add(new BioPaxObjectProperty("Xref", xref.getDb() + ":" + xref.getId(), xref));
+				}
+			}
+			for (Xref xref : xrefList) {
+				if (xref instanceof RelationshipXref){
+					propertyList.add(new BioPaxObjectProperty("Xref (related)", xref.getDb() + ":" + xref.getId(), xref));
+				}
+			}
+			for (Xref xref : xrefList) {
+				if (xref instanceof PublicationXref){
+					propertyList.add(new BioPaxObjectProperty("Publication", xref.getDb() + ":" + xref.getId(), xref));
+				}
+			}
+			// entity::comments
+			for (String comment : entity.getComments()){
+				propertyList.add(new BioPaxObjectProperty("Comment", comment, null));
+			}
+			for(SBVocabulary sbVocab : sbEntity.getSBTerm()) {
+				
+			}
+			for(SBEntity sbSubEntity : sbEntity.getSBSubEntity()) {
+				
 			}
 		}
-//		
-//		//Neighbor: this function will be removed later
-//		PathwayGrouping pathwayGrouping = new PathwayGrouping();
-//		Set<BioPaxObject> neighbors = pathwayGrouping.computeNeighbors(bioModel.getPathwayModel(), (Entity) bioPaxObject);
-//		if(neighbors != null ){
-//			for(BioPaxObject bpo : neighbors){
-//				if(((Entity)bpo).getName() != null && ((Entity)bpo).getName().size()>0)
-//					propertyList.add(new BioPaxObjectProperty("Neighbors", (((Entity)bpo).getName().get(0)), bpo));
-//			}
-//		}
-		
-		// entity::xRef
-		ArrayList<Xref> xrefList = ((Entity) bioPaxObject).getxRef();
-		for (Xref xref : xrefList) {
-			if (xref instanceof UnificationXref){
-				propertyList.add(new BioPaxObjectProperty("Xref", xref.getDb() + ":" + xref.getId(), xref));
-			}
-		}
-		for (Xref xref : xrefList) {
-			if (xref instanceof RelationshipXref){
-				propertyList.add(new BioPaxObjectProperty("Xref (related)", xref.getDb() + ":" + xref.getId(), xref));
-			}
-		}
-		for (Xref xref : xrefList) {
-			if (xref instanceof PublicationXref){
-				propertyList.add(new BioPaxObjectProperty("Publication", xref.getDb() + ":" + xref.getId(), xref));
-			}
-		}
-		// entity::comments
-		for (String comment : entity.getComments()){
-			propertyList.add(new BioPaxObjectProperty("Comment", comment, null));
-		}
+
 	}
 	tableModel.setData(propertyList);
 	
---
+++ src/org/vcell/pathway/sbpax/SBPAXLabelUtil.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 1999-2011 University of Connecticut Health Center
+ *
+ * Licensed under the MIT License (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *  http://www.opensource.org/licenses/mit-license.php
+ */
+
+package org.vcell.pathway.sbpax;
+
+import java.util.List;
+
+public class SBPAXLabelUtil {
+
+	public static String makeLabel(SBEntity entity) {
+		String label = "";
+		if(entity instanceof SBMeasurable) {
+			SBMeasurable measurable = (SBMeasurable) entity;
+			List<Double> numbers = measurable.getNumber();
+			if(numbers != null && !numbers.isEmpty()) {
+				label = label + numbers.get(0) + " ";
+			} else {
+				label = "? ";
+			}
+			List<UnitOfMeasurement> units = measurable.getUnit();
+			if(units != null && !units.isEmpty()) {
+				label = label + makeLabel(units.get(0)) + " ";
+			} else {
+				label = label + "? ";
+			}
+		}
+		List<SBVocabulary> vocabularies = entity.getSBTerm();
+		if(vocabularies != null && !vocabularies.isEmpty()) {
+			label = label + makeLabel(vocabularies.get(0));
+		} else {
+			label = label + "???";
+		}
+		return label;
+	}
+	
+	public static String makeLabel(UnitOfMeasurement unit) {
+		
+		throw new RuntimeException("Not implemented yet");
+	}
+	
+	public static String makeLabel(SBVocabulary vocabulary) {
+		throw new RuntimeException("Not implemented yet");
+	}
+	
+}
