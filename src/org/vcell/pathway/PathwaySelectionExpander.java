20b4c5a3859307783ecfb6b5e748d876c444abed
Oliver Reubenacker <oruebenacker@uchc.edu>
2011-12-15 23:58:28 +0000

    2868 	  	More and clearer pathway import selection expansion options
    svn path=/vcell/trunk/VCell/; revision=10524

--- src/org/vcell/pathway/PathwaySelectionExpander.java
+++ src/org/vcell/pathway/PathwaySelectionExpander.java
@@ -18,58 +18,121 @@ import java.util.Set;
 
 public class PathwaySelectionExpander {
 	
-	public boolean includeAllComponents = false;
-
-	public void expandSelection(PathwayModel pathwayModel, List<BioPaxObject> selectedList, boolean isNeighborsIncluded) {
-		includeAllComponents = isNeighborsIncluded;
+	public void forPhysicalEntitiesAddComplexes(PathwayModel pathwayModel, List<BioPaxObject> selectedList) {
 		Set<BioPaxObject> selectedSet = new HashSet<BioPaxObject> (selectedList);
 		Set<BioPaxObject> selectedToAdd = new HashSet<BioPaxObject>();
-		// Add all entity ancestors (e.g. complexes for components, interactions for participants, etc.)
-		if(includeAllComponents){
-			do {
-				selectedToAdd.clear();
-				for(BioPaxObject selected : selectedSet) {
+		do {
+			selectedToAdd.clear();
+			for(BioPaxObject selected : selectedSet) {
+				if(selected instanceof PhysicalEntity) {
 					ArrayList<BioPaxObject> parents = pathwayModel.getParents(selected);
 					if(parents != null){
 						for(BioPaxObject parent : parents){
-							if((!selectedSet.contains(parent)) && (parent instanceof Entity)) {
+							if((!selectedSet.contains(parent)) && (parent instanceof Complex)) {
 								selectedToAdd.add(parent);							
 							}
 						}
 					}				
 				}
-				selectedSet.addAll(selectedToAdd);
-			} while(selectedToAdd.size() > 0);
+			}
+			selectedSet.addAll(selectedToAdd);
+		} while(selectedToAdd.size() > 0);
+		for(BioPaxObject selected : selectedSet){
+			if(!selectedList.contains(selected)) {
+				selectedList.add(selected);				
+			}
 		}
-		// Add certain children (e.g. participants, components, controlled interactions)
+	}
+
+	public void forComplexesAddComponents(PathwayModel pathwayModel, List<BioPaxObject> selectedList) {
+		Set<BioPaxObject> selectedSet = new HashSet<BioPaxObject> (selectedList);
+		Set<BioPaxObject> selectedToAdd = new HashSet<BioPaxObject>();
 		do {
 			selectedToAdd.clear();
 			for(BioPaxObject selected : selectedSet) {
-				if(selected instanceof InteractionImpl) {
-					for(InteractionParticipant participant : ((Interaction) selected).getParticipants()) {
-						PhysicalEntity physicalEntity = participant.getPhysicalEntity();
-						if(!selectedSet.contains(physicalEntity)) {
-							selectedToAdd.add(physicalEntity);							
-						}
-					}
-					if(selected instanceof Control) {
-						Interaction controlledInteraction = 
-							((Control) selected).getControlledInteraction();
-						if(controlledInteraction != null && !selectedSet.contains(controlledInteraction)) {
-							selectedToAdd.add(controlledInteraction);
-						}
-					} else if(selected instanceof Complex && includeAllComponents) {
-						ArrayList<PhysicalEntity> components = ((Complex) selected).getComponents();
-						for(PhysicalEntity component : components) {
-							if(!selectedSet.contains(component)) {
-								selectedToAdd.add(component);								
-							}
+				if(selected instanceof Complex) {
+					ArrayList<PhysicalEntity> components = ((Complex) selected).getComponents();
+					for(PhysicalEntity component : components) {
+						if(!selectedSet.contains(component)) {
+							selectedToAdd.add(component);													
 						}
 					}
 				}
 			}
 			selectedSet.addAll(selectedToAdd);
 		} while(selectedToAdd.size() > 0);
+		for(BioPaxObject selected : selectedSet){
+			if(!selectedList.contains(selected)) {
+				selectedList.add(selected);				
+			}
+		}
+	}
+
+	
+	public void forPhysicalEntitiesAddInteractions(PathwayModel pathwayModel, List<BioPaxObject> selectedList) {
+		Set<BioPaxObject> selectedSet = new HashSet<BioPaxObject> (selectedList);
+		Set<BioPaxObject> selectedToAdd = new HashSet<BioPaxObject>();
+		for(BioPaxObject selected : selectedList) {
+			if(selected instanceof PhysicalEntity) {
+				ArrayList<BioPaxObject> parents = pathwayModel.getParents(selected);
+				if(parents != null){
+					for(BioPaxObject parent : parents){
+						if((!selectedSet.contains(parent)) && (parent instanceof Interaction)) {
+							selectedToAdd.add(parent);							
+						}
+					}
+				}				
+			}
+		}
+		selectedSet.addAll(selectedToAdd);
+		for(BioPaxObject selected : selectedSet){
+			if(!selectedList.contains(selected)) {
+				selectedList.add(selected);				
+			}
+		}
+	}
+
+	public void forInteractionsAddControls(PathwayModel pathwayModel, List<BioPaxObject> selectedList) {
+		Set<BioPaxObject> selectedSet = new HashSet<BioPaxObject> (selectedList);
+		Set<BioPaxObject> selectedToAdd = new HashSet<BioPaxObject>();
+		// Add all entity ancestors (e.g. complexes for components, interactions for participants, etc.)
+		do {
+			selectedToAdd.clear();
+			for(BioPaxObject selected : selectedSet) {
+				if(selected instanceof Interaction) {
+					ArrayList<BioPaxObject> parents = pathwayModel.getParents(selected);
+					if(parents != null){
+						for(BioPaxObject parent : parents){
+							if((!selectedSet.contains(parent)) && (parent instanceof Control)) {
+								selectedToAdd.add(parent);							
+							}
+						}
+					}				
+				}
+			}
+			selectedSet.addAll(selectedToAdd);
+		} while(selectedToAdd.size() > 0);
+		for(BioPaxObject selected : selectedSet){
+			if(!selectedList.contains(selected)) {
+				selectedList.add(selected);				
+			}
+		}
+	}
+
+	public void forInteractionsAddParticipants(PathwayModel pathwayModel, List<BioPaxObject> selectedList) {
+		Set<BioPaxObject> selectedSet = new HashSet<BioPaxObject> (selectedList);
+		Set<BioPaxObject> selectedToAdd = new HashSet<BioPaxObject>();
+		for(BioPaxObject selected : selectedSet) {
+			if(selected instanceof Interaction) {
+				for(InteractionParticipant participant : ((Interaction) selected).getParticipants()) {
+					PhysicalEntity physicalEntity = participant.getPhysicalEntity();
+					if(!selectedSet.contains(physicalEntity)) {
+						selectedToAdd.add(physicalEntity);							
+					}
+				}
+			}
+		}
+		selectedSet.addAll(selectedToAdd);
 		for(BioPaxObject selected : selectedSet){
 			if(!selectedList.contains(selected)) {
 				selectedList.add(selected);				
