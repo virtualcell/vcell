800632fc1084274ede6217def1f47e258723cf57
Frank Morgan <frm@uchc.edu>
2012-01-05 19:53:24 +0000

    Bug 2893:rollback      Rollback from 1056 to svn 1055.
    svn path=/vcell/trunk/VCell/; revision=10561

--- src/cbit/vcell/VirtualMicroscopy/ImageDatasetReaderFactory.java
+++ src/cbit/vcell/VirtualMicroscopy/ImageDatasetReaderFactory.java
@@ -1,5 +1,6 @@
 package cbit.vcell.VirtualMicroscopy;
 
+import java.io.File;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
@@ -9,42 +10,59 @@ import org.vcell.util.UserCancelException;
 import org.vcell.util.VCellClientClasspathUtils;
 import org.vcell.util.gui.DialogUtils;
 
-import cbit.vcell.client.UserMessage;
 import cbit.vcell.client.server.VCellThreadChecker;
 
 public class ImageDatasetReaderFactory {
-	public static ImageDatasetReader createImageDatasetReader() throws Exception,UserCancelException {
-		String[] options = {UserMessage.OPTION_YES , UserMessage.OPTION_NO};
-		//See if plugin exists in the expected place on the users machine, if not ask user if they want to use BioFormats
+	
+	// needs to be called in the proper thread
+	private static void loadPlugin() throws Exception{
+		VCellThreadChecker.checkRemoteInvocation();
+		VCellClientClasspathUtils.tryToLoadBioformatsLibrary();	
+	}
+	private static final String BIOFORMATS_YES = "yes";
+	private static final String BIOFORMATS_NO = "no";
+	public static ImageDatasetReader createImageDatasetReader() throws Exception {
+		
+		
+		String[] options = {BIOFORMATS_YES , BIOFORMATS_NO};
+		
 	    if (!VCellClientClasspathUtils.getBioformatsJarPath().exists()) {
-	    	String downloadApproval = DialogUtils.showWarningDialog(JOptionPane.getRootFrame(),
-	    		"In order to perform this operation, the BioFormats library plugin, licensed under the General Public License (GPL) must be downloaded.  This only needs to be done once. Do you want to download and install the BioFormats library plugin?", options, UserMessage.OPTION_YES);
-	    	if (!UserMessage.OPTION_YES.equals(downloadApproval)) {
-	    			DialogUtils.showInfoDialog(JOptionPane.getRootFrame(),
-	    			"BioFormats Library Plugin will not be loaded", "If you would like to use the BioFormats Library in the future, just retry the operation and agree to downloading the BioFormats Library under the General Public License.");
+	    	String downloadApproval = DialogUtils.showWarningDialog(JOptionPane.getRootFrame(), "In order to perform this operation, the BioFormats library plugin, licensed under the General Public License (GPL) must be downloaded.  This only needs to be done once. Do you want to download and install the BioFormats library plugin?", options, BIOFORMATS_YES);
+	    	if (!BIOFORMATS_YES.equals(downloadApproval)) {
+	    			DialogUtils.showInfoDialog(JOptionPane.getRootFrame(), "BioFormats Library Plugin will not be loaded", "If you would like to use the BioFormats Library in the future, just retry the operation and agree to downloading the BioFormats Library under the General Public License.");
 	    			//bail
 	    			throw UserCancelException.CANCEL_GENERIC;
 	    	}
-	    	VCellThreadChecker.checkRemoteInvocation();
 	    	VCellClientClasspathUtils.downloadBioformatsJar();
 	    }
-		try {
-			//Assume plugin downloaded (must be at this point) and classloader has path (might not yet).  try to construct an imageDataSetReader
+	    
+		try {	
 			return constructImageDatasetReader();
-		}catch (Exception e) {
+			}
+		catch (Throwable e) {
 			e.printStackTrace();
-			//Ignore exception, add Bioformats plugin to classpath and try to create again
-			VCellClientClasspathUtils.addBioformatsJarToPath();	
-			return constructImageDatasetReader();
+			
+			try {
+				loadPlugin();
+				return constructImageDatasetReader();
+			} catch (Throwable e2) {
+				e2.printStackTrace();
+				throw new Exception(e2.getMessage());
+	
+			}
 		}
 		
 	}
 
-	private static ImageDatasetReader constructImageDatasetReader() throws Exception{
-		Class readerClass = Class.forName("cbit.vcell.VirtualMicroscopy.BioformatsImageDatasetReader");
+	private static ImageDatasetReader constructImageDatasetReader()
+			throws ClassNotFoundException, InstantiationException,
+			IllegalAccessException, InvocationTargetException {
+		Class readerClass = Class
+				.forName("cbit.vcell.VirtualMicroscopy.BioformatsImageDatasetReader");
 		//System.out.println("--------------Starting getConstructors()");
-		Constructor<ImageDatasetReader> constructor = readerClass.getConstructors()[0];
+		Constructor<ImageDatasetReader> constructor = readerClass
+				.getConstructors()[0];
 		//System.out.println("-------------Actually created the Reader after getting the jar from the web");
-		return constructor.newInstance();
+		return constructor.newInstance(null);
 	}
 }
