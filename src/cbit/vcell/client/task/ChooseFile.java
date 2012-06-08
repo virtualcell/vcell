10bc68ed3ad1e34e51d709a2cfbc9f146bd52871
Edward Boyce <boyce@uchc.edu>
2012-06-08 18:31:08 +0000

    Bug 3045:     Fix potential null pointer exception on MacOSX when choosing fileFilters for geometry export
    svn path=/vcell/trunk/VCell/; revision=11384

--- src/cbit/vcell/client/task/ChooseFile.java
+++ src/cbit/vcell/client/task/ChooseFile.java
@@ -445,10 +445,13 @@ private File showGeometryModelXMLFileChooser(Hashtable<String, Object> hashTable
 		fileChooser.addChoosableFileFilter(FileFilters.FILE_FILTER_STL);
 		fileChooser.addChoosableFileFilter(FileFilters.FILE_FILTER_PLY);
 	}
-	// set the default file filter...
-	fileChooser.setFileFilter(FileFilters.FILE_FILTER_VCML);
+
 	// remove all selector
 	fileChooser.removeChoosableFileFilter(fileChooser.getAcceptAllFileFilter());
+	
+	// set the default file filter...
+	fileChooser.setFileFilter(FileFilters.FILE_FILTER_VCML);
+	
     fileChooser.setSelectedFile(new java.io.File(TokenMangler.fixTokenStrict(geom.getName())));
 	
 	fileChooser.setDialogTitle("Export Virtual Cell Geometry As...");
@@ -480,6 +483,11 @@ private File showGeometryModelXMLFileChooser(Hashtable<String, Object> hashTable
 			} else if (fileFilter == FileFilters.FILE_FILTER_PDF && !n.endsWith(".pdf")) {
 				selectedFile = new File(selectedFileName + ".pdf");
 			}
+			
+			if (fileFilter==null) {
+				throw new Exception("No file save type was selected.");
+			}
+			
 			// put the filter in the hash so the export task knows what to do...
 			hashTable.put("fileFilter", fileFilter);
 			resetPreferredFilePath(selectedFile, userPreferences);
