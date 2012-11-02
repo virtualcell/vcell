8d59c1ed75e057c462fdeee7c649a05b68fd9f71
Jim Schaff <schaff@uchc.edu>
2012-11-02 20:47:23 +0000

    patch to trunk (12259) Bug 3279 : handling xml output from SGE (and non-truncated jobNames)
    svn path=/vcell/branches/VCell_5.2/VCell/; revision=12262

--- src/cbit/vcell/message/server/htc/test/HtcTest.java
+++ src/cbit/vcell/message/server/htc/test/HtcTest.java
@@ -1,8 +1,12 @@
 package cbit.vcell.message.server.htc.test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.TreeMap;
 
 import org.vcell.util.ExecutableException;
+import org.vcell.util.PropertyLoader;
 import org.vcell.util.document.VCellServerID;
 
 import cbit.vcell.message.server.cmd.CommandServiceSsh;
@@ -11,6 +15,7 @@ import cbit.vcell.message.server.htc.HtcJobID;
 import cbit.vcell.message.server.htc.HtcJobNotFoundException;
 import cbit.vcell.message.server.htc.HtcJobStatus;
 import cbit.vcell.message.server.htc.HtcProxy;
+import cbit.vcell.message.server.htc.HtcProxy.HtcJobInfo;
 import cbit.vcell.message.server.htc.pbs.PbsJobID;
 import cbit.vcell.message.server.htc.pbs.PbsProxy;
 import cbit.vcell.message.server.htc.sge.SgeJobID;
@@ -41,6 +46,7 @@ public class HtcTest {
 			}else{
 				throw new RuntimeException("unrecognized htc type = "+htcType);
 			}
+			System.setProperty(PropertyLoader.vcellServerIDProperty,"BETA");
 			testHtcProxy1cmd(htcProxy);
 //			testHtcProxy2cmd(htcProxy);
 //			testServices(htcProxy, VCellServerID.getServerID("TEST2"));
@@ -55,22 +61,23 @@ public class HtcTest {
 
 	public static void testGetServiceJobInfos(HtcProxy htcProxy, VCellServerID serverID) throws Exception{
 		cbit.vcell.mongodb.VCMongoMessage.enabled = false;
-		java.util.Vector<cbit.vcell.message.server.htc.HtcProxy.ServiceJobInfo> sjinfos = htcProxy.getServiceJobInfos(serverID);
-		for(cbit.vcell.message.server.htc.HtcProxy.ServiceJobInfo sjInfo : sjinfos){
+		List<HtcJobID> htcJobIDs = htcProxy.getRunningServiceJobIDs(serverID); 
+		List<HtcJobInfo> sjinfos = htcProxy.getJobInfos(htcJobIDs);
+		for(HtcJobInfo sjInfo : sjinfos){
 			String jobID = null;
 			if(sjInfo.getHtcJobID() instanceof cbit.vcell.message.server.htc.pbs.PbsJobID){
 				jobID = ((cbit.vcell.message.server.htc.pbs.PbsJobID)sjInfo.getHtcJobID()).getPbsJobID();
 			}else if(sjInfo.getHtcJobID() instanceof cbit.vcell.message.server.htc.sge.SgeJobID){
 				jobID = ((cbit.vcell.message.server.htc.sge.SgeJobID)sjInfo.getHtcJobID()).getSgeJobID();
 			}
-			System.out.println(sjInfo.getServiceJobName()+" "+jobID+" "+sjInfo.getOutputPath()+" "+sjInfo.getErrorPath());
+			System.out.println(sjInfo.getJobName()+" "+jobID+" "+sjInfo.getOutputPath()+" "+sjInfo.getErrorPath());
 		}
 	}
 	private static void testServices(HtcProxy htcProxy, VCellServerID serverID)	throws ExecutableException, HtcException, HtcJobNotFoundException {
 		try {
 			System.out.println("getting services");
-			TreeMap<HtcJobID, String> jobIDs = htcProxy.getRunningServiceJobIDs(serverID);
-			for (HtcJobID jobID : jobIDs.keySet()){
+			List<HtcJobID> htcJobIDs = htcProxy.getRunningServiceJobIDs(serverID); 
+			for (HtcJobID jobID : htcJobIDs){
 				if (jobID instanceof PbsJobID){
 					System.out.println("serviceJobID : "+((PbsJobID)jobID).getPbsJobID());
 					htcProxy.killJob(jobID);
@@ -89,7 +96,7 @@ public class HtcTest {
 		System.out.println("<<<<<--------------  SUBMITTING SINGLE JOB ------------------>>>>>>");
 		HtcJobID jobID = null;
 		try {
-			jobID = htcProxy.submitJob("myJob1","/home/VCELL/vcell/myJob1.sub",new String[] { "/home/VCELL/vcell/calculatePi.sh", "1000" }, 1, 100, new String[] { "echo", "postCommand exit code is ", "EXIT_CODE"}, "EXIT_CODE");
+			jobID = htcProxy.submitJob("myJob1","/home/VCELL/vcell/myJob1.sub",new String[] { "/home/VCELL/vcell/calculatePi.sh", "100000" }, 1, 100, new String[] { "echo", "postCommand exit code is ", "EXIT_CODE"}, "EXIT_CODE");
 		} catch (Exception e1) {
 			e1.printStackTrace(System.out);
 		}
@@ -103,18 +110,20 @@ public class HtcTest {
 
 		System.out.println("<<<<<--------------  GETTING JOB EXIT STATUS--------------->>>>>>");
 		HtcJobStatus jobStatus2 = null;
-		while (jobStatus2 != null && jobStatus2.isRunning()){
+		while (jobStatus2 == null || jobStatus2.isRunning()){
 			try {
 				try {
 					Thread.sleep(2000);
 				} catch (InterruptedException e) {
 				}
 				jobStatus2 = htcProxy.getJobStatus(jobID);
+				List<HtcJobInfo> htcJobInfo = htcProxy.getJobInfos(Arrays.asList(jobID));
+				System.out.println("jobInfo = "+htcJobInfo);
+				System.out.println("jobStatus = "+jobStatus2);
 			} catch (Exception e) {
 				e.printStackTrace(System.out);
 			}
 		}
-		System.out.println("jobStatus = "+jobStatus2);
 	}
 
 	private static void testHtcProxy2cmd(HtcProxy htcProxy)	throws ExecutableException, HtcException, HtcJobNotFoundException {
