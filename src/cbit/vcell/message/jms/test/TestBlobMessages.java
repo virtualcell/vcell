6ea9d4d30d624a54eedccd547bf8b3567a5d6204
Jim Schaff <schaff@uchc.edu>
2012-12-08 20:30:28 +0000

    Bug 3372 : patch to Bug 3369: support for huge messages (using files).
    svn path=/vcell/branches/VCell_5.2/VCell/; revision=12480

--- src/cbit/vcell/message/jms/test/TestBlobMessages.java
+++ src/cbit/vcell/message/jms/test/TestBlobMessages.java
@@ -11,6 +11,7 @@ import cbit.vcell.message.VCMessagingService;
 import cbit.vcell.message.VCMessagingService.VCMessagingDelegate;
 import cbit.vcell.message.VCQueueConsumer;
 import cbit.vcell.message.VCQueueConsumer.QueueListener;
+import cbit.vcell.message.jms.VCMessageJms;
 import cbit.vcell.message.VCellQueue;
 
 /**
@@ -54,7 +55,7 @@ public class TestBlobMessages {
 	        for (int i=0;i<NUM_MESSAGES;i++){
 	        	for (int s=0;s<NUM_PRODUCERS;s++){
 	        		VCMessageSession session = sessions.get(s);
-		        	VCMessage message = session.createObjectMessage(new byte[1000]);
+		        	VCMessage message = session.createObjectMessage(new byte[40000*(i+1)]);
 //		        	VCMessage message = session.createObjectMessage(new byte[100000000]);
 		        	session.sendQueueMessage(VCellQueue.JimQueue, message);
 		        	session.commit();
@@ -67,7 +68,15 @@ public class TestBlobMessages {
 	        	QueueListener listener = new QueueListener() {
 	        		public void onQueueMessage(VCMessage vcMessage,	VCMessageSession session) throws RollbackException {
 	        			byte[] byteArray = (byte[])vcMessage.getObjectContent();
-	        			System.out.println("timestampMS="+vcMessage.getTimestampMS()+", "+toString()+",  elapsedTimeS="+((System.currentTimeMillis()-vcMessage.getTimestampMS())/1000.0)+", Received: "+byteArray.length+" bytes");
+	        			String blobInfo = "<no blob>";
+	        			if (vcMessage.propertyExists(VCMessageJms.BLOB_MESSAGE_FILE_NAME)){
+	        				String objectType = vcMessage.getStringProperty(VCMessageJms.BLOB_MESSAGE_OBJECT_TYPE);
+							int objectSize = vcMessage.getIntProperty(VCMessageJms.BLOB_MESSAGE_OBJECT_SIZE);
+							String fileName = vcMessage.getStringProperty(VCMessageJms.BLOB_MESSAGE_FILE_NAME);
+							String dirName = vcMessage.getStringProperty(VCMessageJms.BLOB_MESSAGE_PRODUCER_TEMPDIR);
+							blobInfo = "BLOB: object="+objectType+", size="+objectSize+", dir="+dirName+", file="+fileName;
+	        			}
+	        			System.out.println("timestampMS="+vcMessage.getTimestampMS()+", "+toString()+",  elapsedTimeS="+((System.currentTimeMillis()-vcMessage.getTimestampMS())/1000.0)+", Received: "+byteArray.length+": "+blobInfo);
 	        		}
 	        	};
 	        	VCQueueConsumer queueConsumer = new VCQueueConsumer(VCellQueue.JimQueue, listener, null, "Queue["+VCellQueue.JimQueue.getName()+"] ==== Consumer Thread "+i,1);
