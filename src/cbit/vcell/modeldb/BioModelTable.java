115b37df240b097b6f5f2252b5e3e2ae10a4f560
Jim Schaff <schaff@uchc.edu>
2013-05-15 04:14:11 +0000

    Bug 3530 : added vcell api for biomodel query, and BioModelRep
    svn path=/vcell/trunk/VCell/; revision=13206

--- src/cbit/vcell/modeldb/BioModelTable.java
+++ src/cbit/vcell/modeldb/BioModelTable.java
@@ -10,12 +10,13 @@
 
 package cbit.vcell.modeldb;
 
-import cbit.vcell.biomodel.*;
 import java.math.BigDecimal;
-import cbit.sql.*;
-import java.sql.SQLException;
-import java.sql.ResultSet;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
 
 import org.vcell.util.DataAccessException;
 import org.vcell.util.SessionLog;
@@ -24,6 +25,10 @@ import org.vcell.util.document.User;
 import org.vcell.util.document.VCellSoftwareVersion;
 import org.vcell.util.document.Version;
 import org.vcell.util.document.VersionInfo;
+
+import cbit.sql.Field;
+import cbit.sql.Table;
+import cbit.vcell.biomodel.BioModelMetaData;
 
 /**
  * This type was created in VisualAge.
@@ -172,5 +177,111 @@ public String getSQLValueList(BioModelMetaData bioModelMetaData, String serialBM
 	
 	buffer.append(")");
 	return buffer.toString();
+}
+
+public String getPreparedStatement_BioModelReps(String conditions){
+
+	BioModelTable bmTable = BioModelTable.table;
+	BioModelSimulationLinkTable bmsimTable = BioModelSimulationLinkTable.table;
+	BioModelSimContextLinkTable bmscTable = BioModelSimContextLinkTable.table;
+	GroupTable groupTable = GroupTable.table;
+	UserTable userTable = UserTable.table;
+	
+	// query guarantees authorized access to biomodels based on the supplied User authentication.
+	String sql =  
+			"select " +
+			    bmTable.id.getQualifiedColName()+", "+
+			    bmTable.name.getQualifiedColName()+", "+
+			    bmTable.privacy.getQualifiedColName()+", "+
+			    bmTable.versionDate.getQualifiedColName()+", "+
+			    bmTable.versionAnnot.getQualifiedColName()+", "+
+			    bmTable.versionBranchID.getQualifiedColName()+", "+
+			    bmTable.modelRef.getQualifiedColName()+", "+
+			    bmTable.ownerRef.getQualifiedColName()+", "+
+			    UserTable.table.userid.getQualifiedColName()+", "+
+			
+			   "(select '['||wm_concat("+"SQ1_"+bmsimTable.simRef.getQualifiedColName()+")||']' "+
+			   "   from "+bmsimTable.getTableName()+" SQ1_"+bmsimTable.getTableName()+" "+
+			   "   where SQ1_"+bmsimTable.bioModelRef.getQualifiedColName()+" = "+bmTable.id.getQualifiedColName()+") simKeys,  "+
+			
+			   "(select '['||wm_concat("+"SQ2_"+bmscTable.simContextRef.getQualifiedColName()+")||']' "+
+			   "   from "+bmscTable.getTableName()+"  SQ2_"+bmscTable.getTableName()+" "+
+			   "   where SQ2_"+bmscTable.bioModelRef.getQualifiedColName()+ " = " + bmTable.id.getQualifiedColName()+") simContextKeys,  "+
+			
+			   "(select '['||wm_concat(SQ3_"+groupTable.userRef.getQualifiedColName()+"||':'||SQ3_"+userTable.userid.getQualifiedColName()+")||']'  "+
+		   	   "   from "+groupTable.getTableName()+" SQ3_"+groupTable.getTableName()+", "+
+			   "        "+userTable.getTableName()+"  SQ3_"+userTable.getTableName()+" "+
+			   "   where SQ3_"+groupTable.groupid.getQualifiedColName()+" = "+bmTable.privacy.getQualifiedColName()+" "+
+			   "   and   SQ3_"+userTable.id.getQualifiedColName()+" = "+groupTable.userRef.getQualifiedColName()+" "+
+			   "   and   "+bmTable.privacy.getQualifiedColName()+" > 1) groupMembers "+
+			
+			"from "+bmTable.getTableName()+", "+userTable.getTableName()+", "+groupTable.getTableName()+" "+
+			"where "+bmTable.ownerRef.getQualifiedColName()+" = "+userTable.id.getQualifiedColName()+" "+
+			"and   "+bmTable.privacy.getQualifiedColName()+" = "+groupTable.groupid.getQualifiedColName()+" "+
+			"and   (("+bmTable.ownerRef.getQualifiedColName()+" =?) or ("+bmTable.privacy.getQualifiedColName()+" = 0) or ("+groupTable.userRef+" =? ))";
+	
+	if (conditions!=null && conditions.length()>0){
+		sql += " and ("+conditions+")";
+	}
+	
+	return sql;
+}
+
+public void setPreparedStatement_BioModelReps(PreparedStatement stmt, User user) throws SQLException{
+	if (user == null) {
+		throw new IllegalArgumentException("Improper parameters for getBioModelRepsSQL");
+	}
+	BigDecimal userKey = new BigDecimal(user.getID().toString());
+	stmt.setBigDecimal(1, userKey);
+	stmt.setBigDecimal(2, userKey);
+}
+
+public BioModelRep getBioModelRep(User user, ResultSet rset) throws IllegalArgumentException, SQLException {
+	KeyValue bmKey = new KeyValue(rset.getBigDecimal(table.id.toString()));
+	String name = rset.getString(table.name.toString());
+	int privacy = rset.getInt(table.privacy.toString());
+	Date date = getDate(rset, table.versionDate.toString());
+	String annot = rset.getString(table.versionAnnot.toString());
+	BigDecimal branchID = rset.getBigDecimal(table.versionBranchID.toString());
+	KeyValue modelRef = new KeyValue(rset.getBigDecimal(table.modelRef.toString()));
+	KeyValue ownerRef = new KeyValue(rset.getBigDecimal(table.ownerRef.toString()));
+	String ownerName = rset.getString(UserTable.table.userid.toString());
+	User owner = new User(ownerName,ownerRef);
+	
+	String simKeysString = rset.getString("simKeys");
+	ArrayList<KeyValue> simKeyList = new ArrayList<KeyValue>();
+	String[] simKeys = simKeysString.replace("[", "").replace("]", "").split(",");
+	for (String simKey : simKeys) {
+		if (simKey!=null && simKey.length()>0){
+			simKeyList.add(new KeyValue(simKey));
+		}
+	}
+	KeyValue[] simKeyArray = simKeyList.toArray(new KeyValue[0]);
+
+	String simContextsString = rset.getString("simContextKeys");
+	ArrayList<KeyValue> simContextKeyList = new ArrayList<KeyValue>();
+	String[] simContextKeys = simContextsString.replace("[", "").replace("]", "").split(",");
+	for (String simContextKey : simContextKeys) {
+		if (simContextKey!=null && simContextKey.length()>0){
+			simContextKeyList.add(new KeyValue(simContextKey));
+		}
+	}
+	KeyValue[] simContextKeyArray = simContextKeyList.toArray(new KeyValue[0]);
+
+	String groupMembers = rset.getString("groupMembers");
+	ArrayList<User> groupUsers = new ArrayList<User>();
+	String[] groupUserStrings = groupMembers.replace("[", "").replace("]", "").split(",");
+	for (String groupUserString : groupUserStrings) {
+		if (groupUserString!=null && groupUserString.length()>0){
+			String[] groupUserTokens = groupUserString.split(":");
+			KeyValue groupUserKey = new KeyValue(groupUserTokens[0]);
+			String  groupUserid = groupUserTokens[1];
+			groupUsers.add(new User(groupUserid,groupUserKey));
+		}
+	}
+	User[] groupUserArray = groupUsers.toArray(new User[0]);
+		
+	
+	return new BioModelRep(bmKey,name,privacy,groupUserArray,date,annot,branchID,modelRef,owner,simKeyArray,simContextKeyArray);
 }
 }
