c40c9fd3212fc68fac442988d300625bc242ada5
Edward Boyce <boyce@uchc.edu>
2013-05-24 19:47:03 +0000

    Bug 3536:     Make server services insert messages into log every N (initially 5) minutes indicating process is still alive on a basic level
    svn path=/vcell/trunk/VCell/; revision=13224

---
+++ src/cbit/vcell/message/server/LifeSignThread.java
@@ -0,0 +1,29 @@
+package cbit.vcell.message.server;
+
+import org.vcell.util.SessionLog;
+
+public class LifeSignThread extends Thread{
+
+private SessionLog sessionLog = null;
+private int interval;
+
+
+
+public LifeSignThread(SessionLog aSessionLog, int anInterval) {
+	this.sessionLog = aSessionLog;
+	this.interval = anInterval;
+	this.setDaemon(true);
+}
+
+public void run(){
+	for ( ; ; ) {
+		sessionLog.alert("Still alive");
+		try {
+			sleep(interval); 
+		} catch (InterruptedException e) {
+			sessionLog.alert("Interrupted sleep in LifeSignThread.run()\n"+e.getStackTrace().toString());
+			e.printStackTrace();
+		}
+	}
+}
+}
