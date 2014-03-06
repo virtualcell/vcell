0ba9da786a38f49f60388cf303acdf0ae1b31d38
Gerard Weatherby <gweatherby@uchc.edu>
2014-03-06 20:27:57 +0000

    3787; skip call to resources
    svn path=/vcell/trunk/VCell/; revision=14764

--- src/org/vcell/util/logging/Logging.java
+++ src/org/vcell/util/logging/Logging.java
@@ -1,12 +1,10 @@
 package org.vcell.util.logging;
 
 import java.io.File;
-import java.net.URL;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
-import org.apache.log4j.lf5.util.Resource;
 
 /**
  * location for startup of logging files
@@ -30,23 +28,13 @@ public class Logging {
 						+ initFile.getAbsolutePath());
 		}
 		else {
-			Resource cfg = new Resource(PROP_FILE);
-			URL u = cfg.getURL();
-			if (u != null) {
-				PropertyConfigurator.configure(u);
-				if (lg.isInfoEnabled())
-					lg.info("logger initialized from resource "
-							+ cfg.getURL().toString());
-			}
-			else {
-				Properties def = new Properties();
-				def.put("log4j.rootLogger","FATAL, A1");
-				def.put("log4j.appender.A1","org.apache.log4j.ConsoleAppender");
-				def.put("log4j.appender.A1.layout","org.apache.log4j.PatternLayout");
-				def.put("log4j.appender.A1.layout.ConversionPattern","%-4r [%t] %-5p %c %x --> %m %n");
-				def.put("log4j.appender.target","System.err");
-				PropertyConfigurator.configure(def);
-			}
+			Properties def = new Properties();
+			def.put("log4j.rootLogger","FATAL, A1");
+			def.put("log4j.appender.A1","org.apache.log4j.ConsoleAppender");
+			def.put("log4j.appender.A1.layout","org.apache.log4j.PatternLayout");
+			def.put("log4j.appender.A1.layout.ConversionPattern","%-4r [%t] %-5p %c %x --> %m %n");
+			def.put("log4j.appender.target","System.err");
+			PropertyConfigurator.configure(def);
 		}
 	}
 
