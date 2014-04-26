6d4fe3d08496d227e3cdc8a865f06fe176826858
Gerard Weatherby <gweatherby@uchc.edu>
2014-04-26 17:10:00 +0000

    Task 3800: test code for 5.2 model validation
    svn path=/vcell/trunk/VCell/; revision=15039

--- test/cbit/vcell/model/HybridBioModelVisitor.java
+++ test/cbit/vcell/model/HybridBioModelVisitor.java
@@ -4,58 +4,164 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Writer;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import javax.help.UnsupportedOperationException;
 
 import org.junit.Test;
+import org.vcell.util.NullSessionLog;
 import org.vcell.util.PropertyLoader;
 import org.vcell.util.document.BioModelInfo;
 import org.vcell.util.document.User;
+import org.vcell.util.document.VCellSoftwareVersion;
+import org.vcell.util.logging.Logging;
 
 import cbit.vcell.biomodel.BioModel;
+import cbit.vcell.mapping.SimulationContext;
+import cbit.vcell.mapping.SpeciesContextSpec;
 import cbit.vcell.modeldb.VCDatabaseScanner;
+import cbit.vcell.solver.Simulation;
 
 public class HybridBioModelVisitor implements VCMultiBioVisitor { 
 	/**
 	 * user key to use for tests
 	 */
-	private final static String USER_KEY = "gerardwNot" ;
+	private final static String USER_KEY = "gerardw" ;
+	//private final static String USER_KEY = VCDatabaseScanner.ALL_USERS;
 	/**
 	 * output file name
 	 */
 	private final static String OUTPUT = "regen.txt";
 	
 	private BioModel currentModel = null;
+	private List<SimulationContext> applications = new ArrayList<SimulationContext>();
+	private int currentAppIndex;
+	private SpeciesContextSpec[] currentSpeciesContext;
+	private boolean[] toggledForceContinuous;
+	private boolean moreStates;
 	
 	
+	/**
+	 * return true if major version == 5
+	 */
 	public boolean filterBioModel(BioModelInfo bioModelInfo) {
-			return true;
+		VCellSoftwareVersion sv = bioModelInfo.getSoftwareVersion();
+		final boolean recentEnough = ( sv.getMajorVersion() == 5 ); 
+		if (!recentEnough) {
+			System.out.println("skipping old (v" + sv.getMajorVersion() + ")" + bioModelInfo.toString());
+		}
+		return recentEnough;
 	}
+
+	/**
+	 * return true if any solver is spatial stochastic
+	 */
+	@Override
+	public boolean filterBioModel(BioModel bioModel) {
+		Simulation[] sims = bioModel.getSimulations();
+		return isSpatialStoch(sims);
+	}
+	
+	/**
+	 * return true if any simulation is spatial stochastic
+	 * @param sims not null
+	 * @return true if any are
+	 */
+	private static boolean isSpatialStoch(Simulation[] sims) {
+		for (Simulation s: sims) {
+			if (s.getSolverTaskDescription().getSolverDescription().isSpatialStochasticSolver()) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 
 	public void setBioModel(BioModel bioModel, PrintWriter pw) {
 		currentModel = bioModel;
-		return;
+		applications.clear( );
+		SimulationContext[] scs = currentModel.getSimulationContexts();
+		
+		for (SimulationContext sc : scs) {
+			if (isSpatialStoch(sc.getSimulations())) {
+				applications.add(sc);
+			}
+		}
+		assert !applications.isEmpty();
+		moreStates = true;
+		currentAppIndex = -1;
+		incrementApp();
 	}
 	
+	/**
+	 * increments {@link #currentAppIndex} and set ups {@link #currentSpeciesContext}
+	 * detects when no more states
+	 */
+	private void incrementApp( ) {
+		if (++currentAppIndex < applications.size()) {
+			SimulationContext app = applications.get(currentAppIndex);
+			currentSpeciesContext = app.getReactionContext().getSpeciesContextSpecs();
+			toggledForceContinuous = new boolean[currentSpeciesContext.length];
+			return;
+		}
+		moreStates = false;
+	}
 	
+	/**
+	 * moves to next state, if available. If not, calls incrementApp
+	 */
+	private void nextState( ) {
+		for (int i = 0; i < toggledForceContinuous.length;i++){ 
+			if (!toggledForceContinuous[i]) {
+				toggle(currentSpeciesContext[i]);
+				toggledForceContinuous[i] = true;
+				for (int r = 0; r < i; r++) {
+					toggledForceContinuous[r] = false;
+				}
+				return;
+			}
+		}
+		incrementApp( );
+		//recursively call self if more states avaiable
+		if (moreStates) {
+			nextState( );
+		}
+	}
+	
+
+	/**
+	 * set force continues to opposite state
+	 * @param speciesContextSpec
+	 */
+	private void toggle(SpeciesContextSpec speciesContextSpec) {
+		speciesContextSpec.setForceContinuous( ! speciesContextSpec.isForceContinuous());
+	}
 
 	@Override
 	public Iterator<BioModel> iterator() {
 		return new Changer(); 
 	}
 	
+	/**
+	 * implement an iterator which cycles through all states <i>except</i> the first one.
+	 * states are defined by a setting of SpeciesContextSpec force continuous
+	 */
 	private class Changer implements Iterator<BioModel> {
-		boolean first = true;
 
+		/**
+		 * its simpler to find there are no more states by attempting to go to next one
+		 * @return true more states after attempting increment
+		 */
 		@Override
 		public boolean hasNext() {
-			return first;
+			nextState( );
+			return moreStates; 
 		}
 
 		@Override
 		public BioModel next() {
-			first = false;
 			return currentModel; 
 		}
 
@@ -66,19 +172,18 @@ public class HybridBioModelVisitor implements VCMultiBioVisitor {
 		public void remove() {
 			throw new UnsupportedOperationException();
 		}
-		
-		
 	}
 
 	@SuppressWarnings("unused")
 	@Test
 	public void tryit( ) throws IOException {
+		Logging.init();
 		PropertyLoader.loadProperties();
 		//String args[] = {USER_KEY,OUTPUT};
 		HybridBioModelVisitor visitor = new HybridBioModelVisitor();
 		boolean bAbortOnDataAccessException = false;
 		try{
-			VCDatabaseScanner scanner = new VCDatabaseScanner(); 
+			VCDatabaseScanner scanner = new VCDatabaseScanner(new NullSessionLog()); 
 			Writer w = new FileWriter(OUTPUT);
 			User[] users = null; 
 			if (USER_KEY != VCDatabaseScanner.ALL_USERS) { 
