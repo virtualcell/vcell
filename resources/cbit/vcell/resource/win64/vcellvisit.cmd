e0dd7b330b38ac183cf6cd427d746ac3ed573af9
Edward Boyce <boyce@uchc.edu>
2014-04-30 21:04:26 +0000

    Bug 3803:      Try version of Windows64 vcellvisit.cmd that invokes cmd with full hardcoded path
    svn path=/vcell/trunk/VCell/; revision=15070

--- resources/cbit/vcell/resource/win64/vcellvisit.cmd
+++ resources/cbit/vcell/resource/win64/vcellvisit.cmd
@@ -1,3 +1,5 @@
 echo "visitcmd = " %visitcmd%
 echo "pythonscript = " %pythonscript%
-cmd /K "%visitcmd%" -cli -uifile %pythonscript%
+set path="%path%;C:\Windows\system32"
+echo "path = " %path%
+C:\Windows\system32\cmd.exe /K "%visitcmd%" -cli -uifile %pythonscript%
