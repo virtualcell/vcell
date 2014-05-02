b1843ecf82d379d96d552a7558eab4b4398ea595
Edward Boyce <boyce@uchc.edu>
2014-05-02 18:10:37 +0000

    Bug 3803:     Removed superfluous double quote from vcellvisit.cmd
    svn path=/vcell/trunk/VCell/; revision=15072

--- resources/cbit/vcell/resource/win64/vcellvisit.cmd
+++ resources/cbit/vcell/resource/win64/vcellvisit.cmd
@@ -2,4 +2,4 @@ echo "visitcmd = " %visitcmd%
 echo "pythonscript = " %pythonscript%
 set path="%path%;C:\Windows\system32"
 echo "path = " %path%
-C:\Windows\system32\cmd.exe /K "%visitcmd%" -cli -uifile %pythonscript%
+C:\Windows\system32\cmd.exe /K %visitcmd% -cli -uifile %pythonscript%
