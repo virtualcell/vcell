00ff1cdfcfe57be99b2473cc1d01ad128bac18cd
Gerard Weatherby <gweatherby@uchc.edu>
2014-05-02 21:41:21 +0000

    Bug 3785: create alternative to Deprecated for marking methods which support old models without causing warnings when methods are used
    svn path=/vcell/trunk/VCell/; revision=15078

---
+++ src/cbit/vcell/Historical.java
@@ -0,0 +1,18 @@
+package cbit.vcell;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+/**
+ *  method exists for models created in earlier versions
+ *  of virtual cell
+ */
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Historical {
+
+}
