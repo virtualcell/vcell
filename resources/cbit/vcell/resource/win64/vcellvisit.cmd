465e5d4229d6911f20a6f89db26d71549b1abaeb
Edward Boyce <boyce@uchc.edu>
2014-05-06 17:30:01 +0000

    Bug 3803:     Fix Windows vis launch to accommodate paths with spaces.  Added method to FileUtils to copy directories and convenience method to ResourceUtil to get site name string.
    svn path=/vcell/trunk/VCell/; revision=15096

--- resources/cbit/vcell/resource/win64/vcellvisit.cmd
+++ resources/cbit/vcell/resource/win64/vcellvisit.cmd
@@ -1,5 +1,6 @@
-echo "visitcmd = " %visitcmd%
-echo "pythonscript = " %pythonscript%
-set path="%path%;C:\Windows\system32"
-echo "path = " %path%
-C:\Windows\system32\cmd.exe /K "%visitcmd% -cli -uifile %pythonscript%"
+echo visitcmd=%visitcmd%
+echo pythonscript=%pythonscript%
+set path=%path%;C:\Windows\system32
+echo path=%path%
+echo C:\Windows\system32\cmd.exe /K ""%visitcmd%" -cli -uifile "%pythonscript%""
+C:\Windows\system32\cmd.exe /K ""%visitcmd%" -cli -uifile "%pythonscript%""
