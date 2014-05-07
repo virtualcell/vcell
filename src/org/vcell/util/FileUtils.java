48c2b6d08ca8df77fd31aec498048b0817c942fe
Dan Vasilescu <vasilescu@uchc.edu>
2014-05-07 21:29:23 +0000

    Bug 3824:  Utility functions for file name management.
    svn path=/vcell/trunk/VCell/; revision=15107

--- src/org/vcell/util/FileUtils.java
+++ src/org/vcell/util/FileUtils.java
@@ -401,4 +401,178 @@ public static Collection<String> toStrings(Collection<File> input) {
 	throw new IllegalArgumentException("null input to splitPathString");
 }
 
+
+
+public static final int NOT_FOUND = -1;
+public static final char UNIX_SEPARATOR = '/';
+public static final char WINDOWS_SEPARATOR = '\\';
+public static final char EXTENSION_SEPARATOR = '.';
+
+public static String getFullPath(final String filename) {
+    return doGetFullPath(filename, true);
+}
+public static String getFullPathNoEndSeparator(final String filename) {
+    return doGetFullPath(filename, false);
+}
+public static String getBaseName(final String filename) {
+	return removeExtension(getName(filename));
+}
+public static String getExtension(final String filename) {
+    if (filename == null) {
+        return null;
+    }
+    final int index = indexOfExtension(filename);
+    if (index == NOT_FOUND) {
+        return "";
+    } else {
+        return filename.substring(index + 1);
+    }
+}
+public static String getName(final String filename) {
+    if (filename == null) {
+        return null;
+    }
+    final int index = indexOfLastSeparator(filename);
+    return filename.substring(index + 1);
+}
+public static String removeExtension(final String filename) {
+    if (filename == null) {
+        return null;
+    }
+    final int index = indexOfExtension(filename);
+    if (index == NOT_FOUND) {
+        return filename;
+    } else {
+        return filename.substring(0, index);
+    }
+}
+public static int indexOfLastSeparator(final String filename) {
+    if (filename == null) {
+        return NOT_FOUND;
+    }
+    final int lastUnixPos = filename.lastIndexOf(UNIX_SEPARATOR);
+    final int lastWindowsPos = filename.lastIndexOf(WINDOWS_SEPARATOR);
+    return Math.max(lastUnixPos, lastWindowsPos);
+}
+public static int indexOfExtension(final String filename) {
+    if (filename == null) {
+        return NOT_FOUND;
+    }
+    final int extensionPos = filename.lastIndexOf(EXTENSION_SEPARATOR);
+    final int lastSeparator = indexOfLastSeparator(filename);
+    return lastSeparator > extensionPos ? NOT_FOUND : extensionPos;
+}
+private static String doGetFullPath(final String filename, final boolean includeSeparator) {
+    if (filename == null) {
+        return null;
+    }
+    final int prefix = getPrefixLength(filename);
+    if (prefix < 0) {
+        return null;
+    }
+    if (prefix >= filename.length()) {
+        if (includeSeparator) {
+            return getPrefix(filename);  // add end slash if necessary
+        } else {
+            return filename;
+        }
+    }
+    final int index = indexOfLastSeparator(filename);
+    if (index < 0) {
+        return filename.substring(0, prefix);
+    }
+    int end = index + (includeSeparator ?  1 : 0);
+    if (end == 0) {
+        end++;
+    }
+    return filename.substring(0, end);
+}
+public static int getPrefixLength(final String filename) {
+    if (filename == null) {
+        return NOT_FOUND;
+    }
+    final int len = filename.length();
+    if (len == 0) {
+        return 0;
+    }
+    char ch0 = filename.charAt(0);
+    if (ch0 == ':') {
+        return NOT_FOUND;
+    }
+    if (len == 1) {
+        if (ch0 == '~') {
+            return 2;  // return a length greater than the input
+        }
+        return isSeparator(ch0) ? 1 : 0;
+    } else {
+        if (ch0 == '~') {
+            int posUnix = filename.indexOf(UNIX_SEPARATOR, 1);
+            int posWin = filename.indexOf(WINDOWS_SEPARATOR, 1);
+            if (posUnix == NOT_FOUND && posWin == NOT_FOUND) {
+                return len + 1;  // return a length greater than the input
+            }
+            posUnix = posUnix == NOT_FOUND ? posWin : posUnix;
+            posWin = posWin == NOT_FOUND ? posUnix : posWin;
+            return Math.min(posUnix, posWin) + 1;
+        }
+        final char ch1 = filename.charAt(1);
+        if (ch1 == ':') {
+            ch0 = Character.toUpperCase(ch0);
+            if (ch0 >= 'A' && ch0 <= 'Z') {
+                if (len == 2 || isSeparator(filename.charAt(2)) == false) {
+                    return 2;
+                }
+                return 3;
+            }
+            return NOT_FOUND;
+
+        } else if (isSeparator(ch0) && isSeparator(ch1)) {
+            int posUnix = filename.indexOf(UNIX_SEPARATOR, 2);
+            int posWin = filename.indexOf(WINDOWS_SEPARATOR, 2);
+            if (posUnix == NOT_FOUND && posWin == NOT_FOUND || posUnix == 2 || posWin == 2) {
+                return NOT_FOUND;
+            }
+            posUnix = posUnix == NOT_FOUND ? posWin : posUnix;
+            posWin = posWin == NOT_FOUND ? posUnix : posWin;
+            return Math.min(posUnix, posWin) + 1;
+        } else {
+            return isSeparator(ch0) ? 1 : 0;
+        }
+    }
+}
+public static String getPrefix(final String filename) {
+    if (filename == null) {
+        return null;
+    }
+    final int len = getPrefixLength(filename);
+    if (len < 0) {
+        return null;
+    }
+    if (len > filename.length()) {
+        return filename + UNIX_SEPARATOR;  // we know this only happens for unix
+    }
+    return filename.substring(0, len);
+}
+private static boolean isSeparator(final char ch) {
+    return ch == UNIX_SEPARATOR || ch == WINDOWS_SEPARATOR;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
