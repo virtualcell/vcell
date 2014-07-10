6b50edb1878f875c27b2878a6a5e9dcaef5317fb
Gerard Weatherby <gweatherby@uchc.edu>
2014-07-10 11:35:22 +0000

    Batch 43: move Admin user to Batch tester -- avoids changing User and subsequent RMI issues
    svn path=/vcell/trunk/VCell/; revision=15439

--- src/org/vcell/util/document/User.java
+++ src/org/vcell/util/document/User.java
@@ -11,9 +11,6 @@
 package org.vcell.util.document;
 import org.vcell.util.Immutable;
 import org.vcell.util.Matchable;
-import org.vcell.util.PropertyLoader;
-
-import cbit.vcell.modeldb.DatabasePolicySQL;
 
 /**
  * This type was created in VisualAge.
@@ -28,11 +25,6 @@ public class User implements java.io.Serializable, Matchable, Immutable {
 	
 	public static User tempUser = new User("user",new KeyValue("123"));
 	/**
-	 * special user with access to all models (see {@link DatabasePolicySQL#enforceOwnershipSelect(User, cbit.sql.Field[], cbit.sql.Table[], String, String)
-	 */
-	public static final User ADMINISTRATOR = new User(PropertyLoader.ADMINISTRATOR_ACCOUNT,new KeyValue(PropertyLoader.ADMINISTRATOR_ID));
-
-/**
  * User constructor comment.
  */
 public User(String userid, KeyValue key) {
