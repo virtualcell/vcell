06ba15e4f3db70e25b3a1aa7848819fdc8b25eb2
Jim Schaff <schaff@uchc.edu>
2014-09-10 21:04:11 +0000

    Bug 3887: read from database pre VCell_5.3 version FluxReactions into new Reactants/Products design.
    svn path=/vcell/branches/VCell_5.3/VCell/; revision=15858

---
+++ src/cbit/vcell/model/SimpleReaction.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (C) 1999-2011 University of Connecticut Health Center
+ *
+ * Licensed under the MIT License (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *  http://www.opensource.org/licenses/mit-license.php
+ */
+
+package cbit.vcell.model;
+
+import java.beans.PropertyVetoException;
+
+import org.vcell.util.Matchable;
+import org.vcell.util.document.KeyValue;
+
+import cbit.vcell.parser.ExpressionException;
+
+
+@SuppressWarnings("serial")
+public class SimpleReaction extends ReactionStep
+{
+	private String fieldAnnotation = null;
+	
+public SimpleReaction(Model model, Structure structure, KeyValue key, String name, String argAnnotation) throws java.beans.PropertyVetoException {
+	super(model, structure,key,name);
+	this.fieldAnnotation = argAnnotation;
+	try {
+		setKinetics(new MassActionKinetics(this));
+	} catch (ExpressionException e){
+		e.printStackTrace(System.out);
+		throw new RuntimeException(e.getMessage());
+	}
+}
+public SimpleReaction(Model model, Structure structure, KeyValue key, String name) throws java.beans.PropertyVetoException {
+	this(model, structure, key, name, null);
+}
+
+public SimpleReaction(Model model, Structure structure,String name, String argAnnotation) throws java.beans.PropertyVetoException {
+	this(model, structure, null, name, argAnnotation);
+}
+public SimpleReaction(Model model, Structure structure,String name) throws java.beans.PropertyVetoException {
+	this(model, structure, null, name, null);
+}
+
+
+/**
+ * This method was created in VisualAge.
+ * @return boolean
+ * @param obj java.lang.Object
+ */
+public boolean compareEqual(Matchable obj) {
+	if (obj instanceof SimpleReaction){
+		SimpleReaction sr = (SimpleReaction)obj;
+		if (!super.compareEqual0(sr)){
+			return false;
+		}
+		return true;
+	}else{
+		return false;
+	}
+}
+public int getNumProducts() {
+
+	int count = 0;
+
+	ReactionParticipant rp_Array[] = getReactionParticipants();
+	
+	for (int i = 0; i < rp_Array.length; i++) {
+		if (rp_Array[i] instanceof Product){
+			count++;
+		}
+	}
+	
+	return count;
+}   
+public int getNumReactants() {
+
+	int count = 0;
+
+	ReactionParticipant rp_Array[] = getReactionParticipants();
+
+	for (int i = 0; i < rp_Array.length; i++) {
+		if (rp_Array[i] instanceof Reactant){
+			count++;
+		}
+	}
+	
+	return count;
+}   
+
+/**
+ * Insert the method's description here.
+ * Creation date: (5/22/00 10:22:19 PM)
+ * @return java.lang.String
+ */
+public String getTerm() {
+	return "Reaction";
+}
+/**
+ * This method was created by a SmartGuide.
+ * @return boolean
+ */
+public boolean isMembrane() {
+
+	ReactionParticipant rp_Array[] = getReactionParticipants();
+
+	for (int i = 0; i < rp_Array.length; i++) {
+		if (rp_Array[i].getStructure() instanceof Membrane){
+			return true;
+		}
+	}			
+	
+	return false;
+}
+/**
+ * This method was created in VisualAge.
+ * @return java.lang.String
+ */
+public String toString() {
+	return "SimpleReaction@"+Integer.toHexString(hashCode())+"("+getName()+")";
+}
+@Override
+public void setReactionParticipantsFromDatabase(Model model, ReactionParticipant[] reactionParticipants) throws PropertyVetoException {
+	// no special logic needed here.
+	setReactionParticipants(reactionParticipants);
+}
+
+}
