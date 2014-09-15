74422b3a78e9be565de2ad14d5fb62db5da0919d
Gerard Weatherby <gweatherby@uchc.edu>
2014-09-15 17:46:50 +0000

    Bug 3901: provide facility for end user instruction
    svn path=/vcell/branches/VCell_5.3/VCell/; revision=15904

---
+++ src/org/vcell/util/gui/SuggestionPanel.java
@@ -0,0 +1,54 @@
+package org.vcell.util.gui;
+
+import java.awt.BorderLayout;
+import java.util.Collection;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
+
+import java.awt.Font;
+
+/**
+ * Panel which displays list of suggestions
+ * @author gweatherby
+ *
+ */
+@SuppressWarnings("serial")
+public class SuggestionPanel extends JPanel {
+	private JTextPane solutionsText;
+
+	public SuggestionPanel() {
+		setLayout(new BorderLayout(0, 0));
+		solutionsText = new JTextPane();
+		solutionsText.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		solutionsText.setContentType("text/html");
+		add(solutionsText);
+		
+		JPanel panel = new JPanel();
+		add(panel, BorderLayout.NORTH);
+		
+		JLabel lblNewLabel = new JLabel("Possible Solutions(s):");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 16));
+		panel.add(lblNewLabel);
+	}
+	
+	/**
+	 * sets lists of suggestions. Replacing any prior setting
+	 * @param solutions
+	 * @throws IllegalArgumentException if null passed
+	 */
+	public void setSuggestedSolution(Collection<String> solutions) {
+		if (solutions == null) {
+			throw new IllegalArgumentException("null collection passed");
+		}
+		String buffer = "<html><ul>";
+		for (String sol: solutions) {
+			buffer = buffer + "<li>" + sol + "</li>";
+		}
+		buffer = buffer + "</ul></html>";
+		solutionsText.setText(buffer);
+	}
+	
+
+}
