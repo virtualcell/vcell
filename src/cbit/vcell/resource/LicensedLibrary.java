e055456f1459bc7a56caed0857961bb64f8f211b
Gerard Weatherby <gweatherby@uchc.edu>
2014-09-30 15:34:15 +0000

    Bug 3785: documentation only, the functionality of the class wasn't quite obvious from just the source, so added class comment
    svn path=/vcell/branches/VCell_5.3/VCell/; revision=16069

---
+++ src/cbit/vcell/resource/LicensedLibrary.java
@@ -0,0 +1,72 @@
+package cbit.vcell.resource;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Collection;
+
+/**
+ * library (so / dll ) with special license
+ * multiple versions of the same library are supported by making {@link #libraryName()} the same 
+ * but {@link #version()} different. Clients should call {@link #makePresentIn(File)} immediate before use
+ * to ensure the correct version is used.	
+ * @author gweatherby
+ *
+ */
+public interface LicensedLibrary {
+	public final LicensedLibrary CYGWIN_DLL_CHOMBO = new ChomboCygwinDLL();
+	public final LicensedLibrary CYGWIN_DLL_BIONETGEN = new BioNetGenCygwinDLL(); 
+	public final LicensedLibrary NONE = new LicensedPlaceholder(); 
+	
+	/**
+	 * is library licensed?
+	 */
+	public boolean isLicensed( );
+	/**
+	 * is library installed 
+	 */
+	public boolean isInstalled( );
+	
+	/**
+	 * make file present in directory.
+	 * {@link #isLicensed()} must be true
+	 * @param directory
+	 * @throws IllegalStateException if not licensed
+	 */
+	public void makePresentIn(File directory) throws IllegalStateException;
+	
+	/**
+	 * optional; required if {@link #isLicensed()} may return false
+	 * @return license required for this particular library
+	 * @throws UnsupportedOperationException 
+	 */
+	public LibraryLicense licensedRequired( ) throws UnsupportedOperationException;
+	
+	/**
+	 * optional; required if {@link #isInstalled()} may return false
+	 * @return name of library
+	 * @throws UnsupportedOperationException 
+	 */
+	public String libraryName( ) throws UnsupportedOperationException;
+	/**
+	 * optional; required if {@link #isInstalled()} may return false
+	 * @return specific version name of library; 
+	 * @throws UnsupportedOperationException
+	 */
+	public String version( ) throws UnsupportedOperationException;
+	
+	
+	/**
+	 * optional; required if {@link #isInstalled()} may return false
+	 * @return URL to download from  
+	 * @throws UnsupportedOperationException 
+	 */
+	public URL downloadUrl( ) throws UnsupportedOperationException;
+	
+	/**
+	 * unmodifiable list of library names bundled into deliverable
+	 * @return collection of library names; may be empty but not null 
+	 */
+	public Collection<String> bundledLibraryNames( );
+	
+
+}
