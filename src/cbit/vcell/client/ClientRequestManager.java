282f3cb4d6fb09e7b471e312e59d9e01c716859e
Frank Morgan <frm@uchc.edu>
2014-10-08 21:25:28 +0000

    Bug 3955: Don't show error dialog when user cancel lost password operation
    svn path=/vcell/branches/VCell_5.2/VCell/; revision=16158

--- src/cbit/vcell/client/ClientRequestManager.java
+++ src/cbit/vcell/client/ClientRequestManager.java
@@ -1914,6 +1914,8 @@ public void sendLostPassword(final DocumentWindowManager currWindowManager, fina
 					getClientServerManager().getClientServerInfo(), userid, null),
 					LoginManager.USERACTION_LOSTPASSWORD,
 				null);
+	} catch (UserCancelException e) {
+		return;
 	} catch (Exception e) {
 		e.printStackTrace();
 		PopupGenerator.showErrorDialog(currWindowManager, "Update user Registration error:\n" + e.getMessage(), e);
