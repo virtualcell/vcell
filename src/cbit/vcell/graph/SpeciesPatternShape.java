65b190b851be49380593b511e3620137d4e6c66c
Dan Vasilescu <vasilescu@uchc.edu>
2014-12-29 23:32:57 +0000

    Bug 4043 - Graphical representation for species (pass 3, paint Bonds, display State inside component ellipse, after the component name, better way to allocate the component shape)
    svn path=/vcell/trunk/VCell/; revision=16588

--- src/cbit/vcell/graph/SpeciesPatternShape.java
+++ src/cbit/vcell/graph/SpeciesPatternShape.java
@@ -1,12 +1,18 @@
 package cbit.vcell.graph;
 
 import java.awt.Graphics;
+import java.awt.Point;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
-import org.vcell.model.rbm.MolecularType;
+import org.vcell.model.rbm.MolecularComponentPattern;
 import org.vcell.model.rbm.MolecularTypePattern;
 import org.vcell.model.rbm.SpeciesPattern;
+import org.vcell.model.rbm.MolecularComponentPattern.BondType;
+import org.vcell.model.rbm.SpeciesPattern.Bond;
 import org.vcell.util.Displayable;
 
 public class SpeciesPatternShape {
@@ -14,15 +20,39 @@ public class SpeciesPatternShape {
 	private static final int separationWidth = 10;		// width between 2 molecular type patterns
 	private int xPos = 0;
 	private int yPos = 0;
-	
 	private List<SpeciesTypeLargeShape> speciesShapes = new ArrayList<SpeciesTypeLargeShape>();
-	
+
 	final Graphics graphicsContext;
 	
-	private String name;
 	private Displayable owner;
 	private SpeciesPattern sp;
 	
+	class BondPair implements Comparable {
+		int id;
+		Point from;
+		Point to;
+		public BondPair(int id, Point from, Point to) {
+			this.id = id;
+			this.from = from;
+			this.to = to;
+		}
+		@Override
+		public int compareTo(Object o) {
+			if(o instanceof BondPair) {
+				BondPair that = (BondPair)o;
+				int thisLength = to.x - from.x;
+				int thatLength = that.to.x - that.from.x;
+				if(thisLength < thatLength) {
+					return -1;
+				} else {
+					return 1;
+				}
+			}
+			return 0;
+		}
+	}
+	List <BondPair> bondPairs = new ArrayList <BondPair>();
+
 	
 	public SpeciesPatternShape(int xPos, int yPos, Displayable owner, SpeciesPattern sp, Graphics graphicsContext) {
 		this.owner = owner;
@@ -39,8 +69,46 @@ public class SpeciesPatternShape {
 			xPattern += stls.getWidth() + separationWidth; 
 			speciesShapes.add(stls);
 		}
+		
+		// bonds - we have to deal with them here because they may be cross-molecular type patterns
+		// WARNING: we assume that the order of the Species Type Large Shapes in speciesShapes 
+		// is the same as the order of the Molecular Type Patterns in the SpeciesPattern sp
+		for(int i=0; i<numPatterns; i++) {
+			SpeciesTypeLargeShape stlsFrom = speciesShapes.get(i);
+			MolecularTypePattern mtpFrom = stlsFrom.getMolecularTypePattern();
+			int numComponents = mtpFrom.getComponentPatternList().size();
+			for(int j=0; j<numComponents; j++) {
+				MolecularComponentLargeShape mclsFrom = stlsFrom.getComponentShape(j);
+				MolecularComponentPattern mcpFrom = mtpFrom.getComponentPatternList().get(j);
+				if(mcpFrom.getBondType().equals(BondType.Specified)) {
+					Bond b = mcpFrom.getBond();
+					MolecularTypePattern mtpTo = b.molecularTypePattern;
+					SpeciesTypeLargeShape stlsTo = getShape(mtpTo); 
+					MolecularComponentPattern mcpTo = b.molecularComponentPattern;
+					MolecularComponentLargeShape mclsTo = stlsTo.getShape(mcpTo);
+					
+					Point from = new Point(mclsFrom.getX()+mclsFrom.getWidth()/2, mclsFrom.getY()+mclsFrom.getHeight());
+					Point to = new Point(mclsTo.getX()+mclsTo.getWidth()/2, mclsTo.getY()+mclsFrom.getHeight());
+					if(from.x < to.x) {		// the bonds with from.x > to.x are duplicates
+						BondPair bp = new BondPair(mcpFrom.getBondId(), from, to);
+						bondPairs.add(bp);
+					}
+				}
+			}
+		}
+		Collections.sort(bondPairs);
 	}
 	
+	private SpeciesTypeLargeShape getShape(MolecularTypePattern mtpThat) {
+		for(SpeciesTypeLargeShape stls : speciesShapes) {
+			MolecularTypePattern mtpThis = stls.getMolecularTypePattern();
+			if(mtpThis == mtpThat) {
+				return stls;
+			}
+		}
+		return null;
+	}
+
 	public SpeciesPattern getSpeciesPattern() {
 		return sp;
 	}
@@ -55,6 +123,16 @@ public class SpeciesPatternShape {
 	public void paintSelf(Graphics g) {
 		for(SpeciesTypeLargeShape stls : speciesShapes) {
 			stls.paintSelf(g);
+		}
+		for(int i=0; i<bondPairs.size(); i++) {
+			BondPair bp = bondPairs.get(i);
+			final int offset = 7;
+			final int separ = 5;
+			g.drawLine(bp.from.x, bp.from.y, bp.from.x, bp.from.y+offset+i*separ);
+			g.drawLine(bp.to.x, bp.to.y, bp.to.x, bp.to.y+offset+i*separ);
+			g.drawLine(bp.from.x, bp.from.y+offset+i*separ, bp.to.x, bp.to.y+offset+i*separ);
+						
+
 		}
 	}
 	
