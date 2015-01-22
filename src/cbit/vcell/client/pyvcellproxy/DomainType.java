14993541127591957716210ef2b54e64b854ba30
Jim Schaff <schaff@uchc.edu>
2015-01-22 18:43:06 +0000

    Bug 4052 : Initial commit of Apache Thrift mediated Pyside-VisIt-VCell vis client - API cleanup, still exports.
    svn path=/vcell/trunk/VCell/; revision=16723

--- src/cbit/vcell/client/pyvcellproxy/DomainType.java
+++ src/cbit/vcell/client/pyvcellproxy/DomainType.java
@@ -11,13 +11,13 @@ import java.util.Map;
 import java.util.HashMap;
 import org.apache.thrift.TEnum;
 
-public enum ModelType implements org.apache.thrift.TEnum {
-  BIOMODEL(0),
-  MATHMODEL(1);
+public enum DomainType implements org.apache.thrift.TEnum {
+  VOLUME(0),
+  MEMBRANE(1);
 
   private final int value;
 
-  private ModelType(int value) {
+  private DomainType(int value) {
     this.value = value;
   }
 
@@ -32,12 +32,12 @@ public enum ModelType implements org.apache.thrift.TEnum {
    * Find a the enum type by its integer value, as defined in the Thrift IDL.
    * @return null if the value is not found.
    */
-  public static ModelType findByValue(int value) { 
+  public static DomainType findByValue(int value) { 
     switch (value) {
       case 0:
-        return BIOMODEL;
+        return VOLUME;
       case 1:
-        return MATHMODEL;
+        return MEMBRANE;
       default:
         return null;
     }
--- thriftBuild/pyVCell.thrift
+++ thriftBuild/pyVCell.thrift
@@ -4,13 +4,9 @@ namespace py pyvcell
 typedef i32 int
 typedef i64 long
 
-enum ExportType {
-   VTU
-}
-
-enum ModelType {
-   BIOMODEL
-   MATHMODEL
+enum DomainType {
+   VOLUME
+   MEMBRANE
 }
 
 typedef int TimeIndex
@@ -22,80 +18,32 @@ typedef list<TimePoint> TimePoints
 
 struct VariableInfo{
    1: required string variableName;
-   2: optional string domainName;
-   3: optional string variableDomainType;
-   4: optional string unitsLabel;
+   2: required DomainName domainName;
+   3: required DomainType variableDomainType;
+   4: required string unitsLabel;
 }
 
 typedef list<VariableInfo> VariableList
 
-struct User{
-   1: required string userKey;
-   2: optional string userName; 
-}
-
 struct SimulationDataSetRef {
    1: required string simId;
-   2: optional string simName;
-   3: optional VariableList variableList;
-   4: optional string modelId;
+   2: required string simName;
+   4: required string modelId;
 }
-
-struct ModelRef {
-   1: required string modelId;
-   2: optional string modelName;
-   3: optional User user;
-   4: optional ModelType modelType;
-}
-
-typedef list<ModelRef> ModelRefList
 
 typedef list<SimulationDataSetRef> SimulationDataSetRefList
 
-struct ExportRequestSpec {
-   1: required ExportType exportType;
-   2: required string SimID;
-   3: required VariableList variables;
-   4: required int startTime;
-   5: required int endTime;
-}
-
-exception ExportException {
-   1: string message;
-}
-
 exception DataAccessException {
-   1: string message;
+   1: required string message;
 }
-
-exception DatabaseFileException {
-   1: string message;
-}
-
-exception PlotException {
-   1: string message;
-}
-
-#service VisitProxy {
-
-#   void openDatabase(1:string databaseName) throws (1:DatabaseFileException databaseFileException);
-#   void closeDatabase() throws (1:DatabaseFileException databaseFileException);
-#   void drawPlot(1:string variableName) throws (1:PlotException plotException);
-#   oneway void clearPlots() 
-#}
 
 service VCellProxy {
 
-    FilePath exportRequest(1:ExportRequestSpec exportRequestSpec) throws (1:ExportException exportException);
-    FilePath exportAllRequest(1:SimulationDataSetRef simulationDataSetRef) throws (1:ExportException exportException);
     FilePath getDataSetFileOfDomainAtTimeIndex(1:SimulationDataSetRef simulationDataSetRef, 2:DomainName domainName, 3:TimeIndex timeIndex) throws (1:DataAccessException dataAccessException)
-    FilePath getDataSetFileOfDomainAtTimePoint(1:SimulationDataSetRef simulationDataSetRef, 2:DomainName domainName, 3:TimePoint timePoint) throws (1:DataAccessException dataAccessException)
+    
     SimulationDataSetRefList getSimsFromOpenModels() throws (1: DataAccessException dataAccessException);
-    SimulationDataSetRefList getSimsFromModel(1: ModelRef modelRef) throws (1: DataAccessException dataAccessException);
-    TimeIndex getEndTimeIndex(1: SimulationDataSetRef simulationDataSetRef) throws (1:DataAccessException dataAccessException);
+    
     TimePoints getTimePoints(1: SimulationDataSetRef simulationDataSetRef) throws (1:DataAccessException dataAccessException);    
+    
     VariableList getVariableList(1: SimulationDataSetRef simulationDataSetRef) throws (1: DataAccessException dataAccessException);
-    ModelRefList getBioModels() throws (1: DataAccessException dataAccessException);
-    ModelRefList getMathModels() throws (1: DataAccessException dataAccessException);
-    User getUser() throws (1: DataAccessException dataAccessException);
 }
