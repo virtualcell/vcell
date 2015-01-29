d95db54d81dfcbe7267bd0dfca87f12915c4b72b
Gerard Weatherby <gweatherby@uchc.edu>
2015-01-29 20:26:14 +0000

    Bug 3769: check all boxes form level 0, and check indexes before accessing array
    svn path=/vcell/trunk/VCell/; revision=16768

--- src/org/vcell/vis/chombo/ChomboLevel.java
+++ src/org/vcell/vis/chombo/ChomboLevel.java
@@ -2,6 +2,7 @@ package org.vcell.vis.chombo;
 
 import java.util.ArrayList;
 
+import org.vcell.util.DimensionalIndex;
 import org.vcell.util.ISize;
 
 public class ChomboLevel {
@@ -61,22 +62,105 @@ public class ChomboLevel {
 		return absRefinementThisLevel;
 	}
 	
+// Fei's suggested version
+//	public ISize getSize(){
+//		ChomboLevel level0 = chomboMesh.getLevel(0);
+//		int maxX = 0, maxY = 0, maxZ = 0;
+//		for (ChomboBox box: level0.getBoxes())
+//		{
+//			maxX = Math.max(maxX, box.getMaxX());
+//			maxY = Math.max(maxY, box.getMaxY());
+//			maxZ = Math.max(maxZ, box.getMaxZ());
+//		}
+//		int numX_level0 = maxX+1;
+//		int numY_level0 = maxY+1;
+//
+//		int absRefinementThisLevel = getAbsoluteRefinement();
+//
+//		int numX = numX_level0 * absRefinementThisLevel;
+//		int numY = numY_level0 * absRefinementThisLevel;
+//		if (chomboMesh.getDimension()==2){
+//			return new ISize(numX,numY,1);
+//		}else{
+//			int numZ_level0 = maxZ+1;
+//			int numZ = numZ_level0 * absRefinementThisLevel;
+//			return new ISize(numX,numY,numZ);
+//		}
+//	}
+	
+	/**
+	 * get size based on max box values from level0
+	 */
 	public ISize getSize(){
 		ChomboLevel level0 = chomboMesh.getLevel(0);
-		int numX_level0 = level0.getBoxes().get(0).getMaxX()+1;
-		int numY_level0 = level0.getBoxes().get(0).getMaxY()+1;
 
-		int absRefinementThisLevel = getAbsoluteRefinement();
-
-		int numX = numX_level0 * absRefinementThisLevel;
-		int numY = numY_level0 * absRefinementThisLevel;
+		final int absRefinementThisLevel = getAbsoluteRefinement();
 		
-		if (chomboMesh.getDimension()==2){
-			return new ISize(numX,numY,1);
-		}else{
-			int numZ_level0 = level0.getBoxes().get(0).getMaxZ()+1;
-			int numZ = numZ_level0 * absRefinementThisLevel;
-			return new ISize(numX,numY,numZ);
+		BoxMaxes bMaxes = level0.getBoxMaxes(); 
+		bMaxes.addToAllDimensions(1);
+		bMaxes.multiplyAllDimensionsBy(absRefinementThisLevel);
+
+		final int numZ = ( chomboMesh.getDimension() == 2 ) ? 1 : bMaxes.get(BoxMaxes.Z);
+		return new ISize(bMaxes.get(BoxMaxes.X), bMaxes.get(BoxMaxes.Y), numZ);
+	}
+
+	public BoxMaxes getBoxMaxes( ) {
+		BoxMaxes bMaxes = new BoxMaxes(chomboMesh.getDimension() == 3);
+		for (ChomboBox b: getBoxes()) {
+			bMaxes.read(b);
+		}
+	
+		return bMaxes;
+	}
+	
+	/**
+	 * find max values from one or more {@link ChomboBox}es
+	 */
+	private static class BoxMaxes {
+		private final int maxValues[];
+		final boolean is3D;
+		public final static int X = 0;
+		public final static int Y = 1;
+		public final static int Z = 2;
+		
+		public BoxMaxes(boolean is3D) {
+			super();
+			this.is3D = is3D;
+			maxValues = new int[is3D ? 3: 2];
+		}
+		
+		/**
+		 * get specified max
+		 * @param d should be {@link #X}, {@link #Y}, or {@link #Z}
+		 * @return desired max
+		 * @throws ArrayIndexOutOfBoundsException if d < 0 or > max value based on {@link #is3D}
+		 */
+		public int get(int d) {
+			return maxValues[d];
+		}
+		
+		public void read(ChomboBox box) {
+			maxValues[X] = Math.max(maxValues[X], box.getMaxX());
+			maxValues[Y] = Math.max(maxValues[Y], box.getMaxY());
+			if (is3D) {
+				maxValues[Z] = Math.max(maxValues[Z], box.getMaxZ());
+			}
+		}
+		/**
+		 *  add value to all dimensions
+		 */
+		public void addToAllDimensions(int a) {
+			for (int i = 0; i < maxValues.length;i++) {
+				maxValues[i] += a;
+			}
+		}
+		/**
+		 *  multiply value to all dimensions
+		 */
+		public void multiplyAllDimensionsBy(int m) {
+			for (int i = 0; i < maxValues.length;i++) {
+				maxValues[i] *= m;
+			}
 		}
 	}
 	
@@ -147,10 +231,8 @@ public class ChomboLevel {
 		//
 		int absRefinementThisLevel = getAbsoluteRefinement();
 		ISize size = getSize();
-		int numX = size.getX();
-		int numY = size.getY();
-		int numZ = size.getZ();
-		int numXY = size.getX()*size.getY();
+		
+		DimensionalIndex dimIndex = new DimensionalIndex(size);
 
 		//
 		// 1) allocate a 1-D array sized at this level whose value
@@ -179,7 +261,7 @@ public class ChomboLevel {
 	    		for (int z=projectedBox.getMinZ();z<=projectedBox.getMaxZ();z++){
 		    		for (int y=projectedBox.getMinY();y<=projectedBox.getMaxY();y++){
 		    			for (int x=projectedBox.getMinX();x<=projectedBox.getMaxX();x++){
-		    				int index = x+y*numX+z*numXY;
+		    				int index = dimIndex.rollup(x,y,z);
 		    				levelMap[index] = chomboLevel.getLevel();
 		    				boxNumberMap[index] = boxNumber;
 		    				boxIndexMap[index] = boxIndex;
@@ -195,8 +277,8 @@ public class ChomboLevel {
 		//
 		System.out.println();
 		int index = 0;
-		for (int y=0;y<numY;y++){
-			for (int x=0;x<numX;x++){
+		for (int y=0;y<size.getY();y++){
+			for (int x=0;x<size.getX();x++){
 				System.out.print(levelMap[index++]);
 			}
 			System.out.println();
