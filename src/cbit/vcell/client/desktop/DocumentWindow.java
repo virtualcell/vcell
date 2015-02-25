0630880614e42598d0e2996a82e24a8e5b69e3af
Gerard Weatherby <gweatherby@uchc.edu>
2015-02-25 17:11:20 +0000

    Bug 2423: Windows variant GPL for Cygwin packaging.
    svn path=/vcell/trunk/VCell/; revision=16928

--- src/cbit/vcell/client/desktop/DocumentWindow.java
+++ src/cbit/vcell/client/desktop/DocumentWindow.java
@@ -71,6 +71,7 @@ import cbit.vcell.desktop.LoginDelegate;
 import cbit.vcell.desktop.LoginManager;
 import cbit.vcell.microscopy.gui.VirtualFrapLoader;
 import cbit.vcell.model.gui.TransformMassActionPanel;
+import cbit.vcell.resource.OperatingSystemInfo;
 import cbit.vcell.resource.ResourceUtil;
 import cbit.vcell.resource.VisitSupport;
 /**
@@ -2784,6 +2785,11 @@ public void showTransMADialog()
 							"Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\n" +
 							"The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\n"+
 							"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n\n";
+					
+					if (OperatingSystemInfo.getInstance().isWindows()) {
+						licenseStr += "The Virtual Cell Windows Edition is licensed under the GNU General Public License Version 3\n\n";
+					}
+					
 					String result = DialogUtils.showWarningDialog(DocumentWindow.this, "VCell Licensing Information", licenseStr, new String[] {OK,SAVE_THIRD_PARTY,SAVE_LICENSE,}, OK);
 					if(result != null && (result.equals(SAVE_THIRD_PARTY) || result.equals(SAVE_LICENSE))){
 						JFileChooser saveLicenseJFC = new JFileChooser();
