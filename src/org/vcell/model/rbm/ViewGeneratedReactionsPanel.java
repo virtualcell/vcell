70cec1c62996b909cec8342a500e9a9e942116d5
Dan Vasilescu <vasilescu@uchc.edu>
2015-03-09 20:33:32 +0000

    Bug 4122:    Generated reaction viewer shows depiction of the reaction
    svn path=/vcell/trunk/VCell/; revision=16987

--- src/org/vcell/model/rbm/ViewGeneratedReactionsPanel.java
+++ src/org/vcell/model/rbm/ViewGeneratedReactionsPanel.java
@@ -12,6 +12,7 @@ package org.vcell.model.rbm;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -21,9 +22,14 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.event.ListSelectionEvent;
@@ -37,7 +43,9 @@ import org.vcell.util.gui.EditorScrollTable;
 
 import cbit.vcell.bionetgen.BNGReaction;
 import cbit.vcell.client.desktop.biomodel.DocumentEditorSubPanel;
+import cbit.vcell.graph.SpeciesPatternLargeShape;
 import cbit.vcell.model.Model;
+import cbit.vcell.model.Model.RbmModelContainer;
 import cbit.vcell.model.ProductPattern;
 import cbit.vcell.model.ReactantPattern;
 import cbit.vcell.model.ReactionRule;
@@ -49,6 +57,12 @@ public class ViewGeneratedReactionsPanel extends DocumentEditorSubPanel  {
 	private GeneratedReactionTableModel tableModel = null; 
 	private EditorScrollTable table = null;
 	private JTextField textFieldSearch = null;
+	
+	private final NetworkConstraintsPanel owner;
+	JPanel shapePanel = null;
+	List<SpeciesPatternLargeShape> reactantPatternShapeList = new ArrayList<SpeciesPatternLargeShape>();
+	List<SpeciesPatternLargeShape> productPatternShapeList = new ArrayList<SpeciesPatternLargeShape>();
+
 	
 	private class EventHandler implements ActionListener, DocumentListener, ListSelectionListener {
 		public void actionPerformed(java.awt.event.ActionEvent e) {
@@ -62,68 +76,121 @@ public class ViewGeneratedReactionsPanel extends DocumentEditorSubPanel  {
 		public void changedUpdate(DocumentEvent e) {
 			searchTable();
 		}
+		
+		/*
+		 *  TODO: (Warning) this code heavily borrows from RbmUtils.parseReactionRule and ReactionRuleEditorPropertiesPanel
+		 *  Make sure to keep them in sync
+		 */
 		@Override
 		public void valueChanged(ListSelectionEvent e) {
-//			int[] selectedRows = table.getSelectedRows();
-//			if(selectedRows.length == 1 && !e.getValueIsAdjusting()) {
-//				GeneratedReactionTableRow reactionTableRow = tableModel.getValueAt(selectedRows[0]);
-//				String inputString = reactionTableRow.getExpression();
-//				System.out.println(selectedRows[0] + ": " + inputString);
-////				ReactionRule newReactionRule = (ReactionRule)RbmUtils.parseReactionRule(inputString, bioModel);
-//				
-//				Model model = new Model("MyTempModel");
-//				
-//				int arrowIndex = inputString.indexOf("<->");
-//				boolean bReversible = true;
-//				if (arrowIndex < 0) {
-//					arrowIndex = inputString.indexOf("->");
-//					bReversible = false;
-//				}
-//				
-//				String left = inputString.substring(0, arrowIndex).trim();
-//				String right = inputString.substring(arrowIndex + (bReversible ? 3 : 2)).trim();
-//				if (left.length() == 0 && right.length() == 0) {
-//					return;
-//				}
-//				ReactionRule reactionRule = model.getRbmModelContainer().createReactionRule("aaa", bReversible);
-//				
-//				String regex = "[^!]\\+";
-//				String[] patterns = left.split(regex);
-//				for (String sp : patterns) {
-//					try {
-//					BNGLParser parser = new BNGLParser(new StringReader(sp));
-//					ASTSpeciesPattern astSpeciesPattern = parser.SpeciesPattern();
-//					BnglObjectConstructionVisitor constructionVisitor = new BnglObjectConstructionVisitor(model, null, false);
-//					SpeciesPattern speciesPattern = (SpeciesPattern) astSpeciesPattern.jjtAccept(constructionVisitor, null);
-//					System.out.println(speciesPattern.toString());
-//					reactionRule.addReactant(new ReactantPattern(speciesPattern));
-//					} catch(Throwable ex) {
-//						ex.printStackTrace();
-//						return;
-//					}
-//				}
-//				
-//				patterns = right.split(regex);
-//				for (String sp : patterns) {
-//					try {
-//					BNGLParser parser = new BNGLParser(new StringReader(sp));
-//					ASTSpeciesPattern astSpeciesPattern = parser.SpeciesPattern();
-//					BnglObjectConstructionVisitor constructionVisitor = new BnglObjectConstructionVisitor(model, null, false);
-//					SpeciesPattern speciesPattern = (SpeciesPattern) astSpeciesPattern.jjtAccept(constructionVisitor, null);
-//					System.out.println(speciesPattern.toString());
-//					reactionRule.addProduct(new ProductPattern(speciesPattern));
-//					} catch(Throwable ex) {
-//						ex.printStackTrace();
-//						return;
-//					}
-//				}			
-//				System.out.println(" --- done.");
-//			}
+			int[] selectedRows = table.getSelectedRows();
+			if(selectedRows.length == 1 && !e.getValueIsAdjusting()) {
+				GeneratedReactionTableRow reactionTableRow = tableModel.getValueAt(selectedRows[0]);
+				String inputString = reactionTableRow.getExpression();
+				System.out.println(selectedRows[0] + ": " + inputString);
+//				ReactionRule newReactionRule = (ReactionRule)RbmUtils.parseReactionRule(inputString, bioModel);
+				
+				Model model = new Model("MyTempModel");
+				if(owner != null && owner.getSimulationContext() != null) {
+					List <MolecularType> mtList = owner.getSimulationContext().getModel().getRbmModelContainer().getMolecularTypeList();
+					model.getRbmModelContainer().setMolecularTypeList(mtList);
+				} else {
+					return;		// something is wrong, we just show nothing rather than crash
+				}
+				
+				int arrowIndex = inputString.indexOf("<->");
+				boolean bReversible = true;
+				if (arrowIndex < 0) {
+					arrowIndex = inputString.indexOf("->");
+					bReversible = false;
+				}
+				
+				String left = inputString.substring(0, arrowIndex).trim();
+				String right = inputString.substring(arrowIndex + (bReversible ? 3 : 2)).trim();
+				if (left.length() == 0 && right.length() == 0) {
+					return;
+				}
+				ReactionRule reactionRule = model.getRbmModelContainer().createReactionRule("aaa", bReversible);
+				
+				String regex = "[^!]\\+";
+				String[] patterns = left.split(regex);
+				for (String sp : patterns) {
+					try {
+					BNGLParser parser = new BNGLParser(new StringReader(sp));
+					ASTSpeciesPattern astSpeciesPattern = parser.SpeciesPattern();
+					BnglObjectConstructionVisitor constructionVisitor = new BnglObjectConstructionVisitor(model, null, false);
+					SpeciesPattern speciesPattern = (SpeciesPattern) astSpeciesPattern.jjtAccept(constructionVisitor, null);
+					System.out.println(speciesPattern.toString());
+					reactionRule.addReactant(new ReactantPattern(speciesPattern));
+					} catch(Throwable ex) {
+						ex.printStackTrace();
+						return;
+					}
+				}
+				
+				patterns = right.split(regex);
+				for (String sp : patterns) {
+					try {
+					BNGLParser parser = new BNGLParser(new StringReader(sp));
+					ASTSpeciesPattern astSpeciesPattern = parser.SpeciesPattern();
+					BnglObjectConstructionVisitor constructionVisitor = new BnglObjectConstructionVisitor(model, null, false);
+					SpeciesPattern speciesPattern = (SpeciesPattern) astSpeciesPattern.jjtAccept(constructionVisitor, null);
+					System.out.println(speciesPattern.toString());
+					reactionRule.addProduct(new ProductPattern(speciesPattern));
+					} catch(Throwable ex) {
+						ex.printStackTrace();
+						return;
+					}
+				}			
+				System.out.println(" --- done.");
+// ----------------------------------------------------------------------------------------------------
+				
+				List<ReactantPattern> rpList = reactionRule.getReactantPatterns();
+				reactantPatternShapeList.clear();
+				int xOffset = 20;
+				if(rpList != null && rpList.size() > 0) {
+					Graphics gc = shapePanel.getGraphics();
+					for(int i = 0; i<rpList.size(); i++) {
+						SpeciesPattern sp = rpList.get(i).getSpeciesPattern();
+						// TODO: count the number of bonds for this sp and allow enough vertical space for them
+						SpeciesPatternLargeShape sps = new SpeciesPatternLargeShape(xOffset, 20, sp, gc, reactionRule);
+						if(i < rpList.size()-1) {
+							sps.addEndText("+");
+						} else {
+							if(reactionRule.isReversible()) {
+								sps.addEndText("<->");
+							} else {
+								sps.addEndText("->");
+							}
+						}
+						xOffset = sps.getRightEnd() + 35;
+						reactantPatternShapeList.add(sps);
+					}
+				}
+				xOffset += 15;	// space for the <-> sign
+				List<ProductPattern> ppList = reactionRule.getProductPatterns();
+				productPatternShapeList.clear();
+				if(ppList != null && ppList.size() > 0) {
+					Graphics gc = shapePanel.getGraphics();
+					for(int i = 0; i<ppList.size(); i++) {
+						SpeciesPattern sp = ppList.get(i).getSpeciesPattern();
+						SpeciesPatternLargeShape sps = new SpeciesPatternLargeShape(xOffset, 20, sp, gc, reactionRule);
+						if(i < ppList.size()-1) {
+							sps.addEndText("+");
+						}
+						xOffset = sps.getRightEnd() + 35;
+						productPatternShapeList.add(sps);
+					}
+				}
+				shapePanel.repaint();
+				
+			}
 		}
 	}
 	
-public ViewGeneratedReactionsPanel() {
+public ViewGeneratedReactionsPanel(NetworkConstraintsPanel owner) {
 	super();
+	this.owner = owner;
 	initialize();
 }
 
@@ -149,11 +216,11 @@ private void initialize() {
 		
 		DefaultTableCellRenderer rightRenderer = new DefaultTableCellRenderer();
 		rightRenderer.setHorizontalAlignment(JLabel.RIGHT);
-		table.getColumnModel().getColumn(GeneratedReactionTableModel.iColIndex).setCellRenderer(rightRenderer);	// right align
+		table.getColumnModel().getColumn(GeneratedReactionTableModel.iColIndex).setCellRenderer(rightRenderer);	// right align first table column
 		table.getColumnModel().getColumn(GeneratedReactionTableModel.iColIndex).setMaxWidth(60);				// left column wide enough for 6-7 digits
 		
 		int gridy = 0;
-		GridBagConstraints gbc = new java.awt.GridBagConstraints();		
+		GridBagConstraints gbc = new GridBagConstraints();		
 		gbc.gridx = 0;
 		gbc.gridy = gridy;
 		gbc.weightx = 1.0;
@@ -164,9 +231,9 @@ private void initialize() {
 		table.setPreferredScrollableViewportSize(new Dimension(400,200));
 		add(table.getEnclosingScrollPane(), gbc);
 		
-		gbc = new java.awt.GridBagConstraints();
-		gbc.gridx = 9;
-		gbc.gridy = gridy;
+//		gbc = new java.awt.GridBagConstraints();
+//		gbc.gridx = 9;
+//		gbc.gridy = gridy;
 		
 		// add toolTipText for each table cell
 		table.addMouseMotionListener(new MouseMotionAdapter() { 
@@ -191,7 +258,7 @@ private void initialize() {
 		textFieldSearch.getDocument().addDocumentListener(eventHandler);
 		textFieldSearch.putClientProperty("JTextField.variant", "search");
 		
-		gbc = new java.awt.GridBagConstraints();
+		gbc = new GridBagConstraints();
 		gbc.weightx = 1.0;
 		gbc.gridx = 1; 
 		gbc.gridy = gridy;
@@ -201,7 +268,43 @@ private void initialize() {
 		gbc.insets = new Insets(4, 0, 4, 4);
 		add(textFieldSearch, gbc);
 		
-		setBackground(Color.white);		
+		
+// -------------------------------------------------------------------------------------------
+		shapePanel = new JPanel() {
+			@Override
+			public void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				for(SpeciesPatternLargeShape stls : reactantPatternShapeList) {
+					stls.paintSelf(g);
+				}
+				for(SpeciesPatternLargeShape stls : productPatternShapeList) {
+					stls.paintSelf(g);
+				}
+			}
+		};
+		Border loweredEtchedBorder = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
+		Border loweredBevelBorder = BorderFactory.createLoweredBevelBorder();
+		Dimension preferredSize = new Dimension(500, 50);
+		shapePanel.setPreferredSize(preferredSize);
+		shapePanel.setLayout(new GridBagLayout());
+		shapePanel.setBorder(loweredBevelBorder);
+		shapePanel.setBackground(Color.white);
+		shapePanel.setVisible(true);
+
+		gridy ++;	
+		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = gridy;
+		gbc.weightx = 1.0;
+		gbc.weighty = 0.3;
+		gbc.gridwidth = 8;
+		gbc.anchor = GridBagConstraints.LINE_END;
+		gbc.fill = java.awt.GridBagConstraints.BOTH;
+		gbc.insets = new Insets(4,4,4,4);
+		add(shapePanel, gbc);
+		
+//		setBackground(Color.white);
+		
 	} catch (java.lang.Throwable ivjExc) {
 		handleException(ivjExc);
 	}
