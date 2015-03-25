b24b09a91cdf3933e24db2330e4168a48272a522
Dan Vasilescu <vasilescu@uchc.edu>
2015-03-25 21:13:13 +0000

    Bug 4141:    Check if there are identical components within molecule or molecular type pattern during import and give appropriate error. Also generate Issues if the condition becomes manifest in existing models.    Fundamental change in the way we initialize component patterns - now we don't lazily create one hidden component pattern for each component of the molecular type, we wait until a component pattern (or more) is specifically mentioned for a particular molecular type pattern. (code under construction)
    svn path=/vcell/trunk/VCell/; revision=17135

--- src/cbit/vcell/model/ReactionRule.java
+++ src/cbit/vcell/model/ReactionRule.java
@@ -231,8 +231,8 @@ public class ReactionRule implements Serializable, Matchable, ModelProcess, Prop
 				System.out.println("we already fired an issue about component missing");
 				continue;	// nothing to do here, we already fired an issue about component missing 
 			} else if(mcThatList.length > 1) {
-				String msg = "Multiple components with the same name are not yet supported.";
-				issueList.add(new Issue(this, issueContext, IssueCategory.Identifiers, msg, Issue.SEVERITY_WARNING));
+				String msg = "Multiple " + MolecularComponent.typeName + "s with the same name are not yet supported.";
+				issueList.add(new Issue(this, issueContext, IssueCategory.Identifiers, msg, Issue.SEVERITY_ERROR));
 			} else {	// found exactly 1 component
 				MolecularComponent mcThat = mcThatList[0];
 				List<ComponentStateDefinition> csdListThat = mcThat.getComponentStateDefinitions();
