3149f225dd87dacf625930857a9574f768e5599a
Gerard Weatherby <gweatherby@uchc.edu>
2015-04-03 13:17:23 +0000

    Bug 4156: cleanup, BeanUtils.downcast
    svn path=/vcell/trunk/VCell/; revision=17204

--- test/org/vcell/util/BeansUtilTest.java
+++ test/org/vcell/util/BeansUtilTest.java
@@ -1,11 +1,15 @@
 package org.vcell.util;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Test;
 
 public class BeansUtilTest {
 	
 	@SuppressWarnings("null")
-	@Test
+	//@Test
 	public void tryIt( ) {
 		try {
 			String n = null;
@@ -23,6 +27,23 @@ public class BeansUtilTest {
 			npe.printStackTrace();
 		}
 		
+	}
+	
+	@Test
+	public void lookupTest( ) {
+		java.util.concurrent.TimeUnit tu = BeanUtils.lookupEnum(TimeUnit.class, 3);
+		System.out.println(tu);
+	}
+	@Test
+	public void downcast( ) {
+		Integer i = 3;
+		Number n = i;
+		Integer back = BeanUtils.downcast(Integer.class,n);
+		Double fail =  BeanUtils.downcast(Double.class,n);
+		assertTrue(back == i);
+		assertTrue(fail == null);
+		Double notThere =  BeanUtils.downcast(Double.class,null);
+		assertTrue(notThere == null);
 	}
 
 }
