323dd70a05afa0672aeb0e117956718e2656c7d6
Edward Boyce <boyce@uchc.edu>
2015-04-21 17:28:38 +0000

    Bug 4184: Post Processing Statistics Data display by VCell Java client commanded from Python VCell-VisIt client
    svn path=/vcell/trunk/VCell/; revision=17332

--- visTool/pyvcell/ttypes.py
+++ visTool/pyvcell/ttypes.py
@@ -166,6 +166,9 @@ class SimulationDataSetRef:
    - modelId
    - username
    - userkey
+   - jobIndex
+   - isMathModel
+   - simulationContextName
   """
 
   thrift_spec = (
@@ -175,14 +178,20 @@ class SimulationDataSetRef:
     (3, TType.STRING, 'modelId', None, None, ), # 3
     (4, TType.STRING, 'username', None, None, ), # 4
     (5, TType.STRING, 'userkey', None, None, ), # 5
+    (6, TType.I32, 'jobIndex', None, None, ), # 6
+    (7, TType.BOOL, 'isMathModel', None, None, ), # 7
+    (8, TType.STRING, 'simulationContextName', None, None, ), # 8
   )
 
-  def __init__(self, simId=None, simName=None, modelId=None, username=None, userkey=None,):
+  def __init__(self, simId=None, simName=None, modelId=None, username=None, userkey=None, jobIndex=None, isMathModel=None, simulationContextName=None,):
     self.simId = simId
     self.simName = simName
     self.modelId = modelId
     self.username = username
     self.userkey = userkey
+    self.jobIndex = jobIndex
+    self.isMathModel = isMathModel
+    self.simulationContextName = simulationContextName
 
   def read(self, iprot):
     if iprot.__class__ == TBinaryProtocol.TBinaryProtocolAccelerated and isinstance(iprot.trans, TTransport.CReadableTransport) and self.thrift_spec is not None and fastbinary is not None:
@@ -218,6 +227,21 @@ class SimulationDataSetRef:
           self.userkey = iprot.readString();
         else:
           iprot.skip(ftype)
+      elif fid == 6:
+        if ftype == TType.I32:
+          self.jobIndex = iprot.readI32();
+        else:
+          iprot.skip(ftype)
+      elif fid == 7:
+        if ftype == TType.BOOL:
+          self.isMathModel = iprot.readBool();
+        else:
+          iprot.skip(ftype)
+      elif fid == 8:
+        if ftype == TType.STRING:
+          self.simulationContextName = iprot.readString();
+        else:
+          iprot.skip(ftype)
       else:
         iprot.skip(ftype)
       iprot.readFieldEnd()
@@ -248,6 +272,18 @@ class SimulationDataSetRef:
       oprot.writeFieldBegin('userkey', TType.STRING, 5)
       oprot.writeString(self.userkey)
       oprot.writeFieldEnd()
+    if self.jobIndex is not None:
+      oprot.writeFieldBegin('jobIndex', TType.I32, 6)
+      oprot.writeI32(self.jobIndex)
+      oprot.writeFieldEnd()
+    if self.isMathModel is not None:
+      oprot.writeFieldBegin('isMathModel', TType.BOOL, 7)
+      oprot.writeBool(self.isMathModel)
+      oprot.writeFieldEnd()
+    if self.simulationContextName is not None:
+      oprot.writeFieldBegin('simulationContextName', TType.STRING, 8)
+      oprot.writeString(self.simulationContextName)
+      oprot.writeFieldEnd()
     oprot.writeFieldStop()
     oprot.writeStructEnd()
 
@@ -262,6 +298,12 @@ class SimulationDataSetRef:
       raise TProtocol.TProtocolException(message='Required field username is unset!')
     if self.userkey is None:
       raise TProtocol.TProtocolException(message='Required field userkey is unset!')
+    if self.jobIndex is None:
+      raise TProtocol.TProtocolException(message='Required field jobIndex is unset!')
+    if self.isMathModel is None:
+      raise TProtocol.TProtocolException(message='Required field isMathModel is unset!')
+    if self.simulationContextName is None:
+      raise TProtocol.TProtocolException(message='Required field simulationContextName is unset!')
     return
 
 
@@ -272,6 +314,9 @@ class SimulationDataSetRef:
     value = (value * 31) ^ hash(self.modelId)
     value = (value * 31) ^ hash(self.username)
     value = (value * 31) ^ hash(self.userkey)
+    value = (value * 31) ^ hash(self.jobIndex)
+    value = (value * 31) ^ hash(self.isMathModel)
+    value = (value * 31) ^ hash(self.simulationContextName)
     return value
 
   def __repr__(self):
@@ -285,7 +330,205 @@ class SimulationDataSetRef:
   def __ne__(self, other):
     return not (self == other)
 
-class DataAccessException(TException):
+class PlotData:
+  """
+  Attributes:
+   - timePoints
+   - data
+  """
+
+  thrift_spec = (
+    None, # 0
+    (1, TType.LIST, 'timePoints', (TType.DOUBLE,None), None, ), # 1
+    (2, TType.LIST, 'data', (TType.DOUBLE,None), None, ), # 2
+  )
+
+  def __init__(self, timePoints=None, data=None,):
+    self.timePoints = timePoints
+    self.data = data
+
+  def read(self, iprot):
+    if iprot.__class__ == TBinaryProtocol.TBinaryProtocolAccelerated and isinstance(iprot.trans, TTransport.CReadableTransport) and self.thrift_spec is not None and fastbinary is not None:
+      fastbinary.decode_binary(self, iprot.trans, (self.__class__, self.thrift_spec))
+      return
+    iprot.readStructBegin()
+    while True:
+      (fname, ftype, fid) = iprot.readFieldBegin()
+      if ftype == TType.STOP:
+        break
+      if fid == 1:
+        if ftype == TType.LIST:
+          self.timePoints = []
+          (_etype3, _size0) = iprot.readListBegin()
+          for _i4 in xrange(_size0):
+            _elem5 = iprot.readDouble();
+            self.timePoints.append(_elem5)
+          iprot.readListEnd()
+        else:
+          iprot.skip(ftype)
+      elif fid == 2:
+        if ftype == TType.LIST:
+          self.data = []
+          (_etype9, _size6) = iprot.readListBegin()
+          for _i10 in xrange(_size6):
+            _elem11 = iprot.readDouble();
+            self.data.append(_elem11)
+          iprot.readListEnd()
+        else:
+          iprot.skip(ftype)
+      else:
+        iprot.skip(ftype)
+      iprot.readFieldEnd()
+    iprot.readStructEnd()
+
+  def write(self, oprot):
+    if oprot.__class__ == TBinaryProtocol.TBinaryProtocolAccelerated and self.thrift_spec is not None and fastbinary is not None:
+      oprot.trans.write(fastbinary.encode_binary(self, (self.__class__, self.thrift_spec)))
+      return
+    oprot.writeStructBegin('PlotData')
+    if self.timePoints is not None:
+      oprot.writeFieldBegin('timePoints', TType.LIST, 1)
+      oprot.writeListBegin(TType.DOUBLE, len(self.timePoints))
+      for iter12 in self.timePoints:
+        oprot.writeDouble(iter12)
+      oprot.writeListEnd()
+      oprot.writeFieldEnd()
+    if self.data is not None:
+      oprot.writeFieldBegin('data', TType.LIST, 2)
+      oprot.writeListBegin(TType.DOUBLE, len(self.data))
+      for iter13 in self.data:
+        oprot.writeDouble(iter13)
+      oprot.writeListEnd()
+      oprot.writeFieldEnd()
+    oprot.writeFieldStop()
+    oprot.writeStructEnd()
+
+  def validate(self):
+    if self.timePoints is None:
+      raise TProtocol.TProtocolException(message='Required field timePoints is unset!')
+    if self.data is None:
+      raise TProtocol.TProtocolException(message='Required field data is unset!')
+    return
+
+
+  def __hash__(self):
+    value = 17
+    value = (value * 31) ^ hash(self.timePoints)
+    value = (value * 31) ^ hash(self.data)
+    return value
+
+  def __repr__(self):
+    L = ['%s=%r' % (key, value)
+      for key, value in self.__dict__.iteritems()]
+    return '%s(%s)' % (self.__class__.__name__, ', '.join(L))
+
+  def __eq__(self, other):
+    return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+
+  def __ne__(self, other):
+    return not (self == other)
+
+class PostProcessingData:
+  """
+  Attributes:
+   - variableList
+   - plotData
+  """
+
+  thrift_spec = (
+    None, # 0
+    (1, TType.LIST, 'variableList', (TType.STRUCT,(VariableInfo, VariableInfo.thrift_spec)), None, ), # 1
+    (2, TType.LIST, 'plotData', (TType.STRUCT,(PlotData, PlotData.thrift_spec)), None, ), # 2
+  )
+
+  def __init__(self, variableList=None, plotData=None,):
+    self.variableList = variableList
+    self.plotData = plotData
+
+  def read(self, iprot):
+    if iprot.__class__ == TBinaryProtocol.TBinaryProtocolAccelerated and isinstance(iprot.trans, TTransport.CReadableTransport) and self.thrift_spec is not None and fastbinary is not None:
+      fastbinary.decode_binary(self, iprot.trans, (self.__class__, self.thrift_spec))
+      return
+    iprot.readStructBegin()
+    while True:
+      (fname, ftype, fid) = iprot.readFieldBegin()
+      if ftype == TType.STOP:
+        break
+      if fid == 1:
+        if ftype == TType.LIST:
+          self.variableList = []
+          (_etype17, _size14) = iprot.readListBegin()
+          for _i18 in xrange(_size14):
+            _elem19 = VariableInfo()
+            _elem19.read(iprot)
+            self.variableList.append(_elem19)
+          iprot.readListEnd()
+        else:
+          iprot.skip(ftype)
+      elif fid == 2:
+        if ftype == TType.LIST:
+          self.plotData = []
+          (_etype23, _size20) = iprot.readListBegin()
+          for _i24 in xrange(_size20):
+            _elem25 = PlotData()
+            _elem25.read(iprot)
+            self.plotData.append(_elem25)
+          iprot.readListEnd()
+        else:
+          iprot.skip(ftype)
+      else:
+        iprot.skip(ftype)
+      iprot.readFieldEnd()
+    iprot.readStructEnd()
+
+  def write(self, oprot):
+    if oprot.__class__ == TBinaryProtocol.TBinaryProtocolAccelerated and self.thrift_spec is not None and fastbinary is not None:
+      oprot.trans.write(fastbinary.encode_binary(self, (self.__class__, self.thrift_spec)))
+      return
+    oprot.writeStructBegin('PostProcessingData')
+    if self.variableList is not None:
+      oprot.writeFieldBegin('variableList', TType.LIST, 1)
+      oprot.writeListBegin(TType.STRUCT, len(self.variableList))
+      for iter26 in self.variableList:
+        iter26.write(oprot)
+      oprot.writeListEnd()
+      oprot.writeFieldEnd()
+    if self.plotData is not None:
+      oprot.writeFieldBegin('plotData', TType.LIST, 2)
+      oprot.writeListBegin(TType.STRUCT, len(self.plotData))
+      for iter27 in self.plotData:
+        iter27.write(oprot)
+      oprot.writeListEnd()
+      oprot.writeFieldEnd()
+    oprot.writeFieldStop()
+    oprot.writeStructEnd()
+
+  def validate(self):
+    if self.variableList is None:
+      raise TProtocol.TProtocolException(message='Required field variableList is unset!')
+    if self.plotData is None:
+      raise TProtocol.TProtocolException(message='Required field plotData is unset!')
+    return
+
+
+  def __hash__(self):
+    value = 17
+    value = (value * 31) ^ hash(self.variableList)
+    value = (value * 31) ^ hash(self.plotData)
+    return value
+
+  def __repr__(self):
+    L = ['%s=%r' % (key, value)
+      for key, value in self.__dict__.iteritems()]
+    return '%s(%s)' % (self.__class__.__name__, ', '.join(L))
+
+  def __eq__(self, other):
+    return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+
+  def __ne__(self, other):
+    return not (self == other)
+
+class ThriftDataAccessException(TException):
   """
   Attributes:
    - message
@@ -322,7 +565,7 @@ class DataAccessException(TException):
     if oprot.__class__ == TBinaryProtocol.TBinaryProtocolAccelerated and self.thrift_spec is not None and fastbinary is not None:
       oprot.trans.write(fastbinary.encode_binary(self, (self.__class__, self.thrift_spec)))
       return
-    oprot.writeStructBegin('DataAccessException')
+    oprot.writeStructBegin('ThriftDataAccessException')
     if self.message is not None:
       oprot.writeFieldBegin('message', TType.STRING, 1)
       oprot.writeString(self.message)
