71d98c6a083ad93f7612b89e49c96d2606f0e245
Dan Vasilescu <vasilescu@uchc.edu>
2015-05-01 21:08:08 +0000

    Bug 4194: Annotation support added to ReactionRules (user interface)
    svn path=/vcell/trunk/VCell/; revision=17418

--- src/cbit/vcell/client/desktop/biomodel/ReactionRuleKineticsPropertiesPanel.java
+++ src/cbit/vcell/client/desktop/biomodel/ReactionRuleKineticsPropertiesPanel.java
@@ -24,6 +24,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.swing.BorderFactory;
 import javax.swing.JCheckBox;
 import javax.swing.JEditorPane;
 import javax.swing.JLabel;
@@ -34,12 +35,14 @@ import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
 import javax.swing.JSplitPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JTree;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.ToolTipManager;
+import javax.swing.border.Border;
 import javax.swing.event.TreeExpansionEvent;
 import javax.swing.event.TreeExpansionListener;
 import javax.swing.event.TreeSelectionEvent;
@@ -59,11 +62,14 @@ import org.vcell.model.rbm.MolecularType;
 import org.vcell.model.rbm.MolecularTypePattern;
 import org.vcell.model.rbm.SpeciesPattern;
 import org.vcell.model.rbm.SpeciesPattern.Bond;
+import org.vcell.util.Compare;
 import org.vcell.util.gui.DialogUtils;
 import org.vcell.util.gui.GuiUtils;
 import org.vcell.util.gui.ScrollTable;
 
 import cbit.vcell.biomodel.BioModel;
+import cbit.vcell.biomodel.meta.VCMetaData;
+import cbit.vcell.client.PopupGenerator;
 import cbit.vcell.client.constants.GuiConstants;
 import cbit.vcell.client.desktop.biomodel.RbmDefaultTreeModel.ReactionRuleParticipantLocal;
 import cbit.vcell.desktop.BioModelNode;
@@ -86,6 +92,7 @@ public class ReactionRuleKineticsPropertiesPanel extends DocumentEditorSubPanel 
 	private ReactionRulePropertiesTableModel tableModel = null;
 	private ScrollTable table = null;
 	private JTextField nameTextField = null;
+	private JTextArea annotationTextArea;
 
 	private InternalEventHandler eventHandler = new InternalEventHandler();
 
@@ -127,6 +134,9 @@ public class ReactionRuleKineticsPropertiesPanel extends DocumentEditorSubPanel 
 		}
 		@Override
 		public void mouseExited(MouseEvent e) {
+			if(e.getSource() == annotationTextArea){
+				changeFreeTextAnnotation();
+			}
 		}
 		@Override
 		public void focusGained(FocusEvent e) {
@@ -135,7 +145,10 @@ public class ReactionRuleKineticsPropertiesPanel extends DocumentEditorSubPanel 
 		public void focusLost(FocusEvent e) {
 			if (e.getSource() == nameTextField) {
 				changeName();
-			}			
+			} else if (e.getSource() == annotationTextArea) {
+				changeFreeTextAnnotation();
+			}
+
 		}
 	}
 	
@@ -150,14 +163,46 @@ public class ReactionRuleKineticsPropertiesPanel extends DocumentEditorSubPanel 
 			setName("KineticsTypeTemplatePanel");
 			setLayout(new java.awt.GridBagLayout());
 			
+			Border border = BorderFactory.createLineBorder(Color.gray);
+			Dimension d = new Dimension(50, 500);
+			
+			JPanel generalPanel = new JPanel();		// annotation
+			generalPanel.setBorder(border);
+			generalPanel.setLayout(new GridBagLayout());
+			generalPanel.setPreferredSize(d);
+			
+			int gridy = 0;
+			GridBagConstraints gbc = new java.awt.GridBagConstraints();
+			gbc.gridx = 0; 
+			gbc.gridy = gridy;
+			gbc.insets = new Insets(4, 4, 4, 4);
+			gbc.anchor = GridBagConstraints.FIRST_LINE_END;
+			generalPanel.add(new JLabel("Annotation"), gbc);
+
+			annotationTextArea = new javax.swing.JTextArea("", 1, 30);
+			annotationTextArea.setLineWrap(true);
+			annotationTextArea.setWrapStyleWord(true);
+			annotationTextArea.setEditable(false);
+			javax.swing.JScrollPane jsp = new javax.swing.JScrollPane(annotationTextArea);
+			
+			gbc = new java.awt.GridBagConstraints();
+			gbc.weightx = 1.0;
+			gbc.weighty = 0.1;
+			gbc.gridx = 1; 
+			gbc.gridy = gridy;
+			gbc.anchor = GridBagConstraints.LINE_START;
+			gbc.fill = java.awt.GridBagConstraints.BOTH;
+			gbc.insets = new Insets(4, 4, 4, 4);
+			generalPanel.add(jsp, gbc);
+			
+			// ---------------------------------------------------------------------
 			nameTextField = new JTextField();
 			nameTextField.setEditable(false);
 			nameTextField.addFocusListener(eventHandler);
 			nameTextField.addActionListener(eventHandler);
-
 			
-			int gridy = 0;
-			GridBagConstraints gbc = new java.awt.GridBagConstraints();
+			gridy = 0;
+			gbc = new java.awt.GridBagConstraints();
 			gbc.gridx = 0; 
 			gbc.gridy = gridy;
 			gbc.insets = new java.awt.Insets(0, 4, 4, 4);
@@ -183,7 +228,20 @@ public class ReactionRuleKineticsPropertiesPanel extends DocumentEditorSubPanel 
 			gbc.gridwidth = 3;
 			add(getScrollPaneTable().getEnclosingScrollPane(), gbc);
 			
+			gridy ++;
+			gbc = new java.awt.GridBagConstraints();
+			gbc.gridx = 0; 
+			gbc.gridy = gridy;
+			gbc.fill = java.awt.GridBagConstraints.BOTH;
+			gbc.weightx = 1.0;
+			gbc.weighty = 0.2;
+			gbc.gridwidth = 3;
+			add(generalPanel, gbc);
+			
 			setBackground(Color.white);
+			
+			annotationTextArea.addFocusListener(eventHandler);
+			annotationTextArea.addMouseListener(eventHandler);
 		} catch (java.lang.Throwable ivjExc) {
 			handleException(ivjExc);
 		}
@@ -210,11 +268,8 @@ public class ReactionRuleKineticsPropertiesPanel extends DocumentEditorSubPanel 
 		}
 		return tableModel;
 	}
-
-
 	
 	private void handleException(java.lang.Throwable exception) {
-
 		/* Uncomment the following lines to print uncaught exceptions to stdout */
 		 System.out.println("--------- UNCAUGHT EXCEPTION ---------");
 		 exception.printStackTrace(System.out);
@@ -266,7 +321,6 @@ public class ReactionRuleKineticsPropertiesPanel extends DocumentEditorSubPanel 
 		if (oldValue != null) {
 			oldValue.removePropertyChangeListener(eventHandler);
 		}
-		
 		reactionRule = newValue;
 		if (newValue != null) {
 			newValue.addPropertyChangeListener(eventHandler);
@@ -276,29 +330,38 @@ public class ReactionRuleKineticsPropertiesPanel extends DocumentEditorSubPanel 
 	}
 
 	protected void refreshInterface() {
-		if (reactionRule == null){	// sanity check
+		
+		boolean bNonNullRule = reactionRule != null && bioModel != null;
+		annotationTextArea.setEditable(bNonNullRule);
+		if (bNonNullRule) {
+			VCMetaData vcMetaData = bioModel.getModel().getVcMetaData();
+			annotationTextArea.setText(vcMetaData.getFreeTextAnnotation(reactionRule));
+			nameTextField.setEditable(true);
+			nameTextField.setText(reactionRule.getName());
+		} else {
+			annotationTextArea.setText(null);
 			nameTextField.setEditable(false);
 			nameTextField.setText(null);
-			return;
 		}
-//		ArrayList<ReactionRuleObjectProperty> propertyList = new ArrayList<ReactionRuleObjectProperty>();
-//
-//		if(!(reactionRule instanceof SBEntity)) {
-//			tableModel.setData(propertyList);
-//			return;
-//		}
-//		SBEntity sbEntity = (SBEntity) reactionRule;
-//		if (!(sbEntity instanceof Entity)){
-//			tableModel.setData(propertyList);
-//			return;
-//		}
-//		Entity entity = (Entity) sbEntity;
-//		tableModel.setData(propertyList);
-		
-		nameTextField.setEditable(true);
-		nameTextField.setText(reactionRule.getName());
 	}
-	
 
-	
+	private void changeFreeTextAnnotation() {
+		try{
+			if (reactionRule == null) {
+				return;
+			}
+			// set text from annotationTextField in free text annotation for species in vcMetaData (from model)
+			if(bioModel.getModel() != null && bioModel.getModel().getVcMetaData() != null){
+				VCMetaData vcMetaData = bioModel.getModel().getVcMetaData();
+				String textAreaStr = (annotationTextArea.getText() == null || annotationTextArea.getText().length()==0?null:annotationTextArea.getText());
+				if(!Compare.isEqualOrNull(vcMetaData.getFreeTextAnnotation(reactionRule),textAreaStr)){
+					vcMetaData.setFreeTextAnnotation(reactionRule, textAreaStr);	
+				}
+			}
+		} catch(Exception e){
+			e.printStackTrace(System.out);
+			PopupGenerator.showErrorDialog(this,"Edit Reaction Rule Error\n"+e.getMessage(), e);
+		}
+	}
+
 }
