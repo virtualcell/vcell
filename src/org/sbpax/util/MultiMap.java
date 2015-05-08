ae34b3e6caa0fac7325c1aca9cf6b691455160da
Gerard Weatherby <gweatherby@uchc.edu>
2015-05-08 22:59:24 +0000

    Bug 4147: remove unused methods (clash with new 1.8 API)
    svn path=/vcell/trunk/VCell/; revision=17483

--- src/org/sbpax/util/MultiMap.java
+++ src/org/sbpax/util/MultiMap.java
@@ -14,18 +14,11 @@ package org.sbpax.util;
  *   Maps from a key to a set of values. 
  */
 
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
 public interface MultiMap<K, V> extends Map<K, Set<V>>{
 
-	public void clear();
 	public void add(K key, V value);
-	public void remove(K key, V value);
-	public void removeAll(K key);
-	public Iterator<V> getAll(K key);
-	public boolean contains(K key, V value);
-	
 	
 }
