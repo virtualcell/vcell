0f94f5f4a3314488703bf02ca72ec4689b97cf65
Gerard Weatherby <gweatherby@uchc.edu>
2015-06-10 11:48:52 +0000

    Bug 4261: findTypeParentOfComponent -> SwingUtilities.getAncestorOfClass
    svn path=/vcell/trunk/VCell/; revision=17700

--- test/org/vcell/util/BeansUtilTest.java
+++ test/org/vcell/util/BeansUtilTest.java
@@ -2,7 +2,18 @@ package org.vcell.util;
 
 import static org.junit.Assert.assertTrue;
 
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Frame;
+import java.awt.Window;
 import java.util.concurrent.TimeUnit;
+import java.util.function.BiFunction;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 import org.junit.Test;
 
@@ -29,6 +40,41 @@ public class BeansUtilTest {
 		
 	}
 	
+	/**
+	 * expected behavior of {@link SwingUtilities#getAncestorOfClass(Class, Component)}
+	 * @param method not null
+	 */
+	private void aTest(BiFunction<Class<?>, Component, Container> method) {
+		JFrame jf = new JFrame( );
+		JPanel jp = new JPanel( );
+		jf.add(jp);
+		JButton btn = new JButton();
+		jf.add(btn);
+		assertTrue(method.apply(Frame.class, jp) == jf);
+		assertTrue(method.apply(Window.class, jp) == jf);
+		assertTrue(method.apply(JDialog.class, jp) == null);
+		assertTrue(method.apply(Frame.class, null) == null);
+		assertTrue(method.apply(Frame.class, btn) == jf);
+		assertTrue(method.apply(Window.class, btn) == jf);
+		assertTrue(method.apply(JDialog.class, btn) == null);
+	}
+	
+	
+	/**
+	 * ensure {@link SwingUtilities#getAncestorOfClass(Class, Component)} and
+	 * {@link BeanUtils#findTypeParentOfComponent(Component, Class)} have same behavior
+	 */
+	@Test
+	public void ancestorTest( ) {
+		BiFunction<Class<?>, Component, Container> swingMethod =  
+				(clzz, cmpt) -> { return SwingUtilities.getAncestorOfClass(clzz, cmpt); };
+				@SuppressWarnings("deprecation")
+				BiFunction<Class<?>, Component, Container> buMethod =  
+				(clzz, cmpt) -> { return BeanUtils.findTypeParentOfComponent(cmpt, clzz); };
+				aTest(swingMethod);
+				aTest(buMethod);
+	}
+
 	@Test
 	public void lookupTest( ) {
 		java.util.concurrent.TimeUnit tu = BeanUtils.lookupEnum(TimeUnit.class, 3);
