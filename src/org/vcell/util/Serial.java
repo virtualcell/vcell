9e6682dda82dcd7a77d53d6feb91e682dec18aa8
Gerard Weatherby <gweatherby@uchc.edu>
2015-06-25 13:48:56 +0000

    Bug 4300: Default to Species and Observables checked. Display data in tooltip when user hovers. Add "Copy Entire Row" option when cells in one row are selected.
    svn path=/vcell/trunk/VCell/; revision=17833

--- src/org/vcell/util/Serial.java
+++ src/org/vcell/util/Serial.java
@@ -19,17 +19,20 @@ public class Serial {
 	 * convert SVN $Revision keyword into serial number.
 	 * svn::keywords property for "Revision" should be set for the code that calls this
 	 * @param revision
-	 * @return just numeric part of java serialization
-	 * @throws: {@link IllegalArgumentException}, {@link NumberFormatException}, {@link NullPointerException}
+	 * @return just numeric part of java serialization, or 1 if any exception or revision is null
 	 */
 	public static long serialFromSVNRevision(String revision) {
-		int colonIndex = revision.indexOf(REV_SEP);
-		int termIndex = revision.indexOf(REV_TERM,colonIndex);
-		if (colonIndex > 0 && termIndex > 0) {
-			String numberPart = revision.substring(colonIndex + 1,termIndex).trim( );
-			return Long.parseLong(numberPart);
+		try {
+			int colonIndex = revision.indexOf(REV_SEP);
+			int termIndex = revision.indexOf(REV_TERM,colonIndex);
+			if (colonIndex > 0 && termIndex > 0) {
+				String numberPart = revision.substring(colonIndex + 1,termIndex).trim( );
+				return Long.parseLong(numberPart);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
-		throw new IllegalArgumentException("Revision string " + revision + " does not contain " + REV_SEP + " and/or " + REV_TERM);
+		return 1;
 	}
 	
 
