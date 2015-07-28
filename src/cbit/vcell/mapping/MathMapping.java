d3119bf3974566f3430553102bfc4e902f043a02
Dan Vasilescu <vasilescu@uchc.edu>
2015-07-28 21:35:00 +0000

    Bug 4251 - Create Model From Network: modified code to work with the math mapping refactoring, bug fix
    svn path=/vcell/trunk/VCell/; revision=18072

--- src/cbit/vcell/mapping/MathMapping.java
+++ src/cbit/vcell/mapping/MathMapping.java
@@ -2,6 +2,7 @@ package cbit.vcell.mapping;
 
 import org.vcell.util.Issue;
 
+import cbit.vcell.mapping.SimContextTransformer.SimContextTransformation;
 import cbit.vcell.mapping.SimulationContext.MathMappingCallback;
 import cbit.vcell.math.MathDescription;
 import cbit.vcell.math.MathException;
@@ -16,6 +17,8 @@ public interface MathMapping {
 	MathDescription getMathDescription(MathMappingCallback callback) throws MappingException, MathException, MatrixException, ExpressionException, ModelException;
 
 	MathDescription getMathDescription() throws MappingException, MathException, MatrixException, ExpressionException, ModelException;
+	
+	SimContextTransformation getTransformation();
 
 	Issue[] getIssues();
 
