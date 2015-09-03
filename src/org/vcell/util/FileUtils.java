bfdc4c7a2c2860057ecbd0d468d9568154f5c707
Gerard Weatherby <gweatherby@uchc.edu>
2015-09-03 16:47:12 +0000

    Bug 4037: add deleteRecursive for deploy vcell
    svn path=/vcell/trunk/VCell/; revision=18318

--- src/org/vcell/util/FileUtils.java
+++ src/org/vcell/util/FileUtils.java
@@ -573,5 +573,25 @@ public static String getPrefix(final String filename) {
 private static boolean isSeparator(final char ch) {
     return ch == UNIX_SEPARATOR || ch == WINDOWS_SEPARATOR;
 }
+/**
+ * http://stackoverflow.com/questions/779519/delete-files-recursively-in-java/8685959#8685959
+ * By default File#delete fails for non-empty directories, it works like "rm". 
+ * We need something a little more brutual - this does the equivalent of "rm -r"
+ * @param path Root File Path
+ * @return true iff the file and all sub files/directories have been removed
+ * @throws FileNotFoundException
+ */
+public static boolean deleteRecursive(File path) throws FileNotFoundException{
+    if (!path.exists())  {
+    	throw new FileNotFoundException(path.getAbsolutePath());
+    }
+    boolean ret = true;
+    if (path.isDirectory()){
+        for (File f : path.listFiles()){
+            ret = ret && FileUtils.deleteRecursive(f);
+        }
+    }
+    return ret && path.delete();
+}
 
 }
