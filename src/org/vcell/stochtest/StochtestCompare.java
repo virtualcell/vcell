db0b0800b55f08fec2f1b45436eaf3100bbfa901
Jim Schaff <schaff@uchc.edu>
2015-09-04 21:41:26 +0000

    Bug 4424 : New Stochastic Test Framework - database tables and service
    svn path=/vcell/trunk/VCell/; revision=18331

--- src/org/vcell/stochtest/StochtestCompare.java
+++ src/org/vcell/stochtest/StochtestCompare.java
@@ -2,10 +2,20 @@ package org.vcell.stochtest;
 
 import org.vcell.util.document.KeyValue;
 
+
 public class StochtestCompare {
+	public static enum StochtestCompareStatus {
+		none,
+		waiting,
+		accepted,
+		not_verydifferent,
+		verydifferent,
+		failed
+	}
 	public KeyValue key;
 	public KeyValue stochtestRun1ref;
 	public KeyValue stochtestRun2ref;
+	public StochtestCompareStatus status;
+	public String errmsg;
 	public String results;
-	public String status;
 }
