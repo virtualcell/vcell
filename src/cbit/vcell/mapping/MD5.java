838b541c7c1b2f800f97bce16098de9c3419e26b
Jim Schaff <schaff@uchc.edu>
2015-09-18 22:28:09 +0000

    Bug 4423 : Accommodate local user defined parameters in RulebasedTransformer - pass only fake kinetic parameters to BNG
    svn path=/vcell/trunk/VCell/; revision=18415

--- src/cbit/vcell/mapping/MD5.java
+++ src/cbit/vcell/mapping/MD5.java
@@ -7,17 +7,24 @@ import java.security.NoSuchAlgorithmException;
 public class MD5 {
 	
 	
-public static String md5(String s) {
-	try {
-		MessageDigest m = MessageDigest.getInstance("MD5");
-		m.update(s.getBytes(), 0, s.length());
-		BigInteger i = new BigInteger(1,m.digest());
-		return String.format("%1$032x", i);         
-	} catch (NoSuchAlgorithmException e) {
-		e.printStackTrace();
+	public static String md5(String s) {
+		return md5(s,32);
 	}
-	return null;
-}
+
+	public static String md5(String s, int length) {
+		if (length < 1 || length > 32){
+			throw new RuntimeException("expecting length between 1 and 32 hexidecimal characters");
+		}
+		try {
+			MessageDigest m = MessageDigest.getInstance("MD5");
+			m.update(s.getBytes(), 0, s.length());
+			BigInteger i = new BigInteger(1,m.digest());
+			return String.format("%1$032x", i).substring(0, length);         
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 
 
