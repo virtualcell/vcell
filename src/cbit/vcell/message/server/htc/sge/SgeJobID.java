ac1cc3ccde2f80593a13550b1f3b7d74bc94aeeb
Gerard Weatherby <gweatherby@uchc.edu>
2015-12-29 17:12:40 +0000

    Bug 3785: clear warnings
    svn path=/vcell/trunk/VCell/; revision=18890

--- src/cbit/vcell/message/server/htc/sge/SgeJobID.java
+++ src/cbit/vcell/message/server/htc/sge/SgeJobID.java
@@ -2,13 +2,13 @@ package cbit.vcell.message.server.htc.sge;
 
 import cbit.vcell.message.server.htc.HtcJobID;
 
+@SuppressWarnings("serial")
 public class SgeJobID extends HtcJobID {
-	
+
 	public SgeJobID(String jobID){
 		super(jobID,BatchSystemType.SGE);
-		int jobIdInteger = Integer.parseInt(jobID); // attempt to temporarily parse the jobID for input validation.
 	}
-		
+
 	public long getSgeJobNumber() {
 		return getJobNumber();
 	}
