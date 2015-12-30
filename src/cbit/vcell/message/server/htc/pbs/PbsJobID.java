32cdf0fb991fe93355316dbbc4658398ad446eb6
Gerard Weatherby <gweatherby@uchc.edu>
2015-12-30 22:58:59 +0000

    Bug 4568: compare job numbers and subtypes for HtcJobID equals / compareEqual
    svn path=/vcell/trunk/VCell/; revision=18899

--- src/cbit/vcell/message/server/htc/pbs/PbsJobID.java
+++ src/cbit/vcell/message/server/htc/pbs/PbsJobID.java
@@ -2,14 +2,24 @@ package cbit.vcell.message.server.htc.pbs;
 
 import cbit.vcell.message.server.htc.HtcJobID;
 
+@SuppressWarnings("serial")
 public class PbsJobID extends HtcJobID {
-	
+
 	public PbsJobID(String jobID){
-		super(jobID,BatchSystemType.PBS);
+		super(jobID);
 	}
-	
-	public long getPbsJobNumber(){
-		return getJobNumber();
+
+	@Override
+	public BatchSystemType getBatchSystemType() {
+		return BatchSystemType.PBS;
 	}
-		
+
+	@Override
+	public boolean equals(Object other) {
+		if (other instanceof PbsJobID) {
+			return sameNumberAndServer((HtcJobID) other);
+		}
+		return false;
+	}
+
 }
