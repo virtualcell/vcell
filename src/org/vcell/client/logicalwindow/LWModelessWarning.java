ac003c089041b7ef6524fa0c5ed538da8cbfe0ee
Gerard Weatherby <gweatherby@uchc.edu>
2016-01-04 21:51:22 +0000

    Bug 4362: make class names consistent throughout package
    svn path=/vcell/trunk/VCell/; revision=18915

--- src/org/vcell/client/logicalwindow/LWModelessWarning.java
+++ src/org/vcell/client/logicalwindow/LWModelessWarning.java
@@ -5,7 +5,7 @@ import java.awt.HeadlessException;
 import javax.swing.JOptionPane;
 
 @SuppressWarnings("serial")
-public class LWModelessWarning extends LWOptionPaneWindow {
+public class LWModelessWarning extends LWOptionPaneFrame {
 
 	public LWModelessWarning(LWContainerHandle parent, String message) throws HeadlessException {
 		super(parent,"Warning",new JOptionPane(message,JOptionPane.WARNING_MESSAGE));
