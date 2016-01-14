9a067bf832da9e10e4996503fd1a32f5733acf27
Dan Vasilescu <vasilescu@uchc.edu>
2016-01-14 00:37:55 +0000

    Bug 4587 - Added statistics and details to the network free panel of a rule based application (pass 1: general layout).
    svn path=/vcell/trunk/VCell/; revision=18968

--- src/org/vcell/model/rbm/NetworkFreePanel.java
+++ src/org/vcell/model/rbm/NetworkFreePanel.java
@@ -91,6 +91,15 @@ public class NetworkFreePanel extends JPanel implements ApplicationSpecification
 	private IssueManager fieldIssueManager;
 	private SelectionManager fieldSelectionManager;
 	
+	private JLabel speciesLabel;
+	private JLabel reactionsLabel;
+	private JLabel molecularTypesLabel;
+	private JLabel observablesLabel;
+
+	private static String infoText = "<html>Create a new Rule-Based VCell BioModel. <br>"
+			+ "Existing Molecules, Rules, Species consisting of Molecules and Observables will be transferred to the new BioModel"
+			+ "</html>";
+
 	private JButton createModelButton;
 	
 	private class EventHandler implements FocusListener, ActionListener, PropertyChangeListener {
@@ -142,15 +151,16 @@ public class NetworkFreePanel extends JPanel implements ApplicationSpecification
 	}
 
 	private void initialize() {
-		JPanel leftPanel = new JPanel();
+		speciesLabel = new JLabel("3");
+		reactionsLabel = new JLabel("4");
+		molecularTypesLabel = new JLabel("2");
+		observablesLabel = new JLabel("6");
 
 		Border loweredEtchedBorder = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
 		Border loweredBevelBorder = BorderFactory.createLoweredBevelBorder();
 
-		TitledBorder titleTop = BorderFactory.createTitledBorder(loweredEtchedBorder, " Network-free ");
-		titleTop.setTitleJustification(TitledBorder.LEFT);
-		titleTop.setTitlePosition(TitledBorder.TOP);
-
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new GridBagLayout());
 		setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.gridx = 0;
@@ -159,14 +169,14 @@ public class NetworkFreePanel extends JPanel implements ApplicationSpecification
 		gbc.weighty = 1;
 		gbc.fill = GridBagConstraints.BOTH;
 		gbc.insets = new Insets(5, 3, 2, 1);
-		add(leftPanel, gbc);
+		add(mainPanel, gbc);
 		
-		// ------------------------------------------- Populating the left group box ---------------
 		JPanel top = new JPanel();
-		
+		TitledBorder titleTop = BorderFactory.createTitledBorder(loweredEtchedBorder, " Statistics ");
+		titleTop.setTitleJustification(TitledBorder.LEFT);
+		titleTop.setTitlePosition(TitledBorder.TOP);
 		top.setBorder(titleTop);
-		
-		leftPanel.setLayout(new GridBagLayout());
+		top.setLayout(new GridBagLayout());
 		gbc = new GridBagConstraints();
 		gbc.gridx = 0;
 		gbc.gridy = 0;
@@ -174,16 +184,118 @@ public class NetworkFreePanel extends JPanel implements ApplicationSpecification
 		gbc.weighty = 1.0;
 		gbc.fill = GridBagConstraints.BOTH;
 		gbc.insets = new Insets(5, 2, 2, 3);	//  top, left, bottom, right 
-		leftPanel.add(top, gbc);
+		mainPanel.add(top, gbc);
+
+		JPanel bottom = new JPanel();
+		TitledBorder titleBottom = BorderFactory.createTitledBorder(loweredEtchedBorder, " Network-free ");
+		titleBottom.setTitleJustification(TitledBorder.LEFT);
+		titleBottom.setTitlePosition(TitledBorder.TOP);
+		bottom.setBorder(titleBottom);
+		bottom.setLayout(new GridBagLayout());
+		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.weightx = 1.0;
+		gbc.weighty = 1.0;
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.insets = new Insets(5, 2, 2, 3);
+		mainPanel.add(bottom, gbc);
 		
+		// ------------------------------------------- Populating the top group box ---------------
+		int gridy = 0;
+		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = gridy;
+		gbc.weightx = 0;
+		gbc.anchor = GridBagConstraints.WEST;
+		gbc.insets = new Insets(4, 4, 4, 10);	//  top, left, bottom, right 
+		top.add(new JLabel("Species: "), gbc);
+
+		gbc = new GridBagConstraints();
+		gbc.gridx = 1;
+		gbc.gridy = gridy;
+		gbc.weightx = 0;
+		gbc.anchor = GridBagConstraints.WEST;
+		gbc.insets = new Insets(4, 4, 4, 10);
+		top.add(speciesLabel, gbc);
 
 		gbc = new GridBagConstraints();
 		gbc.gridx = 2;
-		gbc.gridy = 0;
+		gbc.gridy = gridy;
+		gbc.weightx = 1.0;
+		gbc.gridwidth = 2;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
-		gbc.anchor = GridBagConstraints.SOUTHEAST;
 		gbc.insets = new Insets(4, 4, 4, 10);
-		top.add(getCreateModelButton(), gbc);
+		top.add(new JLabel(""), gbc);
+		
+		gridy++;	// -------------------------------------------
+		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = gridy;
+		gbc.weightx = 0;
+		gbc.anchor = GridBagConstraints.WEST;
+		gbc.insets = new Insets(4, 4, 4, 10);
+		top.add(new JLabel("Reactions: "), gbc);
+
+		gbc = new GridBagConstraints();
+		gbc.gridx = 1;
+		gbc.gridy = gridy;
+		gbc.weightx = 0;
+		gbc.anchor = GridBagConstraints.WEST;
+		gbc.insets = new Insets(4, 4, 4, 10);
+		top.add(reactionsLabel, gbc);
+		
+		gridy++;	// -------------------------------------------
+		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = gridy;
+		gbc.weightx = 0;
+		gbc.anchor = GridBagConstraints.WEST;
+		gbc.insets = new Insets(4, 4, 4, 10);
+		top.add(new JLabel("Molecules: "), gbc);
+
+		gbc = new GridBagConstraints();
+		gbc.gridx = 1;
+		gbc.gridy = gridy;
+		gbc.weightx = 0;
+		gbc.anchor = GridBagConstraints.WEST;
+		gbc.insets = new Insets(4, 4, 4, 10);
+		top.add(molecularTypesLabel, gbc);
+		
+		gridy++;	// -------------------------------------------
+		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = gridy;
+		gbc.weightx = 0;
+		gbc.anchor = GridBagConstraints.WEST;
+		gbc.insets = new Insets(4, 4, 4, 10);
+		top.add(new JLabel("Observables: "), gbc);
+
+		gbc = new GridBagConstraints();
+		gbc.gridx = 1;
+		gbc.gridy = gridy;
+		gbc.weightx = 0;
+		gbc.anchor = GridBagConstraints.WEST;
+		gbc.insets = new Insets(4, 4, 4, 10);
+		top.add(observablesLabel, gbc);
+		
+		// ------------------------------------------- Populating the bottom group box ---------------
+		gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.gridwidth = 3;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.anchor = GridBagConstraints.WEST;
+		gbc.insets = new Insets(4, 4, 4, 10);
+		bottom.add(new JLabel(infoText), gbc);
+		
+		gbc = new GridBagConstraints();
+		gbc.gridx = 2;
+		gbc.gridy = 2;
+//		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.anchor = GridBagConstraints.CENTER;
+		gbc.insets = new Insets(4, 4, 4, 10);
+		bottom.add(getCreateModelButton(), gbc);
 		
 		getCreateModelButton().addActionListener(eventHandler);
 
@@ -218,6 +330,30 @@ public class NetworkFreePanel extends JPanel implements ApplicationSpecification
 	}
 	
 	private void refreshInterface() {
+		String speciesText = "";
+		String reactionsText = "";
+		
+		int s1 = 0;
+		int s2 = 0;
+		for(SpeciesContext sc : fieldSimulationContext.getModel().getSpeciesContexts()) {
+			if(sc.hasSpeciesPattern()) {
+				s1++;
+			} else {
+				s2++;
+			}
+		}
+		int s3 = fieldSimulationContext.getModel().getNumSpeciesContexts();
+		speciesText = s3 + "   ( out of which " + s1 + " have species pattern and " + s2 + " do not ).";
+
+		int r1 = fieldSimulationContext.getModel().getNumReactions();
+		int r2 = fieldSimulationContext.getModel().getRbmModelContainer().getReactionRuleList().size();
+		reactionsText = (r1+r2) + "   ( out of which " + r1 + " are simple reactions and " + r2 + " are reaction rules ).";
+		
+		speciesLabel.setText(speciesText);
+		reactionsLabel.setText(reactionsText);
+		molecularTypesLabel.setText(fieldSimulationContext.getModel().getRbmModelContainer().getMolecularTypeList().size()+"");
+		observablesLabel.setText(fieldSimulationContext.getModel().getRbmModelContainer().getObservableList().size()+"");
+		
 		createModelButton.setEnabled(true);
 	}
 	
@@ -225,11 +361,13 @@ public class NetworkFreePanel extends JPanel implements ApplicationSpecification
 	
 	private void createModel() {
 
-		DocumentWindow dw = (DocumentWindow)BeanUtils.findTypeParentOfComponent(this, DocumentWindow.class);
-		BioModelWindowManager bmwm = (BioModelWindowManager)(dw.getTopLevelWindowManager());
-		RequestManager rm = dw.getTopLevelWindowManager().getRequestManager();
-			
-		rm.createBioModelFromApplication(bmwm, "Test", fieldSimulationContext);
+		refreshInterface();
+		
+//		DocumentWindow dw = (DocumentWindow)BeanUtils.findTypeParentOfComponent(this, DocumentWindow.class);
+//		BioModelWindowManager bmwm = (BioModelWindowManager)(dw.getTopLevelWindowManager());
+//		RequestManager rm = dw.getTopLevelWindowManager().getRequestManager();
+//			
+//		rm.createBioModelFromApplication(bmwm, "Test", fieldSimulationContext);
 	}
 
 	@Override		// for testing/debugging purposes
