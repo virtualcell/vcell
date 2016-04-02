1c2045ade56573621743cdc668d1b62975b89e25
Gerard Weatherby <gweatherby@uchc.edu>
2016-04-02 18:48:52 +0000

    Bug 3785: replace deprecated API call, add test, javadoc method
    svn path=/vcell/trunk/VCell/; revision=19463

--- test/org/vcell/util/BeansUtilTest.java
+++ test/org/vcell/util/BeansUtilTest.java
@@ -6,6 +6,8 @@ import java.awt.Component;
 import java.awt.Container;
 import java.awt.Frame;
 import java.awt.Window;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 import java.util.function.BiFunction;
 
@@ -106,6 +108,83 @@ public class BeansUtilTest {
 	@Test
 	public void stackTrace( ) {
 		eenie( );
+	}
+	
+	public void downloadString(ClientTaskStatusSupport taskSupport ) throws MalformedURLException {
+		URL url = new URL("http://code3.cam.uchc.edu/slowdownload.php");
+		System.out.println("starting download ...");
+		String out = BeanUtils.downloadBytes(url, taskSupport);
+		System.out.println(out);
+	}
+	
+	
+//	@Test
+	public void downloadWatch( ) throws MalformedURLException {
+		downloadString(new TaskSupport( ));
+	}
+	
+	@Test
+	public void downloadSilent( ) throws MalformedURLException {
+		downloadString(new SilentTaskSupport( ));
+	}
+	@Test
+	public void downloadNull( ) throws MalformedURLException {
+		downloadString(null);
+	}
+	
+	private static class SilentTaskSupport implements ClientTaskStatusSupport {
+
+		@Override
+		public void setMessage(String message) { }
+
+		@Override
+		public void setProgress(int progress) { }
+
+		@Override
+		public int getProgress() {
+			return 0;
+		}
+
+		@Override
+		public boolean isInterrupted() {
+			return false;
+		}
+
+		@Override public void addProgressDialogListener(ProgressDialogListener progressDialogListener) { }
+		
+	}
+	private static class TaskSupport implements ClientTaskStatusSupport {
+
+		private int progress;
+
+		@Override
+		public void setMessage(String message) {
+			System.out.println("message " + message);
+			
+			
+		}
+
+		@Override
+		public void setProgress(int progress) {
+			this.progress = progress;
+			System.out.println("progress " + progress);
+			
+		}
+
+		@Override
+		public int getProgress() {
+			return progress;
+		}
+
+		@Override
+		public boolean isInterrupted() {
+			return false;
+		}
+
+		@Override
+		public void addProgressDialogListener(ProgressDialogListener progressDialogListener) {
+			System.out.println("adpdl");
+		}
 	}
 
 }
