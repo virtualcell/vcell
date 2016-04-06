9b19c492f3f45db53c552f87084eeb6352dc4a6a
Gerard Weatherby <gweatherby@uchc.edu>
2016-04-06 17:48:49 +0000

    Bug 4570: don't listen for DataAccessException until polling starts
    svn path=/vcell/branches/VCell_5.3/VCell/; revision=19483

--- src/cbit/vcell/client/server/AsynchMessageManager.java
+++ src/cbit/vcell/client/server/AsynchMessageManager.java
@@ -72,7 +72,6 @@ public class AsynchMessageManager implements SimStatusListener, DataAccessExcept
  */
 public AsynchMessageManager(ClientServerManager csm) {
 	this.clientServerManager = csm;
-	DataAccessException.addListener(this);
 }
 /**
  * start polling for connection. Should be called after connect
@@ -83,6 +82,7 @@ public synchronized void startPolling() {
 		bPoll.set(true);
 		if (executorService == null) {
 			executorService = VCellExecutorService.get();
+			DataAccessException.addListener(this);
 		}
 		schedule(pollTime);
 	}
