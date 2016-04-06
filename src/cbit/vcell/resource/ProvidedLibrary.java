c3cd7efe9f6a60c3c2aed3e88d1d04e0c0b04fde
Gerard Weatherby <gweatherby@uchc.edu>
2016-04-06 18:59:21 +0000

    Bug 3785: fix wrong comments
    svn path=/vcell/trunk/VCell/; revision=19486

--- src/cbit/vcell/resource/ProvidedLibrary.java
+++ src/cbit/vcell/resource/ProvidedLibrary.java
@@ -5,7 +5,7 @@ package cbit.vcell.resource;
  * immutable class to map URLs to destination library names
  */
 public class ProvidedLibrary {
-	
+
 	/**
 	 * resource url to extract from, without package name
 	 */
@@ -15,25 +15,25 @@ public class ProvidedLibrary {
 	 */
 	public final String libraryName;
 	/**
-	 * see {@link #isCacheable()} 
+	 * see {@link #isCacheable()}
 	 */
 	private final boolean cacheable;
 	/**
 	 * @param resourceUrl may not be null
 	 * @param libraryName may not be null
-	 * sets cacheable true
+	 * sets cacheable false
 	 */
 	public ProvidedLibrary(String resourceUrl, String libraryName) {
 		assert(resourceUrl != null);
 		assert(libraryName != null);
-		
+
 		this.resourceUrl = resourceUrl;
 		this.libraryName = libraryName;
 		cacheable = false;
 	}
 	/**
 	 * @param libraryName resource and file name are the same may not be null
-	 * sets cacheable false
+	 * sets cacheable true
 	 */
 	public ProvidedLibrary(String libraryName) {
 		assert(libraryName != null);
@@ -41,7 +41,7 @@ public class ProvidedLibrary {
 		this.libraryName = libraryName;
 		cacheable = true;
 	}
-	
+
 	/**
 	 * is library sufficiently unique, for the given OperatingSystem that it can be cached?
 	 * @return true if it is
@@ -67,6 +67,6 @@ public class ProvidedLibrary {
 	public int hashCode() {
 		return resourceUrl.hashCode();
 	}
-	
+
 
 }
