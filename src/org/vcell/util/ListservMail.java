bcd04eb5a4be2a3e76aa14c560688bc14e2eb7d4
Frank Morgan <frm@uchc.edu>
2016-11-14 19:08:25 +0000

    Bug 4961: add db query code, cleanup usage
    svn path=/vcell/trunk/VCell/; revision=20434

--- src/org/vcell/util/ListservMail.java
+++ src/org/vcell/util/ListservMail.java
@@ -6,11 +6,14 @@ import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.util.Enumeration;
-import java.util.Iterator;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Properties;
 import java.util.StringTokenizer;
 import java.util.TreeSet;
@@ -18,14 +21,21 @@ import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.mail.*;
-import javax.mail.internet.MimeUtility;
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.Part;
+import javax.mail.Session;
 
 import com.sun.mail.imap.IMAPFolder;
 import com.sun.mail.imap.IMAPInputStream;
 import com.sun.mail.imap.IMAPMessage;
 import com.sun.mail.imap.IMAPNestedMessage;
 import com.sun.mail.imap.IMAPSSLStore;
+
+import cbit.vcell.messaging.db.SimulationJobTable;
+import cbit.vcell.modeldb.SimulationTable;
+import cbit.vcell.modeldb.UserTable;
 
 
 
@@ -37,23 +47,68 @@ public class ListservMail {
     private static String mailServer = "itowa.uchc.edu";
 	private static String mailUser = "frm";
 	private static String mailFolder = "vcell-users-l bounced";
-	private static File bounceFile = new File("C:/Users/frm/workspace/VCell_trunk/listserv/bouncedEmails.txt");
+	private static String vcellDbSchema = "vcell";
+	private static File rawBounceFile = new File("C:/Users/frm/workspace/VCell_trunk/listserv/rawBouncedEmails.txt");
+	private static File cleanBounceFile = new File("C:/Users/frm/workspace/VCell_trunk/listserv/cleanBouncedEmails.txt");
+	private static File listservEmailFileMinusBounce = new File("C:/Users/frm/workspace/VCell_trunk/listserv/listservEmailFileMinusBounce.txt");
 	private static String subjectSearchForBounced = "VCELL-USERS-L: error report from";
 	
+	private static enum listServeCmds {parsebounce,createemaillist}
+	
 	public static void main(String args[]) throws Exception {
+		
+		if(args.length != 1){
+			usage();
+		}
+		if(args[0].toLowerCase().equals(listServeCmds.parsebounce.name())){
+	        getBouncedEmails(getPassword("email"));
+	        cleanupBouncedMailAddresses();
+		}else if(args[0].toLowerCase().equals(listServeCmds.createemaillist.name())){
+			ArrayList<String> allEmails = queryEmails(getPassword("vcellDB"));
+			TreeSet<String> allDbEmailsSet = new TreeSet<>(allEmails);
+			createListservEmails(allDbEmailsSet);
+		}else{
+			usage();
+		}
+	  }
+
+	private static void createListservEmails(TreeSet<String> allDbEmailsSet) throws Exception{
+		  TreeSet<String> bounceSet = new TreeSet<>();
+		  try (BufferedReader br = new BufferedReader(new FileReader(cleanBounceFile));
+				  BufferedWriter bw = new BufferedWriter(new FileWriter(listservEmailFileMinusBounce))){
+			  String line = null;
+			  while((line = br.readLine()) != null){
+				  bounceSet.add(line);
+			  }
+			  TreeSet<String> filteredEmails = (TreeSet<String>)allDbEmailsSet.clone();
+			  filteredEmails.removeAll(bounceSet);
+			  for(String s:filteredEmails){
+				  bw.write(s+"\n");
+			  }
+		  }
+
+	}
+	private static String getPassword(String descr) throws IOException{
+	    System.out.print("Enter "+descr+" password: ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String password = br.readLine();
+        return password;
+	}
+	private static void usage(){
+		System.out.println("Usage: ListServMail {"+listServeCmds.parsebounce.name()+","+listServeCmds.createemaillist.name()+"}");
+		System.exit(1);
+	}
+	private static void getBouncedEmails(String emailPassword) throws Exception{
 		IMAPSSLStore store = null;
 		IMAPFolder[] inboxs = null;
 		BufferedWriter bw = null;
 		try{
-		    System.out.print("Please enter your passwd: ");
-	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	        String password = br.readLine();
 	
 		    // connect to mail server using imaps protocol
 		    Properties properties = System.getProperties();
 		    Session session = Session.getDefaultInstance(properties);
 		    store = (IMAPSSLStore) session.getStore("imaps");// Use this instead of pop3s to access non 'Inbox' folders
-		    store.connect(mailServer, mailUser, password);
+		    store.connect(mailServer, mailUser, emailPassword);
 		    inboxs = new IMAPFolder[]  {(IMAPFolder) store.getFolder(mailFolder)};
 		    inboxs[0].open(Folder.READ_ONLY);
 	
@@ -80,7 +135,7 @@ public class ListservMail {
 		      }
 		    
 		      System.out.println("Found "+bouncedEMails.size()+" bounced emails");
-		      bw = new BufferedWriter(new FileWriter(bounceFile));
+		      bw = new BufferedWriter(new FileWriter(rawBounceFile));
 		      for(String s:bouncedEMails){
 		    	bw.write(s+"\n");  
 		      }
@@ -96,8 +151,67 @@ public class ListservMail {
 			
 			if(store != null){try{store.close();}catch(Exception e){e.printStackTrace();}}
 		}
-	  }
 
+	}
+	
+	public static ArrayList<String> queryEmails(String dbPassword) throws Exception{
+		String driverName = "oracle.jdbc.driver.OracleDriver";
+		String host = "vcell-db.cam.uchc.edu";
+		String db = "vcelldborcl";
+		String connectURL = "jdbc:oracle:thin:@" + host + ":1521:" + db;
+		
+		ArrayList<String> results = new ArrayList<>();
+		Connection con = null;
+		try{
+			String sql =
+				"SELECT " +
+					"DISTINCT("+UserTable.table.email.getQualifiedColName()+")" +
+				" FROM "+
+					UserTable.table.getTableName()+","+SimulationJobTable.table.getTableName()+","+SimulationTable.table.getTableName()+" " +
+				" WHERE "+
+					SimulationJobTable.table.startDate.getQualifiedColName()+" >= (SYSDATE - (5*365)) "+
+				" AND "+
+					SimulationJobTable.table.simRef.getQualifiedColName() + " = " + SimulationTable.table.id.getQualifiedColName() +
+				" AND " +
+					UserTable.table.id.getQualifiedColName() + " = " + SimulationTable.table.ownerRef.getQualifiedColName() +
+	//			" AND " +
+	//				"LOWER("+UserTable.table.notify.getQualifiedColName()+"='on')" +
+				" AND " +
+					UserTable.table.email.getQualifiedColName()+ " NOT LIKE '%@qq.com' " +//these looked like multiple robot generated dates so exclude
+				" AND "+
+					UserTable.table.email.getQualifiedColName()+ " LIKE '%@%' " +// at least make sure the email has an @ sign, reject all others
+				" ORDER BY " + UserTable.table.email.getUnqualifiedColName();
+							
+			Class.forName(driverName);
+			con = java.sql.DriverManager.getConnection(connectURL, vcellDbSchema, dbPassword);
+			con.setAutoCommit(false);
+			con.setReadOnly(true);
+			Statement stmt = con.createStatement();
+			ResultSet rset = stmt.executeQuery(sql);
+			while(rset.next()){
+				results.add(rset.getString(1));
+				System.out.println(results.get(results.size()-1));
+			}
+			rset.close();
+			stmt.close();
+			return results;
+		}finally{
+			if(con != null){try{con.close();}catch(Exception e){e.printStackTrace();}}
+		}
+//		select distinct(vc_userinfo.email)
+//		from
+//		vc_userinfo,
+//		vc_simulationjob,vc_simulation
+//		where
+//		vc_simulationjob.startdate >= to_date('01-NOV-2011','DD-MON-YY') and
+//		vc_simulationjob.simref=vc_simulation.id and
+//		vc_userinfo.id= vc_simulation.ownerref and
+//		--lower(vc_userinfo.notify)='on' and
+//		vc_userinfo.email not like '%@qq.com' and
+//		vc_userinfo.email like'%@%'
+//		order by email;
+	}
+	
 	private static void readMessage(IMAPMessage message,int partNum,int size) throws Exception{
   	  Object obj = message.getContent();
   	  if(obj instanceof Multipart){
@@ -166,51 +280,69 @@ public class ListservMail {
 	  private static final String mailto = "mailto:";
 	  private static final String ptoken = "<p>";
 	  private static final String brtoken = "<br>";
-	  public static void cleanupMailAddresses(File bouncefile,File sentFile) throws Exception{
-		  TreeSet<String> bounceSet = new TreeSet<>();
-		  BufferedReader br = new BufferedReader(new FileReader(bouncefile));
-		  String line = null;
-		  while((line = br.readLine()) != null){
-			  if(line.toLowerCase().contains("vcell-users-l") ||
-				 line.toLowerCase().contains("listserv.uconn.edu")){
-				  System.out.println("--skip: "+line);
-				  continue;
+	  public static void cleanupBouncedMailAddresses(/*File rawBouncefile,File currentListservEmailList*/) throws Exception{
+		  BufferedReader br = null;
+		  BufferedWriter bw = null;
+		  try{
+			  TreeSet<String> bounceSet = new TreeSet<>();
+			  br = new BufferedReader(new FileReader(rawBounceFile));
+			  String line = null;
+			  while((line = br.readLine()) != null){
+				  if(line.toLowerCase().contains("vcell-users-l") ||
+					 line.toLowerCase().contains("listserv.uconn.edu")){
+					  System.out.println("--skip: "+line);
+					  continue;
+				  }
+				  if(line.toLowerCase().startsWith(ptoken)){
+					  line = line.substring(ptoken.length());
+				  }
+				  if(line.toLowerCase().endsWith(brtoken)){
+					  line = line.substring(0, line.length()-brtoken.length());
+				  }
+				  StringTokenizer st = new StringTokenizer(line, "<>@");
+				  if(st.countTokens() != 2){
+					  System.out.println("-----bad: "+line);
+					  continue;
+				  }
+				  String user = st.nextToken();
+				  String mailserver = st.nextToken();
+				  if(user.toLowerCase().startsWith(mailto)){
+					  user = user.substring(mailto.length());
+				  }
+				  user = user.toLowerCase();
+				  mailserver = mailserver.toLowerCase();
+				  String address = user+"@"+mailserver;
+				  bounceSet.add(address);
 			  }
-			  if(line.toLowerCase().startsWith(ptoken)){
-				  line = line.substring(ptoken.length());
-			  }
-			  if(line.toLowerCase().endsWith(brtoken)){
-				  line = line.substring(0, line.length()-brtoken.length());
-			  }
-			  StringTokenizer st = new StringTokenizer(line, "<>@");
-			  if(st.countTokens() != 2){
-				  System.out.println("-----bad: "+line);
-				  continue;
-			  }
-			  String user = st.nextToken();
-			  String mailserver = st.nextToken();
-			  if(user.toLowerCase().startsWith(mailto)){
-				  user = user.substring(mailto.length());
-			  }
-			  user = user.toLowerCase();
-			  mailserver = mailserver.toLowerCase();
-			  String address = user+"@"+mailserver;
-			  bounceSet.add(address);
-		  }
-		  br.close();
-		  
-		  TreeSet<String> sentSet = new TreeSet<>();
-		  br = new BufferedReader(new FileReader(sentFile));
-		  while((line = br.readLine()) != null){
-			  sentSet.add(line.toLowerCase());
-		  }
-		  
-		  System.out.println("Bounceset size = "+bounceSet.size());
-		  TreeSet copyBounceSet = (TreeSet)bounceSet.clone();
-		  copyBounceSet.removeAll(sentSet);
-		  Iterator<String> iter = copyBounceSet.iterator();
-		  while(iter.hasNext()){
-			  System.out.println("leftover: "+iter.next());
+			  br.close();
+		      bw = new BufferedWriter(new FileWriter(cleanBounceFile));
+		      for(String s:bounceSet){
+		    	bw.write(s+"\n");  
+		      }
+			  
+//			  TreeSet<String> sentSet = new TreeSet<>();
+//			  br = new BufferedReader(new FileReader(currentListservEmailList));
+//			  while((line = br.readLine()) != null){
+//				  sentSet.add(line.toLowerCase());
+//			  }
+//			  br.close();
+//			  
+//			  System.out.println("Bounceset size = "+bounceSet.size());
+//			  TreeSet copyBounceSet = (TreeSet)bounceSet.clone();
+//			  copyBounceSet.removeAll(sentSet);
+//			  Iterator<String> iter = copyBounceSet.iterator();
+//			  while(iter.hasNext()){
+//				  System.out.println("leftover: "+iter.next());
+//			  }
+//			  
+//			  sentSet.removeAll(bounceSet);
+//		      bw = new BufferedWriter(new FileWriter(cleanBounceFile));
+//		      for(String s:sentSet){
+//		    	bw.write(s+"\n");  
+//		      }
+		  }finally{
+			  if(br != null){try{br.close();}catch(Exception e){e.printStackTrace();}}
+			  if(bw != null){try{bw.close();}catch(Exception e){e.printStackTrace();}}
 		  }
 	  }
 }
