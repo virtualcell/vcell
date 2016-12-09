984ecc8d9cd79c7f901442a3d04c6af1a941ae22
Jim Schaff <schaff@uchc.edu>
2016-12-09 19:46:31 +0000

    Bug 4978 - Patch to Beta of Bug 4925 - EBChombo: parallel sim is not using parallel dir to write output
    svn path=/vcell/branches/VCell_6.1/VCell/; revision=20502

---
+++ src/cbit/vcell/solver/server/SolverFactory.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (C) 1999-2011 University of Connecticut Health Center
+ *
+ * Licensed under the MIT License (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *  http://www.opensource.org/licenses/mit-license.php
+ */
+
+package cbit.vcell.solver.server;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.vcell.solver.nfsim.NFSimSolver;
+import org.vcell.solver.smoldyn.SmoldynSolver;
+import org.vcell.util.SessionLog;
+
+import cbit.util.xml.XmlUtil;
+import cbit.vcell.messaging.server.SimulationTask;
+import cbit.vcell.solver.SolverDescription;
+import cbit.vcell.solver.SolverException;
+import cbit.vcell.solver.ode.AdamsMoultonFiveSolver;
+import cbit.vcell.solver.ode.CVodeSolverStandalone;
+import cbit.vcell.solver.ode.ForwardEulerSolver;
+import cbit.vcell.solver.ode.IDASolverStandalone;
+import cbit.vcell.solver.ode.RungeKuttaFehlbergSolver;
+import cbit.vcell.solver.ode.RungeKuttaFourSolver;
+import cbit.vcell.solver.ode.RungeKuttaTwoSolver;
+import cbit.vcell.solver.stoch.GibsonSolver;
+import cbit.vcell.solver.stoch.HybridSolver;
+import cbit.vcell.solvers.CombinedSundialsSolver;
+import cbit.vcell.solvers.FVSolverStandalone;
+import cbit.vcell.solvers.MovingBoundarySolver;
+import cbit.vcell.xml.XmlHelper;
+import edu.uchc.connjur.wb.ExecutionTrace;
+
+/**
+ * The Abstract definition for the solver factory that creates
+ * solver-related objects.
+ * Creation date: (8/24/2000 11:23:26 PM)
+ * @author: John Wagner
+ */
+public class SolverFactory {
+/**
+ * create Solvers according to the solver description.
+ */
+	
+	private interface Maker {
+		Solver make(SimulationTask task, File userDir, File parallelWorkingDirectory, SessionLog log, boolean messaging) throws SolverException;
+	}
+	
+	private static final Map<SolverDescription,Maker> FACTORY = new HashMap<SolverDescription, SolverFactory.Maker>( );
+	static {
+		{  //finite volume Java solvers
+			Maker fv = (t,d,pwd,sl,m) -> new FVSolverStandalone(t, d, sl, m); 
+			FACTORY.put(SolverDescription.FiniteVolumeStandalone, fv); 
+			FACTORY.put(SolverDescription.FiniteVolume, fv); 
+			FACTORY.put(SolverDescription.SundialsPDE, fv); 
+			Maker chomboMaker = new Maker(){
+				@Override
+				public Solver make(SimulationTask t, File userDir, File parallelWorkingDir, SessionLog sl, boolean m) throws SolverException {
+					if (t.getSimulation().getSolverTaskDescription().isParallel()){
+						File workingDir = parallelWorkingDir;
+						File destinationDirectory = userDir;
+						return new FVSolverStandalone(t,workingDir,destinationDirectory,sl,m);
+					}else{
+						File workingDir = userDir;
+						File destinationDirectory = userDir;
+						return new FVSolverStandalone(t,workingDir,destinationDirectory,sl,m);
+					}
+				}
+			};
+			FACTORY.put(SolverDescription.Chombo, chomboMaker); 
+		}
+
+		FACTORY.put(SolverDescription.ForwardEuler, (t,d,pwd,sl,m) -> new ForwardEulerSolver(t, d, sl) );
+		FACTORY.put(SolverDescription.RungeKutta2, (t,d,pwd,sl,m) -> new RungeKuttaTwoSolver(t, d, sl) );
+		FACTORY.put(SolverDescription.RungeKutta4, (t,d,pwd,sl,m) -> new RungeKuttaFourSolver(t, d, sl) );
+		FACTORY.put(SolverDescription.AdamsMoulton, (t,d,pwd,sl,m) -> new AdamsMoultonFiveSolver(t, d, sl) );
+		FACTORY.put(SolverDescription.RungeKuttaFehlberg, (t,d,pwd,sl,m) -> new RungeKuttaFehlbergSolver(t, d, sl) );
+		FACTORY.put(SolverDescription.IDA, (t,d,pwd,sl,m) -> new IDASolverStandalone(t, d, sl,m) ); 
+		FACTORY.put(SolverDescription.CVODE, (t,d,pwd,sl,m) -> new CVodeSolverStandalone (t, d, sl,m) ); 
+		FACTORY.put(SolverDescription.CombinedSundials, (t,d,pd,sl,m) -> new CombinedSundialsSolver(t, d, sl,m) ); 
+		FACTORY.put(SolverDescription.StochGibson, (t,d,pwd,sl,m) -> new GibsonSolver(t, d, sl,m) ); 
+		FACTORY.put(SolverDescription.HybridEuler, (t,d,pwd,sl,m) -> new HybridSolver(t, d, sl,HybridSolver.EMIntegrator,m) ); 
+		FACTORY.put(SolverDescription.HybridMilstein, (t,d,pwd,sl,m) -> new HybridSolver(t, d, sl,HybridSolver.MilsteinIntegrator,m) ); 
+		FACTORY.put(SolverDescription.HybridMilAdaptive , (t,d,pwd,sl,m) -> new HybridSolver(t, d, sl,HybridSolver.AdaptiveMilsteinIntegrator,m) ); 
+		FACTORY.put(SolverDescription.Smoldyn, (t,d,pwd,sl,m) -> new SmoldynSolver(t, d, sl,m) ); 
+		FACTORY.put(SolverDescription.NFSim, (t,d,pwd,sl,m) -> new NFSimSolver(t, d, sl,m) ); 
+		FACTORY.put(SolverDescription.MovingBoundary, (t,d,pwd,sl,m) -> new MovingBoundarySolver(t, d, sl,m) ); 
+	}
+	
+public static Solver createSolver(SessionLog sessionLog, File userDir, SimulationTask simTask, boolean bMessaging) throws SolverException {
+	{
+		return createSolver(sessionLog, userDir, null, simTask, bMessaging);
+	}
+}
+
+public static Solver createSolver(SessionLog sessionLog, File userDir, File parallelDir, SimulationTask simTask, boolean bMessaging) throws SolverException {
+	SolverDescription solverDescription = simTask.getSimulationJob().getSimulation().getSolverTaskDescription().getSolverDescription();
+
+	File simTaskFile = new File(userDir,simTask.getSimulationJobID()+"_"+simTask.getTaskID()+".simtask.xml");
+	if (userDir.exists() && !simTaskFile.exists()){
+		try {
+			String simTaskXmlText = XmlHelper.simTaskToXML(simTask);
+			XmlUtil.writeXMLStringToFile(simTaskXmlText, simTaskFile.toString(), true);
+		}catch (Exception e){
+			e.printStackTrace(System.out);
+			throw new SolverException("unable to write SimulationTask file");
+		}
+	}
+
+	if (solverDescription == null) {
+		throw new IllegalArgumentException("SolverDescription cannot be null");
+	}
+	Maker maker = FACTORY.get(solverDescription);
+	if (maker != null) {
+		Solver s = maker.make(simTask, userDir, parallelDir, sessionLog,bMessaging); 
+		sessionLog.print("LocalSolverFactory.createSolver() returning " + solverDescription + " solver " + ExecutionTrace.justClassName(s));
+		return s;
+	}
+	throw new SolverException("Unknown solver: " + solverDescription);
+}
+}
---
+++ src/cbit/vcell/solvers/AbstractSolver.java
@@ -0,0 +1,377 @@
+/*
+ * Copyright (C) 1999-2011 University of Connecticut Health Center
+ *
+ * Licensed under the MIT License (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *  http://www.opensource.org/licenses/mit-license.php
+ */
+
+package cbit.vcell.solvers;
+
+
+import java.io.File;
+
+import org.vcell.util.ConfigurationException;
+import org.vcell.util.SessionLog;
+
+import cbit.vcell.math.Constant;
+import cbit.vcell.math.MathUtilities;
+import cbit.vcell.math.VolVariable;
+import cbit.vcell.messaging.server.SimulationTask;
+import cbit.vcell.mongodb.VCMongoMessage;
+import cbit.vcell.parser.Expression;
+import cbit.vcell.parser.ExpressionException;
+import cbit.vcell.simdata.SimDataConstants;
+import cbit.vcell.solver.SimulationJob;
+import cbit.vcell.solver.SimulationSymbolTable;
+import cbit.vcell.solver.SolverException;
+import cbit.vcell.solver.ode.ODEStateVariable;
+import cbit.vcell.solver.ode.SensStateVariable;
+import cbit.vcell.solver.ode.SensVariable;
+import cbit.vcell.solver.ode.StateVariable;
+import cbit.vcell.solver.server.SimulationMessage;
+import cbit.vcell.solver.server.Solver;
+import cbit.vcell.solver.server.SolverEvent;
+import cbit.vcell.solver.server.SolverListener;
+import cbit.vcell.solver.server.SolverStatus;
+/**
+ * Insert the type's description here.
+ * Creation date: (6/26/2001 2:48:23 PM)
+ * @author: Ion Moraru
+ */
+public abstract class AbstractSolver implements Solver, SimDataConstants {
+	private javax.swing.event.EventListenerList listenerList = new javax.swing.event.EventListenerList();
+	private org.vcell.util.SessionLog fieldSessionLog = null;
+	private SolverStatus fieldSolverStatus = new SolverStatus(SolverStatus.SOLVER_READY, SimulationMessage.MESSAGE_SOLVER_READY);
+	private File saveDirectory = null;
+	private boolean saveEnabled = true;
+	protected final SimulationTask simTask;
+	public static boolean bMakeUserDirs = true;
+
+/**
+ * AbstractSolver constructor comment.
+ */
+public AbstractSolver(SimulationTask simTask, File directory, SessionLog sessionLog) throws SolverException {
+
+	this.simTask = simTask;
+	this.fieldSessionLog = sessionLog;
+	if (!directory.exists() && !simTask.getSimulation().getSolverTaskDescription().isParallel()){
+		if (bMakeUserDirs && !directory.mkdirs()){
+			String msg = "could not create directory "+directory;
+			sessionLog.alert(msg);
+			throw new ConfigurationException(msg);
+		}
+	}		 
+	this.saveDirectory = directory;
+//	ArrayList<Issue> issueList = new ArrayList<Issue>();
+//	IssueContext issueContext = new IssueContext(ContextType.Simulation,simTask.getSimulation(),null);
+//	simTask.getSimulation().getMathDescription().gatherIssues(issueContext,issueList);
+//	simTask.getSimulation().gatherIssues(issueContext,issueList);
+//	for (Issue issue : issueList) {
+//		if (issue.getSeverity() == Issue.SEVERITY_ERROR){
+//			throw new SolverException(issue.getMessage());
+//		}
+//	}
+}
+
+
+/**
+ * Insert the method's description here.
+ * Creation date: (3/29/2001 5:18:16 PM)
+ * @param listener cbit.vcell.solver.SolverListener
+ */
+public synchronized void addSolverListener(cbit.vcell.solver.server.SolverListener listener) {
+	listenerList.add(cbit.vcell.solver.server.SolverListener.class, listener);
+}
+
+
+/**
+ * Method to support listener events.
+ */
+protected void fireSolverAborted(SimulationMessage message) {
+	// Create event
+	SolverEvent event = new SolverEvent(this, SolverEvent.SOLVER_ABORTED, message, getProgress(), getCurrentTime(), message.getHtcJobId());
+	VCMongoMessage.sendSolverEvent(event);
+	// Guaranteed to return a non-null array
+	Object[] listeners = listenerList.getListenerList();
+	// Process the listeners last to first, notifying
+	// those that are interested in this event
+	for (int i = listeners.length-2; i>=0; i-=2) {
+	    if (listeners[i]==SolverListener.class) {
+		((SolverListener)listeners[i+1]).solverAborted(event);
+	    }	       
+	}
+}
+
+
+/**
+ * Method to support listener events.
+ */
+protected void fireSolverFinished() {
+	// Create event
+	SolverEvent event = new SolverEvent(this, SolverEvent.SOLVER_FINISHED, SimulationMessage.MESSAGE_SOLVEREVENT_FINISHED, getProgress(), getCurrentTime(), null);
+	VCMongoMessage.sendSolverEvent(event);
+	// Guaranteed to return a non-null array
+	Object[] listeners = listenerList.getListenerList();
+	// Process the listeners last to first, notifying
+	// those that are interested in this event
+	for (int i = listeners.length-2; i>=0; i-=2) {
+	    if (listeners[i]==SolverListener.class) {
+		((SolverListener)listeners[i+1]).solverFinished(event);
+	    }	       
+	}
+}
+
+
+/**
+ * Method to support listener events.
+ */
+protected void fireSolverPrinted(double timepoint) {
+	// Create event
+	SolverEvent event = new SolverEvent(this, SolverEvent.SOLVER_PRINTED, SimulationMessage.solverPrinted(timepoint), getProgress(), timepoint, null);
+	VCMongoMessage.sendSolverEvent(event);
+	// Guaranteed to return a non-null array
+	Object[] listeners = listenerList.getListenerList();
+	// Process the listeners last to first, notifying
+	// those that are interested in this event
+	for (int i = listeners.length-2; i>=0; i-=2) {
+	    if (listeners[i]==SolverListener.class) {
+		((SolverListener)listeners[i+1]).solverPrinted(event);
+	    }	       
+	}
+}
+
+
+/**
+ * Method to support listener events.
+ */
+protected void fireSolverProgress(double progress) {
+	// Create event
+	SolverEvent event = new SolverEvent(this, SolverEvent.SOLVER_PROGRESS, SimulationMessage.solverProgress(progress), progress, getCurrentTime(), null);
+	VCMongoMessage.sendSolverEvent(event);
+	// Guaranteed to return a non-null array
+	Object[] listeners = listenerList.getListenerList();
+	// Process the listeners last to first, notifying
+	// those that are interested in this event
+	for (int i = listeners.length-2; i>=0; i-=2) {
+	    if (listeners[i]==SolverListener.class) {
+		((SolverListener)listeners[i+1]).solverProgress(event);
+	    }	       
+	}
+}
+
+
+/**
+ * Method to support listener events.
+ */
+protected void fireSolverStarting(SimulationMessage message) {
+	// Create event
+	SolverEvent event = new SolverEvent(this, SolverEvent.SOLVER_STARTING, message, 0, 0, message.getHtcJobId());
+	VCMongoMessage.sendSolverEvent(event);
+	// Guaranteed to return a non-null array
+	Object[] listeners = listenerList.getListenerList();
+	// Process the listeners last to first, notifying
+	// those that are interested in this event
+	for (int i = listeners.length-2; i>=0; i-=2) {
+	    if (listeners[i]==SolverListener.class) {
+		((SolverListener)listeners[i+1]).solverStarting(event);
+	    }	       
+	}
+}
+
+
+/**
+ * Method to support listener events.
+ */
+protected void fireSolverStopped() {
+	// Create event
+	SolverEvent event = new SolverEvent(this, SolverEvent.SOLVER_STOPPED, SimulationMessage.solverStopped("stopped by user"), getProgress(), getCurrentTime(), null);
+	VCMongoMessage.sendSolverEvent(event);
+	// Guaranteed to return a non-null array
+	Object[] listeners = listenerList.getListenerList();
+	// Process the listeners last to first, notifying
+	// those that are interested in this event
+	for (int i = listeners.length-2; i>=0; i-=2) {
+	    if (listeners[i]==SolverListener.class) {
+		((SolverListener)listeners[i+1]).solverStopped(event);
+	    }	       
+	}
+}
+
+
+/**
+ * get name for solver files, without extension 
+ */
+protected final String getBaseName() {
+	return new File(getSaveDirectory(), simTask.getSimulationJob().getSimulationJobID()).getPath();
+}
+
+
+/**
+ * Insert the method's description here.
+ * Creation date: (6/28/01 2:51:46 PM)
+ * @return double
+ */
+public abstract double getCurrentTime();
+protected abstract void initialize() throws SolverException;
+
+/**
+ * This method was created by a SmartGuide.
+ * @return double[]
+ * @param identifier java.lang.String
+ */
+public Expression getFunctionSensitivity(Expression funcExpr, Constant constant, StateVariable stateVariables[]) throws ExpressionException {
+	if (stateVariables==null || stateVariables.length == 0) {
+		return null;
+	}
+	// this uses the chain rule
+	//   d F(x)   del F(x)        del F(x)   del xi
+	//   ------ = -------- + Sum (-------- * ------)
+	//    d k      del k      i    del xi    del k
+	//            explicit     via state variables
+	//            dependence
+
+	//
+	// collect the explicit term
+	//
+	SimulationSymbolTable simSymbolTable = simTask.getSimulationJob().getSimulationSymbolTable();
+	
+	Expression sensFuncExp = funcExpr.differentiate(constant.getName());
+	sensFuncExp.bindExpression(simSymbolTable);
+	sensFuncExp = sensFuncExp.flatten();
+	
+	for (int i = 0; i < stateVariables.length; i++) {
+		if (stateVariables[i] instanceof ODEStateVariable) {
+			ODEStateVariable sv = (ODEStateVariable) stateVariables[i];
+			VolVariable volVar = (VolVariable) sv.getVariable();
+			//
+			// get corresponding sensitivity state variable
+			//
+			SensStateVariable sensStateVariable = null;
+			for (int j = 0; j < stateVariables.length; j++){
+				if (stateVariables[j] instanceof SensStateVariable && 
+					((SensVariable)((SensStateVariable)stateVariables[j]).getVariable()).getVolVariable().getName().equals(volVar.getName()) &&
+					((SensStateVariable)stateVariables[j]).getParameter().getName().equals(constant.getName())){
+					sensStateVariable = (SensStateVariable)stateVariables[j];
+				}	
+			}
+			if (sensStateVariable == null){
+				System.out.println("sens of "+volVar.getName()+" wrt "+constant.getName()+" is not found");
+				return null;
+			}
+
+			//
+			// get coefficient of proportionality   (e.g.  A = total + b*B + c*C ... gives dA/dK = b*dB/dK + c*dC/dK)
+			//
+			Expression coeffExpression = funcExpr.differentiate(volVar.getName());
+			coeffExpression.bindExpression(simSymbolTable);
+			coeffExpression = MathUtilities.substituteFunctions(coeffExpression,simSymbolTable);
+			coeffExpression.bindExpression(simSymbolTable);
+			coeffExpression = coeffExpression.flatten();
+			sensFuncExp = Expression.add(sensFuncExp,Expression.mult(coeffExpression,new Expression(sensStateVariable.getVariable().getName())));
+		}
+	}
+
+	return new Expression(sensFuncExp);
+}
+
+
+/**
+ * Insert the method's description here.
+ * Creation date: (6/26/2001 3:42:59 PM)
+ * @return int
+ */
+public int getJobIndex() {
+	return getSimulationJob().getJobIndex();
+}
+
+/**
+ * Insert the method's description here.
+ * Creation date: (6/26/2001 3:42:59 PM)
+ * @return int
+ */
+public int getTaskID() {
+	return simTask.getTaskID();
+}
+
+
+/**
+ * Insert the method's description here.
+ * Creation date: (6/28/01 2:36:57 PM)
+ * @return double
+ */
+public abstract double getProgress();
+
+
+/**
+ * Insert the method's description here.
+ * Creation date: (6/27/2001 10:52:44 AM)
+ * @return java.io.File
+ */
+protected final java.io.File getSaveDirectory() {
+	return saveDirectory;
+}
+
+
+/**
+ * This method was created in VisualAge.
+ * @return cbit.vcell.math.MathDescription
+ */
+protected final SessionLog getSessionLog() {
+	return (fieldSessionLog);
+}
+
+/**
+ * Gets the running property (boolean) value.
+ * @return The running property value.
+ */
+public final SolverStatus getSolverStatus() {
+	return (fieldSolverStatus);
+}
+
+
+/**
+ * Insert the method's description here.
+ * Creation date: (6/27/2001 10:53:53 AM)
+ * @return boolean
+ */
+public final boolean isSaveEnabled() {
+	return saveEnabled;
+}
+
+
+/**
+ * Insert the method's description here.
+ * Creation date: (3/29/2001 5:18:16 PM)
+ * @param listener cbit.vcell.solver.SolverListener
+ */
+public synchronized void removeSolverListener(SolverListener listener) {
+	listenerList.remove(SolverListener.class, listener);
+}
+
+
+/**
+ * Gets the running property (boolean) value.
+ * @return The running property value.
+ */
+protected final void setSolverStatus(SolverStatus solverStatus) {
+	fieldSolverStatus = solverStatus;
+}
+
+
+public SimulationJob getSimulationJob() {
+	return simTask.getSimulationJob();
+}
+
+
+/**
+ * no-op
+ */
+@Override
+public void setUnixMode() {
+	
+}
+
+}
