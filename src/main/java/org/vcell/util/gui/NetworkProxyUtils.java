ece3153aceca4109023ec05a503775b9ded3daef
Jim Schaff <schaff@uchc.edu>
2017-05-08 12:47:07 +0000

    svn path=/vcell/branches/VCell_6.2_maven/VCell/; revision=20951

--- src/main/java/org/vcell/util/gui/NetworkProxyUtils.java
+++ src/main/java/org/vcell/util/gui/NetworkProxyUtils.java
@@ -2,67 +2,12 @@ package org.vcell.util.gui;
 
 import java.awt.Component;
 import java.rmi.server.RMISocketFactory;
-import java.util.StringTokenizer;
-import java.util.prefs.Preferences;
 
 import org.vcell.util.Compare;
-import org.vcell.util.UtilCancelException;
 
-import cbit.vcell.message.server.bootstrap.client.RMIVCellConnectionFactory;
 import cbit.vcell.resource.ResourceUtil;
 
 public class NetworkProxyUtils {
-	public static final String prefProxyType = "proxyType";
-	public static final String prefProxyHost = "proxyHost";
-	public static final String prefProxyPort = "proxyPort";
-	private static final String PROXY_FORMAT = "[http,socks]:proxyHost:portNumber";
-	public static void setProxyPrefs(Component requester) throws UtilCancelException{
-		Preferences prefs = Preferences.userNodeForPackage(RMIVCellConnectionFactory.class);
-		String proxyType = prefs.get(prefProxyType,prefProxyType);
-		String proxyHost = prefs.get(prefProxyHost,prefProxyHost);
-		String proxyPort = prefs.get(prefProxyPort,prefProxyPort);
-		do {
-			try {
-				String init = proxyType+":"+proxyHost+":"+proxyPort;
-				String s = DialogUtils.showInputDialog0(requester, "Enter 3 : separated values, "+PROXY_FORMAT,init);
-				String proxyTypeNew = null;
-				String proxyHostNew = null;
-				String proxyPortNew = null;
-				if(s== null || s.trim().length()==0){
-					//clear proxy info
-					prefs.remove(prefProxyType);
-					prefs.remove(prefProxyHost);
-					prefs.remove(prefProxyPort);
-				}else{
-					StringTokenizer st = new StringTokenizer(s, ":");
-					proxyTypeNew = st.nextToken().toLowerCase();
-					if(!proxyTypeNew.equals("http") && !proxyTypeNew.equals("socks")){
-						throw new IllegalArgumentException("Unkown proxyType='"+proxyTypeNew+"'");
-					}
-					proxyHostNew = st.nextToken();
-					proxyPortNew = st.nextToken();
-					prefs.put(prefProxyType, proxyTypeNew);
-					prefs.put(prefProxyHost, proxyHostNew);
-					prefs.put(prefProxyPort, Integer.parseInt(proxyPortNew)+"");
-				}
-				break;
-			} catch (UtilCancelException uce) {
-				// User didn't set proxy info
-				throw uce;
-			} catch (NumberFormatException nfe) {
-				nfe.printStackTrace();
-				DialogUtils.showErrorDialog(requester,nfe.getMessage() + "\nportNumber must be integer between 0 and 65535");
-			} catch (IllegalArgumentException iae) {
-				iae.printStackTrace();
-				DialogUtils.showErrorDialog(requester,iae.getMessage() + "\nProxy type must be 'http' or 'socks'");
-			} catch(Exception e){
-				e.printStackTrace();
-				DialogUtils.showErrorDialog(requester,e.getMessage() + "\nEntry must be of form ("+PROXY_FORMAT+")");
-			}
-		} while (true);
-		
-		setProxyProperties(requester,true,proxyType,proxyHost,proxyPort,prefs.get(prefProxyType,prefProxyType),prefs.get(prefProxyHost,prefProxyHost),prefs.get(prefProxyPort,prefProxyPort));
-	}
 
 	public static final String PROXY_HTTP_HOST = "http.proxyHost";
 	public static final String PROXY_HTTP_PORT = "http.proxyPort";
