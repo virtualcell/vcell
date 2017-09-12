ac3b7c3e6a22143ec3d071a2e209c49433c25cdc
Jim Schaff <schaff@uchc.edu>
2017-09-12 13:14:19 -0400

    restore vcell-visit python binding to thrift 0.9.2
    thrift version 0.9.2 python module bundled for use by VisIt.
    pyvcell (generated python module) for thrift version 0.9.2 restored.

--- thriftBuild/genVCellThriftClasses.sh
+++ thriftBuild/genVCellThriftClasses.sh
@@ -1,23 +1,30 @@
 #!/usr/bin/env bash
 
+THRIFT_0_10_0=thrift
+THRIFT_0_9_2=
+
 # Utility to (re-)generate Java and Python classes from thrift IDL specifications
 # writes Java and Python source files as specified in the .thrift files.
 
 # Calling thrift executable separately for Java and Python code generation.  Comment out either if suppression of one is desired.
-thrift --out ../vcell-core/src/main/java --gen java:generated_annotations=undated pyVCell.thrift
-thrift --out ../visTool --gen py pyVCell.thrift
+$THRIFT_0_10_0 --out ../vcell-core/src/main/java --gen java:generated_annotations=undated pyVCell.thrift
+if [ ! -z "$THRIFT_0_9_2" ]; then
+	$THRIFT_0_9_2 --out ../visTool --gen py pyVCell.thrift
+else
+	echo "skipping python visit binding, thrift 0.9.2 binary required for currently bundled thrift python module."
+fi
 
 # Done. Remember to refresh Eclipse and Visual Studio projects.
-thrift --out ../vcell-core/src/main/java --gen java:generated_annotations=undated VisMesh.thrift
-thrift --out ../visTool --gen py VisMesh.thrift
+$THRIFT_0_10_0 --out ../vcell-core/src/main/java --gen java:generated_annotations=undated VisMesh.thrift
+$THRIFT_0_10_0 --out ../visTool --gen py VisMesh.thrift
 
 # Copasi stuff
-thrift --out ../vcell-core/src/main/java --gen java:generated_annotations=undated VCellOpt.thrift
-thrift --out ../visTool --gen py VCellOpt.thrift
+$THRIFT_0_10_0 --out ../vcell-core/src/main/java --gen java:generated_annotations=undated VCellOpt.thrift
+$THRIFT_0_10_0 --out ../visTool --gen py VCellOpt.thrift
 
 # Image Simulation service
-thrift --out ../vcell-core/src/main/java --gen java:generated_annotations=undated VCellImageJ.thrift
-thrift --out ../visTool --gen py VCellImageJ.thrift
+$THRIFT_0_10_0 --out ../vcell-core/src/main/java --gen java:generated_annotations=undated VCellImageJ.thrift
+$THRIFT_0_10_0 --out ../visTool --gen py VCellImageJ.thrift
 
 # ImageDataset Service
-thrift --out ../vcell-core/src/main/java --gen java:generated_annotations=undated ImageDataset.thrift
+$THRIFT_0_10_0 --out ../vcell-core/src/main/java --gen java:generated_annotations=undated ImageDataset.thrift
