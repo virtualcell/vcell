92cba30f38628bd3a70a76e86c9f4c08f0ba2ba7
Dan Vasilescu <vasilescu@uchc.edu>
2019-01-30 17:11:01 -0500

    New look for Saved MathModel Info panel.
    Replicates the Database file Info look.

--- vcell-client/src/main/java/cbit/vcell/client/desktop/mathmodel/MathModelPropertiesPanel.java
+++ vcell-client/src/main/java/cbit/vcell/client/desktop/mathmodel/MathModelPropertiesPanel.java
@@ -10,22 +10,41 @@
 
 package cbit.vcell.client.desktop.mathmodel;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Desktop;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Enumeration;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTree;
 import javax.swing.SwingConstants;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeNode;
+import javax.swing.tree.TreePath;
 
 import org.vcell.util.DataAccessException;
+import org.vcell.util.document.BioModelInfo;
+import org.vcell.util.document.KeyValue;
 import org.vcell.util.document.MathModelInfo;
+import org.vcell.util.document.PublicationInfo;
 import org.vcell.util.document.Version;
 import org.vcell.util.document.VersionInfo;
+import org.vcell.util.gui.GuiUtils;
 import org.vcell.util.gui.JLabelLikeTextField;
 import org.vcell.util.gui.VCellIcons;
 
@@ -33,6 +52,11 @@ import cbit.vcell.client.MathModelWindowManager;
 import cbit.vcell.client.desktop.biomodel.DocumentEditorSubPanel;
 import cbit.vcell.clientdb.DatabaseEvent;
 import cbit.vcell.clientdb.DatabaseListener;
+import cbit.vcell.desktop.BioModelInfoCellRenderer;
+import cbit.vcell.desktop.BioModelInfoTreeModel;
+import cbit.vcell.desktop.BioModelNode;
+import cbit.vcell.desktop.MathModelMetaDataCellRenderer;
+import cbit.vcell.desktop.MathModelMetaDataTreeModel;
 import cbit.vcell.geometry.Geometry;
 import cbit.vcell.mathmodel.MathModel;
 
@@ -45,6 +69,7 @@ public class MathModelPropertiesPanel extends DocumentEditorSubPanel {
 	private MathModelWindowManager mathModelWindowManager;
 	private JLabel geometryLabel, detStochIconLabel;
 	private JLabelLikeTextField detStochLabel;
+	private LocalMetaDataPanel applicationsPanel = null;
 
 	private class EventHandler implements ActionListener, DatabaseListener {
 		public void actionPerformed(ActionEvent e) {
@@ -112,6 +137,8 @@ private void initialize() {
 		detStochIconLabel = new JLabel();
 		geometryLabel.setIcon(VCellIcons.geometryIcon);
 		detStochIconLabel.setIcon(VCellIcons.mathTypeIcon);
+		applicationsPanel = new LocalMetaDataPanel();
+		applicationsPanel.initialize();
 		
 		setLayout(new GridBagLayout());
 		setBackground(Color.white);
@@ -213,46 +240,13 @@ private void initialize() {
 		gbc = new java.awt.GridBagConstraints();
 		gbc.gridx = 0; 
 		gbc.gridy = gridy;
+		gbc.fill = java.awt.GridBagConstraints.BOTH;
 		gbc.insets = new Insets(4, 4, 4, 4);
-		gbc.anchor = GridBagConstraints.FIRST_LINE_END;		
-		label = new JLabel("Type:");
-		add(label, gbc);
-		
-		gbc = new java.awt.GridBagConstraints();
-		gbc.gridx = 1; 
-		gbc.gridy = gridy;
+		gbc.anchor = GridBagConstraints.FIRST_LINE_START;	
 		gbc.weighty = 1.0;
-		gbc.fill = java.awt.GridBagConstraints.HORIZONTAL;
-		gbc.insets = new Insets(4, 4, 4, 4);
-		gbc.anchor = GridBagConstraints.FIRST_LINE_START;		
-		add(detStochIconLabel, gbc);
-		
-		gbc = new java.awt.GridBagConstraints();
-		gbc.gridx = 2; 
-		gbc.gridy = gridy;
-		gbc.weighty = 1.0;
-		gbc.fill = java.awt.GridBagConstraints.HORIZONTAL;
-		gbc.insets = new Insets(4, 4, 4, 4);
-		gbc.anchor = GridBagConstraints.FIRST_LINE_START;		
-		add(detStochLabel, gbc);
-
-//		gridy ++;
-//		gbc = new java.awt.GridBagConstraints();
-//		gbc.gridx = 0; 
-//		gbc.gridy = gridy;
-//		gbc.insets = new Insets(4, 4, 4, 4);
-//		gbc.anchor = GridBagConstraints.FIRST_LINE_END;		
-//		label = new JLabel("Geometry:");
-//		add(label, gbc);
-//		
-//		gbc = new java.awt.GridBagConstraints();
-//		gbc.gridx = 1; 
-//		gbc.gridy = gridy;
-//		gbc.weighty = 1.0;
-//		gbc.fill = java.awt.GridBagConstraints.HORIZONTAL;
-//		gbc.insets = new Insets(4, 4, 4, 4);
-//		gbc.anchor = GridBagConstraints.FIRST_LINE_START;		
-//		add(geometryLabel, gbc);
+		gbc.weightx = 1.0;
+		gbc.gridwidth = 3;
+		add(applicationsPanel, gbc);
 				
 		changePermissionButton.addActionListener(eventHandler);
 	} catch (java.lang.Throwable ivjExc) {
@@ -318,21 +312,7 @@ private void updateInterface() {
 			permissionLabel.setText(mathModelInfo.getVersion().getGroupAccess().getDescription());
 			changePermissionButton.setEnabled(true);
 		}
-		Geometry geometry = mathModel.getGeometry();
-		String geometryText = "Compartmental geometry";
-		if (geometry != null) {
-			Version geometryVersion = geometry.getVersion();
-			int dimension = geometry.getDimension();
-			if (dimension > 0){
-				String description = geometry.getDimension() + "D " + (geometry.getGeometrySpec().hasImage() ? "image" : "analytic") + " geometry";
-				geometryText = description;
-				if (geometryVersion != null) {
-					geometryText += " - " + geometryVersion.getName() + " ("+geometryVersion.getDate() + ")";
-				}
-			}
-		}
-		geometryLabel.setText(geometryText);			
-		detStochLabel.setText(mathModel.getMathDescription().getMathType().getDescription());
+		applicationsPanel.updateInterface();
 	} catch (DataAccessException e) {
 		e.printStackTrace();
 	}
@@ -342,5 +322,88 @@ private void updateInterface() {
 protected void onSelectedObjectsChange(Object[] selectedObjects) {
 	
 }
+
+private class LocalMetaDataPanel extends JPanel {
+
+	private JTree jTree = null;
+	private MathModelMetaDataCellRenderer mathModelInfoCellRenderer = new MathModelMetaDataCellRenderer();
+	private MathModelMetaDataTreeModel mathModelInfoTreeModel = new MathModelMetaDataTreeModel();
+	private MathModelInfo fieldMathModelInfo = null;
+
+	public LocalMetaDataPanel() {
+		super();
+		initialize();
+	}
+	private void initialize() {
+		try {
+			setName("LocalMetaDataPanel");
+			setLayout(new BorderLayout());
+			add(new JScrollPane(getJTree()), BorderLayout.CENTER);
+//			this.addPropertyChangeListener(ivjEventHandler);
+			getJTree().setModel(mathModelInfoTreeModel);
+			getJTree().setCellRenderer(mathModelInfoCellRenderer);
+			javax.swing.ToolTipManager.sharedInstance().registerComponent(getJTree());
+			getJTree().getSelectionModel().setSelectionMode(javax.swing.tree.TreeSelectionModel.SINGLE_TREE_SELECTION);
+		} catch (java.lang.Throwable ivjExc) {
+			handleException(ivjExc);
+		}
+	}
+	
+	private void updateInterface() {
+		try {
+			if(mathModel == null) {
+				return;
+			}
+//			Dimension preferredSize = new Dimension(100, 50);
+//			setPreferredSize(preferredSize);
+			
+			KeyValue keyValue = mathModel.getVersion().getVersionKey();
+			fieldMathModelInfo = mathModelWindowManager.getRequestManager().getDocumentManager().getMathModelInfo(keyValue);
+			mathModelInfoTreeModel.setMathModelInfo(fieldMathModelInfo);
+			GuiUtils.treeExpandAllRows(getJTree());
+		} catch(DataAccessException ex) {
+			handleException(ex);
+		}
+	}
+	
+	private JTree getJTree() {
+		if (jTree == null) {
+			try {
+				jTree = new JTree();
+				jTree.setName("JTree1");
+				jTree.setToolTipText("Contents of saved MathModel");
+				jTree.setEnabled(true);
+				jTree.setRootVisible(false);
+				jTree.setRequestFocusEnabled(false);
+				jTree.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					if(e.getClickCount() == 1) {
+						DefaultMutableTreeNode value = (DefaultMutableTreeNode)jTree.getLastSelectedPathComponent();
+						if(value instanceof BioModelNode) {
+							BioModelNode node = (BioModelNode) value;
+							try {
+								if(node.getUserObject() instanceof PublicationInfo && "PublicationInfoDoi".equals(node.getRenderHint("type"))) {
+									PublicationInfo info = (PublicationInfo)node.getUserObject();
+									Desktop.getDesktop().browse(new URI("https://doi.org/" + info.getDoi()));
+								} else if (node.getUserObject() instanceof PublicationInfo && "PublicationInfoUrl".equals(node.getRenderHint("type"))) {
+									PublicationInfo info = (PublicationInfo)node.getUserObject();
+									Desktop.getDesktop().browse(new URI(info.getUrl()));
+								}
+							} catch (URISyntaxException | IOException ex) {
+								handleException(ex);
+							}
+						}
+					}
+				}
+			});
+			} catch (java.lang.Throwable ex) {
+				handleException(ex);
+			}
+		}
+		return jTree;
+	}
+}
+
 
 }
