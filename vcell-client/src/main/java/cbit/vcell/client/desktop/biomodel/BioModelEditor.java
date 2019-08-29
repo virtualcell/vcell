cb6121f05135da65fef1c6fd1fcc71aa51c55a55
Dan Vasilescu <vasilescu@uchc.edu>
2019-08-29 17:26:48 -0400

    Fixed navigating to the wrong window when dbl click on annotation icons.
    Order of evaluating conditions in a multiple if statement was wrong. The
    most general conditions are now evaluated last.

--- vcell-client/src/main/java/cbit/vcell/client/desktop/biomodel/BioModelEditor.java
+++ vcell-client/src/main/java/cbit/vcell/client/desktop/biomodel/BioModelEditor.java
@@ -855,16 +855,14 @@ protected void setRightBottomPanelOnSelection(Object[] selections) {
 		rightSplitPane.repaint();
 	}
 	Component selectedComponent = rightBottomTabbedPane.getSelectedComponent();
-	if (selectedComponent != bottomComponent 
-//			&& rightBottomTabbedPane.getSelectedComponent() != annotationComponent
-			) {
-		rightBottomTabbedPane.setSelectedComponent(bottomComponent);
-	} else if(selections.length == 1 && selections[0] instanceof BioModel && bottomComponent instanceof BioModelEditorAnnotationPanel) {
+	if(selections.length == 1 && selections[0] instanceof BioModel && bottomComponent instanceof BioModelEditorAnnotationPanel) {
 		// if the biomodel name is selected in the upper left tree, we select the Annotations tab
 		rightBottomTabbedPane.setSelectedComponent(annotationComponent);
 	} else if(selections.length == 1 && selections[0] instanceof SelectionManager.AnnotationNavigator && bottomComponent instanceof BioModelEditorAnnotationPanel) {
 		// we want to navigate to the AnnotationPanel
 		rightBottomTabbedPane.setSelectedComponent(annotationComponent);
+	} else if(selectedComponent != bottomComponent /* && rightBottomTabbedPane.getSelectedComponent() != annotationComponent */ ) {
+		rightBottomTabbedPane.setSelectedComponent(bottomComponent);
 	}
 }
 
