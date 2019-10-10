e7cde5ba93775259870506bd789de4e465c0c088
Frank Morgan <frm@uchc.edu>
2019-10-09 23:10:19 -0400

    Fix marshal exception when load image in vfrap, BioFormats 0.0.9

--- vcell.sh
+++ vcell.sh
@@ -5,8 +5,8 @@ MAIN_CLASS=cbit.vcell.client.test.VCellClientTest
 
 props="-Dvcell.installDir=$PWD"
 props="${props} -Dvcell.softwareVersion=standalone_VCell_7.0"
-props="${props} -Dvcell.bioformatsJarFileName=vcell-bioformats-0.0.8-jar-with-dependencies.jar"
-props="${props} -Dvcell.bioformatsJarDownloadURL=http://vcell.org/webstart/vcell-bioformats-0.0.8-jar-with-dependencies.jar"
+props="${props} -Dvcell.bioformatsJarFileName=vcell-bioformats-0.0.9-jar-with-dependencies.jar"
+props="${props} -Dvcell.bioformatsJarDownloadURL=http://vcell.org/webstart/vcell-bioformats-0.0.9-jar-with-dependencies.jar"
 
 echo java ${props} -cp $CLASSPATH $MAIN_CLASS vcellapi.cam.uchc.edu:443
 java ${props} -cp $CLASSPATH $MAIN_CLASS vcellapi.cam.uchc.edu:443
