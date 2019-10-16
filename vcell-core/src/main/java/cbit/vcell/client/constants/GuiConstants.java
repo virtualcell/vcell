9fcf5e75243281cf31368fae334345ba33415316
Dan Vasilescu <vasilescu@uchc.edu>
2019-10-16 18:40:38 -0400

    Add 'etc' to 'Parameters, functions, units' tree entry.

--- vcell-core/src/main/java/cbit/vcell/client/constants/GuiConstants.java
+++ vcell-core/src/main/java/cbit/vcell/client/constants/GuiConstants.java
@@ -31,7 +31,7 @@ public class GuiConstants {
 	public final static String DOCUMENT_EDITOR_FOLDERNAME_MODEL = "Physiology";
 	public final static String DOCUMENT_EDITOR_FOLDERNAME_DATA = "Experimental Data";
 	public final static String DOCUMENT_EDITOR_FOLDERNAME_APPLICATIONS = "Applications";
-	public final static String DOCUMENT_EDITOR_FOLDERNAME_BIOMODEL_PARAMETERS = "Parameters, functions, units, etc.";
+	public final static String DOCUMENT_EDITOR_FOLDERNAME_BIOMODEL_PARAMETERS = "Parameters, Functions, Units, etc.";
 	public final static String DOCUMENT_EDITOR_FOLDERNAME_SCRIPTING = "Scripting";
 	public final static String DOCUMENT_EDITOR_FOLDERNAME_REACTIONS = "Reactions";
 	public final static String DOCUMENT_EDITOR_FOLDERNAME_DIAGRAM = "Reaction Diagram";
