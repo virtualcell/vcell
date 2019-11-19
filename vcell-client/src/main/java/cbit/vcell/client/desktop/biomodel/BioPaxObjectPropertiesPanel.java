e39a9072e27de0694d5ecbef58185a613c20fc7b
Dan Vasilescu <vasilescu@uchc.edu>
2019-11-19 15:00:37 -0500

    Enable navigation from Pathway reaction to linked vCell reaction.

--- vcell-client/src/main/java/cbit/vcell/client/desktop/biomodel/BioPaxObjectPropertiesPanel.java
+++ vcell-client/src/main/java/cbit/vcell/client/desktop/biomodel/BioPaxObjectPropertiesPanel.java
@@ -84,6 +84,7 @@ import cbit.vcell.client.desktop.biomodel.SelectionManager.ActiveViewID;
 import cbit.vcell.client.task.AsynchClientTask;
 import cbit.vcell.client.task.ClientTaskDispatcher;
 import cbit.vcell.model.BioModelEntityObject;
+import cbit.vcell.model.SimpleReaction;
 import cbit.vcell.model.SpeciesContext;
 
 
@@ -335,6 +336,8 @@ private void initialize() {
 									selectionManager.followHyperlink(new ActiveView(null,DocumentEditorTreeFolderClass.REACTION_DIAGRAM_NODE, ActiveViewID.reaction_diagram),new Object[]{bioModelEntityObject});
 								} else if(bioModelEntityObject instanceof MolecularType) {
 									selectionManager.followHyperlink(new ActiveView(null,DocumentEditorTreeFolderClass.MOLECULAR_TYPES_NODE, ActiveViewID.species_definitions),new Object[]{bioModelEntityObject});
+								} else if(bioModelEntityObject instanceof SimpleReaction) {
+									selectionManager.followHyperlink(new ActiveView(null,DocumentEditorTreeFolderClass.REACTION_DIAGRAM_NODE, ActiveViewID.reaction_diagram),new Object[]{bioModelEntityObject});
 								}
 							}else if(((Entity)BioPaxObjectPropertiesPanel.this.bioPaxObject).getFormalNames() == null || ((Entity)BioPaxObjectPropertiesPanel.this.bioPaxObject).getFormalNames().size() == 0){
 								lookupFormalName(crow);
