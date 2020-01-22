3ba25582f0187c02d32a395d07df899f88b327d3
vcfrmgit <frm@uchc.edu>
2020-01-22 09:15:12 -0500

    update imagej/vcell scripts

--- vcell-imagej-helper/photoactivation_analyze.groovy
+++ vcell-imagej-helper/photoactivation_analyze.groovy
@@ -26,11 +26,12 @@ import org.jfree.data.category.CategoryDataset
 import org.jfree.data.xy.XYSeriesCollection
 import org.jfree.data.xy.XYSeries
 import javax.swing.JFrame
-
+import java.awt.BasicStroke
 
 //public IJDataList getTimePointData(String simulationDataCacheKey,String variableName,int[] timePointIndexes,int simulationJobIndex) throws Exception{
 
 simID = 109811359 //109808709
+stroke = new BasicStroke(1, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER, 10,[5] as float[], 0)
 //jobIndex = 0
 int t0Sim = 32;
 int t0Exp = 2
@@ -39,10 +40,15 @@ int ANALYZE_END_TIMEINDEX = 2;
 simVar = "fluor"
 simVarDataType = VCellHelper.VARTYPE_POSTPROC.PostProcessDataPDEDataContext;// null for SimulationData, "DataProcessingOutputInfo" for postprocessStats
 expDataVar = "abp" //Expected to be open ImageJ dataset window
-roix = 55
-roiy = 55
-roixs = 8;
-roiys = 8
+//roix = 30
+//roiy = 55
+//roixs = 2;
+//roiys = 2
+BOX_X = 0
+BOX_Y = 1
+BOX_XS = 2
+BOX_YS = 3
+boxes = [[55,26,8,8],[25,33,8,8]] as int[][]
 
 if(false){waitForSolverGetCacheForData(simID,jobIndex);}
 
@@ -75,32 +81,50 @@ exp_frames = ij.IJ.getImage().getNFrames()
 //SOMETIMES fdStackArr.length != exp_frames, use exp_frames
 println("xsize="+exp_xsize+" ysize="+exp_ysize+" frames="+exp_frames+" scancount="+ijVarInfos.scancount)
 double[] Kr_Bind_OverrideIndexes = new double[ijVarInfos.scancount]
-//new float[tCount*exp_xsize*exp_ysize], 
+//new float[tCount*exp_xsize*exp_ysize],
+	SIM_WIN_NAME = "Sim fluor"
 			fluorImage = ArrayImgs.floats([exp_xsize,exp_ysize,tCount] as long[])//SCIFIOImgPlus<DoubleType> fluorImage = null
-			netij.ui().show("Sim fluor",fluorImage)
+			netij.ui().show(SIM_WIN_NAME,fluorImage)
 			simImage = ij.IJ.getImage()
-			simImage.setRoi(roix,roiy,roixs,roiys)
+//			simImage.setRoi(roix,roiy,roixs,roiys)
 			ij.IJ.run("In [+]");
 			ij.IJ.run("In [+]");
 			ij.IJ.run("In [+]");
-double[][] diffSqrTimeAndJob = new double[tCount][Kr_Bind_OverrideIndexes.length]
-double[] jobSum = new double[Kr_Bind_OverrideIndexes.length]
-//Init Graph
-SERIES_PREFIX = "Sim "
-XYSeriesCollection dataset = new XYSeriesCollection();
+double[][][] diffSqrTimeAndJob = new double[tCount][Kr_Bind_OverrideIndexes.length][boxes.length]
+double[][] jobSum = new double[Kr_Bind_OverrideIndexes.length][boxes.length]
+ROI_EXP_INDEX = 0
+ROI_SIM_INDEX = 1
+double[][][][] roiMean = new double[tCount][Kr_Bind_OverrideIndexes.length][boxes.length][2]
+//Init diffSqr Graph
+DIFFSQR_SERIES_PREFIX = "Sim "
+XYSeriesCollection diffSqrDataset = new XYSeriesCollection();
 for(int i=0;i<Kr_Bind_OverrideIndexes.length;i++){
-	dataset.addSeries(new XYSeries(SERIES_PREFIX+i));
+	diffSqrDataset.addSeries(new XYSeries(DIFFSQR_SERIES_PREFIX+i));
 }
+//Init roi Graph
+ROI_SERIES_PREFIX = "Roi_"
+ROI_EXP_NAME = "Exp"
+ROI_SIM_NAME = "Sim"
+XYSeriesCollection roiDataset = new XYSeriesCollection();
+for(int boxIndex=0;boxIndex<boxes.length;boxIndex++){
+	for(int job=0;job<Kr_Bind_OverrideIndexes.length;job++){
+		if(job == 0){
+		roiDataset.addSeries(new XYSeries(ROI_SERIES_PREFIX+ROI_EXP_NAME+"_"+boxIndex+"_"+job))
+		}
+		roiDataset.addSeries(new XYSeries(ROI_SERIES_PREFIX+ROI_SIM_NAME+"_"+boxIndex+"_"+job))
+	}
+}
+
 for(int t = 0;t<tCount;t++){
 	actualExpTime = t+t0Exp
 	actualSimTime = t+t0Sim
 	expImage.setSlice(actualExpTime)
-	expImage.setRoi(roix,roiy,roixs,roiys)
 	//short[] expData = (short[])fdStackArr.getAt(actualExpTime)
 	for(int job=0;job<Kr_Bind_OverrideIndexes.length;job++){
 		println("t="+t+" "+" job="+job+" expTime="+(actualExpTime)+" simTime="+(actualSimTime))
 		//Get Sim Data - PostProcessing 'fluor'
 		IJDataList fluorIJD = null
+		println("Getting VCell data...")
 		try{
 			fluorIJD = vh.getTimePointData(simulationDataCacheKey, simVar, simVarDataType, [actualSimTime] as int[], job);
 		}catch(Exception e){
@@ -108,6 +132,7 @@ for(int t = 0;t<tCount;t++){
 			job--;//try again
 			continue;
 		}
+		println("Convert float...")
 		float[] fluorData = new float[fluorIJD.ijData[0].getDoubleData().length]
 		for(int i=0;i<fluorData.length;i++){
 			fluorData[i] = fluorIJD.ijData[0].getDoubleData()[i]
@@ -120,6 +145,7 @@ for(int t = 0;t<tCount;t++){
 //			throw new Exception("Expecting Sim and Exp data arrays to be same length")
 //		}
 		//Thread.sleep(500)
+		println("create Sim ArrayImg...")
 		ArrayImg<FloatType, FloatArray> simTimePointData = ArrayImgs.floats(fluorData, [simStackInfo.xsize,simStackInfo.ysize,1] as long[])
 	//	if(t == 0 && job == 0){
 			//ArrayImg<DoubleType, DoubleArray> simAllTimes = ArrayImgs.doubles(fluorData, [simStackInfo.xsize,simStackInfo.ysize,tCount] as long[]);
@@ -130,16 +156,19 @@ for(int t = 0;t<tCount;t++){
 			//ij.IJ.run("In [+]");
 			//ij.IJ.run("In [+]");
 			//ij.IJ.run("In [+]");
-	//	}else 
-		if(job == 1){
-			ij.IJ.selectWindow("Sim fluor")
+	//	}else
+		//if(job == 1){
+			//ij.IJ.selectWindow(SIM_WIN_NAME)
 			//println(ij.IJ.getImage().getProcessor().getPixels())
 			//if(true){return}
 			//ImageProcessor ip = ij.IJ.getImage().getStack().getProcessor(t+1)
 			//float[] tempf = simTimePointData.update(null).getCurrentStorageArray()
 			//println(simTimePointData.update(null).getCurrentStorageArray())
-			ij.IJ.getImage().setSlice(t+1)
-			float[] srcFloats = ij.IJ.getImage().getProcessor().getPixels()
+			//ij.IJ.getImage().setSlice(t+1)
+			println("Copy Sim double to float...")
+			simImage.setSlice(t+1)
+			//float[] srcFloats = ij.IJ.getImage().getProcessor().getPixels()
+			float[] srcFloats = simImage.getProcessor().getPixels()
 			cnt = 0
 			for(int y= 0;y<exp_ysize;y++){
 				for(int x = 0;x<exp_xsize;x++){
@@ -160,54 +189,111 @@ for(int t = 0;t<tCount;t++){
 				println()
 			}
 */
-		}
+		//}//if(job == 1){
+//BOX_X = 0
+//BOX_Y = 1
+//BOX_XS = 2
+//BOX_YS = 3
 
-		//if(t==1 && job==0){return}
-		
-		simRA = simTimePointData.randomAccess()
-		simSum = 0.0;
-		expSum = 0;
-		for(int y= 0;y<roiys;y++){
-			for(int x = 0;x<roixs;x++){
-				xp = x+roix
-				yp = y+roiy
-				simRA.setPosition(xp,0)
-				simRA.setPosition(yp,1)
-				simSum+= simRA.get().get()
-				expVal = expImage.getStack().getProcessor(actualExpTime).getPixel(xp,yp)
-				print(expVal+":"+simRA.get());print(" ")
-				expSum+= expVal
+		for(int boxIndex=0;boxIndex<boxes.length;boxIndex++){
+			println("boxIndex="+boxIndex)
+			//if(t==1 && job==0){return}
+			roix = boxes[boxIndex][BOX_X]
+			roiy = boxes[boxIndex][BOX_Y]
+			roixs = boxes[boxIndex][BOX_XS]
+			roiys = boxes[boxIndex][BOX_YS]
+			simImage.setRoi(roix,roiy,roixs,roiys)
+			expImage.setRoi(roix,roiy,roixs,roiys)
+
+			simRA = simTimePointData.randomAccess()
+			simSum = 0.0;
+			expSum = 0.0;
+			for(int y= 0;y<roiys;y++){
+				for(int x = 0;x<roixs;x++){
+					xp = x+roix
+					yp = y+roiy
+					simRA.setPosition(xp,0)
+					simRA.setPosition(yp,1)
+					simSum+= simRA.get().get()
+					expVal = expImage.getStack().getProcessor(actualExpTime).getPixel(xp,yp)
+					//print(expVal+":"+simRA.get());print(" ")
+					expSum+= expVal
+				}
+					//println()
 			}
-				println()
+			println(" sums exp:sim="+expSum+":"+simSum)
+			if(job == 0){
+			roiDataset.getSeries(ROI_SERIES_PREFIX+ROI_EXP_NAME+"_"+boxIndex+"_"+job).add(t,expSum/(roixs*roiys))
+			}
+			roiDataset.getSeries(ROI_SERIES_PREFIX+ROI_SIM_NAME+"_"+boxIndex+"_"+job).add(t,simSum/(roixs*roiys))
+			//roiMean[t][job][boxIndex][ROI_EXP_INDEX] = expSum/(roixs*roiys)
+			//roiMean[t][job][boxIndex][ROI_SIM_INDEX] = simSum/(roixs*roiys)
+			diffSqrTimeAndJob[t][job][boxIndex] = Math.pow(simSum-expSum,2)/1e9
+			jobSum[job][boxIndex]+= diffSqrTimeAndJob[t][job][boxIndex]
+			//diffSqrDataset.getSeries(DIFFSQR_SERIES_PREFIX+job).add(t,diffSqrTimeAndJob[t][job])
+			//println(simSum+" "+expSum+" "+diffSqrTimeAndJob[t][job])
 		}
-		diffSqrTimeAndJob[t][job] = Math.pow(simSum-expSum,2)/1e9
-		jobSum[job]+= diffSqrTimeAndJob[t][job]
-		dataset.getSeries(SERIES_PREFIX+job).add(t,diffSqrTimeAndJob[t][job])
-		//println(simSum+" "+expSum+" "+diffSqrTimeAndJob[t][job])
 	}
-	println(diffSqrTimeAndJob)
-	//if(t==2){break}
+	//println(diffSqrTimeAndJob)
+//if(t==1){break}
 }
-minVal = Double.POSITIVE_INFINITY;
-minJobIndex = -1
-for(int i=0;i<jobSum.length;i++){
-	if(jobSum[i] < minVal){
-		minJobIndex = i;
-		minVal = jobSum[i];
+minminVal = Double.POSITIVE_INFINITY
+minminJobIndex = [-1,-1] as int[]
+double[] minVal = new double[boxes.length]
+Arrays.fill(minVal,Double.POSITIVE_INFINITY)
+int[] minJobIndex = new int[boxes.length]
+Arrays.fill(minJobIndex,-1)
+for(int job=0;job<jobSum.length;job++){
+	for(int boxIndex=0;boxIndex<boxes.length;boxIndex++){
+		if(jobSum[job][boxIndex] < minVal[boxIndex]){
+			minJobIndex[boxIndex] = job;
+			minVal[boxIndex] = jobSum[job][boxIndex];
+			if(minVal[boxIndex] < minminVal){
+				minminVal = minVal[boxIndex]
+				minminJobIndex = [job,boxIndex]
+			}
+		}
+		if(job == 0){
+		println("Exp="+roiDataset.getSeries(ROI_SERIES_PREFIX+ROI_EXP_NAME+"_"+boxIndex+"_"+job).getItems())
+		}
+		println("Sim="+roiDataset.getSeries(ROI_SERIES_PREFIX+ROI_SIM_NAME+"_"+boxIndex+"_"+job).getItems())
 	}
 }
-println("Minimum Job Index="+minJobIndex+" minimum sum(squared(Sim-Exp))="+minVal)
-//Create Chart
-JFreeChart chart = ChartFactory.createXYLineChart("Exp VS Sim", "Time", "((Sim-Exp)^2)/1e9", dataset);
-chartPanel = new ChartPanel(chart)
+println(minVal)
+println(minJobIndex)
+println("minminVal="+minminVal)
+println("minminJobIndex="+minminJobIndex)
 
-frame = new JFrame("Chart");
-        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-frame.getContentPane().add(chartPanel)
-        //Display the window.
-frame.pack();
-frame.setVisible(true);
+	JFreeChart chart = ChartFactory.createXYLineChart("Exp/Sim Roi mean", "Time", "roi mean", roiDataset);
+	for(int boxIndex=0;boxIndex<boxes.length;boxIndex++){
+		chart.getXYPlot().getRenderer().setSeriesStroke(roiDataset.indexOf(ROI_SERIES_PREFIX+ROI_EXP_NAME+"_"+boxIndex+"_0"),stroke)
+	}
+	chartPanel = new ChartPanel(chart)
+	
+	frame = new JFrame("Chart");
+	        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.getContentPane().add(chartPanel)
+	        //Display the window.
+	frame.pack();
+	frame.setVisible(true);	
 if(true){return}
+
+/*
+for(int boxIndex=0;boxIndex<boxes.length;boxIndex++){
+	println("Minimum Job "+boxIndex+" Index="+minJobIndex[boxIndex]+" minimum sum(squared(Sim-Exp))="+minVal[boxIndex])
+	//Create Chart
+	JFreeChart chart = ChartFactory.createXYLineChart("Exp VS Sim", "Time", "((Sim-Exp)^2)/1e9", diffSqrDataset);
+	chartPanel = new ChartPanel(chart)
+	
+	frame = new JFrame("Chart");
+	        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.getContentPane().add(chartPanel)
+	        //Display the window.
+	frame.pack();
+	frame.setVisible(true);	
+}
+*/
+
 //int xyzsize = ijDataList.ijData[0].stackInfo.xsize*ijDataList.ijData[0].stackInfo.ysize*ijDataList.ijData[0].stackInfo.zsize;
 println(xyzsize+" "+temp.length+" "+ijDataList.ijData[0].notInDomainValue)
 accum = 0.0
