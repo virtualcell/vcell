03b60f30d2ffb8d9d693d1b1972130ec4e097fac
gmarupilla <marupilla@uchc.edu>
2020-02-16 01:47:13 -0500

    Add if tagged as TRAVIS_TAG
    Former-commit-id: 0fa6697957f9b04cc152f1e201d7665bcddeed97

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-if tag_name
+export TRAVIS_TAG=${TRAVIS_TAG:-$(date +'%Y%m%d%H%M%S')-$(git log --format=%h -1)}
+git tag $TRAVIS_TAG
+
+if $TRAVIS_TAG
 then
 
-        docker build --file docker/build/Dockerfile-sedml-solver --tag crbm/biosimulations_vcell_api:tag_name . 
-        docker push crbm/biosimulations_vcell_api:tag_name
+        docker build --file docker/build/Dockerfile-sedml-solver --tag crbm/biosimulations_vcell_api:$TRAVIS_TAG . 
+        docker push crbm/biosimulations_vcell_api:$TRAVIS_TAG
 
 else
 
