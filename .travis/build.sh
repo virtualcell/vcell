a3b43a9051172d85c43eb37675b217971f5eeaf6
GMarupilla <53095348+gmarupilla@users.noreply.github.com>
2020-02-15 23:20:08 -0500

    Update build script with shell scrpt
    Former-commit-id: 9baec6b95e72ee2681add7d1d786ce2e2a5cfab7

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
-curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
-sh travis-build.sh
+#!/bin/bash
+
+if $TRAVIS_TAG
+then
+
+        docker build -f docker/build/Dockerfile-sedml-solver -t crbm/biosimulations_vcell_api:"$TRAVIS_TAG" . 
+        docker push crbm/biosimulations_vcell_api:"$TRAVIS_TAG"
+
+else
+
+        docker build -f docker/build/Dockerfile-sedml-solver -t crbm/biosimulations_vcell_api:latest .
+
+fi
