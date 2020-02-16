63d26be0be9ea77c1a7202a31824b8d7b76948d4
gmarupilla <marupilla@uchc.edu>
2020-02-16 07:45:17 -0500

    Add deploy key to push it from build.sh

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-export TRAVIS_TAG=${TRAVIS_TAG:-$(date +'%Y%m%d%H%M%S')-$(git log --format=%h -1)}
-git tag $TRAVIS_TAG
+# export TRAVIS_TAG=${TRAVIS_TAG:-$(date +'%Y%m%d%H%M%S')-$(git log --format=%h -1)}
+# git tag $TRAVIS_TAG
 
-if $TRAVIS_TAG
+if deploy
 then
 
-        docker build --file docker/build/Dockerfile-sedml-solver --tag crbm/biosimulations_vcell_api:$TRAVIS_TAG . 
-        docker push crbm/biosimulations_vcell_api:$TRAVIS_TAG
+        docker build --file docker/build/Dockerfile-sedml-solver --tag crbm/biosimulations_vcell_api:$tag_name . 
+        docker push crbm/biosimulations_vcell_api:$tag_name
 
 else
 
