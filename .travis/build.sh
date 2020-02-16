da6fb61a9c29d17cb80ecce16cef6de1b770331d
GMarupilla <53095348+gmarupilla@users.noreply.github.com>
2020-02-15 23:39:20 -0500

    Remove double quotes for tag

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,8 +3,8 @@
 if $TRAVIS_TAG
 then
 
-        docker build --file docker/build/Dockerfile-sedml-solver --tag crbm/biosimulations_vcell_api:"$TRAVIS_TAG" . 
-        docker push crbm/biosimulations_vcell_api:"$TRAVIS_TAG"
+        docker build --file docker/build/Dockerfile-sedml-solver --tag crbm/biosimulations_vcell_api:$TRAVIS_TAG . 
+        docker push crbm/biosimulations_vcell_api:$TRAVIS_TAG
 
 else
 
