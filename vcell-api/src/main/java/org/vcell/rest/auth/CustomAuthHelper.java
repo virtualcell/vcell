4f48a53dd56a3093caa0b511bf05d3a8246657f1
vcfrmgit <frm@uchc.edu>
2020-03-27 13:28:34 -0400

    Fix api changes from github dependabot merges (revert)

--- vcell-api/src/main/java/org/vcell/rest/auth/CustomAuthHelper.java
+++ vcell-api/src/main/java/org/vcell/rest/auth/CustomAuthHelper.java
@@ -9,9 +9,9 @@ import org.restlet.Response;
 import org.restlet.data.ChallengeRequest;
 import org.restlet.data.ChallengeResponse;
 import org.restlet.data.ChallengeScheme;
-import org.restlet.data.Header;
 import org.restlet.data.Reference;
 import org.restlet.engine.header.ChallengeWriter;
+import org.restlet.engine.header.Header;
 import org.restlet.engine.security.AuthenticatorHelper;
 import org.restlet.util.Series;
 
