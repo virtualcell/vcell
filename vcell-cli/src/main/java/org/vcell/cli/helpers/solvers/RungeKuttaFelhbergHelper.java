30ebc018f1ad60454f4da4e4c86fceea739d1962
GMarupilla <marupilla@mail.com>
2020-05-29 15:10:34 -0400

    Make RKF solver work, TODO: Store timeseries data in variable

--- vcell-cli/src/main/java/org/vcell/cli/helpers/solvers/RungeKuttaFelhbergHelper.java
+++ vcell-cli/src/main/java/org/vcell/cli/helpers/solvers/RungeKuttaFelhbergHelper.java
@@ -2,6 +2,8 @@ package org.vcell.cli.helpers.solvers;
 
 import cbit.vcell.biomodel.BioModel;
 import cbit.vcell.client.ClientSimManager;
+import cbit.vcell.mapping.SimulationContext;
+import cbit.vcell.mapping.SpeciesContextSpec;
 import cbit.vcell.math.MathException;
 import cbit.vcell.messaging.server.SimulationTask;
 import cbit.vcell.parser.Discontinuity;
@@ -16,6 +18,7 @@ import org.vcell.cli.CLIUtils;
 import org.vcell.cli.helpers.sbml.SBMLSolverHelper;
 import org.vcell.util.ClientTaskStatusSupport;
 import org.vcell.util.DataAccessException;
+import org.vcell.util.ObjectNotFoundException;
 import org.vcell.util.ProgressDialogListener;
 import org.vcell.util.exe.Executable;
 import org.vcell.util.exe.ExecutableException;
@@ -24,6 +27,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import static cbit.vcell.simdata.SimDataConstants.LOGFILE_EXTENSION;
@@ -33,7 +37,16 @@ public class RungeKuttaFelhbergHelper {
 
     public static ODESolverResultSet solve(File outDir, String taskId, BioModel bioModel){
         String docName = bioModel.getName();
-        Simulation sim = bioModel.getSimulation(0);
+
+//        Simulation sim = bioModel.getSimulation(0);
+
+        Simulation sim = new TempSimulation(bioModel.getSimulation(0), false);
+        SimulationContext simContext = null;
+        try {
+            simContext = bioModel.getSimulationContext(sim);
+        } catch (ObjectNotFoundException e) {
+            e.printStackTrace();
+        }
 
         SimulationJob simJob = new SimulationJob(sim, 0, null);
         SimulationTask simTask = new SimulationTask(simJob, 0);
@@ -47,33 +60,33 @@ public class RungeKuttaFelhbergHelper {
         }
 
 //        ODESolverResultSet resultSet = null;
-//        ClientTaskStatusSupport support = new ClientTaskStatusSupport() {
-//            @Override
-//            public void setMessage(String message) {
-//
-//            }
-//
-//            @Override
-//            public void setProgress(int progress) {
-//
-//            }
-//
-//            @Override
-//            public int getProgress() {
-//                return 0;
-//            }
-//
-//            @Override
-//            public boolean isInterrupted() {
-//                return false;
-//            }
-//
-//            @Override
-//            public void addProgressDialogListener(ProgressDialogListener progressDialogListener) {
-//
-//            }
-//        };
-        ODESimData odeSimData = null;
+        ClientTaskStatusSupport support = new ClientTaskStatusSupport() {
+            @Override
+            public void setMessage(String message) {
+
+            }
+
+            @Override
+            public void setProgress(int progress) {
+
+            }
+
+            @Override
+            public int getProgress() {
+                return 0;
+            }
+
+            @Override
+            public boolean isInterrupted() {
+                return false;
+            }
+
+            @Override
+            public void addProgressDialogListener(ProgressDialogListener progressDialogListener) {
+
+            }
+        };
+        ODESimData odeSimData;
 
         //            Solver solver = new RungeKuttaFehlbergSolver(simTask, outDir);
 //
@@ -108,6 +121,36 @@ public class RungeKuttaFelhbergHelper {
 //            });
 //
 //            solver.startSolver();
+
+//                    newSolver.addSolverListener(new SolverListener() {
+//                public void solverStopped(SolverEvent event) {
+//                    support.setMessage(event.getSimulationMessage().getDisplayMessage());
+//                }
+//                public void solverStarting(SolverEvent event) {
+//                    String displayMessage = event.getSimulationMessage().getDisplayMessage();
+//                    System.out.println(displayMessage);
+//                    support.setMessage(displayMessage);
+//                    if(displayMessage.equals(SimulationMessage.MESSAGE_SOLVEREVENT_STARTING_INIT.getDisplayMessage())) {
+//                        support.setProgress(75);
+//                    } else if(displayMessage.equals(SimulationMessage.MESSAGE_SOLVER_RUNNING_INPUT_FILE.getDisplayMessage())) {
+//                        support.setProgress(90);
+//                    }
+//                }
+//                public void solverProgress(SolverEvent event) {
+//                    support.setMessage("Running...");
+//                    int progress = (int)(event.getProgress() * 100);
+//                    support.setProgress(progress);
+//                }
+//                public void solverPrinted(SolverEvent event) {
+//                    support.setMessage("Running...");
+//                }
+//                public void solverFinished(SolverEvent event) {
+//                    support.setMessage(event.getSimulationMessage().getDisplayMessage());
+//                }
+//                public void solverAborted(SolverEvent event) {
+//                    support.setMessage(event.getSimulationMessage().getDisplayMessage());
+//                }
+//            });
 
         newSolver.startSolver();
         while (true){
@@ -147,6 +190,30 @@ public class RungeKuttaFelhbergHelper {
         }
         odeSimData = odeDataBlock.getODESimData();
 
+        ArrayList<String> varNameList = new ArrayList<String>();
+        for (SpeciesContextSpec scs : simContext.getReactionContext().getSpeciesContextSpecs()){
+            varNameList.add(scs.getSpeciesContext().getName());
+        }
+        String[] varNames = varNameList.toArray(new String[0]);
+
+        OutputTimeSpec outputTimeSpec = sim.getSolverTaskDescription().getOutputTimeSpec();
+
+        ArrayList<Double> sampleTimeList = new ArrayList<Double>();
+        if (outputTimeSpec instanceof UniformOutputTimeSpec){
+            double endingTime = sim.getSolverTaskDescription().getTimeBounds().getEndingTime();
+            double dT = ((UniformOutputTimeSpec)outputTimeSpec).getOutputTimeStep();
+            int currTimeIndex=0;
+            while (currTimeIndex*dT <= (endingTime+1e-8)){
+                sampleTimeList.add(currTimeIndex*dT);
+                currTimeIndex++;
+            }
+        }
+        double[] sampleTimes = new double[sampleTimeList.size()];
+        for (int i=0;i<sampleTimes.length;i++){
+            sampleTimes[i] = sampleTimeList.get(i);
+        }
+
+        TimeSeriesMultitrialData sampleDataDeterministic = new TimeSeriesMultitrialData("determinstic",varNames, sampleTimes, 1);
 //            timeSeriesMultitrialData.addDataSet(odeSimData,trialIndex);
 
 
