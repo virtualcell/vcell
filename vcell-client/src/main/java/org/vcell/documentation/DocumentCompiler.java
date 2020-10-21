833a76b5dd1acb4743488214b67dbe173d53e555
acowan <acowan@0901152-laptop.cam.uchc.edu>
2020-10-21 16:01:00 -0400

    update help ver 7.2
    Edits to help relate to move of current alpha to release

--- vcell-client/src/main/java/org/vcell/documentation/DocumentCompiler.java
+++ vcell-client/src/main/java/org/vcell/documentation/DocumentCompiler.java
@@ -5,7 +5,7 @@
  * You may not use this file except in compliance with the License.
  * You may obtain a copy of the License at:
  *
- *  http://www.opensource.org/licenses/mit-license.php
+ *  https://www.opensource.org/licenses/mit-license.php
  */
 
 package org.vcell.documentation;
