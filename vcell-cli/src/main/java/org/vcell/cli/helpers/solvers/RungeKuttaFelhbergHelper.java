930a7872180d00128b367c3a7b7e63c3ab828946
GMarupilla <marupilla@mail.com>
2020-10-29 14:48:58 -0400

    Code clean

--- vcell-cli/src/main/java/org/vcell/cli/helpers/solvers/RungeKuttaFelhbergHelper.java
+++ vcell-cli/src/main/java/org/vcell/cli/helpers/solvers/RungeKuttaFelhbergHelper.java
@@ -4,39 +4,26 @@ import cbit.vcell.biomodel.BioModel;
 import cbit.vcell.client.ClientSimManager;
 import cbit.vcell.mapping.SimulationContext;
 import cbit.vcell.mapping.SpeciesContextSpec;
-import cbit.vcell.math.MathException;
 import cbit.vcell.messaging.server.SimulationTask;
-import cbit.vcell.parser.Discontinuity;
 import cbit.vcell.parser.ExpressionException;
-import cbit.vcell.simdata.*;
+import cbit.vcell.simdata.ODEDataBlock;
+import cbit.vcell.simdata.SimulationData;
 import cbit.vcell.solver.*;
-import cbit.vcell.solver.ode.*;
-import cbit.vcell.solver.server.*;
+import cbit.vcell.solver.ode.ODESimData;
+import cbit.vcell.solver.ode.ODESolverResultSet;
+import cbit.vcell.solver.server.Solver;
+import cbit.vcell.solver.server.SolverStatus;
 import org.vcell.cli.CLIUtils;
-import org.vcell.cli.helpers.sbml.SBMLSolverHelper;
-import org.vcell.stochtest.StochtestFileUtils;
 import org.vcell.stochtest.TimeSeriesMultitrialData;
-import org.vcell.util.ClientTaskStatusSupport;
 import org.vcell.util.DataAccessException;
-import org.vcell.util.ObjectNotFoundException;
-import org.vcell.util.ProgressDialogListener;
-import org.vcell.util.exe.Executable;
-import org.vcell.util.exe.ExecutableException;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
-
-import static cbit.vcell.simdata.SimDataConstants.LOGFILE_EXTENSION;
-import static cbit.vcell.simdata.SimDataConstants.ODE_DATA_EXTENSION;
 
 public class RungeKuttaFelhbergHelper {
 
-    public static ODESolverResultSet solve(File outDir, String taskId, BioModel bioModel){
+    public static ODESolverResultSet solve(File outDir, String taskId, BioModel bioModel) {
         String docName = bioModel.getName();
 
 //        Simulation sim = bioModel.getSimulation(0);
@@ -62,7 +49,7 @@ public class RungeKuttaFelhbergHelper {
         if (newSolver != null) {
             newSolver.startSolver();
         }
-        while (true){
+        while (true) {
             try {
                 Thread.sleep(250);
             } catch (InterruptedException e) {
@@ -79,7 +66,7 @@ public class RungeKuttaFelhbergHelper {
                 }
                 if (solverStatus.getStatus() != SolverStatus.SOLVER_STARTING &&
                         solverStatus.getStatus() != SolverStatus.SOLVER_READY &&
-                        solverStatus.getStatus() != SolverStatus.SOLVER_RUNNING){
+                        solverStatus.getStatus() != SolverStatus.SOLVER_RUNNING) {
                     break;
                 }
             }
@@ -107,99 +94,51 @@ public class RungeKuttaFelhbergHelper {
         }
 
 
-
-
         ArrayList<String> varNameList = new ArrayList<String>();
-        for (SpeciesContextSpec scs : simContext.getReactionContext().getSpeciesContextSpecs()){
+        for (SpeciesContextSpec scs : simContext.getReactionContext().getSpeciesContextSpecs()) {
             varNameList.add(scs.getSpeciesContext().getName());
         }
         String[] varNames = varNameList.toArray(new String[0]);
 
-        Simulation solvedSim =newSolver.getSimulationJob().getSimulation();
+        Simulation solvedSim = newSolver.getSimulationJob().getSimulation();
         OutputTimeSpec outputTimeSpec = solvedSim.getSolverTaskDescription().getOutputTimeSpec();
 
         ArrayList<Double> sampleTimeList = new ArrayList<Double>();
-        if (outputTimeSpec != null){
+        if (outputTimeSpec != null) {
             double endingTime = solvedSim.getSolverTaskDescription().getTimeBounds().getEndingTime();
-            double initialTime  = solvedSim.getSolverTaskDescription().getTimeBounds().getStartingTime();
+            double initialTime = solvedSim.getSolverTaskDescription().getTimeBounds().getStartingTime();
             int rowCount = odeSimData.getRowCount();
-            //double dT = 1.1862396206;
-            double dT = (endingTime - initialTime)/rowCount;
-            int currTimeIndex=0;
-//            while (currTimeIndex*dT <= (endingTime+1e-8)){
-//                sampleTimeList.add(currTimeIndex*dT);
-//                currTimeIndex++;
-//            }
+            double dT = (endingTime - initialTime) / rowCount;
+            int currTimeIndex = 0;
 
-            while (currTimeIndex < rowCount){
-                sampleTimeList.add(currTimeIndex*dT);
+            while (currTimeIndex < rowCount) {
+                sampleTimeList.add(currTimeIndex * dT);
                 currTimeIndex++;
             }
 
         }
         double[] sampleTimes = new double[sampleTimeList.size()];
-        for (int i=0;i<sampleTimes.length;i++){
+        for (int i = 0; i < sampleTimes.length; i++) {
             sampleTimes[i] = sampleTimeList.get(i);
         }
 
         boolean[] wantThisTime = new boolean[sampleTimes.length];
-        for(int i=0; i<wantThisTime.length; i++) {
+        for (int i = 0; i < wantThisTime.length; i++) {
             wantThisTime[i] = true;
         }
 
         double data[][][] = new double[0][][];
 
         boolean[] wantTime = {true};
-//        try {
-//            data = simData.getSimDataTimeSeries(null, varNames, null, wantTime, new DataSetControllerImpl.ProgressListener() {
-//                @Override
-//                public void updateProgress(double progress) {
-//                    System.out.println(progress);
-//                }
-//
-//                @Override
-//                public void updateMessage(String message) {
-//                    System.out.println(message);
-//
-//                }
-//            });
-//        } catch (DataAccessException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
 
         System.out.println(data);
+        TimeSeriesMultitrialData sampleDataDeterministic = new TimeSeriesMultitrialData(taskId, varNames, sampleTimes, 1);
 
-//        DataManager dataManager = null;
-//        if (sim.isSpatial()) {
-//            dataManager = new PDEDataManager(outputContext,vcDataManager, vcDataId);
-//        } else {
-//            dataManager = new ODEDataManager(outputContext,vcDataManager, vcDataId);
-//        }
-        TimeSeriesMultitrialData sampleDataDeterministic = new TimeSeriesMultitrialData(taskId,varNames, sampleTimes, 1);
-
-//        int numberOfDataPoints = sampleTimes.length;
-//        int numberOfVariables = varNameList.size();
-
-//        for (int varIndex = 0; varIndex < varNames.length; varIndex++) {
-//
-//            for(int timeIndex = 0; timeIndex < sampleTimes.length; timeIndex++) {
-//                try {
-//                    sampleDataDeterministic.data[varIndex][timeIndex][0] = simData.getSimDataBlock(null, varNames[varIndex], sampleTimes[timeIndex]).getData()[0];
-//                } catch (DataAccessException e) {
-//                    e.printStackTrace();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
         try {
             sampleDataDeterministic.addDataSet(odeSimData, 0);
         } catch (ExpressionException e) {
             e.printStackTrace();
         }
-     //       timeSeriesMultitrialData.addDataSet(odeSimData,trialIndex);
 
 
         CLIUtils.saveTimeSeriesMultitrialDataAsCSV(sampleDataDeterministic, outDir);
