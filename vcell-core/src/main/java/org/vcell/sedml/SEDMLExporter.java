d11101afcf83157ebd69e873fd77b1f67e6afab0
vasilescu <vasilescu@Cotzo.cam.uchc.edu>
2020-12-17 01:10:36 -0500

    Exporting algorithm parameters to sedml.
    absolute, relative error tolerance, with complex dependencies based on
    solver type

--- vcell-core/src/main/java/org/vcell/sedml/SEDMLExporter.java
+++ vcell-core/src/main/java/org/vcell/sedml/SEDMLExporter.java
@@ -293,21 +293,32 @@ public class SEDMLExporter {
 						UniformTimeCourse utcSim = new UniformTimeCourse(TokenMangler.mangleToSName(simName), simName, startingTime, startingTime, 
 								vcSimTimeBounds.getEndingTime(), (int) simTaskDesc.getExpectedNumTimePoints(), sedmlAlgorithm);
 						
-//						String algorithmNotesStr = "";
-						if(vcSolverDesc.hasErrorTolerance()) {			// deal with error tolerance
+						boolean enableAbsoluteErrorTolerance;		// --------- deal with error tolerance
+						boolean enableRelativeErrorTolerance;
+						if (vcSolverDesc.isSemiImplicitPdeSolver() || vcSolverDesc.isChomboSolver()) {
+							enableAbsoluteErrorTolerance = false;
+							enableRelativeErrorTolerance = true;
+						} else if (vcSolverDesc.hasErrorTolerance()) {
+							enableAbsoluteErrorTolerance = true;
+							enableRelativeErrorTolerance = true;
+						} else {
+							enableAbsoluteErrorTolerance = false;
+							enableRelativeErrorTolerance = false;
+						}	
+						if(enableAbsoluteErrorTolerance) {
 							ErrorTolerance et = simTaskDesc.getErrorTolerance();
 							String kisaoStr = ErrorTolerance.ErrorToleranceDescription.Absolute.getKisao();
 							AlgorithmParameter sedmlAlgorithmParameter = new AlgorithmParameter(kisaoStr, et.getAbsoluteErrorTolerance()+"");
 							sedmlAlgorithm.addAlgorithmParameter(sedmlAlgorithmParameter);
-//							String str = ErrorTolerance.ErrorToleranceDescription.Absolute.getDescription() + " : " + kisaoStr;
-//							algorithmNotesStr += str;
-							kisaoStr = ErrorTolerance.ErrorToleranceDescription.Relative.getKisao();
-							sedmlAlgorithmParameter = new AlgorithmParameter(kisaoStr, et.getRelativeErrorTolerance()+"");
+						}
+						if(enableRelativeErrorTolerance) {
+							ErrorTolerance et = simTaskDesc.getErrorTolerance();
+							String kisaoStr = ErrorTolerance.ErrorToleranceDescription.Relative.getKisao();
+							AlgorithmParameter sedmlAlgorithmParameter = new AlgorithmParameter(kisaoStr, et.getRelativeErrorTolerance()+"");
 							sedmlAlgorithm.addAlgorithmParameter(sedmlAlgorithmParameter);
 						}
-
 						
-						boolean enableDefaultTimeStep;		// deal with time step (code adapted from TimeSpecPanel.refresh()
+						boolean enableDefaultTimeStep;		// ---------- deal with time step (code adapted from TimeSpecPanel.refresh()
 						boolean enableMinTimeStep;
 						boolean enableMaxTimeStep;
 						if (vcSolverDesc.compareEqual(SolverDescription.StochGibson)) { // stochastic time
@@ -351,14 +362,10 @@ public class SEDMLExporter {
 							sedmlAlgorithm.addAlgorithmParameter(sedmlAlgorithmParameter);
 						}
 						
-						
-						if(simTaskDesc.getSimulation().getMathDescription().isNonSpatialStoch()) {	// deal with seed
+						if(simTaskDesc.getSimulation().getMathDescription().isNonSpatialStoch()) {	// ------- deal with seed
 							NonspatialStochSimOptions nssso = simTaskDesc.getStochOpt();
 							if(nssso.isUseCustomSeed()) {
 								// TODO: don't know where the kisao belongs, maybe we should consolidate all in one single ontology file
-//								// TODO: our jlibsedml has an old subset of the kisao ontology (below KISAO:0000100), we need a complete one
-//								KisaoOntology ko = KisaoOntology.getInstance();		// usage example
-//								KisaoTerm kt = ko.getTermById("KISAO:0000064");
 								AlgorithmParameter sedmlAlgorithmParameter = new AlgorithmParameter("KISAO:0000488", nssso.getCustomSeed()+"");
 								sedmlAlgorithm.addAlgorithmParameter(sedmlAlgorithmParameter);
 							}
