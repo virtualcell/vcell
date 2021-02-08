d59ec4aef2daa30a07f9df88c6cef98ff908da13
GMarupilla <marupilla@mail.com>
2021-02-08 13:54:38 -0500

    Add sample status YAML format

--- vcell-cli/src/main/java/org/vcell/cli/simstatus/StatusYaml.java
+++ vcell-cli/src/main/java/org/vcell/cli/simstatus/StatusYaml.java
@@ -4,3 +4,28 @@ public class StatusYaml {
     SedDocument[] sedDocuemtns;
     String status;
 }
+
+// Sample STATUS YML
+/*
+sedDocuments:
+  BIOMD0000000912_sim.sedml:
+    outputs:
+      BIOMD0000000912_sim:
+        dataSets:
+          data_set_E: SKIPPED
+          data_set_I: PASSED
+          data_set_T: SKIPPED
+          data_set_time: SKIPPED
+        status: SKIPPED
+      plot_1:
+        curves:
+          plot_1_E_time: SKIPPED
+          plot_1_I_time: SKIPPED
+          plot_1_T_time: SKIPPED
+        status: SKIPPED
+    status: SUCCEEDED
+    tasks:
+      BIOMD0000000912_sim:
+        status: SKIPPED
+status: SUCCEEDED
+* */
