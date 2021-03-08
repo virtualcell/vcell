78fc215900d38a66abeda8e637c757f9d21a26f4
GMarupilla <marupilla@mail.com>
2021-03-08 15:36:02 -0500

    VCML to OMEX conversion working, make it dynamic from CLI

--- vcell-cli/src/main/java/org/vcell/cli/vcml/VcmlOmexConversion.java
+++ vcell-cli/src/main/java/org/vcell/cli/vcml/VcmlOmexConversion.java
@@ -1,48 +1,85 @@
 package org.vcell.cli.vcml;
 
+import cbit.util.xml.XmlUtil;
 import cbit.vcell.biomodel.BioModel;
-import cbit.vcell.mapping.SimulationContext;
+import cbit.vcell.resource.ResourceUtil;
+import cbit.vcell.util.NativeLoader;
 import cbit.vcell.xml.XMLSource;
 import cbit.vcell.xml.XmlHelper;
-import org.vcell.sbml.vcell.SBMLExporter;
+import org.sbml.libcombine.CombineArchive;
+import org.sbml.libcombine.KnownFormats;
+import org.vcell.sedml.SEDMLExporter;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.nio.file.Paths;
 
 public class VcmlOmexConversion {
 
-    public static void vcmlToBiomodel(String vcmlPath, String xmlPath) throws Exception {
-        String sbmlString = null;
+    public static void vcmlToSedmlSbml(String vcmlPath, String xmlPath) throws Exception {
         File vcmlFilePath = new File(vcmlPath);
-
         BioModel bioModel = XmlHelper.XMLToBioModel(new XMLSource(vcmlFilePath));
-
         bioModel.refreshDependencies();
 
-        int simContextLength = bioModel.getNumSimulations();
-        for (int i = 0; i < simContextLength; i++) {
-            System.out.println();
-            SimulationContext sc2 = bioModel.getSimulationContext(i);
-            boolean isSpatial = sc2.getGeometry().getDimension()>0;
-            SBMLExporter exporter = new SBMLExporter(bioModel,3,1,isSpatial);
-            sc2.refreshMathDescription(null, SimulationContext.NetworkGenerationRequirements.ComputeFullNoTimeout);
-            exporter.setSelectedSimContext(sc2);
-            sbmlString = exporter.getSBMLString();
-            try {
-                PrintWriter out = new PrintWriter(new File(String.valueOf(Paths.get(System.getProperty("user.dir"),xmlPath))));
-                out.print(sbmlString);
-                out.flush();
-            } catch (FileNotFoundException e) {
-                System.err.println("Unable to find path, failed with err: " + e.getMessage());
-            }
-        }
+        // NOTE: SEDML exporter exports both SEDML as well as required SBML
+        SEDMLExporter sedmlExporter = new SEDMLExporter(bioModel, 1, 1);
+        String sedmlString = sedmlExporter.getSEDMLFile(String.valueOf(Paths.get(System.getProperty("user.dir"), xmlPath)));
+        XmlUtil.writeXMLStringToFile(sedmlString, String.valueOf(Paths.get(System.getProperty("user.dir"), xmlPath, xmlPath + ".sedml")), true);
+        createCliOmexArchive(String.valueOf(Paths.get(System.getProperty("user.dir"), xmlPath)), "try", vcmlPath);
 
     }
 
+    public static void createCliOmexArchive(String srcFolder, String sFileName, String vcmlPath) {
+        ResourceUtil.setNativeLibraryDirectory();
+        NativeLoader.load("combinej");
+        try {
+            CombineArchive archive = new CombineArchive();
+
+            String[] files;
+
+            File dir = new File(srcFolder);
+            files = dir.list();
+
+            for (String sd : files) {
+                if (sd.endsWith(".sedml")) {
+                    archive.addFile(
+                            Paths.get(srcFolder, sd).toString(),
+                            sd, // target file name
+                            KnownFormats.lookupFormat("sedml"),
+                            true // mark file as master
+                    );
+                } else {
+                    archive.addFile(
+                            Paths.get(srcFolder, sd).toString(),
+                            sd, // target file name
+                            KnownFormats.lookupFormat("sbml"),
+                            false // mark file as master
+                    );
+                }
+            }
+
+            archive.addFile(
+                    Paths.get(vcmlPath).toString(),
+                    sFileName + ".vcml",
+                    KnownFormats.lookupFormat("vcml"),
+                    false
+            );
+
+
+            archive.writeToFile(Paths.get(srcFolder, sFileName + ".omex").toString());
+
+            // Removing files after archiving
+            boolean isDeleted = false;
+            for (String sd : files) {
+                isDeleted = Paths.get(srcFolder, sd).toFile().delete();
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException("createZipArchive threw exception: " + e.getMessage());
+        }
+    }
+
     public static void main(String[] args) throws Exception {
-        vcmlToBiomodel("/Users/akhilteja/projects/virtualCell/vcell/sample_omex_files/_00_omex_test_cvode.vcml", "sbml.xml");
+        vcmlToSedmlSbml("/Users/akhilteja/projects/virtualCell/vcell/sample_omex_files/_00_omex_test_cvode.vcml", "model");
     }
 
 
