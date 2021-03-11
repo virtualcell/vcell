dbf96caa64c6c6d3eed5cb9fa186a3a16bc0d037
GMarupilla <marupilla@mail.com>
2021-03-11 14:29:02 -0500

    Fix the issue when importing nested SED document

--- vcell-core/src/main/java/org/jlibsedml/execution/ArchiveModelResolver.java
+++ vcell-core/src/main/java/org/jlibsedml/execution/ArchiveModelResolver.java
@@ -1,8 +1,10 @@
 package org.jlibsedml.execution;
 
+import java.io.File;
 import java.net.URI;
 import java.util.List;
 
+import cbit.vcell.resource.OperatingSystemInfo;
 import org.jlibsedml.ArchiveComponents;
 import org.jlibsedml.IModelContent;
 
@@ -12,10 +14,19 @@ public class ArchiveModelResolver implements IModelResolver {
         this.ac=ac;
     }
     public String getModelXMLFor(URI modelURI) {
-        String rc=null;
+        String rc = null;
+        // Considering the import for nested SED-ML document
+        String regExp = null;
         List<IModelContent> children = ac.getModelFiles();
         for (IModelContent imc: children) {
-            if(imc.getName().equals(modelURI.toString())){
+            if (OperatingSystemInfo.getInstance().isWindows()) {
+                regExp = "\\\\";
+            } else {
+                regExp = "/";
+            }
+            String[] splitURI = modelURI.toString().split(regExp);
+            String filename = splitURI[splitURI.length-1];
+            if(imc.getName().equals(filename)){
                 rc= imc.getContents();
             }
         }
