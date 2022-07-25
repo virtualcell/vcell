cb771350e26cc4c214fd7978d81e44d81eb5df1c
CodeByDrescher <drescher@uchc.edu>
2022-07-25 10:57:45 -0400

    Bugfix; refactor of class names not corrected in CLIStandalone

--- vcell-cli/src/main/java/org/vcell/cli/CLIStandalone.java
+++ vcell-cli/src/main/java/org/vcell/cli/CLIStandalone.java
@@ -1,13 +1,11 @@
 package org.vcell.cli;
 
-import org.vcell.cli.run.ExecuteImplementation;
-import org.vcell.cli.vcml.ConvertImplementation;
 import org.vcell.cli.commands.*;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 
-@Command(name = "CLIStandalone", subcommands = {ConvertImplementation.class, ExecuteImplementation.class, VersionCommand.class, CommandLine.HelpCommand.class})
+@Command(name = "CLIStandalone", subcommands = {ConvertCommand.class, ExecuteCommand.class, VersionCommand.class, CommandLine.HelpCommand.class})
 public class CLIStandalone {
 
     public static void main(String[] args) {
