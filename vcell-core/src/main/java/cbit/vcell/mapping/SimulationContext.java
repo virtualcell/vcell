750ae9356cb7980d1fe1ce42877fe19f00a038aa
Jim Schaff <schaff.jim@gmail.com>
2022-08-16 18:00:55 -0400

    revert hard-coded disable of mass conservation

--- vcell-core/src/main/java/cbit/vcell/mapping/SimulationContext.java
+++ vcell-core/src/main/java/cbit/vcell/mapping/SimulationContext.java
@@ -2721,8 +2721,7 @@ public boolean isUsingConcentration() {
 }
 
 public boolean isUsingMassConservationModelReduction() {
-	//return bMassConservationModelReduction;
-	return false;
+	return bMassConservationModelReduction;
 }
 public void setUsingMassConservationModelReduction(boolean bMassConservationModelReduction) {
 	this.bMassConservationModelReduction = bMassConservationModelReduction;
