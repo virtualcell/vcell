fbde03c57e2c44131c81022f8e584ee7b0b4a746
Jim Schaff <schaff.jim@gmail.com>
2022-09-19 19:01:40 -0400

    spatial apps with lumped reactions: SBML Export unsupported (math fails)

--- vcell-core/src/main/java/org/vcell/sbml/vcell/SBMLExporter.java
+++ vcell-core/src/main/java/org/vcell/sbml/vcell/SBMLExporter.java
@@ -2492,6 +2492,14 @@ public static void validateSimulationContextSupport(SimulationContext simulation
 	if (simulationContext.getMicroscopeMeasurement()!=null && simulationContext.getMicroscopeMeasurement().getFluorescentSpecies().size()>0){
 		throw new UnsupportedSbmlExportException("MicroscopyMeasurement '"+simulationContext.getMicroscopeMeasurement().getName()+"' defined involving convolution with kernel (point spread function), SBML Export is not supported");
 	}
+
+	// Check if this is a spatial deterministic application and the model contains lumped kinetics.
+	// VCell math has vcRegionVolume('cyt') functions, SBML round trip will use a constant size instead.
+	// The round trip validation will always fail.
+	Optional<ReactionStep> lumpedReaction = Arrays.stream(simulationContext.getModel().getReactionSteps()).filter(rs -> rs.getKinetics() instanceof LumpedKinetics).findFirst();
+	if (simulationContext.getGeometry().getDimension()>0 && simulationContext.getApplicationType()==Application.NETWORK_DETERMINISTIC && lumpedReaction.isPresent()){
+		throw new UnsupportedSbmlExportException("Lumped reaction '"+lumpedReaction.get().getName()+"' in spatial application, SBML Export is not supported");
+	}
 }
 
 /**
