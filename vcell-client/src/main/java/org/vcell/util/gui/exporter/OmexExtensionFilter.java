608f2b6eae7d203a1d15104218b0230c4e9b6e82
Jim Schaff <schaff.jim@gmail.com>
2022-12-07 16:14:13 -0500

    Omex export in GUI calls BioModel.updateAll(true) to fix size mappings.

--- vcell-client/src/main/java/org/vcell/util/gui/exporter/OmexExtensionFilter.java
+++ vcell-client/src/main/java/org/vcell/util/gui/exporter/OmexExtensionFilter.java
@@ -66,6 +66,7 @@ public class OmexExtensionFilter extends SedmlExtensionFilter {
 			}
 			boolean bRoundTripSBMLValidation = true;
 			boolean bFromCLI = false;
+			bioModel.updateAll(true);
 			SEDMLExporter.writeBioModel(bioModel, exportFile, modelFormat, bFromCLI, bRoundTripSBMLValidation, bCreateOmexArchive);
 		}
 	}
