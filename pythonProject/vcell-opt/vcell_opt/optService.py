d0fca1deeaa8d1a4bb603795edb2dc7cbdd4f359
Jim Schaff <schaff.jim@gmail.com>
2022-12-08 11:09:38 -0500

    python param estimation w/ copasi-basico, pytest, poetry, mypy, python3

---
+++ pythonProject/vcell-opt/vcell_opt/optService.py
@@ -0,0 +1,66 @@
+import json
+from pathlib import Path
+
+import basico
+import pandas as pd
+import typer
+from typing import Dict
+
+from vcell_opt.data import OptProblem, Vcellopt, VcelloptStatus, OptResultSet
+from vcell_opt.optUtils import get_reference_data, get_fit_parameters, get_copasi_opt_method_settings, \
+    result_set_from_fit
+
+
+def run_command(opt_file: Path = typer.Argument(..., file_okay=True, dir_okay=False, exists=True,
+                                                help="optimization input json file"),
+                result_file: Path = typer.Argument(..., file_okay=True, dir_okay=False,
+                                                   help="optimization result output json file")):
+    if opt_file is None or result_file is None:
+        print("use --help for help")
+        return typer.Exit(-1)
+
+    with open(opt_file, "rb") as f_optfile:
+        opt_problem: OptProblem = OptProblem.from_json_data(json.load(f_optfile))
+
+    basico.load_model(opt_problem.math_model_sbml_contents)
+
+    exp_data = get_reference_data(opt_problem)
+    basico.add_experiment('exp1', data=exp_data)
+
+    task_settings = basico.get_task_settings('Parameter Estimation')
+    task_settings['method'] = get_copasi_opt_method_settings(opt_problem)
+    basico.set_task_settings('Parameter Estimation', task_settings)
+
+    fit_items = get_fit_parameters(opt_problem)
+    basico.set_fit_parameters(fit_items)
+
+    results: pd.DataFrame = basico.run_parameter_estimation(update_model=True)
+    assert results is not None
+
+    fit_solution: pd.DataFrame = basico.task_parameterestimation.get_parameters_solution()
+
+    opt_parameter_values: Dict[str, float] = result_set_from_fit(fit_solution)
+    fit_statistics: Dict[str, float] = basico.task_parameterestimation.get_fit_statistic(include_parameters=True,
+                                                                                         include_experiments=True,
+                                                                                         include_fitted=True)
+    objective_function = fit_statistics['obj']
+    num_function_evaluations = fit_statistics['f_evals']
+
+    result_set = OptResultSet(num_function_evaluations=int(num_function_evaluations),
+                              objective_function=objective_function, opt_parameter_values=opt_parameter_values)
+
+    opt_result_set: Dict[str, float] = result_set_from_fit(fit_solution)
+
+    status = VcelloptStatus.COMPLETE
+    status_message = str(basico.task_parameterestimation.get_fit_statistic(
+        include_parameters=True, include_experiments=True, include_fitted=True))
+
+    opt_run: Vcellopt = Vcellopt(opt_problem=opt_problem, opt_result_set=result_set, status=VcelloptStatus.COMPLETE,
+                                 status_message=status_message)
+    with open(result_file, "w") as f_result_file:
+        json_data = opt_run.to_json_data()
+        json.dump(json_data, f_result_file)
+
+
+if __name__ == "__main__":
+    typer.run(run_command)
---
+++ pythonProject/vcell-opt/vcell_opt/optUtils.py
@@ -0,0 +1,94 @@
+from typing import Dict, List, Union
+
+import basico
+import pandas as pd
+
+from .data import CopasiOptimizationMethodOptimizationMethodType, OptProblem, CopasiOptimizationParameterParamType
+
+
+def get_copasi_opt_method_settings(vcell_opt_problem: OptProblem) -> Dict[str, Union[str, float]]:
+    method: str = _get_copasi_opt_method(vcell_opt_problem.copasi_optimization_method.optimization_method_type)
+    settings: Dict[str, Union[str, float]] = dict(name=method)
+    for method_param in vcell_opt_problem.copasi_optimization_method.optimization_parameter:
+        settings[_get_copasi_method_param(method_param.param_type)] = method_param.value
+    return settings
+
+
+def result_set_from_fit(fit_solution: pd.DataFrame) -> Dict[str, float]:
+    return {name.replace('Values[', '').replace(']', ''): value for name, value in
+     zip(fit_solution.index, fit_solution['sol'])}
+
+
+def _get_copasi_method_param(param_type: CopasiOptimizationParameterParamType) -> str:
+    if param_type == CopasiOptimizationParameterParamType.NUMBER_OF_GENERATIONS:
+        return "Number of Generations"
+    if param_type == CopasiOptimizationParameterParamType.PF:
+        return "Pf"
+    if param_type == CopasiOptimizationParameterParamType.COOLING_FACTOR:
+        return "Cooling Factor"
+    if param_type == CopasiOptimizationParameterParamType.ITERATION_LIMIT:
+        return "Iteration Limit"
+    if param_type == CopasiOptimizationParameterParamType.NUMBER_OF_ITERATIONS:
+        return "Number of Iterations"
+    if param_type == CopasiOptimizationParameterParamType.POPULATION_SIZE:
+        return "Population Size"
+    if param_type == CopasiOptimizationParameterParamType.RANDOM_NUMBER_GENERATOR:
+        return "Random Number Generator"
+    if param_type == CopasiOptimizationParameterParamType.RHO:
+        return "Rho"
+    if param_type == CopasiOptimizationParameterParamType.SCALE:
+        return "Scale"
+    if param_type == CopasiOptimizationParameterParamType.SEED:
+        return "Seed"
+    if param_type == CopasiOptimizationParameterParamType.START_TEMPERATURE:
+        return "Start Temperature"
+    if param_type == CopasiOptimizationParameterParamType.TOLERANCE:
+        return "Tolerance"
+    raise Exception(f"unexpected parameter type {param_type}")
+
+
+def _get_copasi_opt_method(vcell_opt_type: CopasiOptimizationMethodOptimizationMethodType) -> str:
+    if vcell_opt_type == CopasiOptimizationMethodOptimizationMethodType.SRES:
+        return basico.PE.EVOLUTIONARY_STRATEGY_SRES
+    if vcell_opt_type == CopasiOptimizationMethodOptimizationMethodType.PRAXIS:
+        return basico.PE.PRAXIS
+    if vcell_opt_type == CopasiOptimizationMethodOptimizationMethodType.NELDER_MEAD:
+        return basico.PE.NELDER_MEAD
+    if vcell_opt_type == CopasiOptimizationMethodOptimizationMethodType.HOOKE_JEEVES:
+        return basico.PE.HOOKE_JEEVES
+    if vcell_opt_type == CopasiOptimizationMethodOptimizationMethodType.EVOLUTIONARY_PROGRAM:
+        return basico.PE.EVOLUTIONARY_PROGRAMMING
+    if vcell_opt_type == CopasiOptimizationMethodOptimizationMethodType.GENETIC_ALGORITHM:
+        return basico.PE.GENETIC_ALGORITHM
+    if vcell_opt_type == CopasiOptimizationMethodOptimizationMethodType.GENETIC_ALGORITHM_SR:
+        return basico.PE.GENETIC_ALGORITHM_SR
+    if vcell_opt_type == CopasiOptimizationMethodOptimizationMethodType.LEVENBERG_MARQUARDT:
+        return basico.PE.LEVENBERG_MARQUARDT
+    if vcell_opt_type == CopasiOptimizationMethodOptimizationMethodType.PARTICLE_SWARM:
+        return basico.PE.PARTICLE_SWARM
+    if vcell_opt_type == CopasiOptimizationMethodOptimizationMethodType.RANDOM_SEARCH:
+        return basico.PE.RANDOM_SEARCH
+    if vcell_opt_type == CopasiOptimizationMethodOptimizationMethodType.SIMULATED_ANNEALING:
+        return basico.PE.SIMULATED_ANNEALING
+    if vcell_opt_type == CopasiOptimizationMethodOptimizationMethodType.STEEPEST_DESCENT:
+        return basico.PE.STEEPEST_DESCENT
+    if vcell_opt_type == CopasiOptimizationMethodOptimizationMethodType.TRUNCATED_NEWTON:
+        return basico.PE.TRUNCATED_NEWTON
+    raise Exception(f"unexpected optimization type {vcell_opt_type}")
+
+
+def _fix_refvar_name(name: str) -> str:
+    if name == 't':
+        return 'Time'
+    return f"Values[{name}]"
+
+
+def get_reference_data(vcell_opt_problem: OptProblem) -> pd.DataFrame:
+    columns = [_fix_refvar_name(ref_var.var_name) for ref_var in vcell_opt_problem.reference_variable]
+    df = pd.DataFrame(vcell_opt_problem.data_set, columns=columns)
+    return df
+
+
+def get_fit_parameters(vcell_opt_problem: OptProblem) -> List[Dict[str, Union[str, float]]]:
+    fit_items: List[Dict[str, Union[str, float]]] = [dict(name=f"Values[{a.name}]", lower=a.min_value, upper=a.max_value) for a in vcell_opt_problem.parameter_description_list]
+    return fit_items
