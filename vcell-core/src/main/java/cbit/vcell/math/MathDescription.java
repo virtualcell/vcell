af23e3fc6cfefc2df8253babdd93b11a355422ad
moraru <moraru@uchc.edu>
2022-12-10 16:15:07 -0500

    try legacy var/domain mapping/naming in math compare

--- vcell-core/src/main/java/cbit/vcell/math/MathDescription.java
+++ vcell-core/src/main/java/cbit/vcell/math/MathDescription.java
@@ -3497,13 +3497,24 @@ public static MathCompareResults testEquivalencyWithRename(MathSymbolTableFactor
 public static MathCompareResults testEquivalency(MathSymbolTableFactory mathSymbolTableFactory, MathDescription mathDescription1, MathDescription mathDescription2) {
 // test commit
 	try {
+	
 		MathCompareResults invariantResults = mathDescription2.compareInvariantAttributes(mathDescription1,false);
 		if (!invariantResults.isEquivalent()){
-			//
-			// can't be equivalent or equal
-			//
-			return invariantResults;
-		}else if (mathDescription2.compareEqual(mathDescription1)){
+			if (invariantResults.decision.equals(Decision.MathDifferent_VARIABLE_NOT_FOUND_AS_FUNCTION)) {
+				tryLegacyVarNameDomain(mathDescription1, mathDescription2);
+				invariantResults = mathDescription2.compareInvariantAttributes(mathDescription1,false);
+				//
+				// can't be equivalent or equal
+				//
+				return invariantResults;
+			} else {
+				//
+				// can't be equivalent or equal
+				//
+				return invariantResults;				
+			}
+		}
+		if (mathDescription2.compareEqual(mathDescription1)){
 			return new MathCompareResults(Decision.MathEquivalent_NATIVE);
 		}else{
 			//
@@ -3535,6 +3546,33 @@ public static MathCompareResults testEquivalency(MathSymbolTableFactory mathSymb
 		String msg = "failure while testing for math equivalency: "+e.getMessage();
 		logger.error(msg, e);
 		throw new RuntimeException(msg, e);
+	}
+}
+
+private static void tryLegacyVarNameDomain(MathDescription mathDescription1, MathDescription mathDescription2)
+		throws ExpressionException {
+	TreeSet<String> stateVarNames1 = new TreeSet<String>(mathDescription1.getStateVariableNames());
+	TreeSet<String> stateVarNames2 = new TreeSet<String>(mathDescription2.getStateVariableNames());
+	ArrayList<String> sv1 = new ArrayList<String>(stateVarNames1);
+	ArrayList<String> sv2 = new ArrayList<String>(stateVarNames2);
+	List<Expression> exps = new ArrayList<Expression>();
+	mathDescription1.getAllExpressions(exps);
+
+	for (int i = 0; i < sv1.size(); i++) {
+		Variable var = mathDescription1.getVariable(sv1.get(i));
+		var.rename(sv2.get(i));
+		for (Expression exp : exps) {
+			exp.substituteInPlace(new Expression(sv1.get(i)), new Expression(sv2.get(i)));
+		}
+		if (var.getDomain() == null) {
+			SubDomain sd = mathDescription1.getSubDomain(mathDescription2.getVariable(var.getName()).getDomain().getName());
+			var.setDomain(new Domain(sd));
+			for (SubDomain sd1 : mathDescription1.getSubDomainCollection().toArray(new SubDomain[mathDescription1.getSubDomainCollection().size()])) {
+				if (sd1 != sd) {
+					sd1.removeEquation(var);
+				}
+			}
+		}
 	}
 }
 
