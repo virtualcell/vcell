01b7ae0d9e4b85636540823dfd6bab8872497a74
Jim Schaff <schaff.jim@gmail.com>
2023-01-04 09:22:13 -0500

    bug fix handles 23 more models with renamed overrides, more to come

--- vcell-core/src/test/java/org/vcell/sbml/SEDMLExporterSBMLTest.java
+++ vcell-core/src/test/java/org/vcell/sbml/SEDMLExporterSBMLTest.java
@@ -73,15 +73,12 @@ public class SEDMLExporterSBMLTest extends SEDMLExporterCommon {
 		faults.put("__export_adv_test.vcml", SEDML_FAULT.SIMULATION_NOT_FOUND_BY_NAME);  // roundtripped simulation not found with name 'spatialnoscan'
 		faults.put("biomodel_100596964.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_100961371.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
-		faults.put("biomodel_102061382.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT); // simulation 'Arp23 variation' in simContext 'Application0'
-		faults.put("biomodel_105608907.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT); // simulation 'F/G=10/190, Kd=0.01-0.2'  in simContext 'wt uniform'
 		faults.put("biomodel_10829774.vcml", SEDML_FAULT.MATH_OVERRIDE_NAMES_DIFFERENT);  // simulation 'set1' in simContext 'maximumAmplificationFig6b'
 		faults.put("biomodel_113655498.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_116929912.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_116929971.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_116930032.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
-		faults.put("biomodel_123269393.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT); // simulation 'Simulation1' in simContext 'Application2'
-		faults.put("biomodel_123269480.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT); // simulation 'Simulation1' in simContext 'Application2'
+		faults.put("biomodel_123269393.vcml", SEDML_FAULT.SIMULATION_NOT_FOUND_BY_NAME); // roundtripped simulation not found with name 'Simulation2'
 		faults.put("biomodel_123465498.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_123465505.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_124562627.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
@@ -91,8 +88,6 @@ public class SEDMLExporterSBMLTest extends SEDMLExporterCommon {
 		faults.put("biomodel_145545992.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_148700996.vcml", SEDML_FAULT.MATH_OVERRIDE_NAMES_DIFFERENT); // simulation 'Full sim' in simContext 'Fast B Compartmental'
 		faults.put("biomodel_149491513.vcml", SEDML_FAULT.MATH_OVERRIDE_NAMES_DIFFERENT); // simulation 'ss check 1' in simContext 'steady state compartment'
-		faults.put("biomodel_154208982.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT); // simulation 'RISC_mRNA_Deg_MA' in simContext 'Application0'
-		faults.put("biomodel_154210963.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT); // simulation 'Simulation0' in simContext 'Deterministic'
 		faults.put("biomodel_154961582.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_155016832.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_156134818.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
@@ -102,7 +97,6 @@ public class SEDMLExporterSBMLTest extends SEDMLExporterCommon {
 		faults.put("biomodel_168717401.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_169993006.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_17098642.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
-		faults.put("biomodel_171423920.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT); // simulation 'Prob 6  kf2 set to 10, A varied 1 to 500 (expand scale to see early behavior)' in simContext 'default for prob 5; multi sims for probs 5, 6 & 7'
 		faults.put("biomodel_17257105.vcml", SEDML_FAULT.MATH_OVERRIDE_NAMES_DIFFERENT);  // simulation '1 uM iso' in simContext 'compartmental'
 		faults.put("biomodel_17263179.vcml", SEDML_FAULT.MATH_OVERRIDE_NAMES_DIFFERENT);  // simulation '0.1 uM iso DcAMP 300' in simContext 'Figure 3'
 		faults.put("biomodel_188880263.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
@@ -110,13 +104,10 @@ public class SEDMLExporterSBMLTest extends SEDMLExporterCommon {
 		faults.put("biomodel_189321805.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);  // new
 		faults.put("biomodel_189512756.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_189513183.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
-		faults.put("biomodel_200965116.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT); // simulation 'Simulation0' in simContext 'Deterministic'
-		faults.put("biomodel_200999311.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT); // simulation 'Simulation1' in simContext 'Deterministic'
 		faults.put("biomodel_20253928.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_209284198.vcml", SEDML_FAULT.MATH_OVERRIDE_NAMES_DIFFERENT); // simulation 'Microinjection of FRanGDP' in simContext 'Normal model parameters'
 		faults.put("biomodel_211839191.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
-		faults.put("biomodel_220138697.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT); // simulation 'Fitting Results' in simContext '0D'
-		faults.put("biomodel_220138948.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT); // simulation 'Scan of albumin conc' in simContext '0D'
+		faults.put("biomodel_220138948.vcml", SEDML_FAULT.SIMULATION_NOT_FOUND_BY_NAME); // roundtripped simulation not found with name 'Uptake_Larger Jumps'
 		faults.put("biomodel_22403233.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_22403238.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_22403244.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
@@ -128,9 +119,7 @@ public class SEDMLExporterSBMLTest extends SEDMLExporterCommon {
 		faults.put("biomodel_232498815.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_26454052.vcml", SEDML_FAULT.MATH_OVERRIDE_NAMES_DIFFERENT);  // simulation 'Spine_using spine parameter values that fall in ranges of Harris et al (28)' in simContext '1D PIP2_fits 3D model that uses constructed 3D geometry'
 		faults.put("biomodel_26454463.vcml", SEDML_FAULT.MATH_OVERRIDE_NAMES_DIFFERENT);  // simulation 'lat diff & stim syn - decreased IP3' in simContext '3D PIP2_constructed geometry'
-		faults.put("biomodel_26928347.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT);  // simulation 'Actin Alone Opt' in simContext '0.5 ?M PRF'
 		faults.put("biomodel_27088120.vcml", SEDML_FAULT.MATH_OVERRIDE_NAMES_DIFFERENT);  // simulation 'Simulation36' in simContext 'DecayPC12-4block'
-		faults.put("biomodel_28139443.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT);  // simulation 'Actin Alone Opt' in simContext '0.5 ?M PRF'
 		faults.put("biomodel_2912851.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_2913730.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_2915537.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
@@ -140,18 +129,13 @@ public class SEDMLExporterSBMLTest extends SEDMLExporterCommon {
 		faults.put("biomodel_2930915.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_2962862.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_29897263.vcml", SEDML_FAULT.MATH_OVERRIDE_NAMES_DIFFERENT);  // simulation 'less GEF, broader range' in simContext 'Simulations for switch paper'
-		faults.put("biomodel_31523791.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT);  // simulation 'various cAMP conc steady state' in simContext '10 spines'
-		faults.put("biomodel_31584491.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT);  // simulation 'Simulation0' in simContext 'baseline condition'
 		faults.put("biomodel_32568171.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_32568356.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_34826524.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT); // simulation 'Cap=1, cof, prof varied' in simContext 'Steady State Turnover'
 		faults.put("biomodel_34855932.vcml", SEDML_FAULT.MATH_OVERRIDE_NAMES_DIFFERENT); // simulation 'INIT' in simContext 'cell4'
-		faults.put("biomodel_36053554.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT); // simulation 'WT' in simContext 'steady-state'
+		faults.put("biomodel_36053554.vcml", SEDML_FAULT.MATH_OVERRIDE_NAMES_DIFFERENT); // simulation 'EEE' in simContext 'steady-state'
 		faults.put("biomodel_36230715.vcml", SEDML_FAULT.MATH_OVERRIDE_NAMES_DIFFERENT); // simulation 'clonidine dose response' in simContext 'Simulations for switch paper'
 		faults.put("biomodel_38086434.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX); // new
-		faults.put("biomodel_40882931.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT); // simulation 'with laser' in simContext '3d image'
-		faults.put("biomodel_40883509.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT); // simulation 'with laser' in simContext '3d image'
-		faults.put("biomodel_49411430.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT); // simulation 'Simulation4' in simContext 'pombe actin polymerization'
 		faults.put("biomodel_55178308.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX); // new
 		faults.put("biomodel_55396830.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_59280306.vcml", SEDML_FAULT.MATH_OVERRIDE_NAMES_DIFFERENT); // simulation '1 uM iso' in simContext 'compartmental'
@@ -167,7 +151,7 @@ public class SEDMLExporterSBMLTest extends SEDMLExporterCommon {
 		faults.put("biomodel_7803961.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_7803976.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_81284732.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
-		faults.put("biomodel_81992349.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT); // simulation 'rpcamp' in simContext 'Compart'
+		faults.put("biomodel_81992349.vcml", SEDML_FAULT.MATH_OVERRIDE_NAMES_DIFFERENT); // simulation 'Rolipram2' in simContext 'Compart'
 		faults.put("biomodel_82250339.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_83446023.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT); // simulation 'Simulation0' in simContext 'compartmental'
 		faults.put("biomodel_83932806.vcml", SEDML_FAULT.MATH_OVERRIDE_NAMES_DIFFERENT); // simulation 'Fig 6A influx pKa 9.6, logKow=1.88, c=1' in simContext 'Matching w/ Matlab data'
@@ -175,22 +159,9 @@ public class SEDMLExporterSBMLTest extends SEDMLExporterCommon {
 		faults.put("biomodel_84985561.vcml", SEDML_FAULT.MATH_OVERRIDE_NAMES_DIFFERENT); // simulation 'copy of Simulation 2' in simContext 'comp'
 		faults.put("biomodel_89712092.vcml", SEDML_FAULT.MATH_OVERRIDE_NAMES_DIFFERENT); // simulation 'ss check 1' in simContext 'steady state compartment'
 		faults.put("biomodel_89712092_nonspatial.vcml", SEDML_FAULT.MATH_OVERRIDE_NAMES_DIFFERENT); // simulation 'First Try' in simContext 'uncaging'
-		faults.put("biomodel_91133993.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT); // simulation Simulation0 in simContext test
-		faults.put("biomodel_91134220.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT);
-		faults.put("biomodel_91134296.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT);
-		faults.put("biomodel_91134339.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT);
-		faults.put("biomodel_91141200.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT);
-		faults.put("biomodel_91141358.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT);
-		faults.put("biomodel_91147280.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT);
-		faults.put("biomodel_91162809.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT);
-		faults.put("biomodel_91162818.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT);
-		faults.put("biomodel_91164078.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT);
-		faults.put("biomodel_91164682.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT);
 		faults.put("biomodel_9254662.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_92705462.vcml", SEDML_FAULT.SIMCONTEXT_NOT_FOUND_BY_NAME);  // roundtripped simulationContext not found with name 'stoch'
 		faults.put("biomodel_94891280.vcml", SEDML_FAULT.MATH_OVERRIDE_NAMES_DIFFERENT);
-		faults.put("biomodel_95094548.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT);
-		faults.put("biomodel_95177642.vcml", SEDML_FAULT.MATH_OVERRIDE_NOT_EQUIVALENT);
 		faults.put("biomodel_98147638.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
 		faults.put("biomodel_98150237.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX); // new
 		faults.put("biomodel_98174143.vcml", SEDML_FAULT.NO_MODELS_IN_OMEX);
