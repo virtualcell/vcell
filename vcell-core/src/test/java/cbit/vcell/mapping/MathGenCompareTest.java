e8c197d9424593d862b39a7d3c6799c7a7a13f36
Jim Schaff <schaff.jim@gmail.com>
2023-01-06 18:02:18 -0500

    made legacy test with/without model reduction - now only 10 apps fail.

--- vcell-core/src/test/java/cbit/vcell/mapping/MathGenCompareTest.java
+++ vcell-core/src/test/java/cbit/vcell/mapping/MathGenCompareTest.java
@@ -96,59 +96,28 @@ public class MathGenCompareTest {
 	 */
 	public static Map<String, MathCompareResults.Decision> knownLegacyFaults() {
 		HashMap<String, MathCompareResults.Decision> faults = new HashMap();
-		faults.put("lumped_reaction_proper_size_in_rate.vcml:Application0", MathCompareResults.Decision.MathDifferent_DIFFERENT_EXPRESSION);
-		faults.put("biomodel_12522025.vcml:purkinje9", MathCompareResults.Decision.MathDifferent_DIFFERENT_NUMBER_OF_VARIABLES);
-		faults.put("biomodel_12522025.vcml:purkinje9-ss", MathCompareResults.Decision.MathDifferent_DIFFERENT_NUMBER_OF_VARIABLES);
-		faults.put("biomodel_12522025_spatial.vcml:purkinje9", MathCompareResults.Decision.MathDifferent_DIFFERENT_NUMBER_OF_VARIABLES);
-		faults.put("biomodel_16404713.vcml:PDGFgradient_TIRF", MathCompareResults.Decision.MathDifferent_DIFFERENT_NUMBER_OF_VARIABLES);
-		faults.put("biomodel_22403233.vcml:Spatial", MathCompareResults.Decision.MathDifferent_DIFFERENT_NUMBER_OF_VARIABLES);
-		faults.put("biomodel_22403238.vcml:Spatial", MathCompareResults.Decision.MathDifferent_DIFFERENT_NUMBER_OF_VARIABLES);
-		faults.put("biomodel_22403244.vcml:spatial", MathCompareResults.Decision.MathDifferent_DIFFERENT_NUMBER_OF_VARIABLES);
-		faults.put("biomodel_22403250.vcml:Spatial", MathCompareResults.Decision.MathDifferent_DIFFERENT_NUMBER_OF_VARIABLES);
-		faults.put("biomodel_22403358.vcml:Spatial", MathCompareResults.Decision.MathDifferent_DIFFERENT_NUMBER_OF_VARIABLES);
-		faults.put("biomodel_22403576.vcml:Spatial", MathCompareResults.Decision.MathDifferent_DIFFERENT_NUMBER_OF_VARIABLES);
-		faults.put("biomodel_34855932.vcml:cell4", MathCompareResults.Decision.MathDifferent_FAILURE_UNKNOWN); // create canonicalMath cannot handle function of variables of type 'FilamentVariable'
-		faults.put("biomodel_34855932.vcml:cell5", MathCompareResults.Decision.MathDifferent_FAILURE_UNKNOWN); // create canonicalMath cannot handle function of variables of type 'FilamentVariable'
-		faults.put("biomodel_34855932.vcml:cell6", MathCompareResults.Decision.MathDifferent_FAILURE_UNKNOWN); // create canonicalMath cannot handle function of variables of type 'FilamentVariable'
-		faults.put("biomodel_34855932.vcml:cell7", MathCompareResults.Decision.MathDifferent_FAILURE_UNKNOWN); // create canonicalMath cannot handle function of variables of type 'FilamentVariable'
-		faults.put("biomodel_36275161.vcml:PDGFgradient_TIRF", MathCompareResults.Decision.MathDifferent_DIFFERENT_NUMBER_OF_VARIABLES);
-		faults.put("biomodel_55178308.vcml:Spatial 1 - PIP2 seq at PSD - HALF", MathCompareResults.Decision.MathDifferent_DIFFERENT_NUMBER_OF_VARIABLES);
-		faults.put("biomodel_18894555.vcml:compartmental", MathCompareResults.Decision.MathDifferent_FAILURE_UNKNOWN); // create canonicalMath cannot handle function of variables of type 'FilamentVariable'
-		faults.put("biomodel_47429473.vcml:Steady State Turnover", MathCompareResults.Decision.MathDifferent_EQUATION_REMOVED);
-		faults.put("biomodel_47429473.vcml:NWASP at Lam Tip in 3D Geometry", MathCompareResults.Decision.MathDifferent_DIFFERENT_FASTINV_EXPRESSION);
-		faults.put("biomodel_47429473.vcml:NWasp Activation Cap=1", MathCompareResults.Decision.MathDifferent_EQUATION_REMOVED);
-		faults.put("biomodel_47429473.vcml:NWasp Activation Cap=2", MathCompareResults.Decision.MathDifferent_EQUATION_REMOVED);
-		faults.put("biomodel_47429473.vcml:NWasp Activation Cap=4", MathCompareResults.Decision.MathDifferent_EQUATION_REMOVED);
-		faults.put("biomodel_47429473.vcml:Nwasp Activation Cap=4 Cof=10", MathCompareResults.Decision.MathDifferent_EQUATION_REMOVED);
-		faults.put("biomodel_47429473.vcml:NWasp Activation Cap=2 Cof=10", MathCompareResults.Decision.MathDifferent_EQUATION_REMOVED);
-		faults.put("biomodel_47429473.vcml:NWasp Activation Cap=4 Prof=20", MathCompareResults.Decision.MathDifferent_EQUATION_REMOVED);
-		faults.put("biomodel_47429473.vcml:NWasp Activation Cap=4 Prof=20 Cof=10", MathCompareResults.Decision.MathDifferent_EQUATION_REMOVED);
-		faults.put("biomodel_47429473.vcml:NWasp Activation Cap=1 Cof=10", MathCompareResults.Decision.MathDifferent_EQUATION_REMOVED);
-		faults.put("biomodel_47429473.vcml:NWasp Activation Cap=1 Prof=20", MathCompareResults.Decision.MathDifferent_EQUATION_REMOVED);
-		faults.put("biomodel_47429473.vcml:NWasp Activation Cap=1 Prof=20 Cof=10", MathCompareResults.Decision.MathDifferent_EQUATION_REMOVED);
-		faults.put("biomodel_47429473.vcml:NWasp Activation Cap=2 Prof=20", MathCompareResults.Decision.MathDifferent_EQUATION_REMOVED);
-		faults.put("biomodel_47429473.vcml:NWASP Activation Cap=2 Prof=20 Cof = 10", MathCompareResults.Decision.MathDifferent_EQUATION_REMOVED);
-		faults.put("biomodel_47429473.vcml:NWASP Activation Cap=1 Prof=20 No Arp Binding", MathCompareResults.Decision.MathDifferent_EQUATION_REMOVED);
-		faults.put("biomodel_55178308.vcml:Spatial 1 - 3D -  electrophysiology", MathCompareResults.Decision.MathDifferent_FAILURE_UNKNOWN); // create canonicalMath cannot handle function of variables of type 'FilamentVariable'
-		faults.put("biomodel_59361239.vcml:full model", MathCompareResults.Decision.MathDifferent_FAILURE_UNKNOWN); // create canonicalMath cannot handle function of variables of type 'FilamentVariable'
-		faults.put("biomodel_59361239.vcml:individual knockouts", MathCompareResults.Decision.MathDifferent_FAILURE_UNKNOWN); // create canonicalMath cannot handle function of variables of type 'FilamentVariable'
-		faults.put("biomodel_59361239.vcml:time delay 60 s", MathCompareResults.Decision.MathDifferent_FAILURE_UNKNOWN); // create canonicalMath cannot handle function of variables of type 'FilamentVariable'
-		faults.put("biomodel_59361239.vcml:receptor density", MathCompareResults.Decision.MathDifferent_FAILURE_UNKNOWN); // create canonicalMath cannot handle function of variables of type 'FilamentVariable'
-		faults.put("biomodel_59361239.vcml:sensitivity analysis", MathCompareResults.Decision.MathDifferent_FAILURE_UNKNOWN); // create canonicalMath cannot handle function of variables of type 'FilamentVariable'
-		faults.put("biomodel_59361239.vcml:individual knockouts with delay", MathCompareResults.Decision.MathDifferent_FAILURE_UNKNOWN); // create canonicalMath cannot handle function of variables of type 'FilamentVariable'
-		faults.put("biomodel_59361239.vcml:integrin knockout", MathCompareResults.Decision.MathDifferent_FAILURE_UNKNOWN); // create canonicalMath cannot handle function of variables of type 'FilamentVariable'
+		faults.put("lumped_reaction_proper_size_in_rate.vcml:Application0", MathCompareResults.Decision.MathDifferent_DIFFERENT_EXPRESSION); // =LEGACY= MathDifferent:DifferentExpression:expressions are different: ' - (0.001660538783162726 * s1)' vs ' -
+		faults.put("biomodel_47429473.vcml:NWASP at Lam Tip in 3D Geometry", MathCompareResults.Decision.MathDifferent_DIFFERENT_FASTINV_EXPRESSION); // =LEGACY= MathDifferent:DifferentFastInvExpression:could not find a match for fast invariant expression'Expres
+		faults.put("biomodel_55178308.vcml:Spatial 1 - 3D -  electrophysiology", MathCompareResults.Decision.MathDifferent_VARIABLES_DONT_MATCH); // =LEGACY= MathDifferent:VariablesDontMatch:variable 'Spine::CGB_spine' not matched
+		faults.put("biomodel_59361239.vcml:full model", MathCompareResults.Decision.MathDifferent_DIFFERENT_EXPRESSION); // =LEGACY= MathDifferent:DifferentExpression:expressions are different: '( - (3.0199335548172735E-4 * ((66.2266
+		faults.put("biomodel_59361239.vcml:individual knockouts", MathCompareResults.Decision.MathDifferent_DIFFERENT_EXPRESSION); // =LEGACY= MathDifferent:DifferentExpression:expressions are different: '( - (3.0199335548172735E-4 * ((66.2266
+		faults.put("biomodel_59361239.vcml:time delay 60 s", MathCompareResults.Decision.MathDifferent_DIFFERENT_EXPRESSION); // =LEGACY= MathDifferent:DifferentExpression:expressions are different: '( - (3.0199335548172735E-4 * ((66.2266
+		faults.put("biomodel_59361239.vcml:receptor density", MathCompareResults.Decision.MathDifferent_DIFFERENT_EXPRESSION); // =LEGACY= MathDifferent:DifferentExpression:expressions are different: '( - (3.0199335548172735E-4 * ((66.2266
+		faults.put("biomodel_59361239.vcml:sensitivity analysis", MathCompareResults.Decision.MathDifferent_DIFFERENT_EXPRESSION); // =LEGACY= MathDifferent:DifferentExpression:expressions are different: '( - (3.0199335548172735E-4 * ((66.2266
+		faults.put("biomodel_59361239.vcml:individual knockouts with delay", MathCompareResults.Decision.MathDifferent_DIFFERENT_EXPRESSION); // =LEGACY= MathDifferent:DifferentExpression:expressions are different: '( - (3.0199335548172735E-4 * ((66.2266
+		faults.put("biomodel_59361239.vcml:integrin knockout", MathCompareResults.Decision.MathDifferent_DIFFERENT_EXPRESSION); // =LEGACY= MathDifferent:DifferentExpression:expressions are different: '( - (3.0199335548172735E-4 * ((6680.83
 		return faults;
 	}
 
 	public static Map<String, MathCompareResults.Decision> knownReductionFaults() {
 		HashMap<String, MathCompareResults.Decision> faults = new HashMap();
 		faults.put("biomodel_100961371.vcml:modelinOct2010", MathCompareResults.Decision.MathDifferent_DIFFERENT_EXPRESSION); // =REDUCED= MathDifferent:DifferentExpression:expressions are different: ' - (9.963232698976356E-4 * ((1.2000000
-		faults.put("biomodel_148700996.vcml:Fast B Compartmental", MathCompareResults.Decision.MathDifferent_DIFFERENT_FASTINV_EXPRESSION); // =REDUCED= MathDifferent:DifferentFastInvExpression:fast invariant expressions are different, old='Expression@6
+		faults.put("biomodel_148700996.vcml:Fast B Compartmental", MathCompareResults.Decision.MathDifferent_DIFFERENT_FASTINV_EXPRESSION); // =REDUCED= MathDifferent:DifferentFastInvExpression:could not find a match for fast invariant expression'Expres
 		faults.put("biomodel_148700996.vcml:2DGeom nonuniform dens v2", MathCompareResults.Decision.MathDifferent_DIFFERENT_NUMBER_OF_EXPRESSIONS); // =REDUCED= MathDifferent:DifferentNumberOfExpressions:fast invariants have different number of expressions
 		faults.put("biomodel_156134818.vcml:unnamed", MathCompareResults.Decision.MathDifferent_DIFFERENT_EXPRESSION); // =REDUCED= MathDifferent:DifferentExpression:expressions are different: '( - (7.961783439490447E-5 * EGF_EGFR_2
 		faults.put("biomodel_17257105.vcml:compartmental", MathCompareResults.Decision.MathDifferent_DIFFERENT_EXPRESSION); // =REDUCED= MathDifferent:DifferentExpression:expressions are different: '((0.0011 * (L_b1AR_cell + L_b1AR_Gs_ce
 		faults.put("biomodel_17257105.vcml:spatial", MathCompareResults.Decision.MathDifferent_DIFFERENT_NUMBER_OF_EXPRESSIONS); // =REDUCED= MathDifferent:DifferentNumberOfExpressions:fast invariants have different number of expressions
-		faults.put("biomodel_209284198.vcml:fast compart with 3D S:V & V:V", MathCompareResults.Decision.MathDifferent_DIFFERENT_FASTINV_EXPRESSION); // =REDUCED= MathDifferent:DifferentFastInvExpression:fast invariant expressions are different, old='Expression@2
+		faults.put("biomodel_209284198.vcml:fast compart with 3D S:V & V:V", MathCompareResults.Decision.MathDifferent_DIFFERENT_FASTINV_EXPRESSION); // =REDUCED= MathDifferent:DifferentFastInvExpression:could not find a match for fast invariant expression'Expres
 		faults.put("biomodel_2917788.vcml:Figure 4.3: Fast Approximation", MathCompareResults.Decision.MathDifferent_DIFFERENT_EXPRESSION); // =REDUCED= MathDifferent:DifferentExpression:expressions are different: '0.0' vs '( - ((20.0 * x_o) - (50.0 * x
 		faults.put("biomodel_32568171.vcml:Sim5_1", MathCompareResults.Decision.MathDifferent_DIFFERENT_NUMBER_OF_EXPRESSIONS); // =REDUCED= MathDifferent:DifferentNumberOfExpressions:fast invariants have different number of expressions
 		faults.put("biomodel_32568171.vcml:GoodModel17_5", MathCompareResults.Decision.MathDifferent_DIFFERENT_NUMBER_OF_EXPRESSIONS); // =REDUCED= MathDifferent:DifferentNumberOfExpressions:fast invariants have different number of expressions
@@ -198,7 +167,7 @@ public class MathGenCompareTest {
 		faults.put("biomodel_66264973.vcml:compartmental - IC-G2736X", MathCompareResults.Decision.MathDifferent_EQUATION_ADDED); // =REDUCED= MathDifferent:EquationAdded:only one mathDescription had equation for 'Compartment::ICpeptide_mAtaxi
 		faults.put("biomodel_66265579.vcml:compartmental - IC4", MathCompareResults.Decision.MathDifferent_DIFFERENT_EXPRESSION); // =REDUCED= MathDifferent:DifferentExpression:expressions are different: '0.0' vs '((0.005860724109735934 * ((17
 		faults.put("biomodel_91134339.vcml:test", MathCompareResults.Decision.MathDifferent_DIFFERENT_EXPRESSION); // =REDUCED= MathDifferent:DifferentExpression:expressions are different: '( - ((0.03 * C_Unnamed_compartment * (
-		faults.put("biomodel_9254662.vcml:FastBuffExpReceptor", MathCompareResults.Decision.MathDifferent_DIFFERENT_FASTINV_EXPRESSION); // =REDUCED= MathDifferent:DifferentFastInvExpression:fast invariant expressions are different, old='Expression@8
+		faults.put("biomodel_9254662.vcml:FastBuffExpReceptor", MathCompareResults.Decision.MathDifferent_DIFFERENT_FASTINV_EXPRESSION); // =REDUCED= MathDifferent:DifferentFastInvExpression:could not find a match for fast invariant expression'Expres
 		faults.put("biomodel_98147638.vcml:compartmental - IC4", MathCompareResults.Decision.MathDifferent_DIFFERENT_EXPRESSION); // =REDUCED= MathDifferent:DifferentExpression:expressions are different: '0.0' vs '((0.005860724109735934 * ((17
 		return faults;
 	}
@@ -226,6 +195,7 @@ public class MathGenCompareTest {
 			}
 		}
 		return appTestCases;
+//		return Arrays.asList("biomodel_12522025.vcml:purkinje9");
 	}
 
 
@@ -258,7 +228,7 @@ public class MathGenCompareTest {
 		MathDescription originalMath = orig_simContext.getMathDescription();
 		MathDescription origMathClone = new MathDescription(originalMath); // test round trip to/from MathDescription.readFromDatabase()
 		SimulationContext new_simContext = transformed_biomodel.getSimulationContexts(orig_simContext.getName());
-		new_simContext.setUsingMassConservationModelReduction(false);
+		//new_simContext.setUsingMassConservationModelReduction(false);
 		new_simContext.updateAll(false);
 		MathDescription newMath = new_simContext.getMathDescription();
 		MathDescription newMathClone = new MathDescription(newMath); // test round trip to/from MathDescription.readFromDatabase()
@@ -273,19 +243,27 @@ public class MathGenCompareTest {
 			Assert.fail("math equivalent for '"+filename_colon_appname+"', but expecting '"+knownFault+"', remove known fault");
 		}
 		if (!results.isEquivalent()){
-			try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(knownProblemFile, true));){
-				bufferedWriter.write("faults.put(\""+filename_colon_appname+"\", MathCompareResults.Decision."+results.decision+"); // =LEGACY= "
-						+results.toDatabaseStatus().substring(0, Math.min(results.toDatabaseStatus().length(), 100))+"\n");
-			}
-			if (knownFault == null) {
-				Assert.fail("'"+filename_colon_appname+"' expecting equivalent, " +
-						"computed '"+results.decision+"', " +
-						"details: " + results.toDatabaseStatus());
-			}else{
-				Assert.assertTrue("'"+filename_colon_appname+"' expecting '"+knownFault+"', " +
-								"computed '"+results.decision+"', " +
-								"details: " + results.toDatabaseStatus(),
-						knownFault == results.decision);
+			// try again using non-reduced math
+			new_simContext.setUsingMassConservationModelReduction(false);
+			new_simContext.updateAll(false);
+			newMath = new_simContext.getMathDescription();
+			MathCompareResults results2 = MathDescription.testEquivalency(SimulationSymbolTable.createMathSymbolTableFactory(), originalMath, newMath);
+
+			if (!results2.isEquivalent()) {
+				try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(knownProblemFile, true));) {
+					bufferedWriter.write("faults.put(\"" + filename_colon_appname + "\", MathCompareResults.Decision." + results2.decision + "); // =LEGACY= "
+							+ results2.toDatabaseStatus().substring(0, Math.min(results2.toDatabaseStatus().length(), 100)) + "\n");
+				}
+				if (knownFault == null) {
+					Assert.fail("'" + filename_colon_appname + "' expecting equivalent, " +
+							"computed '" + results2.decision + "', " +
+							"details: " + results2.toDatabaseStatus());
+				} else {
+					Assert.assertTrue("'" + filename_colon_appname + "' expecting '" + knownFault + "', " +
+									"computed '" + results2.decision + "', " +
+									"details: " + results2.toDatabaseStatus(),
+							knownFault == results2.decision);
+				}
 			}
 		}
 	}
