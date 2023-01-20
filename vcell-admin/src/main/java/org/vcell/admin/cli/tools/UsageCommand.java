fcb1d77768b7de22bfe795764f82d4c1212f7970
Jim Schaff <schaff.jim@gmail.com>
2023-01-19 23:42:10 -0500

    standard authn for /rpc?stats, add usage to admin cli, add CSS to report

---
+++ vcell-admin/src/main/java/org/vcell/admin/cli/tools/UsageCommand.java
@@ -0,0 +1,51 @@
+package org.vcell.admin.cli.tools;
+
+import cbit.vcell.resource.PropertyLoader;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.vcell.admin.cli.CLIDatabaseService;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.concurrent.Callable;
+
+@Command(name = "usage", description = "reports regarding vcell usage")
+public class UsageCommand implements Callable<Integer> {
+
+    private final static Logger logger = LogManager.getLogger(UsageCommand.class);
+
+    @Option(names = { "-o", "--outputFile"}, required = true)
+    private File outputFilePath;
+
+    @Option(names = {"-d", "--debug"}, description = "full application debug mode")
+    private boolean bDebug = false;
+
+    public Integer call() {
+        Level logLevel = bDebug ? Level.DEBUG : logger.getLevel(); 
+        
+        LoggerContext config = (LoggerContext)(LogManager.getContext(false));
+        config.getConfiguration().getLoggerConfig(LogManager.getLogger("org.vcell").getName()).setLevel(logLevel);
+        config.getConfiguration().getLoggerConfig(LogManager.getLogger("cbit").getName()).setLevel(logLevel);
+        config.updateLoggers();
+
+        try {
+            PropertyLoader.loadProperties();
+            try (CLIDatabaseService cliDatabaseService = new CLIDatabaseService()) {
+                String basicStatistics = cliDatabaseService.getBasicStatistics();
+                try (FileWriter fileWriter = new FileWriter(outputFilePath)){
+                    fileWriter.write(basicStatistics);
+                }
+             } catch (RuntimeException e) {
+                e.printStackTrace(System.err);
+            }
+            return 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+}
--- vcell-api/src/main/java/org/vcell/rest/rpc/RpcRestlet.java
+++ vcell-api/src/main/java/org/vcell/rest/rpc/RpcRestlet.java
@@ -66,63 +66,25 @@ public final class RpcRestlet extends Restlet {
 	public void handle(Request req, Response response) {
 		if (req.getMethod().equals(Method.GET)){
 			try {
-				User authuser = null;
+				VCellApiApplication application = ((VCellApiApplication)getApplication());
+				User vcellUser = application.getVCellUser(req.getChallengeResponse(),AuthenticationPolicy.prohibitInvalidCredentials);
 				HttpRequest request = (HttpRequest)req;
-				//Use "WWW-Authenticate - Basic" authentication scheme
-				//Browser takes care of asking user for credentials and sending them
-				//Must be used with https connection to hide credentials
-				Header authHeader = request.getHeaders().getFirst("Authorization");
-				if(authHeader != null) {//caller included a user and password
-					String typeAndCredential = authHeader.getValue();
-//					System.out.println("--"+up);
-					java.util.StringTokenizer st = new java.util.StringTokenizer(typeAndCredential," ");
-					String type=st.nextToken();
-					String userAndPasswordB64 = st.nextToken();
-					String s = new String(Base64.getDecoder().decode(userAndPasswordB64));
-//					System.out.println("type="+type+" decoded="+s);
-					if(type.equals("Basic")) {
-						java.util.StringTokenizer st2 = new java.util.StringTokenizer(s,":");
-						if(st2.countTokens() == 2) {
-							String usr=st2.nextToken();
-							String pw = st2.nextToken();
-	//						System.out.println("user="+usr+" password="+pw);
-							UserLoginInfo.DigestedPassword dpw = new UserLoginInfo.DigestedPassword(pw);
-	//						System.out.println(dpw);
-							VCellApiApplication application = ((VCellApiApplication)getApplication());
-							authuser = application.getUserVerifier().authenticateUser(usr,dpw.getString().toCharArray());
-	//						System.out.println(authuser);
-						}
-					}
-				}
-				if(authuser == null) {
-					//If we get here either there was not user/pw or user/pw didn't authenticate
-					//We need to add a response header
-					//Response headers container might be null so add one if necessary
-					if(((HttpResponse)response).getHeaders() == null) {
-						((HttpResponse)response).getAttributes().
-							put(HeaderConstants.ATTRIBUTE_HEADERS,new Series(Header.class));
-					}
-					//Tell whoever called us we want a user and password that we will check against admin vcell users
-					HttpResponse.addHeader(response,"WWW-Authenticate", "Basic");
-					response.setStatus(Status.CLIENT_ERROR_UNAUTHORIZED);
-					return;
-				}
-				
-				Form form = request.getResourceRef().getQueryAsForm();
-				if (form.getFirst("stats") != null){
-					String requestTypeString = form.getFirstValue("stats", true);//get .../rpc?stats=value 'value'
-					if((authuser.getName().equals("frm") || 
-							authuser.getName().equals("les") ||
-							authuser.getName().equals("ion") ||
-							authuser.getName().equals("danv") ||
-							authuser.getName().equals("mblinov") ||
-							authuser.getName().equals("ACowan"))) {
-							String result = restDatabaseService.getBasicStatistics();
-							response.setStatus(Status.SUCCESS_OK);
-							response.setEntity(result, MediaType.TEXT_HTML);
-							return;
-						} 								
 
+				Form form = request.getResourceRef().getQueryAsForm();
+				if (form.getFirst("stats") != null) {
+					String requestTypeString = form.getFirstValue("stats", true);//get .../rpc?stats=value 'value'
+					if ((vcellUser.getName().equals("frm") ||
+							vcellUser.getName().equals("les") ||
+							vcellUser.getName().equals("ion") ||
+							vcellUser.getName().equals("danv") ||
+							vcellUser.getName().equals("mblinov") ||
+							vcellUser.getName().equals("schaff") ||
+							vcellUser.getName().equals("ACowan"))) {
+						String result = restDatabaseService.getBasicStatistics();
+						response.setStatus(Status.SUCCESS_OK);
+						response.setEntity(result, MediaType.TEXT_HTML);
+						return;
+					}
 				}
 			} catch (Exception e) {
 				String errMesg = "<html><body>Error RpcRestlet.handle(...) req='"+req.toString()+"' <br>err='"+e.getMessage()+"'</br>"+"</body></html>";
