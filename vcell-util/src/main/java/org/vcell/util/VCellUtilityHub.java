b04c1bd45ba0d584eecadeb5c81c64c388591f37
Logan Drescher <drescher@uchc.edu>
2023-02-07 10:11:50 -0500

    Fixed limiting error-out of VCellUtilityHub

--- vcell-util/src/main/java/org/vcell/util/VCellUtilityHub.java
+++ vcell-util/src/main/java/org/vcell/util/VCellUtilityHub.java
@@ -4,12 +4,19 @@ import org.vcell.util.recording.CLIRecordManager;
 import org.vcell.util.recording.RecordManager;
 import org.vcell.util.recording.VCellRecordManager;
 
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Class to hold all singletons and resources needed across VCell.
  * 
  * @since VCell 7.4.0.62
  */
 public class VCellUtilityHub {
+
+    private final static Logger logger = LogManager.getLogger(VCellUtilityHub.class);
 
     /*
      * For each way we want to initialize VCell:
@@ -38,8 +45,13 @@ public class VCellUtilityHub {
      * @param mode the mode in which VCell was launched.
      */
     public static void startup(MODE mode){
-        if (instance == null) instance = new VCellUtilityHub(mode);
-        else throw new RuntimeException("VCellUtilityHub already started.");
+        if (VCellUtilityHub.instance == null) VCellUtilityHub.instance = new VCellUtilityHub(mode);
+        else {
+            logger.warn("Restarting VCellUtilityHub...");
+            VCellUtilityHub.shutdown();
+            VCellUtilityHub.instance = null;
+            VCellUtilityHub.startup(mode);
+        };
     }
 
     /**
@@ -47,8 +59,14 @@ public class VCellUtilityHub {
      * 
      * @throws Exception if closing anything produced errors
      */
-    public static void shutdown() throws Exception {
-        logManager.close();
+    public static void shutdown() {
+        try {
+            logManager.close();
+        } catch (IOException e){
+            String message = "Log Manager could not close.";
+            logger.error(message);
+            throw new RuntimeException(message, e);
+        }
     }
 
     private VCellUtilityHub(MODE m){
