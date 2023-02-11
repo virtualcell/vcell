feea963e72ec9e32a363a10986c384adb616abcb
Jim Schaff <schaff.jim@gmail.com>
2023-02-11 04:17:31 -0500

    added Dockerfile-admin-dev to run vcell-admin CLI

---
+++ docker/build/admin/entrypoint.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+show_help() {
+	echo "VCell admin CLI"
+	echo "type help for usage"
+	exit 1
+}
+
+if [ "$#" -lt 1 ]; then
+    show_help
+fi
+
+echo $@
+
+shopt -s -o nounset
+
+echo "softwareVersion is $softwareVersion"
+echo "dbpswdfile is $dbpswdfile"
+echo "VCELL_VERSION is $VCELL_VERSION"
+arguments=$*
+java -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n \
+  -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1 -Xms64M \
+	-Dvcell.softwareVersion="$VCELL_VERSION" \
+	-Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager \
+	-Dlog4j.configurationFile=/usr/local/app/vcell-admin.log4j.xml \
+	-Dvcell.server.id="$VCELL_SITE" \
+	-Dvcell.python.executable=/usr/bin/python \
+	-Dvcell.installDir=/usr/local/app \
+	-Dvcell.primarySimdatadir.internal=/simdata \
+	-Dvcell.secondarySimdatadir.internal=/simdata_secondary \
+	-Dvcell.server.dbConnectURL="$VCELL_DB_URL" \
+	-Dvcell.server.dbDriverName="$VCELL_DB_DRIVER" \
+	-Dvcell.server.dbUserid="$VCELL_DB_USER" \
+	-Dvcell.db.pswdfile=/run/secrets/dbpswd.txt \
+	-cp	'./lib/*' \
+	org.vcell.admin.cli.AdminCLI $arguments
+
+exit $?
