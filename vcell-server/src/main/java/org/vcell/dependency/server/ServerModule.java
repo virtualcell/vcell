85ac99f73ca327a18e0f2a2b3f3e4732cb2b7a37
Jim Schaff <schaff.jim@gmail.com>
2023-02-27 14:31:12 -0500

    replace SciJava Dependency Injection (for RegistrationService)

--- vcell-server/src/main/java/org/vcell/dependency/server/ServerModule.java
+++ vcell-server/src/main/java/org/vcell/dependency/server/ServerModule.java
@@ -5,12 +5,22 @@ import cbit.vcell.message.server.bootstrap.LocalVCellConnectionServiceImpl;
 import cbit.vcell.server.LocalVCellConnectionService;
 import cbit.vcell.simdata.ExternalDataIdentifierService;
 import com.google.inject.AbstractModule;
+import org.vcell.service.registration.RegistrationService;
+import org.vcell.service.registration.localdb.LocaldbRegistrationService;
 
 public class ServerModule extends AbstractModule {
 
     @Override
     protected void configure() {
+        // only one implementation, to break compile-time cyclic dependency (vcell-core -> vcell-service)
         bind(ExternalDataIdentifierService.class).toInstance(new LocalExternalDataIdentifierServiceImpl());
+
+        // only one implementation, to break compile-time cyclic dependency (vcell-core -> vcell-service)
         bind(LocalVCellConnectionService.class).toInstance(new LocalVCellConnectionServiceImpl());
+
+        // server-side implementation (talk directly to database)
+        // RegistrationService interface is not clean - mixes new registration with updates - is there another way?
+        bind(RegistrationService.class).toInstance(new LocaldbRegistrationService());
+//        bind(RegistrationService.class).toInstance(new RemoteRegistrationService()); // used on remote client.
     }
 }
