18492a4e996f08c9ab9e077a0c589dacf8fe254f
Jim Schaff <schaff.jim@gmail.com>
2023-04-18 18:15:22 -0400

    simdata directory fixes for vcell-su cli util.

--- docker/build/admin/entrypoint.sh
+++ docker/build/admin/entrypoint.sh
@@ -19,15 +19,14 @@ arguments=$*
 # -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n
 
 java \
-  -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1 -Xms64M \
 	-Dvcell.softwareVersion="$VCELL_VERSION" \
 	-Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager \
 	-Dlog4j.configurationFile=/usr/local/app/vcell-admin.log4j.xml \
 	-Dvcell.server.id="$VCELL_SITE" \
 	-Dvcell.python.executable=/usr/bin/python \
 	-Dvcell.installDir=/usr/local/app \
-	-Dvcell.primarySimdatadir.internal=/simdata \
-	-Dvcell.secondarySimdatadir.internal=/simdata_secondary \
+	-Dvcell.primarySimdatadir.internal="$VCELL_SIMDATADIR_EXTERNAL" \
+	-Dvcell.secondarySimdatadir.internal="$VCELL_SIMDATADIR_SECONDARY_HOST" \
 	-Dvcell.server.dbConnectURL="$VCELL_DB_URL" \
 	-Dvcell.server.dbDriverName="$VCELL_DB_DRIVER" \
 	-Dvcell.server.dbUserid="$VCELL_DB_USER" \
