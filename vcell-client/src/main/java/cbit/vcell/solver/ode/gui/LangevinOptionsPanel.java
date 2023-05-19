9139cbf50e9bb06140d4598439db0e9ea2e69ca5
Dan Vasilescu <vasilescu@uchc.edu>
2023-05-19 13:01:02 -0400

    Panel for specific langevin solver parameters.
    Number of trials, interval image, interval spring

--- vcell-client/src/main/java/cbit/vcell/solver/ode/gui/LangevinOptionsPanel.java
+++ vcell-client/src/main/java/cbit/vcell/solver/ode/gui/LangevinOptionsPanel.java
@@ -1,11 +1,13 @@
 package cbit.vcell.solver.ode.gui;
 
+import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import org.vcell.util.gui.CollapsiblePanel;
 import org.vcell.util.gui.DialogUtils;
@@ -24,10 +26,14 @@ public class LangevinOptionsPanel  extends CollapsiblePanel {
 	private javax.swing.JRadioButton multiRunRadioButton = null;
 	private javax.swing.ButtonGroup buttonGroupTrials = null;
 
-	private javax.swing.JLabel numOfTrialsLabel = null;
-	private javax.swing.JTextField ivjJTextFieldNumOfTrials = null;
+	private JLabel numOfTrialsLabel = null;
+	private JTextField ivjJTextFieldNumOfTrials = null;
+	
+	private JTextField intervalImageTextField = null;
+	private JTextField intervalSpringTextField = null;
 
 	private IvjEventHandler ivjEventHandler = new IvjEventHandler();
+
 
 	private class IvjEventHandler implements java.awt.event.ActionListener, java.awt.event.FocusListener, java.beans.PropertyChangeListener {
 		public void propertyChange(java.beans.PropertyChangeEvent evt) {
@@ -77,10 +83,44 @@ public class LangevinOptionsPanel  extends CollapsiblePanel {
 	private void initialize() {
 		try {
 
-			getContentPanel().setLayout(new java.awt.GridBagLayout());
-			JPanel trialPanel = new JPanel(new GridBagLayout());
-			GridBagConstraints gbc = new GridBagConstraints();
+			getContentPanel().setLayout(new GridBagLayout());
 			
+			JPanel trialPanel = new JPanel(new GridBagLayout());		// left panel
+			trialPanel.setLayout(new GridBagLayout());
+			JPanel centerPanel = new JPanel(new GridBagLayout());
+			centerPanel.setLayout(new GridBagLayout());
+			JPanel rightPanel = new JPanel(new GridBagLayout());		// fake panel, for looks
+			rightPanel.setLayout(new GridBagLayout());
+
+			GridBagConstraints gbc = new GridBagConstraints();
+			gbc.gridx = 0;
+			gbc.gridy = 0;
+			gbc.fill = GridBagConstraints.BOTH;
+//			gbc.weightx = 1.0;
+//			gbc.weighty = 1.0;
+			gbc.insets = new Insets(1,1,1,1);
+			getContentPanel().add(trialPanel, gbc);
+			
+			gbc = new GridBagConstraints();
+			gbc.gridx = 1;
+			gbc.gridy = 0;
+			gbc.fill = GridBagConstraints.BOTH;
+//			gbc.weightx = 1.0;
+//			gbc.weighty = 1.0;
+			gbc.insets = new Insets(1,1,1,1);
+			getContentPanel().add(centerPanel, gbc);
+			
+			gbc = new GridBagConstraints();
+			gbc.gridx = 2;
+			gbc.gridy = 0;
+			gbc.fill = GridBagConstraints.BOTH;
+			gbc.weightx = 1.0;
+			gbc.weighty = 1.0;
+			gbc.insets = new Insets(1,1,1,1);
+			getContentPanel().add(rightPanel, gbc);
+
+			// ----- trialPanel --------------------------------------------------------------
+			gbc = new GridBagConstraints();
 			gbc.gridx = 0;
 			gbc.gridy = 0;
 			gbc.anchor = GridBagConstraints.WEST;
@@ -120,18 +160,66 @@ public class LangevinOptionsPanel  extends CollapsiblePanel {
 			gbc.fill = GridBagConstraints.HORIZONTAL;
 			gbc.weightx = 1.0;
 			trialPanel.add(new JLabel(""), gbc);
-
 			
+			// ----- centerPanel -----------------------------------------------------
 			gbc = new GridBagConstraints();
 			gbc.gridx = 0;
 			gbc.gridy = 0;
+			gbc.anchor = GridBagConstraints.WEST;
+			gbc.fill = GridBagConstraints.HORIZONTAL;
+			gbc.insets = new Insets(0,26,1,5);
+			centerPanel.add(new JLabel("Spring Interval"), gbc);
+
+			gbc = new GridBagConstraints();
+			gbc.gridx = 1;
+			gbc.gridy = 0;
+			gbc.anchor = GridBagConstraints.WEST;
+			gbc.fill = GridBagConstraints.HORIZONTAL;
+			gbc.insets = new Insets(0,5,3,1);
+			centerPanel.add(getJTextFieldIntervalSpring(), gbc);
+
+			gbc = new GridBagConstraints();
+			gbc.gridx = 2;
+			gbc.gridy = 0;
+			gbc.anchor = GridBagConstraints.WEST;
+			gbc.insets = new Insets(0,6,1,22);
+			centerPanel.add(new JLabel("s"), gbc);
+
+			gbc = new GridBagConstraints();
+			gbc.gridx = 0;
+			gbc.gridy = 1;
+			gbc.anchor = GridBagConstraints.WEST;
+			gbc.fill = GridBagConstraints.HORIZONTAL;
+			gbc.insets = new Insets(0,26,1,5);
+			centerPanel.add(new JLabel("Image Interval"), gbc);
+
+			gbc = new GridBagConstraints();
+			gbc.gridx = 1;
+			gbc.gridy = 1;
+			gbc.anchor = GridBagConstraints.WEST;
+			gbc.fill = GridBagConstraints.HORIZONTAL;
+			gbc.insets = new Insets(0,5,3,1);
+			centerPanel.add(getJTextFieldIntervalImage(), gbc);
+
+			gbc = new GridBagConstraints();
+			gbc.gridx = 2;
+			gbc.gridy = 1;
+			gbc.anchor = GridBagConstraints.WEST;
+			gbc.insets = new Insets(0,6,1,22);
+			centerPanel.add(new JLabel("s"), gbc);
+
+			// ----- rightPanel ----------------------------------------------------
+			gbc = new GridBagConstraints();
+			gbc.gridx = 3;
+			gbc.gridy = 0;
+			gbc.anchor = GridBagConstraints.WEST;
 			gbc.fill = GridBagConstraints.BOTH;
 			gbc.weightx = 1.0;
 			gbc.weighty = 1.0;
-			gbc.insets = new Insets(1,1,1,1);
-			
-			getContentPanel().add(trialPanel, gbc);
+			gbc.insets = new Insets(0,22,1,6);
+			rightPanel.add(new JLabel(""), gbc);	// fake, just for looks
 
+			// ----------------------------------------------------------------------
 			getButtonGroupTrials().add(getTrajectoryButton());
 			getButtonGroupTrials().add(getMultiRunButton());
 			getButtonGroupTrials().setSelected(getTrajectoryButton().getModel(), true);
@@ -202,7 +290,35 @@ public class LangevinOptionsPanel  extends CollapsiblePanel {
 		}
 		return numOfTrialsLabel;
 	}
-	
+	private Component getJTextFieldIntervalImage() {
+		if (intervalImageTextField == null) {
+			try {
+				intervalImageTextField = new javax.swing.JTextField();
+				intervalImageTextField.setName("IntervalImageTextField");
+				intervalImageTextField.setColumns(9);
+				intervalImageTextField.setText("1.00E-4");
+			} catch (java.lang.Throwable ivjExc) {
+				handleException(ivjExc);
+			}
+		}
+		return intervalImageTextField;
+	}
+
+	private Component getJTextFieldIntervalSpring() {
+		if (intervalSpringTextField == null) {
+			try {
+				intervalSpringTextField = new javax.swing.JTextField();
+				intervalSpringTextField.setName("IntervalSpringTextField");
+				intervalSpringTextField.setColumns(9);
+				intervalSpringTextField.setText("1.00E-9");
+			} catch (java.lang.Throwable ivjExc) {
+				handleException(ivjExc);
+			}
+		}
+		return intervalSpringTextField;
+	}
+
+
 	private void handleException(java.lang.Throwable exception) {
 		/* Uncomment the following lines to print uncaught exceptions to stdout */
 		System.out.println("--------- UNCAUGHT EXCEPTION ---------");
@@ -267,9 +383,11 @@ public class LangevinOptionsPanel  extends CollapsiblePanel {
 		if(getMultiRunButton().isSelected()) {
 			numTrials = Integer.parseInt(getJTextFieldNumOfTrials().getText());
 		}
-//		sso.setNumOfTrials(numTrials);
-		LangevinSimulationOptions nsso = new LangevinSimulationOptions(sso);
-		solverTaskDescription.setLangevinSimulationOptions(nsso);
+		sso.setNumOfTrials(numTrials);
+		
+		// make a copy
+		LangevinSimulationOptions lso = new LangevinSimulationOptions(sso);
+		solverTaskDescription.setLangevinSimulationOptions(lso);
 		
 		} catch(Exception e) {
 			PopupGenerator.showErrorDialog(this, e.getMessage(), e);
