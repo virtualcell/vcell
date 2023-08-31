605b950d85e68c897e1c9a371434f8e107243a2d
Ezequiel Valencia <casualcucumber59@gmail.com>
2023-08-31 10:17:04 -0400

    Arbitrary 5-Dimensional VSim Data Export
    Can now handle 5-Dimensional sim data and export it into the N5 file format. Still need to see if doubles can be stay as doubles instead of being converted to floats and test the exportation results.

--- vcell-core/src/main/java/cbit/vcell/simdata/n5/N5Exporter.java
+++ vcell-core/src/main/java/cbit/vcell/simdata/n5/N5Exporter.java
@@ -1,48 +1,153 @@
 package cbit.vcell.simdata.n5;
 
 import cbit.vcell.export.server.ExportConstants;
+import cbit.vcell.math.MathException;
+import cbit.vcell.math.VariableType;
 import cbit.vcell.resource.PropertyLoader;
 import cbit.vcell.simdata.*;
+import cbit.vcell.solver.AnnotatedFunction;
+import cbit.vcell.solver.SimulationOwner;
 import cbit.vcell.solver.VCSimulationDataIdentifier;
 import cbit.vcell.solver.VCSimulationIdentifier;
+import com.google.gson.GsonBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.janelia.saalfeldlab.n5.*;
+import org.janelia.saalfeldlab.n5.hdf5.N5HDF5Reader;
+import org.janelia.saalfeldlab.n5.hdf5.N5HDF5Writer;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import org.vcell.util.DataAccessException;
 import org.vcell.util.document.KeyValue;
 import org.vcell.util.document.User;
 
 import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
 
+/*
+    Variables can be considered channels, and each variable may not have a spatial representation, but for the ones that do
+    they should be considered separate domains and segmented within their own channel.
+
+    The chunking of content is such that each block contains X,Y,Z information, and they are separated by channels and time.
+    This way external libraries won't have to be used to manipulate the matrices represented as arrays and can save computational time due to not
+    having to rearrange the already existing matrices by adding time and channel data to them. Although the current chunking may not
+    be the most efficient for retrieval.
+
+    It seems that it doesn't matter whether the data is row or column major since N5 data block writer handles both appropriately, only when
+    I started to add extra dimensions to the array matrix did it start to misbehave.
+
+      //vcData.addFunction(new AugmentedFunction("function_name", new Expression("x + 100*y + sin(t)"))
+
+    // X,Y,Z raster for time 0 for variable cAMP
+        // index in matrix c + nc * ri  2D
+        // (c + nc * ri) * block_size + zi 3D
+    //positive y goes down instead of up
+ */
 public class N5Exporter implements ExportConstants {
     private final static Logger lg = LogManager.getLogger(N5Exporter.class);
+    private static OutputContext outputContext;
 
     public N5Exporter() {
+        // Have the HDF5 exporter export its data to a file, then read with N5HDF5 reader, then block by block write it into N5
+        // Grab the object that has the data for sim files in blocks, then instead of writting the file, feed it into N5FSwriter
+        // Get the simulated datablock output, then rewrite that as chunks within the N5 dataset, not having to
+    }
+
+    public static void main(String[] args) throws MathException, IOException, DataAccessException {
+        String threeDImage = "1115478432";
+        String twoDImage = "2132749480";
+        String twoDDomain = "L";
+        String threeDDomain = "IP3_Cyt";
+        new N5Exporter().exportToN5("/home/zeke/.vcell/simdata/temp", threeDImage, "/home/zeke/Downloads/fullTest.n5");
+    }
+
+    public void exportToN5(String dataDir, String simKeyID, String outPutDir) throws MathException, DataAccessException, IOException {
+        VCData vcData = this.getVCData(dataDir, simKeyID);
+        double[] allTimes = vcData.getDataTimes();
+
+        // output context expects a list of annotated functions, vcData seems to already have a set of annotated functions
+
+        OutputContext outputContext = new OutputContext(new AnnotatedFunction[0]);
+        DataIdentifier[] dataIdentifiers = vcData.getVarAndFunctionDataIdentifiers(outputContext);
+
+//        DataIdentifier DexDataIdentifier = Arrays.stream(dataIdentifiers).filter(di -> di.getName().equals(threeDDomain)).findFirst().get();
+
+        // With Dex's dataset ID get the data block during the first time instance t
+
+        // Filter different dataset ID's
+        ArrayList<DataIdentifier> volumeDataIDs = new ArrayList<>();
+        ArrayList<DataIdentifier> otherDataIDs = new ArrayList<>();
+
+        for (DataIdentifier dataIdentifier : dataIdentifiers) {
+            try {
+                vcData.getSimDataBlock(outputContext, dataIdentifier.getName(), allTimes[0]);
+                if (dataIdentifier.getVariableType().equals(VariableType.VOLUME)) {
+                    volumeDataIDs.add(dataIdentifier);
+                } else {
+                    otherDataIDs.add(dataIdentifier);
+                }
+            }
+            catch (Exception e){
+                otherDataIDs.add(dataIdentifier);
+            }
+        }
+
+
+//        DexDataIdentifier.getVariableType();
+
+        int numChannels = volumeDataIDs.size();
+        int numTimes = allTimes.length;
+        long[] dimensions = {vcData.getMesh().getSizeX(), vcData.getMesh().getSizeY(), numChannels, vcData.getMesh().getSizeZ(), numTimes};
+        // 51X, 51Y, 1Z, 1C, 2T
+        int[] blockSize = {vcData.getMesh().getSizeX(), vcData.getMesh().getSizeY(), 1, vcData.getMesh().getSizeZ(), 1};
+        String dataSet = "DataSet";
+
+
+
+        N5FSWriter n5FSWriter = new N5FSWriter(outPutDir, new GsonBuilder());
+        RawCompression rawCompression = new RawCompression();
+        DatasetAttributes datasetAttributes = new DatasetAttributes(dimensions, blockSize, org.janelia.saalfeldlab.n5.DataType.FLOAT64, rawCompression);
+        n5FSWriter.createDataset(dataSet, datasetAttributes);
+        N5MetaData.imageJMetaData(n5FSWriter, dataSet, vcData);
+
+
+
+        for (int channelIndex=0; channelIndex < numChannels; channelIndex++){
+            for (int timeIndex=0; timeIndex < numTimes; timeIndex++){
+                double [] data = vcData.getSimDataBlock(outputContext, volumeDataIDs.get(channelIndex).getName(), allTimes[timeIndex]).getData();
+                DoubleArrayDataBlock doubleArrayDataBlock = new DoubleArrayDataBlock(blockSize, new long[]{0, 0, channelIndex, 0, timeIndex}, data);
+                n5FSWriter.writeBlock(dataSet, datasetAttributes, doubleArrayDataBlock);
+            }
+        }
 
     }
 
-    public static void main(String[] args) throws Exception {
-        String primaryDirStr = "~/.vcell/simdata/temp";
+    public VCData getVCData(String dataDir, String simKeyID) throws IOException, DataAccessException {
         PropertyLoader.loadProperties();
-        //System.setProperty(PropertyLoader.installationRoot,"/Users/schaff/workspace/vcell");
-        User user = new User("schaff", new KeyValue("17"));
-        KeyValue simKey = new KeyValue("1128909032");
+
+        // tell where installation is
+        System.setProperty(PropertyLoader.installationRoot,"/media/zeke/DiskDrive/App_Installations/VCell_Rel");
+
+        // Set my user ID associated with VC database
+        // set simulation key
+        // make an object that ties the user to that simulation key
+        // make an object that then identifies the simulation
+        User user = new User("ezequiel23", new KeyValue("258925427"));
+
+        KeyValue simKey = new KeyValue(simKeyID);
         VCSimulationIdentifier vcSimID = new VCSimulationIdentifier(simKey, user);
         VCSimulationDataIdentifier vcdID = new VCSimulationDataIdentifier(vcSimID, 0);
 
+        // Point a data controller to the directory where the sim data is and use the vcdID to retrieve information regarding the sim
         Cachetable cachetable = new Cachetable(10 * Cachetable.minute, 1000000L);
-        File primaryDir = new File(primaryDirStr);
-        DataSetControllerImpl dataSetControllerImpl = new DataSetControllerImpl(cachetable, primaryDir, null);
+        DataSetControllerImpl dataSetControllerImpl = new DataSetControllerImpl(cachetable, new File(dataDir), null);
         double[] allTimes = dataSetControllerImpl.getDataSetTimes(vcdID);
 
-        // get dataset identifier for vcSimID
-        VCData vcData = dataSetControllerImpl.getVCData(vcdID);
-        OutputContext outputContext = new OutputContext(null);
-        DataIdentifier[] dataIdentifiers = vcData.getVarAndFunctionDataIdentifiers(outputContext);
-        DataIdentifier DexDataIdentifier = Arrays.stream(dataIdentifiers).filter(di -> di.getName().equals("Dex")).findFirst().get();
-        SimDataBlock simDataBlock = vcData.getSimDataBlock(outputContext, DexDataIdentifier.getName(), allTimes[0]);
-        double[] Dex_dataBlockValues = simDataBlock.getData(); // X,Y,Z raster for time 0 for variable cAMP
-        System.out.println("Dex_dataBlockValues.length = " + Dex_dataBlockValues.length);
-        System.out.println("Dex_dataBlockValues[0] = " + Dex_dataBlockValues[0]);
+        // get dataset identifier from the simulation
 
+        return dataSetControllerImpl.getVCData(vcdID);
     }
 }
