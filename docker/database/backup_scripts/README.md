665c9bd861d5a4cb1276ff063891c7738716cf4a
Jim Schaff <schaff.jim@gmail.com>
2023-08-31 15:00:32 -0400

    updated database backup scripts.

--- docker/database/backup_scripts/README.md
+++ docker/database/backup_scripts/README.md
@@ -3,6 +3,13 @@
 ###  Data Pump dump to /data/backup
 cron job runs `archivedb.sh` every 1:00am as `root`
 
+```
+0  1  * * * /data/backup/scripts/archivedb.sh >> /data/backup/backup.cron-log 2>&1
+```
+
 ### Compress and Archive to ~vcell/database_backups
 cron job runs `move_files_to_vcellhome.sh` every 9:00pm as `vcell`
 
+```
+0  21 * * * /data/backup/scripts/move_files_to_vcellhome.sh >> /data/backup/move_files.cron-log 2>&1
+```
--- docker/database/backup_scripts/archivedb.sh
+++ docker/database/backup_scripts/archivedb.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # run export of oracle 19c database running in the container named 'oracle-database' using the Data Pump 'expdp' command
-sudo docker exec -it oracle-database /opt/oracle/backup/scripts/run.sh 2>&1 | tee -a ../archivedb_$(date +%Y_%m_%d_%H_%M_%S).log
+sudo docker exec oracle-database /opt/oracle/backup/scripts/run.sh 2>&1 | tee -a /data/backup/archivedb_$(date +%Y_%m_%d_%H_%M_%S).log
 
-# change permission so that vcell can move them
+# change permission so that vcell can move them in a separate script and cron job
 chmod 666 /data/backup/*.log
 chmod 666 /data/backup/*.dmp
 
 # copy move dump file to remote storage
-echo "move dump files to remote storage - not yet mounted - do this manually"
+echo "vcell runs separate script to archive to ~/vcell/database_backups"
