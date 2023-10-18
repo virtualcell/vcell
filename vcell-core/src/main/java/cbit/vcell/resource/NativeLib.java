5f3dfc077c3e9b5f830dace3e0563c047bf5149f
Logan Drescher <106617880+CodeByDrescher@users.noreply.github.com>
2023-10-18 11:57:48 -0400

    Merge pull request #989 from virtualcell/Win11DLLFix
    Win11 dll fix

--- vcell-core/src/main/java/cbit/vcell/resource/NativeLib.java
+++ vcell-core/src/main/java/cbit/vcell/resource/NativeLib.java
@@ -10,23 +10,32 @@ import cbit.vcell.util.NativeLoader;
  */
 public enum NativeLib {
 	// enum values
-	combinej("combinej", true), HDF5("jhdf5", false);
+	combinej("combinej", true),
+	HDF5("jhdf5", false,
+			(OperatingSystemInfo.getInstance().isWindows()) ? "msvcr100" : null );
 
 	private static final Logger logger = LogManager.getLogger(NativeLib.class);
 
 	private final String libName;
+	public final boolean autoLoad;
 	private boolean loaded;
-	public boolean autoLoad;
+	private final String[] dependentLibNames;
 
-	private NativeLib(String libName, boolean autoLoad) {
+	private NativeLib(String libName, boolean autoLoad, String... dependentLibNames) {
 		this.libName = libName;
 		this.autoLoad = autoLoad;
 		this.loaded = false;
+		this.dependentLibNames = dependentLibNames;
 	}
 
 	public void load( ) {
 		if (this.loaded) return;
 		logger.info("Loading " + this.libName);
+		for (String name: this.dependentLibNames){
+			if (name == null) continue;
+			NativeLoader.load(name);
+		}
+
 		NativeLoader.load(this.libName);
 		this.loaded = true;
 	}
