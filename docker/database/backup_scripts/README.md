88b823520f4e219ab71c788edd90c77da6aa0aa7
Jim Schaff <schaff.jim@gmail.com>
2023-10-19 12:15:15 -0400

    merge PR 961 (tag 7.5.0.61.2)

--- docker/database/backup_scripts/README.md
+++ docker/database/backup_scripts/README.md
@@ -1,1 +1,8 @@
-run ./archivedb.sh as root or with sudo to generate a dump in /data/backup
+## Nightly Oracle database backups
+
+###  Data Pump dump to /data/backup
+cron job runs `archivedb.sh` every 1:00am as `root`
+
+### Compress and Archive to ~vcell/database_backups
+cron job runs `move_files_to_vcellhome.sh` every 9:00pm as `vcell`
+
--- docker/database/backup_scripts/archivedb.sh
+++ docker/database/backup_scripts/archivedb.sh
@@ -3,5 +3,9 @@
 # run export of oracle 19c database running in the container named 'oracle-database' using the Data Pump 'expdp' command
 sudo docker exec -it oracle-database /opt/oracle/backup/scripts/run.sh 2>&1 | tee -a ../archivedb_$(date +%Y_%m_%d_%H_%M_%S).log
 
+# change permission so that vcell can move them
+chmod 666 /data/backup/*.log
+chmod 666 /data/backup/*.dmp
+
 # copy move dump file to remote storage
 echo "move dump files to remote storage - not yet mounted - do this manually"
