8b3b671b5aeb62869808477d1399731d6ef4d010
Jim Schaff <schaff.jim@gmail.com>
2023-10-19 16:41:46 -0400

    switch python citation lookup to pubmed API

--- vcell-cli-utils/tests/citation_test.py
+++ vcell-cli-utils/tests/citation_test.py
@@ -8,7 +8,7 @@ def test_citation() -> None:
     assert citation.author == "Nosbisch, Jamie L"
     assert citation.title == "A kinetic model of phospholipase C-γ1 linking structure-based insights to dynamics of enzyme autoinhibition and activation"
     assert citation.year == 2022
-    assert citation.journal == "J. Biol. Chem."
+    assert citation.journal == "J Biol Chem"
 
     suggestedProjectName = getSuggestedProjectName(bm_key="12345", pub_info=None, citation_info=citation)
     assert suggestedProjectName == "VCDB_12345_Nosbisch_JBiolChem_2022"
