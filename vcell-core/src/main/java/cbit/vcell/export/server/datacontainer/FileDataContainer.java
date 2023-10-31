7ce022973a96b9903098a0d2575ea65147c548fd
Logan Drescher <drescher@uchc.edu>
2023-10-31 09:08:36 -0400

    quick commit

--- vcell-core/src/main/java/cbit/vcell/export/server/datacontainer/FileDataContainer.java
+++ vcell-core/src/main/java/cbit/vcell/export/server/datacontainer/FileDataContainer.java
@@ -1,22 +1,37 @@
 package cbit.vcell.export.server.datacontainer;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 
 public class FileDataContainer implements ResultDataContainer {
+    private int size = 0;
+    private final File dataFile;
 
-    @Override
-    public void append(String csq) throws IOException {
-
+    public FileDataContainer() throws IOException {
+        this.dataFile = Files.createTempFile("vcellFDC", "").toFile();
     }
 
     @Override
-    public void append(ResultDataContainer container) throws IOException {
+    public void append(@NotNull String csq) throws IOException {
+        this.append(csq.getBytes());
+    }
 
+    @Override
+    public void append(@NotNull ResultDataContainer container) throws IOException {
+        this.append(container.getDataBytes());
     }
 
     @Override
     public void append(byte[] bytesToAppend) throws IOException {
+        this.size += bytesToAppend.length;
+        try (FileWriter writer = new FileWriter(this.dataFile)){
+            writer.append(bytesToAppend);
+        }
 
     }
 
@@ -27,6 +42,6 @@ public class FileDataContainer implements ResultDataContainer {
 
     @Override
     public int getDataSize() {
-        return 0;
+        return this.size;
     }
 }
