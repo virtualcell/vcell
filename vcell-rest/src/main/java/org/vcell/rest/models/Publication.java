1d32db4f3acae46f28b8af8e932b389fd2500f2e
Jim Schaff <schaff.jim@gmail.com>
2023-11-22 01:45:54 -0500

    publication get,post,delete endpoint with testing

--- vcell-rest/src/main/java/org/vcell/rest/models/Publication.java
+++ vcell-rest/src/main/java/org/vcell/rest/models/Publication.java
@@ -3,41 +3,42 @@ package org.vcell.rest.models;
 import cbit.vcell.modeldb.BioModelReferenceRep;
 import cbit.vcell.modeldb.MathModelReferenceRep;
 import cbit.vcell.modeldb.PublicationRep;
-import cbit.vcell.parser.ExpressionException;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.vcell.util.document.KeyValue;
 
 import java.util.Arrays;
 import java.util.Date;
 
 public record Publication(
-        String pubKey,
+        Long pubKey,
         String title,
         String[] authors,
         Integer year,
         String citation,
         String pubmedid,
         String doi,
-        String endnoteid,
+        Integer endnoteid,
         String url,
-        String wittid,
+        Integer wittid,
         BiomodelRef[] biomodelRefs,
         MathmodelRef[] mathmodelRefs,
+        @JsonFormat(pattern="yyyy-MM-dd")
         Date date) {
 
     public static Publication fromPublicationRef(PublicationRep publicationRep) {
         BiomodelRef[] bioModelRefs = Arrays.stream(publicationRep.getBiomodelReferenceReps()).map(BiomodelRef::fromBioModelReferenceRep).toArray(BiomodelRef[]::new);
         MathmodelRef[] mathModelRefs = Arrays.stream(publicationRep.getMathmodelReferenceReps()).map(MathmodelRef::fromMathModelReferenceRep).toArray(MathmodelRef[]::new);
         return new Publication(
-                publicationRep.getPubKey().toString(),
+                Long.parseLong(publicationRep.getPubKey().toString()),
                 publicationRep.getTitle(),
                 publicationRep.getAuthors(),
                 publicationRep.getYear(),
                 publicationRep.getCitation(),
                 publicationRep.getPubmedid(),
                 publicationRep.getDoi(),
-                publicationRep.getEndnoteid(),
+                Integer.parseInt(publicationRep.getEndnoteid()),
                 publicationRep.getUrl(),
-                publicationRep.getWittid(),
+                Integer.parseInt(publicationRep.getWittid()),
                 bioModelRefs,
                 mathModelRefs,
                 publicationRep.getDate()
@@ -48,18 +49,18 @@ public record Publication(
         BioModelReferenceRep[] bioModelReferenceReps = Arrays.stream(this.biomodelRefs).map(b -> b.toBioModelReferenceRep()).toArray(BioModelReferenceRep[]::new);
         MathModelReferenceRep[] mathModelReferenceReps = Arrays.stream(this.mathmodelRefs).map(m -> m.toMathModelReferenceRep()).toArray(MathModelReferenceRep[]::new);
         return new PublicationRep(
-                new KeyValue(pubKey),
+                pubKey!=null ? new KeyValue(Long.toString(pubKey)) : null,
                 title,
                 authors,
                 year,
                 citation,
                 pubmedid,
                 doi,
-                endnoteid,
+                Integer.toString(endnoteid),
                 url,
                 bioModelReferenceReps,
                 mathModelReferenceReps,
-                wittid,
+                Integer.toString(wittid),
                 date
         );
 
