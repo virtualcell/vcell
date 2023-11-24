90af841b75566a6e3565a1ba285bd4bcd64173b7
Jim Schaff <schaff.jim@gmail.com>
2023-11-24 08:35:05 -0500

    revert JUnit 4 style Quarkus test category, not used or needed for CI

--- vcell-rest/src/test/java/org/vcell/restq/GreetingResourceIT.java
+++ vcell-rest/src/test/java/org/vcell/restq/GreetingResourceIT.java
@@ -1,10 +1,8 @@
 package org.vcell.restq;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
-import org.junit.experimental.categories.Category;
 
 @QuarkusIntegrationTest
-@Category(org.vcell.test.Quarkus.class)
 public class GreetingResourceIT extends GreetingResourceTest {
     // Execute the same tests but in packaged mode.
 }
