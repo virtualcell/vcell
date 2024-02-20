76a752f57ca72127ecd32cad3d7916830daaa660
Ezequiel Valencia <casualcucumber59@gmail.com>
2024-02-20 11:23:33 -0500

    Rest DB Classes
    Classes that act as an abstraction layer to accessing the DB.

---
+++ vcell-rest/src/main/java/org/vcell/restq/db/BioModelRestDB.java
@@ -0,0 +1,67 @@
+package org.vcell.restq.db;
+
+import cbit.vcell.modeldb.*;
+import jakarta.inject.Inject;
+import org.vcell.restq.auth.AuthUtils;
+import org.vcell.util.DataAccessException;
+import org.vcell.util.ObjectNotFoundException;
+import org.vcell.util.document.KeyValue;
+import org.vcell.util.document.User;
+
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class BioModelRestDB {
+
+    private final DatabaseServerImpl databaseServerImpl;
+    private final SimulationRestDB simulationRestDB;
+
+    public BioModelRestDB(OracleAgroalConnectionFactory agroalConnectionFactory) throws DataAccessException {
+        databaseServerImpl = new DatabaseServerImpl(agroalConnectionFactory, agroalConnectionFactory.getKeyFactory());
+        simulationRestDB = new SimulationRestDB(agroalConnectionFactory);
+    }
+    public BioModelRep getBioModelRep(KeyValue bmKey, User vcellUser) throws SQLException, ObjectNotFoundException, DataAccessException {
+        if (vcellUser==null){
+            vcellUser = AuthUtils.DUMMY_USER;
+        }
+        ArrayList<String> conditions = new ArrayList<String>();
+        if (bmKey != null){
+            conditions.add("(" + BioModelTable.table.id.getQualifiedColName() + " = " + bmKey.toString() + ")");
+        }else{
+            throw new RuntimeException("bioModelID not specified");
+        }
+
+        StringBuffer conditionsBuffer = new StringBuffer();
+        for (String condition : conditions) {
+            if (conditionsBuffer.length() > 0) {
+                conditionsBuffer.append(" AND ");
+            }
+            conditionsBuffer.append(condition);
+        }
+        int startRow = 1;
+        int maxRows = 1;
+        BioModelRep[] bioModelReps = databaseServerImpl.getBioModelReps(vcellUser, conditionsBuffer.toString(), null, startRow, maxRows);
+        for (BioModelRep bioModelRep : bioModelReps) {
+            KeyValue[] simContextKeys = bioModelRep.getSimContextKeyList();
+            for (KeyValue scKey : simContextKeys) {
+                SimContextRep scRep = simulationRestDB.getSimContextRep(scKey);
+                if (scRep != null){
+                    bioModelRep.addSimContextRep(scRep);
+                }
+            }
+            KeyValue[] simulationKeys = bioModelRep.getSimKeyList();
+            for (KeyValue simKey : simulationKeys) {
+                SimulationRep simulationRep = simulationRestDB.getSimulationRep(simKey);
+                if (simulationRep != null){
+                    bioModelRep.addSimulationRep(simulationRep);
+                }
+            }
+
+        }
+        if (bioModelReps==null || bioModelReps.length!=1){
+            throw new ObjectNotFoundException("failed to get biomodel");
+        }
+        return bioModelReps[0];
+    }
+}
