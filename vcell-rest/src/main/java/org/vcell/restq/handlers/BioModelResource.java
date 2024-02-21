6f54f9fe29ebcc28799beebfc789a2cd034fe64c
Ezequiel Valencia <casualcucumber59@gmail.com>
2024-02-21 10:45:15 -0500

    Upload and Delete Endpoints
    Added endpoints that can delete and upload biomodels. Also wrote the boiler plate for other endpoints regarding biomodels.

--- vcell-rest/src/main/java/org/vcell/restq/handlers/BioModelResource.java
+++ vcell-rest/src/main/java/org/vcell/restq/handlers/BioModelResource.java
@@ -1,53 +1,103 @@
 package org.vcell.restq.handlers;
 
 import cbit.vcell.modeldb.BioModelRep;
+import cbit.vcell.modeldb.DatabaseServerImpl;
 import cbit.vcell.parser.ExpressionException;
-import com.google.gson.Gson;
-import io.quarkus.qute.Location;
-import io.quarkus.qute.Template;
-import io.quarkus.qute.TemplateInstance;
+import cbit.vcell.xml.XmlParseException;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.UriInfo;
 import org.eclipse.microprofile.openapi.annotations.Operation;
-import org.vcell.restq.Main;
 import org.vcell.restq.auth.AuthUtils;
 import org.vcell.restq.db.BioModelRestDB;
 import org.vcell.restq.db.OracleAgroalConnectionFactory;
 import org.vcell.restq.models.BioModel;
+import org.vcell.util.BigString;
 import org.vcell.util.DataAccessException;
 import org.vcell.util.document.KeyValue;
 import org.vcell.util.document.User;
 
-import java.net.MalformedURLException;
-import java.net.URLEncoder;
-import java.nio.charset.Charset;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Path("/api/biomodel")
 public class BioModelResource {
     private static final User dummyUser = AuthUtils.DUMMY_USER; // replace when OAuth is implemented
     private final BioModelRestDB bioModelRestDB;
+    private final DatabaseServerImpl databaseServer;
 
     @Inject
     public BioModelResource(OracleAgroalConnectionFactory oracleAgroalConnectionFactory) throws DataAccessException {
         bioModelRestDB = new BioModelRestDB(oracleAgroalConnectionFactory);
+        databaseServer = new DatabaseServerImpl(oracleAgroalConnectionFactory, oracleAgroalConnectionFactory.getKeyFactory());
     }
 
     @GET
-    @Path("{biomodelID}/json")
-    @Operation(operationId = "getBiomodelInfoJSONById", summary = "Get biomodel information in JSON format by ID")
+    @Path("{bioModelID}")
+    @Operation(operationId = "getBiomodelById", summary = "Get BioModel information in JSON format by ID.")
     @Produces(MediaType.APPLICATION_JSON)
-    public BioModel getBioModelInfoJSON(@PathParam("biomodelID") String bioModelID) throws SQLException, DataAccessException, ExpressionException {
+    public BioModel getBioModelInfo(@PathParam("bioModelID") String bioModelID) throws SQLException, DataAccessException, ExpressionException {
         User vcellUser = dummyUser;
         BioModelRep bioModelRep = bioModelRestDB.getBioModelRep(new KeyValue(bioModelID), vcellUser);
         return BioModel.fromBioModelRep(bioModelRep);
     }
+
+    // TODO: Specify the media type instead of leaving it as wildcard
+    @GET
+    @Path("{biomodelID}/vcml_download")
+    @Operation(operationId = "getBioModelVCML", summary = "Get the BioModel in VCML format.")
+    @Produces(MediaType.MEDIA_TYPE_WILDCARD)
+    public void getBioModelVCML(){
+
+    }
+
+    @GET
+    @Path("{biomodelID}/sbml_download")
+    @Operation(operationId = "getBioModelSBML", summary = "Get the BioModel in SBML format.")
+    @Produces(MediaType.MEDIA_TYPE_WILDCARD)
+    public void getBioModelSBML(){
+
+    }
+
+    @GET
+    @Path("{biomodelID}/omex_download")
+    @Operation(operationId = "getBioModelOMEX", summary = "Get the BioModel in OMEX format.")
+    @Produces(MediaType.MEDIA_TYPE_WILDCARD)
+    public void getBioModelOMEX(){
+
+    }
+
+    @GET
+    @Path("{biomodelID}/bngl_download")
+    @Operation(operationId = "getBioModelBNGL", summary = "Get the BioModel in BNGL format.")
+    @Produces(MediaType.MEDIA_TYPE_WILDCARD)
+    public void getBioModelBNGL(){
+
+    }
+
+    @GET
+    @Path("{biomodelID}/diagram_download")
+    @Operation(operationId = "getBioModelDIAGRAM", summary = "Get the BioModels diagram.")
+    @Produces(MediaType.MEDIA_TYPE_WILDCARD)
+    public void getBioModelDiagram(){
+
+    }
+
+    @POST
+    @Path("upload_bioModel")
+    @Operation(operationId = "uploadBioModel", summary = "Upload the BioModel to VCell database.")
+    @Consumes(MediaType.TEXT_XML)
+    public void uploadBioModel(String bioModelXML) throws DataAccessException {
+        User user = dummyUser;
+        databaseServer.saveBioModel(user, new BigString(bioModelXML), null);
+    }
+
+    @DELETE
+    @Path("{bioModelID}/delete_bioModel")
+    @Operation(operationId = "deleteBioModel", summary = "Delete the BioModel from VCell's database.")
+    public void deleteBioModel(@PathParam("bioModelID") String bioModelID) throws DataAccessException {
+        User user = dummyUser;
+        databaseServer.deleteBioModel(user, new KeyValue(bioModelID));
+    }
+
+
 }
