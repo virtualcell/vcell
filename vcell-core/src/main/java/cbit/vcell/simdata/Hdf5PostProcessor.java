7ffbd5ab44f321b688b87e6db0a766ad71309a0e
Jim Schaff <schaff.jim@gmail.com>
2024-03-07 03:03:32 -0500

    new pure java postprocessor hdf5 reader with unit test, like python impl

---
+++ vcell-core/src/main/java/cbit/vcell/simdata/Hdf5PostProcessor.java
@@ -0,0 +1,186 @@
+package cbit.vcell.simdata;
+
+import io.jhdf.HdfFile;
+import io.jhdf.api.Attribute;
+import io.jhdf.api.Dataset;
+import io.jhdf.api.Group;
+import io.jhdf.api.Node;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Hdf5PostProcessor {
+
+    enum Statistic {
+        average(0), total(1), min(2), max(3);
+        private final int index;
+        private Statistic(int index) {
+            this.index = index;
+        }
+        static Statistic fromIndex(int index) {
+            for (Statistic statistic : Statistic.values()) {
+                if (statistic.index == index) {
+                    return statistic;
+                }
+            }
+            throw new IllegalArgumentException("No Statistic with index " + index);
+        }
+
+        static Statistic fromName(String name) {
+            for (Statistic statistic : Statistic.values()) {
+                if (statistic.name().equals(name)) {
+                    return statistic;
+                }
+            }
+            throw new IllegalArgumentException("No Statistic with name " + name);
+        }
+    }
+
+    record VariableInfo(int channel_index, String stat_var_name, String unit, int var_index, String var_name) {}
+    public static class ImageMetadata {
+        String groupPath;
+        String name;
+        double[] extent;
+        double[] origin;
+        int[] shape;
+        ImageMetadata(String groupPath, String name) {
+            this.groupPath = groupPath;
+            this.name = name;
+        }
+
+        Dataset getDataset(HdfFile hdfFile, int timeIndex) {
+            return hdfFile.getDatasetByPath(groupPath + "time" + String.format("%06d", timeIndex));
+        }
+
+        void read(HdfFile hdfFile) {
+            Dataset imageDataset_t0 = getDataset(hdfFile, 0);
+            this.shape = imageDataset_t0.getDimensions();
+            if (this.shape.length == 2){
+                this.shape = new int[] {1, this.shape[0], this.shape[1]};
+            }
+
+            Group parentGroup = imageDataset_t0.getParent();
+            List<Double> extents_list = new ArrayList<>();
+            List<Double> origin_list = new ArrayList<>();
+            Attribute extentX = parentGroup.getAttribute("ExtentX");
+            Attribute extentY = parentGroup.getAttribute("ExtentY");
+            Attribute extentZ = parentGroup.getAttribute("ExtentZ");
+            Attribute originX = parentGroup.getAttribute("OriginX");
+            Attribute originY = parentGroup.getAttribute("OriginY");
+            Attribute originZ = parentGroup.getAttribute("OriginZ");
+            this.extent = new double[] { (Float)extentX.getData(), (Float)extentY.getData(), (Float)extentZ.getData() };
+            this.origin = new double[] { (Float)originX.getData(), (Float)originY.getData(), (Float)originZ.getData() };
+        }
+    }
+
+    public static class PostProcessing {
+        Path postprocessingHdf5Path;
+        double[] times;
+        List<VariableInfo> variables;
+        List<double[]> statistics_by_channel = new ArrayList<>();
+        List<ImageMetadata> imageMetadata;
+
+        public PostProcessing(Path postprocessingHdf5Path) {
+            this.postprocessingHdf5Path = postprocessingHdf5Path;
+            this.variables = new ArrayList<>();
+            this.imageMetadata = new ArrayList<>();
+        }
+
+        public void read() {
+            try (HdfFile hdfFile = new HdfFile(postprocessingHdf5Path.toFile())) {
+                // Read dataset with path /PostProcessing/Times
+                Dataset timesDataset = hdfFile.getDatasetByPath("/PostProcessing/Times");
+                this.times = (double[]) timesDataset.getData();
+
+                // Read attributes from group /PostProcessing/VariableStatistics
+                Node postProcessing_node = hdfFile.getChild("PostProcessing");
+                if (!(postProcessing_node instanceof Group postProcessing_group)) {
+                    throw new RuntimeException("PostProcessing is not a group");
+                }
+                Node variableStatistics_node = postProcessing_group.getChild("VariableStatistics");
+                if (!(variableStatistics_node instanceof Group variableStatistics_group)) {
+                    throw new RuntimeException("VariableStatistics is not a group");
+                }
+
+                // read attributes of a variableStatistics_group
+                Map<Integer,String> var_name_by_channel = new HashMap<>();
+                Map<Integer,String> var_unit_by_channel = new HashMap<>();
+                for (Attribute attribute : variableStatistics_group.getAttributes().values()) {
+                    // attributes have keys like "comp_0_name" or "comp_0_unit" and values like "Ca" or "uM"
+                    String[] parts = attribute.getName().split("_");
+                    if (attribute.getName().endsWith("_name")) {
+                        var_name_by_channel.put(Integer.parseInt(parts[1]), (String) attribute.getData());
+                    } else if (attribute.getName().endsWith("_unit")) {
+                        var_unit_by_channel.put(Integer.parseInt(parts[1]), (String) attribute.getData());
+                    }
+                }
+                this.variables = new ArrayList<>();
+                for (int channel : var_name_by_channel.keySet()) {
+                    String stat_var_name = var_name_by_channel.get(channel);
+                    String unit = var_unit_by_channel.get(channel);
+                    int var_index = channel / 4;  // because there are 4 statistics per variable
+                    String var_name = stat_var_name.substring(0, stat_var_name.lastIndexOf("_"));
+                    this.variables.add(new VariableInfo(channel, stat_var_name, unit, var_index, var_name));
+                }
+
+                // read VariableStatistics datasets for each time
+                List<double[]> statistics_by_time = new ArrayList<>();
+                for (int time_index=0; time_index<times.length; time_index++) {
+                    // Read dataset with path /PostProcessing/VariableStatistics/time000000
+                    Dataset variableStatisticsDataset = hdfFile.getDatasetByPath("/PostProcessing/VariableStatistics/time" + String.format("%06d", time_index));
+                    statistics_by_time.add((double[])variableStatisticsDataset.getData());
+                }
+                // transpose statistics_by_time into statistics_by_channel
+                for (int channel=0; channel<variables.size(); channel++) {
+                    double[] statistics = new double[times.length];
+                    for (int time_index=0; time_index<times.length; time_index++) {
+                        statistics[time_index] = statistics_by_time.get(time_index)[channel];
+                    }
+                    this.statistics_by_channel.add(statistics);
+                }
+
+                // Get list of child groups from /PostProcessing which are not Times or VariableStatistics
+                for (Node node : postProcessing_group.getChildren().values()) {
+                    if (node instanceof Group group) {
+                        if (group.getName().equals("Times") || group.getName().equals("VariableStatistics")) {
+                            continue;
+                        }
+                        ImageMetadata imageMetadata = new ImageMetadata(group.getPath(), group.getName());
+                        imageMetadata.read(hdfFile);
+                        this.imageMetadata.add(imageMetadata);
+                    }
+                }
+            }
+        }
+
+        public List<VariableInfo> getVariables() {
+            return this.variables;
+        }
+
+        public double[] readVariableData(VariableInfo variableInfo) {
+            return this.statistics_by_channel.get(variableInfo.channel_index);
+        }
+
+        /**
+         * Read image data from the HDF5 file
+         * @param imageMetadata
+         * @param timeIndex
+         * @return double[][][] 3D array of doubles (z, y, x) z=1 for 2D images
+         */
+        public double[][][] readImageData(ImageMetadata imageMetadata, int timeIndex) {
+            try (HdfFile hdfFile = new HdfFile(postprocessingHdf5Path.toFile())) {
+                Dataset imageDataset = hdfFile.getDatasetByPath(imageMetadata.groupPath + "time" + String.format("%06d", timeIndex));
+                if (imageDataset.getDimensions().length == 2) {
+                    return new double[][][] { (double[][]) imageDataset.getData() };
+                } else if (imageDataset.getDimensions().length == 3) {
+                    return (double[][][]) imageDataset.getData();
+                } else {
+                    throw new RuntimeException("Image dataset has unexpected number of dimensions: " + imageDataset.getDimensions().length);
+                }
+            }
+        }
+    }
+}
