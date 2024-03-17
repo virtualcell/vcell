dda7d30d75b906368f1c7e6d6ed35b88e52fe729
Jim Schaff <schaff@uchc.edu>
2024-03-17 11:15:47 -0400

    upgraded mongodb java driver and fixed mongo service deployment

--- docker/kustomize/scripts/sealed_secret_api.sh
+++ docker/kustomize/scripts/sealed_secret_api.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
 # This script is used to create a sealed secret for the database and jms passwords
-# this script should take 3 arguments as input: namespace, db_password and jms_password and output a sealed secret to stdout
-# Example: ./sealed_secret_api.sh devjim pswd12345 pswd39393 > output.yaml
+# this script should take 5 arguments as input:
+#   namespace
+#   db_password
+#   jms_password
+#   mongo_user
+#   mongo_pswd
+#
+#   and outputs a sealed secret to stdout
+# Example: ./sealed_secret_api.sh devjim pswd12345 pswd39393 mongo_user pswd292929 > output.yaml
 
 # validate the number of arguments
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 5 ]; then
     echo "Illegal number of parameters"
-    echo "Usage: ./sealed_secret_db.sh <namespace> <db_password> <jms_password>"
+    echo "Usage: ./sealed_secret_db.sh <namespace> <db_password> <jms_password> <mongo_user> <mongo_pswd>"
     exit 1
 fi
 
@@ -15,8 +22,12 @@ SECRET_NAME="api-secrets"
 NAMESPACE=$1
 DATABASE_PASSWORD=$2
 JMS_PASSWORD=$3
+MONGO_USERNAME=$4
+MONGO_PASSWORD=$5
 
 kubectl create secret generic ${SECRET_NAME} --dry-run=client \
       --from-literal=dbpswd="${DATABASE_PASSWORD}" \
       --from-literal=jmspswd="${JMS_PASSWORD}" \
+      --from-literal=mongo-username="${MONGO_USERNAME}" \
+      --from-literal=mongo-password="${MONGO_PASSWORD}" \
       --namespace="${NAMESPACE}" -o yaml | kubeseal --format yaml
