adf6fb6d2b562e2985a46b34df12e6b039a1c58b
Jim Schaff <schaff@uchc.edu>
2024-04-24 11:39:50 -0400

    factor exportTableToHDF5 out of Plot2DDataPanel

--- vcell-core/src/main/java/cbit/vcell/simdata/Hdf5Utils.java
+++ vcell-core/src/main/java/cbit/vcell/simdata/Hdf5Utils.java
@@ -1,10 +1,14 @@
 package cbit.vcell.simdata;
 
+import java.io.File;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.ListIterator;
 
+import cbit.vcell.math.ReservedVariable;
 import org.apache.commons.lang.ArrayUtils;
 
 import cbit.vcell.math.Variable;
@@ -38,7 +42,184 @@ public class Hdf5Utils {
 		return HDF5_GROUP_EXTRAPOLATED_VOLUMES + HDF5_GROUP_DIRECTORY_SEPARATOR + "__" + Variable.getNameFromCombinedIdentifier(varName) + "_extrapolated__"; 
 	}
 
-	/**
+    public static File exportTableToHDF5(boolean bHistogram, String blankCellValue, int[] columns, int[] rows, String xVarColumnName, String hdf5DescriptionText, String[] columnNames, String[] paramScanParamNames, Double[][] paramScanParamValues, Object[][] rowColValues) throws Exception {
+		int hdf5FileID = -1;//Used if HDF5 format
+		File hdf5TempFile = null;
+		try {
+			hdf5TempFile = File.createTempFile("plot2D", ".hdf");
+			//System.out.println("/home/vcell/Downloads/hdf5/HDFView/bin/HDFView "+hdf5TempFile.getAbsolutePath());
+			hdf5FileID = H5.H5Fcreate(hdf5TempFile.getAbsolutePath(), HDF5Constants.H5F_ACC_TRUNC,HDF5Constants.H5P_DEFAULT, HDF5Constants.H5P_DEFAULT);
+			ArrayList<ArrayList<Integer>> paramScanJobs = new ArrayList<>();
+			if(!bHistogram && !columnNames[0].equals((xVarColumnName==null? ReservedVariable.TIME.getName():xVarColumnName))) {
+				throw new Exception("Expecting first column in table to have name '"+xVarColumnName+"'");
+			}
+			//Add arraylist for the parameter scan job, add the index of the xval column
+			for(int i=0;i<columnNames.length;i++) {
+				if(bHistogram) {
+					ArrayList<Integer> tempAL = new ArrayList<Integer>();
+					paramScanJobs.add(tempAL);
+					break;
+				} else if(columnNames[i].equals((xVarColumnName==null?ReservedVariable.TIME.getName():xVarColumnName))){
+					if(i==0) {
+						ArrayList<Integer> tempAL = new ArrayList<Integer>();
+						tempAL.add(i);
+						paramScanJobs.add(tempAL);
+					}else {
+						String str1 = columnNames[i-1];
+						int str1Index = str1.lastIndexOf("Set ");
+						String str2 = columnNames[i+1];
+						int str2Index = str2.lastIndexOf("Set ");
+						if(!str1.substring(str1Index).equals(str2.substring(str2Index))) {
+							ArrayList<Integer> tempAL = new ArrayList<Integer>();
+							tempAL.add(i);
+							paramScanJobs.add(tempAL);
+						}
+					}
+				}
+			}
+			//Add selected columns to the proper paramscan arraylist
+			for(int j = 0; j< columns.length; j++) {
+				if(bHistogram) {
+					paramScanJobs.get(0).add(columns[j]);
+				}else {
+					if(columnNames[columns[j]].equals((xVarColumnName==null?ReservedVariable.TIME.getName():xVarColumnName))){
+						continue;//skip xcolumns
+					}
+					for(int k=0;k<paramScanJobs.size();k++) {
+						if(columns[j] >= paramScanJobs.get(k).get(0) && ((k+1) == paramScanJobs.size() || columns[j] < paramScanJobs.get(k+1).get(0))) {
+							paramScanJobs.get(k).add(columns[j]);
+//								System.out.println("HDF5frm"+columnNames[columns[j]));
+						}
+					}
+				}
+			}
+			//Remove unselected indexes from set lists
+			for(int k=0;k<paramScanJobs.size();k++) {
+				final ListIterator<Integer> listIterator = paramScanJobs.get(k).listIterator();
+				if(paramScanJobs.get(k).size() > 1) {// keep x val is there more selections for this set
+					listIterator.next();
+				}
+				while(listIterator.hasNext()) {
+					final Integer columIndex = listIterator.next();
+					boolean bFound = false;
+					for(int j = 0; j< columns.length; j++) {
+						if(columIndex == columns[j]) {
+							bFound = true;
+							break;
+						}
+					}
+					if(!bFound) {
+						listIterator.remove();
+					}
+				}
+			}
+			//Write out the data to HDF5 file
+			for(int k=0;k<paramScanJobs.size();k++) {
+				int selectedColCount = paramScanJobs.get(k).size();
+				if(selectedColCount == 0) {
+					continue;
+				}
+				int jobGroupID = -1;//(int) Hdf5Utils.createGroup(hdf5FileID, "Set "+k);
+						//writeHDF5Dataset(hdf5FileID, "Set "+k, null, null, false);
+				HDF5WriteHelper help0 = null;//Hdf5Utils.createDataset(jobGroupID, "data", new long[] {selectedColCount,rows.length});
+						//(HDF5WriteHelper) Hdf5Utils.writeHDF5Dataset(jobGroupID, "data", new long[] {selectedColCount,rows.length}, new Object[] {}, false);
+				//((DefaultTableModel)getScrollPaneTable().getModel()).getDataVector()
+				double[] fromData = new double[rows.length*selectedColCount];
+				int actualLength = -1;
+				int index = 0;
+				ArrayList<String> dataTypes = new ArrayList<String>();
+				ArrayList<String> dataIDs = new ArrayList<String>();
+				ArrayList<String> dataShapes = new ArrayList<String>();
+				ArrayList<String> dataLabels = new ArrayList<String>();
+				ArrayList<String> dataNames = new ArrayList<String>();
+				ArrayList<String> paramNames = new ArrayList<String>();
+				ArrayList<String> paramValues = new ArrayList<String>();
+				boolean bParamsDone = false;
+				for(int cols=0;cols<paramScanJobs.get(k).size();cols++) {
+					final Integer column = paramScanJobs.get(k).get(cols);
+					dataTypes.add("float64");
+					dataIDs.add("data_set_"+columnNames[column]);
+					dataShapes.add(rows.length+"");
+					dataLabels.add(columnNames[column]);
+					String name = "--";
+					if(columnNames[column].equals((xVarColumnName==null?ReservedVariable.TIME.getName():xVarColumnName))) {
+						name = columnNames[column];
+					}else {
+						int indx = columnNames[column].lastIndexOf("-- Set ");
+						if(indx != -1) {
+							name = columnNames[column].substring(0, indx);
+						}else {
+							name = columnNames[column];
+						}
+					}
+					dataNames.add(name);
+					for(int myrows = 0; myrows< rows.length; myrows++) {
+						final int row = rows[myrows];
+						final Object valueAt = rowColValues[row][column];
+						if(valueAt == null && actualLength == -1) {
+							actualLength = myrows;
+						}
+//							System.out.println(row+" "+column+" "+valueAt);
+						fromData[index] = Double.parseDouble((valueAt==null? blankCellValue :valueAt.toString()));
+						index++;
+					}
+					actualLength = (actualLength==-1? rows.length:actualLength);
+					String colName = columnNames[column];
+//						System.out.println("HDF5frm "+colName);
+					if(colName.lastIndexOf("Set ") != -1) {
+						if(!bParamsDone) {
+							bParamsDone = true;
+							int set = Integer.parseInt(colName.substring(colName.lastIndexOf("Set ")+4));
+							jobGroupID = (int) createGroup(hdf5FileID, "Set "+set);
+							help0 = createDataset(jobGroupID, "data", new long[] {selectedColCount,actualLength});
+							for(int z=0;z<paramScanParamNames.length;z++) {
+								paramNames.add(paramScanParamNames[z]);
+								paramValues.add(paramScanParamValues[set][z]+"");
+//									System.out.print(" "+paramScanParamValues[set][z]);
+							}
+//							System.out.println();
+						}
+					}
+
+				}
+
+				double[] fromData2 = new double[actualLength*selectedColCount];
+				for(int i=0;i<selectedColCount;i++) {
+					System.arraycopy(fromData, i* rows.length, fromData2, i*actualLength, actualLength);
+				}
+				if(help0 == null) {
+					jobGroupID = (int) createGroup(hdf5FileID, "Set "+k);
+					help0 = createDataset(jobGroupID, "data", new long[] {selectedColCount,actualLength});
+				}
+				copySlice(help0.hdf5DatasetValuesID,fromData2,new long[] {0,0},new long[] {selectedColCount,actualLength},new long[] {selectedColCount,actualLength},new long[] {0,0},new long[] {selectedColCount,actualLength},help0.hdf5DataSpaceID);
+					//writeHDF5Dataset(help0.hdf5DatasetValuesID, null, null, objArr, false);
+				insertAttribute(help0.hdf5DatasetValuesID, "_type", "ODE Data Export");//.writeHDF5Dataset(help0.hdf5DatasetValuesID, "_type", null, "ODE Data Export", true);
+				insertAttributes(help0.hdf5DatasetValuesID,"dataSetDataTypes", dataTypes);//.writeHDF5Dataset(help0.hdf5DatasetValuesID, "dataSetDataTypes", null, dataTypes, true);
+				insertAttributes(help0.hdf5DatasetValuesID,"dataSetIds",dataIDs);//Hdf5Utils.writeHDF5Dataset(help0.hdf5DatasetValuesID, "dataSetIds", null,dataIDs , true);
+				insertAttributes(help0.hdf5DatasetValuesID,"dataSetLabels",dataLabels);//Hdf5Utils.writeHDF5Dataset(help0.hdf5DatasetValuesID, "dataSetLabels", null,dataLabels , true);
+				insertAttributes(help0.hdf5DatasetValuesID,"dataSetNames",dataNames);//Hdf5Utils.writeHDF5Dataset(help0.hdf5DatasetValuesID, "dataSetNames", null,dataNames , true);
+				insertAttributes(help0.hdf5DatasetValuesID,"dataSetShapes",dataShapes);//Hdf5Utils.writeHDF5Dataset(help0.hdf5DatasetValuesID, "dataSetShapes", null,dataShapes , true);
+				insertAttribute(help0.hdf5DatasetValuesID,"id","report");//Hdf5Utils.writeHDF5Dataset(help0.hdf5DatasetValuesID, "id", null,"report" , true);
+				if(paramNames.size() != 0) {
+					insertAttributes(help0.hdf5DatasetValuesID,"paramNames",paramNames);
+					insertAttributes(help0.hdf5DatasetValuesID,"paramValues",paramValues);
+				}
+				help0.close();
+				H5.H5Gclose(jobGroupID);
+			}
+			if(hdf5DescriptionText != null) {
+				insertAttributes(hdf5FileID,"dataSourceDescr", Arrays.asList(new String[] {hdf5DescriptionText}));
+			}
+			H5.H5Fclose(hdf5FileID);
+			hdf5FileID = -1;
+		}finally {
+			if(hdf5FileID != -1) {try{H5.H5Fclose(hdf5FileID);}catch(Exception e){e.printStackTrace();}}
+			if(hdf5TempFile != null && hdf5TempFile.exists()) {try{hdf5TempFile.delete();}catch(Exception e){e.printStackTrace();}}
+		}
+		return hdf5TempFile;
+	}
+
+    /**
 	 * Helper class to ensure HDF5 documents are closed properly.
 	 */
 	public static class HDF5WriteHelper {
