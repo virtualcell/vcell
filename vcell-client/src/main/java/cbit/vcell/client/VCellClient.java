3b2fce5aa698cf46613ebbaa427a59c859167707
Ezequiel Valencia <casualcucumber59@gmail.com>
2024-05-21 08:46:30 -0400

    Dynamic Java Popup for Redirect Browser
    Popup a GUI telling the enduser about the redirection of the browser. Initial creation still needs refinement.

--- vcell-client/src/main/java/cbit/vcell/client/VCellClient.java
+++ vcell-client/src/main/java/cbit/vcell/client/VCellClient.java
@@ -10,35 +10,6 @@
 
 package cbit.vcell.client;
 
-import java.awt.Window;
-import java.util.Hashtable;
-import java.util.Map;
-
-import javax.swing.JComponent;
-import javax.swing.JPanel;
-import javax.swing.RepaintManager;
-import javax.swing.SwingUtilities;
-
-import cbit.vcell.server.VCellConnectionFactory;
-import com.google.inject.Inject;
-import org.apache.http.client.protocol.HttpClientContext;
-import org.vcell.api.client.VCellApiClient;
-import org.vcell.api.common.AccessTokenRepresentation;
-import org.vcell.restclient.api.UsersResourceApi;
-import org.vcell.restclient.auth.AuthApiClient;
-import org.vcell.restclient.model.AccesTokenRepresentationRecord;
-import org.vcell.restclient.model.MapUser;
-import org.vcell.util.UserCancelException;
-import org.vcell.util.document.UserLoginInfo;
-import org.vcell.util.document.UserLoginInfo.DigestedPassword;
-import org.vcell.util.document.VCDocument;
-import org.vcell.util.document.VCDocument.VCDocumentType;
-import org.vcell.util.gui.DialogUtils;
-
-import com.install4j.api.launcher.ApplicationLauncher;
-import com.install4j.api.launcher.ApplicationLauncher.Callback;
-import com.install4j.api.launcher.Variables;
-
 import cbit.vcell.biomodel.BioModel;
 import cbit.vcell.client.desktop.DocumentWindowAboutBox;
 import cbit.vcell.client.server.ClientServerInfo;
@@ -52,6 +23,20 @@ import cbit.vcell.desktop.LoginManager;
 import cbit.vcell.geometry.Geometry;
 import cbit.vcell.mathmodel.MathModel;
 import cbit.vcell.resource.ErrorUtils;
+import cbit.vcell.resource.ResourceUtil;
+import cbit.vcell.server.VCellConnectionFactory;
+import com.google.inject.Inject;
+import com.install4j.api.launcher.ApplicationLauncher;
+import org.vcell.util.UserCancelException;
+import org.vcell.util.document.UserLoginInfo;
+import org.vcell.util.document.UserLoginInfo.DigestedPassword;
+import org.vcell.util.document.VCDocument;
+import org.vcell.util.document.VCDocument.VCDocumentType;
+
+import javax.swing.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Hashtable;
 /**
  * Insert the type's description here.
  * Creation date: (5/5/2004 1:24:03 PM)
@@ -265,27 +250,38 @@ public void startClient(final VCDocument startupDoc, final ClientServerInfo clie
 	AsynchClientTask task2b = new AsynchClientTask("Login With Auth0...", AsynchClientTask.TASKTYPE_NONSWING_BLOCKING) {
 		@Override
 		public void run(Hashtable<String, Object> hashTable) throws Exception {
+			Path appState = Path.of(ResourceUtil.getVcellHome().getAbsolutePath(), "/state.json");
+			boolean showPopupMenu = true;
+			try{
+				if (Files.exists(appState)) {
+					String json = Files.readString(appState);
+					showPopupMenu = false;
+				}
+			}
+			catch(Exception e){
+				throw new Exception("Failed to read state file");
+			}
+			if(showPopupMenu){
+				int accept = JOptionPane.showConfirmDialog(null,
+						"VCell is going to redirect you to your browser to login. Do you wish to proceed?");
+				if(accept==JOptionPane.NO_OPTION || accept==JOptionPane.CLOSED_OPTION || accept==JOptionPane.CANCEL_OPTION){
+					return;
+				}
+			}
 			vcellConnectionFactory.auth0SignIn();
 			DocumentWindowManager currWindowManager = (DocumentWindowManager)hashTable.get("currWindowManager");
-			if (!vcellConnectionFactory.isVCellIdentityMappedToAuth0Identity()){
-				// pop up with ability to input login info
-				loginAuth0(getRequestManager(), clientServerInfo, currWindowManager);
+			int numberOfPolls = 0;
+			while(!vcellConnectionFactory.isVCellIdentityMappedToAuth0Identity()){
+				if (numberOfPolls==20) {
+					return;
+				}
+				numberOfPolls++;
+				Thread.sleep(5000); // Poll every 5 seconds
 			}
-			else{
-				ClientServerInfo newClientServerInfo = createClientServerInfo(clientServerInfo, vcellConnectionFactory.getAuth0MappedUser(), null);
-				getRequestManager().connectToServer(currWindowManager, newClientServerInfo);
-			}
-		}
-	};
 
-	AsynchClientTask task2c  = new AsynchClientTask("Login...", AsynchClientTask.TASKTYPE_SWING_BLOCKING) {
-
-		@Override
-		public void run(Hashtable<String, Object> hashTable) throws Exception {
-		    if (clientServerInfo.getUsername() == null) {
-			    // we were not supplied login credentials; pop-up dialog
-				VCellClient.this.login(VCellClient.this.getRequestManager(), clientServerInfo, ((DocumentWindowManager)hashTable.get("currWindowManager")));
-		    }
+			ClientServerInfo newClientServerInfo = createClientServerInfo(clientServerInfo, vcellConnectionFactory.getAuth0MappedUser(), null);
+			getRequestManager().connectToServer(currWindowManager, newClientServerInfo);
+			Files.createFile(appState);
 		}
 	};
 	
