64ab5a7b5cd2a23ca968dd1b752b64bac25ae571
Ezequiel Valencia <casualcucumber59@gmail.com>
2024-06-05 14:18:46 -0400

    Insecure Certificate Toggle in Parent Class
    Moved the toggle that allow for insecure certificates to be used to the parent class.

--- vcell-restclient/src/main/java/org/vcell/restclient/auth/AuthApiClient.java
+++ vcell-restclient/src/main/java/org/vcell/restclient/auth/AuthApiClient.java
@@ -42,28 +42,7 @@ public class AuthApiClient extends ApiClient {
     }
 
     public AuthApiClient(URI apiBaseUrl, URI oidcProviderTokenEndpoint, AccessToken accessToken, RefreshToken refreshToken, boolean ignoreSSLCertProblems) {
-        if (ignoreSSLCertProblems) {
-            try {
-                HttpClient.Builder customBuilder = createDefaultHttpClientBuilder();
-                TrustManager[] trustAllCerts = new TrustManager[]{
-                        new X509TrustManager() {
-                            public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
-                            public void checkClientTrusted(X509Certificate[] certs, String authType) { }
-                            public void checkServerTrusted(X509Certificate[] certs, String authType) { }
-                        }
-                };
-
-                // Install the all-trusting trust manager
-                SSLContext sslContext = SSLContext.getInstance("SSL");
-                sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-                customBuilder.sslContext(sslContext);
-
-                // Create an HttpClient that uses the custom SSLContext
-                setHttpClientBuilder(customBuilder);
-            } catch (Exception e) {
-                throw new RuntimeException("failed to create custom HttpClient: " + e.getMessage(), e);
-            }
-        }
+        allowInsecureCertificates(ignoreSSLCertProblems);
         this.oidcProviderTokenEndpoint = oidcProviderTokenEndpoint;
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
