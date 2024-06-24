5db832fcf45e3a18fc4a374a7ba874fa92f4198d
Ezequiel Valencia <casualcucumber59@gmail.com>
2024-06-24 12:30:17 -0400

    KeyValue Class Serializable by Jackson

--- vcell-core/src/main/java/org/vcell/util/document/KeyValue.java
+++ vcell-core/src/main/java/org/vcell/util/document/KeyValue.java
@@ -10,35 +10,27 @@
 
 package org.vcell.util.document;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import org.vcell.util.Matchable;
-/**
- * This class is Immutable (must stay immutable).
- *
- * encapsuates primary keys in the database (oracle)
- */
+
 @SuppressWarnings("serial")
+
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class KeyValue implements java.io.Serializable, Matchable {
 	private java.math.BigDecimal value = null;
-/**
- * KeyValue constructor comment.
- */
+
 public KeyValue(String value) throws NumberFormatException {
 	this.value = new java.math.BigDecimal(value);
 }
-/**
- * KeyValue constructor comment.
- */
+
 public KeyValue(java.math.BigDecimal key) throws IllegalArgumentException {
 	if (key==null){
 		throw new IllegalArgumentException("key is null");
 	}
 	this.value = key;
 }
-/**
- * This method was created in VisualAge.
- * @return boolean
- * @param obj java.lang.Object
- */
+
 public boolean compareEqual(Matchable obj) {
 	if (obj == this){
 		return true;
@@ -52,36 +44,22 @@ public boolean compareEqual(Matchable obj) {
 	}
 	return false;
 }
-/**
- * This method was created in VisualAge.
- * @return boolean
- * @param obj java.lang.Object
- */
+
 public boolean equals(Object obj) {
 	if (obj instanceof KeyValue){
 		return ((KeyValue)obj).value.equals(value);
 	}
 	return false;
 }
-/**
- * This method was created in VisualAge.
- * @return cbit.sql.KeyValue
- * @param strValue java.lang.String
- */
+
 public static KeyValue fromString(String strValue) throws NumberFormatException {
 	return new KeyValue(strValue);
 }
-/**
- * This method was created in VisualAge.
- * @return int
- */
+
 public int hashCode() {
 	return value.hashCode();
 }
-/**
- * This method was created in VisualAge.
- * @return java.lang.String
- */
+
 public String toString() {
 	return value.toString();
 }
