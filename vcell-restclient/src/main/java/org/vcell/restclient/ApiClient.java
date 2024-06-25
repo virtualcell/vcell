bc66d7cd8b38a4f8768b4d7c26b8a590b8482ae8
Ezequiel Valencia <casualcucumber59@gmail.com>
2024-06-25 11:47:27 -0400

    Allow Insecure Certificate Function Seperated

--- vcell-restclient/src/main/java/org/vcell/restclient/ApiClient.java
+++ vcell-restclient/src/main/java/org/vcell/restclient/ApiClient.java
@@ -177,31 +177,6 @@ public class ApiClient {
     asyncResponseInterceptor = null;
   }
 
-  public void allowInsecureCertificates(boolean allowInsecureCertificates) {
-    if (allowInsecureCertificates) {
-      try {
-        HttpClient.Builder customBuilder = createDefaultHttpClientBuilder();
-        TrustManager[] trustAllCerts = new TrustManager[]{
-                new X509TrustManager() {
-                  public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
-                  public void checkClientTrusted(X509Certificate[] certs, String authType) { }
-                  public void checkServerTrusted(X509Certificate[] certs, String authType) { }
-                }
-        };
-
-        // Install the all-trusting trust manager
-        SSLContext sslContext = SSLContext.getInstance("SSL");
-        sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-        customBuilder.sslContext(sslContext);
-
-        // Create an HttpClient that uses the custom SSLContext
-        setHttpClientBuilder(customBuilder);
-      } catch (Exception e) {
-        throw new RuntimeException("failed to create custom HttpClient: " + e.getMessage(), e);
-      }
-    }
-  }
-
   /**
    * Create an instance of ApiClient.
    *
