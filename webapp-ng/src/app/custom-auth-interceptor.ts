3686b7786cd6339f1eb72a7e301dc1bac574df66
jcschaff <schaff.jim@gmail.com>
2024-07-04 17:04:49 -0400

    webapp CustomAuthInterceptor wasn't injecting Bearer <idToken> in header

--- webapp-ng/src/app/custom-auth-interceptor.ts
+++ webapp-ng/src/app/custom-auth-interceptor.ts
@@ -3,19 +3,20 @@ import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/c
 import { AuthService } from '@auth0/auth0-angular';
 import { Observable, throwError } from 'rxjs';
 import { catchError, mergeMap } from 'rxjs/operators';
+import { BaseuriConfigService } from './config/baseuri-config.service';
 
 @Injectable()
 export class CustomAuthInterceptor implements HttpInterceptor {
 
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private baseuriConfigService: BaseuriConfigService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    const url = 'http://localhost:3000/api'; // your API base url to check against
-    const requiresToken = req.method === 'POST' || req.method === 'PUT';
+    const url = this.baseuriConfigService.config?.baseUri; // your API base url to check against
+    const requiresToken = req.method === 'POST' || req.method === 'PUT' || req.method === 'GET' || req.method === 'DELETE';
 
-    // Check the request url starts with your api url, and if request method is POST or PUT
-    if (req.url.startsWith(url) && requiresToken) {
+    // Check the request url starts with your api url, and if request method is POST or PUT or GET or DELETE
+    if (url && req.url.startsWith(url) && requiresToken) {
       // Get the access token and clone the request to add the Authorization header
       return this.auth.idTokenClaims$.pipe(
         mergeMap(token => {
