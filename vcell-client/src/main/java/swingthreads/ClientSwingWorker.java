337ac53e3088aba8b73a9883d3f36f176628d428
Logan Drescher <drescher@uchc.edu>
2024-07-22 11:02:37 -0400

    Create instance of SwingWorker to help separate responsibility.

---
+++ vcell-client/src/main/java/swingthreads/ClientSwingWorker.java
@@ -0,0 +1,11 @@
+package swingthreads;
+
+public class ClientSwingWorker extends SwingWorker {
+    /**
+     * Compute the value to be returned by the <code>get</code> method.
+     */
+    @Override
+    public Object construct() {
+        return null;
+    }
+}
