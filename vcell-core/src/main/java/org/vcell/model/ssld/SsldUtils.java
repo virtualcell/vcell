038b9428fb04d16ae4aa80dd13dc544309d61109
Dan Vasilescu <vasilescu@uchc.edu>
2024-07-30 16:01:24 -0400

    WIP, convert ssldModel to bioModel, reaction rules

--- vcell-core/src/main/java/org/vcell/model/ssld/SsldUtils.java
+++ vcell-core/src/main/java/org/vcell/model/ssld/SsldUtils.java
@@ -1,31 +1,91 @@
 package org.vcell.model.ssld;
 
 import cbit.vcell.biomodel.BioModel;
+import cbit.vcell.mapping.ReactionRuleSpec;
 import cbit.vcell.model.*;
-import org.vcell.model.rbm.ComponentStateDefinition;
-import org.vcell.model.rbm.MolecularComponent;
-import org.vcell.model.rbm.MolecularType;
+import org.vcell.model.rbm.*;
 import org.vcell.solver.langevin.LangevinLngvWriter;
 
-import java.util.ArrayList;
+import java.util.*;
 import java.io.BufferedReader;
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Scanner;
 import java.util.prefs.Preferences;
 
 import static org.vcell.solver.langevin.LangevinLngvWriter.*;
 
 public class SsldUtils {
 
+    private class Mapping {
 
-    public static BioModel fromSsld(SsldModel ssldModel) throws Exception {
+        final SsldModel ssldModel;
+        final BioModel bioModel;
+
+        Map<Molecule, MolecularType> moleculeToMolecularTypeMap = new LinkedHashMap<>();
+        Map<MolecularType, Molecule> molecularTypeToMoleculeMap = new LinkedHashMap<>();
+        Map<SiteType, MolecularComponent> typeToComponentMap = new LinkedHashMap<>();
+        Map<MolecularComponent, SiteType> componentToTypeMap = new LinkedHashMap<>();
+
+        Map<Reaction, ReactionRule> reactionToReactionRuleMap = new LinkedHashMap<>();
+        Map<ReactionRule, Reaction> reactionRuleToReactionMap = new LinkedHashMap<>();
+        Map<ReactionRule, ReactionRuleSpec.Subtype> reactionRuleToSubtype = new LinkedHashMap<>();
+
+        public Mapping(SsldModel ssldModel, BioModel bioModel) {
+            this.ssldModel = ssldModel;
+            this.bioModel = bioModel;
+        }
+
+        void put(Molecule ssldMolecule, MolecularType mt) {
+            moleculeToMolecularTypeMap.put(ssldMolecule, mt);
+            molecularTypeToMoleculeMap.put(mt, ssldMolecule);
+        }
+        Molecule get(MolecularType mt) {
+            return molecularTypeToMoleculeMap.get(mt);
+        }
+        MolecularType get(Molecule ssldMolecule) {
+            return moleculeToMolecularTypeMap.get(ssldMolecule);
+        }
+
+        void put(SiteType st, MolecularComponent mc) {
+            typeToComponentMap.put(st, mc);
+            componentToTypeMap.put(mc, st);
+        }
+        SiteType get(MolecularComponent mc) {
+            return componentToTypeMap.get(mc);
+        }
+        MolecularComponent get(SiteType st) {
+            return typeToComponentMap.get(st);
+        }
+
+        void put(Reaction r, ReactionRule rr) {
+            reactionToReactionRuleMap.put(r, rr);
+            reactionRuleToReactionMap.put(rr, r);
+        }
+        Reaction get(ReactionRule rr) {
+            return reactionRuleToReactionMap.get(rr);
+        }
+        ReactionRule get(Reaction r) {
+            return reactionToReactionRuleMap.get(r);
+        }
+
+        void put(ReactionRule rr, ReactionRuleSpec.Subtype st) {
+            reactionRuleToSubtype.put(rr, st);
+        }
+        ReactionRuleSpec.Subtype getSubtype(ReactionRule rr) {
+            return reactionRuleToSubtype.get(rr);
+        }
+
+    }
+
+
+    public BioModel fromSsld(SsldModel ssldModel) throws Exception {
 
         ModelUnitSystem mus = ModelUnitSystem.createDefaultVCModelUnitSystem();
         BioModel bioModel = new BioModel(null);
         bioModel.setName(ssldModel.getSystemName());
         Model model = new Model("model", mus);
         bioModel.setModel(model);
+
+        Mapping m = new Mapping(ssldModel, bioModel);
 
         // structures
         if(bioModel.getModel().getStructures().length > 0) {
@@ -34,7 +94,7 @@ public class SsldUtils {
                 bioModel.getModel().removeStructure(struct, true);
             }
         }
-        LinkedList<Structure> newstructures = new LinkedList<Structure>();
+        LinkedList<Structure> newstructures = new LinkedList<>();
         Feature intra = new Feature(Structure.SpringStructureEnum.Intracellular.columnName);
         newstructures.add(intra);
         Membrane membr = new Membrane(Structure.SpringStructureEnum.Membrane.columnName);
@@ -56,15 +116,56 @@ public class SsldUtils {
                     ComponentStateDefinition csd = new ComponentStateDefinition(ssldState.getName());
                     mc.addComponentStateDefinition(csd);
                 }
+                m.put(ssldType, mc);
                 mt.addMolecularComponent(mc);
             }
+            // TODO: if membrane molecule, anchor it to membrane
+            m.put(ssldMolecule, mt);
             model.getRbmModelContainer().addMolecularType(mt, false);
         }
 
+        // reaction rules
+       for(BindingReaction ssldReaction : ssldModel.getBindingReactions()) {
+            boolean reversible = ssldReaction.getkoff() != 0 ? true : false;
+            Molecule ssldReactantOne = ssldReaction.getMolecule(0);
+            Molecule ssldReactantTwo = ssldReaction.getMolecule(1);
+            String locationOne = ssldReactantOne.getLocation();
+            String locationTwo = ssldReactantTwo.getLocation();
+            Structure structure;
+            if(locationOne.contentEquals(SystemGeometry.MEMBRANE) || locationTwo.contentEquals(SystemGeometry.MEMBRANE)) {
+                // reaction is on the Membrane if at least one reactant is on the Membrane, the product stay on the membrane
+                // actually the membrane molecule must be anchored to the membrane
+                structure = model.getStructure(SystemGeometry.MEMBRANE);
+            } else {
+                // both reactants must be in the same compartment, the product will also stay there
+                structure = model.getStructure(locationOne);
+            }
+            ReactionRule reactionRule = new ReactionRule(model, ssldReaction.getName(), structure, reversible);
+            RbmKineticLaw.RateLawType rateLawType = RbmKineticLaw.RateLawType.MassAction;
+            reactionRule.setKineticLaw(new RbmKineticLaw(reactionRule, rateLawType));
+            SpeciesPattern sp = getSpeciesPattern(ssldReactantOne, m);
+
+
+
+        }
 
 
         return bioModel;
     }
+
+    private SpeciesPattern getSpeciesPattern(Molecule molecule, Mapping m) {
+        SpeciesPattern sp = new SpeciesPattern();
+        MolecularTypePattern mtp = getMolecularTypePattern(molecule, m);
+        sp.addMolecularTypePattern(mtp);
+        return sp;
+    }
+    private MolecularTypePattern getMolecularTypePattern(Molecule molecule, Mapping m) {
+        MolecularTypePattern mtp = new MolecularTypePattern(m.get(molecule));
+
+
+        return mtp;
+    }
+
     public static SsldModel importSsldFile(File file) {
 
         SsldModel model = new SsldModel();
