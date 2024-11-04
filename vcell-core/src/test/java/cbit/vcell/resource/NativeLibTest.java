191fc933a3120649f1a5a62392cd9ed3451ec070
jcschaff <schaff.jim@gmail.com>
2024-11-04 14:18:12 -0500

    ncsa.jhdf5 MacOS ARM64 libs not loaded (until port Chombo/MB to io.jhdf)

--- vcell-core/src/test/java/cbit/vcell/resource/NativeLibTest.java
+++ vcell-core/src/test/java/cbit/vcell/resource/NativeLibTest.java
@@ -15,6 +15,10 @@ public class NativeLibTest {
 	@Test
 	public void loadEm( ) {
 		for (NativeLib nl : NativeLib.values()) {
+			// skip if nl is HDF5 and system is a macos arm64
+			if (nl == NativeLib.HDF5 && System.getProperty("os.arch").equals("aarch64") && System.getProperty("os.name").equals("Mac OS X")) {
+				continue;
+			}
 			nl.load();
 		}
 	}
