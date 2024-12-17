18aacaa2ae1aeb6cfa3b389c0ff4c57e2a9a72dd
Ezequiel Valencia <casualcucumber59@gmail.com>
2024-12-17 10:33:01 -0500

    Server Side Add Field Data

--- vcell-rest/src/main/java/org/vcell/restq/handlers/FieldData/FieldDataDB.java
+++ vcell-rest/src/main/java/org/vcell/restq/handlers/FieldData/FieldDataDB.java
@@ -1,24 +1,41 @@
 package org.vcell.restq.handlers.FieldData;
 
+import cbit.image.ImageException;
+import cbit.image.VCImageUncompressed;
 import cbit.vcell.field.FieldDataDBOperationResults;
 import cbit.vcell.field.FieldDataDBOperationSpec;
+import cbit.vcell.field.FieldDataFileConversion;
+import cbit.vcell.field.io.FieldDataFileOperationResults;
+import cbit.vcell.field.io.FieldDataFileOperationSpec;
+import cbit.vcell.geometry.RegionImage;
+import cbit.vcell.math.VariableType;
 import cbit.vcell.modeldb.DatabaseServerImpl;
+import cbit.vcell.resource.PropertyLoader;
+import cbit.vcell.simdata.DataSetControllerImpl;
+import cbit.vcell.solvers.CartesianMesh;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.apache.commons.io.IOUtils;
 import org.vcell.restq.db.AgroalConnectionFactory;
 import org.vcell.util.DataAccessException;
+import org.vcell.util.document.ExternalDataIdentifier;
+import org.vcell.util.document.KeyValue;
 import org.vcell.util.document.User;
 
+import java.io.*;
 import java.sql.SQLException;
+import java.util.zip.DataFormatException;
 
 @ApplicationScoped
 public class FieldDataDB {
 
     private final DatabaseServerImpl databaseServer;
+    private final DataSetControllerImpl dataSetController;
 
     @Inject
-    public FieldDataDB(AgroalConnectionFactory agroalConnectionFactory) throws DataAccessException {
+    public FieldDataDB(AgroalConnectionFactory agroalConnectionFactory) throws DataAccessException, FileNotFoundException {
         databaseServer = new DatabaseServerImpl(agroalConnectionFactory, agroalConnectionFactory.getKeyFactory());
+        dataSetController = new DataSetControllerImpl(null, new File("/simdata"), new File("/simdata"));
     }
 
     public FieldDataDBOperationResults copyNoConflict(User user, FieldDataDBOperationSpec spec) throws DataAccessException {
@@ -29,7 +46,59 @@ public class FieldDataDB {
         return databaseServer.fieldDataDBOperation(user, spec);
     }
 
-    public FieldDataDBOperationResults saveNewFieldData(User user, FieldDataDBOperationSpec spec) throws DataAccessException {
+    public FieldDataFileOperationSpec generateFieldDataFromFile(InputStream imageFile, String fileName) throws DataAccessException, ImageException, DataFormatException {
+        if (imageFile == null) {
+            throw new DataAccessException("No file present");
+        }
+        if (!fileName.contains(".vfrap")) {
+            try {
+                File tmpFile = File.createTempFile("tmp", "file");
+                tmpFile.deleteOnExit();
+                FileOutputStream out = new FileOutputStream(tmpFile);
+                IOUtils.copy(imageFile, out);
+                out.close();
+                return FieldDataFileConversion.createFDOSFromImageFile(tmpFile, false, null);
+            } catch (DataFormatException | IOException ex) {
+                throw new RuntimeException("Cannot read image " + fileName + "\n" + ex.getMessage());
+            }
+        } else {
+            throw new DataFormatException("Can't use .vfrap files");
+        }
+    }
+
+
+    public FieldDataFileOperationResults saveNewFieldDataFromFile(FieldDataResource.SaveFieldDataFromFile saveFieldData, User user) throws DataAccessException, ImageException, DataFormatException {
+
+
+        VariableType[] varTypes = new VariableType[saveFieldData.varNames().length];
+        for (int j = 0; j < saveFieldData.varNames().length; j++){
+            varTypes[j] = VariableType.VOLUME;
+        }
+        FieldDataDBOperationSpec fieldDataDBOperationSpec = FieldDataDBOperationSpec.createSaveNewExtDataIDSpec(user, saveFieldData.name(), saveFieldData.annotation());
+        FieldDataDBOperationResults results = databaseServer.fieldDataDBOperation(user, fieldDataDBOperationSpec);
+        FieldDataFileOperationSpec fdos = new FieldDataFileOperationSpec(saveFieldData.shortSpecData(), null, null,
+                results.extDataID, saveFieldData.varNames(), varTypes, saveFieldData.times(), user,
+                saveFieldData.origin(), saveFieldData.extent(), saveFieldData.isize(), saveFieldData.annotation(),
+                -1, null, user);
+        CartesianMesh cartesianMesh;
+        fdos.opType = FieldDataFileOperationSpec.FDOS_ADD;
+        try {
+            cartesianMesh = CartesianMesh.createSimpleCartesianMesh(fdos.origin, fdos.extent, fdos.isize,
+                    new RegionImage(new VCImageUncompressed(null, new byte[fdos.isize.getXYZ()],//empty regions
+                            fdos.extent, fdos.isize.getX(), fdos.isize.getY(), fdos.isize.getZ()),
+                            0, null, null, RegionImage.NO_SMOOTHING));
+            fdos.cartesianMesh = cartesianMesh;
+            FieldDataFileOperationResults fileSaveResults = dataSetController.fieldDataFileOperation(fdos);
+            fileSaveResults.externalDataIdentifier = results.extDataID;
+            return fileSaveResults;
+        } catch (Exception e) {
+            // Remove DB entry if file creation fails
+            databaseServer.fieldDataDBOperation(user, FieldDataDBOperationSpec.createDeleteExtDataIDSpec(results.extDataID));
+            throw new RuntimeException(e);
+        }
+    }
+
+    public FieldDataDBOperationResults saveNewFieldDataIntoDB(User user, FieldDataDBOperationSpec spec) throws DataAccessException {
         return databaseServer.fieldDataDBOperation(user, spec);
     }
 
--- vcell-rest/src/main/java/org/vcell/restq/handlers/FieldData/FieldDataResource.java
+++ vcell-rest/src/main/java/org/vcell/restq/handlers/FieldData/FieldDataResource.java
@@ -1,24 +1,35 @@
 package org.vcell.restq.handlers.FieldData;
 
+import cbit.image.ImageException;
 import cbit.vcell.field.FieldDataDBOperationResults;
 import cbit.vcell.field.FieldDataDBOperationSpec;
+import cbit.vcell.field.io.FieldDataFileOperationResults;
+import cbit.vcell.field.io.FieldDataFileOperationSpec;
 import io.quarkus.security.identity.SecurityIdentity;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.jboss.resteasy.reactive.PartType;
 import org.vcell.restq.db.UserRestDB;
 import org.vcell.util.DataAccessException;
+import org.vcell.util.Extent;
+import org.vcell.util.ISize;
+import org.vcell.util.Origin;
 import org.vcell.util.document.ExternalDataIdentifier;
 import org.vcell.util.document.KeyValue;
+import org.vcell.util.document.User;
 
+import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Vector;
+import java.util.zip.DataFormatException;
 
 @Path("/api/v1/fieldData")
 @RequestScoped
@@ -56,18 +67,47 @@ public class FieldDataResource {
     }
 
     @POST
-    @Path("/new")
+    @Path("/createFieldDataFromSimulation")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    @Operation(operationId = "createNewFieldData", summary = "Create a new field data component.")
-    public ExternalDataIdentifier createNewFieldData(FieldDataDBOperationSpec fieldDataDBOperationSpec){
+    @Operation(operationId = "createNewFieldDataFromSimulation", summary = "Create new field data from a simulation.")
+    public ExternalDataIdentifier submitNewFieldDataToDB(FieldDataDBOperationSpec fieldDataDBOperationSpec){
         FieldDataDBOperationResults results = null;
         try {
-            results = fieldDataDB.saveNewFieldData(userRestDB.getUserFromIdentity(securityIdentity), fieldDataDBOperationSpec);
+            results = fieldDataDB.saveNewFieldDataIntoDB(userRestDB.getUserFromIdentity(securityIdentity), fieldDataDBOperationSpec);
         } catch (DataAccessException e) {
             throw new WebApplicationException(e.getMessage(), 500);
         }
         return results.extDataID;
+    }
+
+    @POST
+    @Path("/generateFieldDataEstimate")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Operation(operationId = "generateFieldDataEstimate")
+    public FieldDataFileOperationSpec generateFieldDataEstimate(SubmittedFieldDataFile spec){
+        try{
+            return fieldDataDB.generateFieldDataFromFile(spec.file, spec.fileName);
+        } catch (ImageException | DataFormatException | DataAccessException e) {
+            throw new WebApplicationException("Can't create new field data file", 500);
+        }
+    }
+
+    @POST
+    @Path("/createFieldDataFromFile")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(operationId = "createNewFieldDataFromFile")
+    public FieldDataFileOperationResults createNewFieldDataFromFile(SaveFieldDataFromFile saveFieldData){
+        FieldDataFileOperationResults fileResults = null;
+        try{
+            User user = userRestDB.getUserFromIdentity(securityIdentity);
+            fileResults = fieldDataDB.saveNewFieldDataFromFile(saveFieldData, user);
+        } catch (ImageException | DataFormatException | DataAccessException e) {
+            throw new WebApplicationException(e.getMessage(), Response.Status.INTERNAL_SERVER_ERROR);
+        }
+        return fileResults;
     }
 
     @POST
@@ -108,5 +148,26 @@ public class FieldDataResource {
             String[] externalDataAnnotations,
             HashMap<ExternalDataIdentifier, Vector<KeyValue>> externalDataIDSimRefs
     ) { }
+
+    public record SaveFieldDataFromFile(
+            short[][][] shortSpecData,  //[time][var][data]
+            String[] varNames,
+            double[] times,
+            Origin origin,
+            Extent extent,
+            ISize isize,
+            String annotation,
+            String name
+    ){ }
+
+    public static class SubmittedFieldDataFile {
+        @FormParam("file")
+        @PartType(MediaType.APPLICATION_OCTET_STREAM)
+        public InputStream file;
+
+        @FormParam("fieldDataName")
+        @PartType(MediaType.TEXT_PLAIN)
+        public String fileName;
+    }
 
 }
