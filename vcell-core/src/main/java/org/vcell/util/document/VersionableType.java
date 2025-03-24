4d221e9d1b9f8b0026973f2d5bde1963af85da61
Ezequiel Valencia <casualcucumber59@gmail.com>
2025-03-24 07:54:20 -0400

    Update Versionables

--- vcell-core/src/main/java/org/vcell/util/document/VersionableType.java
+++ vcell-core/src/main/java/org/vcell/util/document/VersionableType.java
@@ -20,14 +20,23 @@ public class VersionableType implements java.io.Serializable {
 	private Class<?> vClass;
 	private boolean bTopLevel;
 
-	public static final VersionableType VCImage				= new VersionableType(0,"VCImage", "cbit.image.VCImage",true);
-	public static final VersionableType Geometry			= new VersionableType(1,"Geometry", "cbit.vcell.geometry.Geometry",true);
-	public static final VersionableType MathDescription		= new VersionableType(2,"MathDescription", "cbit.vcell.math.MathDescription",false);
-	public static final VersionableType Model				= new VersionableType(3,"Physiology", "cbit.vcell.model.Model",false);
-	public static final VersionableType SimulationContext	= new VersionableType(5,"SimulationSpecification", "cbit.vcell.mapping.SimulationContext",false);
-	public static final VersionableType Simulation			= new VersionableType(6,"Simulation", "cbit.vcell.solver.Simulation",false);
-	public static final VersionableType BioModelMetaData	= new VersionableType(7,"BioModel", "cbit.vcell.biomodel.BioModelMetaData",true);
-	public static final VersionableType MathModelMetaData	= new VersionableType(8,"MathModel", "cbit.vcell.mathmodel.MathModelMetaData",true);
+	private static final String VCImageName = "VCImage";
+	private static final String GeometryName = "Geometry";
+	private static final String MathDescriptionName = "MathDescription";
+	private static final String ModelName = "Physiology";
+	private static final String SimulationContextName = "SimulationSpecification";
+	private static final String SimulationName = "Simulation";
+	private static final String BioModelName = "BioModel";
+	private static final String MathModelName = "MathModel";
+
+	public static final VersionableType VCImage				= new VersionableType(0, VCImageName, "cbit.image.VCImage",true);
+	public static final VersionableType Geometry			= new VersionableType(1, GeometryName, "cbit.vcell.geometry.Geometry",true);
+	public static final VersionableType MathDescription		= new VersionableType(2, MathDescriptionName, "cbit.vcell.math.MathDescription",false);
+	public static final VersionableType Model				= new VersionableType(3, ModelName, "cbit.vcell.model.Model",false);
+	public static final VersionableType SimulationContext	= new VersionableType(5, SimulationContextName, "cbit.vcell.mapping.SimulationContext",false);
+	public static final VersionableType Simulation			= new VersionableType(6, SimulationName, "cbit.vcell.solver.Simulation",false);
+	public static final VersionableType BioModelMetaData	= new VersionableType(7, BioModelName, "cbit.vcell.biomodel.BioModelMetaData",true);
+	public static final VersionableType MathModelMetaData	= new VersionableType(8, MathModelName, "cbit.vcell.mathmodel.MathModelMetaData",true);
 /**
  * VersionableType constructor comment.
  */
@@ -79,6 +88,26 @@ public boolean getIsTopLevel() {
 public String getTypeName() {
 	return name;
 }
+
+public static VersionableType getTypeFromName(String name){
+	VersionableType versionableType = null;
+	switch (name){
+		case VCImageName -> {
+			versionableType = VCImage;
+		}
+		case GeometryName -> {
+			versionableType = Geometry;
+		}
+		case MathDescriptionName -> versionableType = MathDescription;
+		case ModelName -> versionableType = Model;
+		case SimulationContextName -> versionableType = SimulationContext;
+		case SimulationName -> versionableType = Simulation;
+		case BioModelName -> versionableType = BioModelMetaData;
+		case MathModelName -> versionableType = MathModelMetaData;
+	}
+	return versionableType;
+}
+
 /**
  * This method was created in VisualAge.
  * @return java.lang.Class
