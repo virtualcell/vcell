0bf1f7fbeb8be3bc70fa58a66d63918cbc365742
jcschaff <schaff.jim@gmail.com>
2025-04-04 11:25:53 -0400

    need access to DataSet constructor and variableType from libvcell

--- vcell-core/src/main/java/cbit/vcell/simdata/DataSet.java
+++ vcell-core/src/main/java/cbit/vcell/simdata/DataSet.java
@@ -28,7 +28,7 @@ public class DataSet implements java.io.Serializable
    private FileHeader fileHeader;
    private String fileName;
 
-DataSet() {
+public DataSet() {
 	dataBlockList = new Vector<DataBlock>();
 	fileHeader = new FileHeader();
 }
@@ -196,7 +196,7 @@ int getSizeZ() {
  * @return double[]
  * @param varName java.lang.String
  */
-int getVariableTypeInteger(String varName) throws IOException {
+public int getVariableTypeInteger(String varName) throws IOException {
 	for (int i=0;i<dataBlockList.size();i++){
 		DataBlock dataBlock = (DataBlock)dataBlockList.elementAt(i);
 		if (varName.trim().equals(dataBlock.getVarName().trim())){
