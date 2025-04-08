9103f33770ec95c5ba7acf511116067db6a6c51c
Ezequiel Valencia <casualcucumber59@gmail.com>
2025-04-08 13:55:45 -0400

    Update Webapp Auth's Config
    Simplify the configuration process by only having the remote API audience be the changing variable for each environment.
    Also ask for refresh tokens from Auth0 by specifying the audience and that we want offline access.

--- webapp-ng/src/environments/environment.remote.ts
+++ webapp-ng/src/environments/environment.remote.ts
@@ -1,44 +1,12 @@
-import config from '../../auth_config.json';
-
-const { domain, clientId, authorizationParams: { audience }, apiUri, errorPath } = config as {
-  domain: string;
-  clientId: string;
-  authorizationParams: {
-    audience?: string;
-  },
-  apiUri: string;
-  errorPath: string;
-};
 
 export const environment = {
-  production: true,
+  production: false,
   auth: {
-    domain,
-    clientId,
     authorizationParams: {
-      audience: `${audience}`,
-      redirect_uri: window.location.origin,
+      audience: `https://minikube.remote`,
     },
-    errorPath,
   },
-  apiUri: `${apiUri}`,
   httpInterceptor: {
-    allowedList: [
-      {
-        // uri: `${config.apiUri}/api/*`,
-        // uri: `${apiUri}/api/*`,
-        // uri: '/api/*',
-        uri: 'https://minikube-remote/api/*',
-
-        // allowAnonymous: true,
-        tokenOptions: {
-          authorizationParams: {
-            audience: `${audience}`,
-            scope: 'openid profile email'
-          }
-        }
-      },
-      ],
-
+    allowedList: [ 'https://minikube.remote/api/*' ],
   },
 };
