2a2408cdd7fd807d56493730b1f0248996fd7b4d
Ezequiel Valencia <casualcucumber59@gmail.com>
2025-05-16 08:25:21 -0400

    Update Data Access Exception Code

--- vcell-rest/src/main/java/org/vcell/restq/errors/exceptions/NotFoundException.java
+++ vcell-rest/src/main/java/org/vcell/restq/errors/exceptions/NotFoundException.java
@@ -4,7 +4,7 @@ import org.vcell.restq.errors.APIException;
 
 public class NotFoundException extends APIException {
     public final static int HTTP_CODE = 404;
-    public final static String DEFAULT_MESSAGE = "Not found.";
+    public final static String DEFAULT_MESSAGE = "Not found";
 
     public NotFoundException(String message, Exception e){
         super(HTTP_CODE, message, e);
