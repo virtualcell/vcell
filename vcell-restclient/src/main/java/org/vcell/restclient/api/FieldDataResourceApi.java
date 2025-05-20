67ee52c9dcee6b624a88f4b923070751826768ca
Ezequiel Valencia <ezq.valencia@gmail.com>
2025-05-20 14:34:44 -0400

    Update Field Data Patch

--- vcell-restclient/src/main/java/org/vcell/restclient/api/FieldDataResourceApi.java
+++ vcell-restclient/src/main/java/org/vcell/restclient/api/FieldDataResourceApi.java
@@ -12,6 +12,7 @@
 
 package org.vcell.restclient.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.vcell.restclient.ApiClient;
 import org.vcell.restclient.ApiException;
 import org.vcell.restclient.ApiResponse;
@@ -174,16 +175,20 @@ public class FieldDataResourceApi {
     multiPartBuilder.addBinaryBody("file", _file);
     hasFiles = true;
     multiPartBuilder.addTextBody("fileName", fileName.toString());
-    multiPartBuilder.addTextBody("extent", extent.toString());
-    multiPartBuilder.addTextBody("iSize", iSize.toString());
-    for (int i=0; i < channelNames.size(); i++) {
+      try {
+        multiPartBuilder.addTextBody("extent", memberVarObjectMapper.writeValueAsString(extent));
+        multiPartBuilder.addTextBody("iSize", memberVarObjectMapper.writeValueAsString(iSize));
+        multiPartBuilder.addTextBody("origin", memberVarObjectMapper.writeValueAsString(origin));
+      } catch (JsonProcessingException e) {
+          throw new RuntimeException(e);
+      }
+      for (int i=0; i < channelNames.size(); i++) {
         multiPartBuilder.addTextBody("channelNames", channelNames.get(i).toString());
     }
     for (int i=0; i < times.size(); i++) {
         multiPartBuilder.addTextBody("times", times.get(i).toString());
     }
     multiPartBuilder.addTextBody("annotation", annotation.toString());
-    multiPartBuilder.addTextBody("origin", origin.toString());
     HttpEntity entity = multiPartBuilder.build();
     HttpRequest.BodyPublisher formDataPublisher;
     if (hasFiles) {
