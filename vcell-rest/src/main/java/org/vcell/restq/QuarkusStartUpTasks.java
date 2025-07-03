d8b9eb04cba418571b8ac17594cae19cadf60f1d
Ezequiel Valencia <ezq.valencia@gmail.com>
2025-07-03 13:26:34 -0400

    Manual Queue Connection

--- vcell-rest/src/main/java/org/vcell/restq/QuarkusStartUpTasks.java
+++ vcell-rest/src/main/java/org/vcell/restq/QuarkusStartUpTasks.java
@@ -3,20 +3,29 @@ package org.vcell.restq;
 import cbit.sql.ServerStartUpTasks;
 import cbit.vcell.modeldb.AdminDBTopLevel;
 import io.quarkus.runtime.StartupEvent;
+import io.smallrye.common.annotation.Identifier;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.Produces;
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.vcell.restq.db.AgroalConnectionFactory;
 import org.vcell.util.DataAccessException;
 import org.vcell.util.document.User;
 
+import javax.jms.Connection;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.QueueConnection;
 import java.sql.SQLException;
 
 @ApplicationScoped
 public class QuarkusStartUpTasks {
     private final static Logger logger = LogManager.getLogger(QuarkusStartUpTasks.class);
+
+    public static final String internalMQBeanIdentifier = "internal";
 
     @Inject
     AgroalConnectionFactory connectionFactory;
@@ -25,6 +34,16 @@ public class QuarkusStartUpTasks {
         logger.info("Executing startup tasks");
         ServerStartUpTasks.executeStartUpTasks(connectionFactory);
         logger.info("Startup tasks executed successfully");
+    }
+
+    @Produces
+    @ApplicationScoped
+    @Identifier(internalMQBeanIdentifier)
+    private QueueConnection getInternalQueueConnectionFactory() throws JMSException {
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+        QueueConnection connection = connectionFactory.createQueueConnection("admin", "admin");
+        connection.start();
+        return connection;
     }
 
 }
