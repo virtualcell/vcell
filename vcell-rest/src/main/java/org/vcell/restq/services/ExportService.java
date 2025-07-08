c9a50b6cf4dd0b962b20d5c92d0c6fb7ee7dc376
Ricky Paul <67162435+paulricky@users.noreply.github.com>
2025-07-08 11:50:13 -0400

    Fixed implementation error and added TODO

--- vcell-rest/src/main/java/org/vcell/restq/services/ExportService.java
+++ vcell-rest/src/main/java/org/vcell/restq/services/ExportService.java
@@ -1,6 +1,7 @@
 package org.vcell.restq.services;
 
 import cbit.vcell.modeldb.DatabaseServerImpl;
+import cbit.vcell.modeldb.ExportHistoryDBDriver;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.vcell.restq.db.AgroalConnectionFactory;
@@ -18,12 +19,13 @@ public class ExportService {
     }
 
 
-    public ExportResource.ExportHistory getExportHistory(User user) throws DataAccessException {
-        return new ExportResource.ExportHistory("Hello");
+    public ExportHistoryDBDriver.ExportHistory getExportHistory(User user) throws DataAccessException {
+        // TODO: Pipe down to DB layer
+        return null;
     }
 
-    public void addExportHistory(User user, ExportResource.ExportHistory history) throws DataAccessException {
-        databaseServer.addExportHistory(user, history.exportHistory());
+    public void addExportHistory(User user, ExportHistoryDBDriver.ExportHistory history) throws DataAccessException {
+        databaseServer.addExportHistory(user, history);
     }
 
 }
