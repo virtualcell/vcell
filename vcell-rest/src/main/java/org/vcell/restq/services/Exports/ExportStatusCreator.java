8d82478de782b13ae550f4696d829de532e8dbad
Ezequiel Valencia <ezq.valencia@gmail.com>
2025-07-16 10:35:19 -0400

    Update Export Creation and Status

--- vcell-rest/src/main/java/org/vcell/restq/services/Exports/ExportStatusCreator.java
+++ vcell-rest/src/main/java/org/vcell/restq/services/Exports/ExportStatusCreator.java
@@ -8,6 +8,7 @@ import cbit.vcell.export.server.TimeSpecs;
 import cbit.vcell.export.server.VariableSpecs;
 import cbit.vcell.solver.VCSimulationDataIdentifier;
 import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.helpers.MultiEmitterProcessor;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.vcell.util.ObjectNotFoundException;
@@ -17,11 +18,13 @@ import org.vcell.util.document.User;
 import org.vcell.util.document.VCDataIdentifier;
 
 import java.util.Hashtable;
+import java.util.concurrent.ConcurrentHashMap;
 
 @ApplicationScoped
 public class ExportStatusCreator implements ExportStatusEventCreator {
-    private final Hashtable<Long, User> jobRequestToUser = new Hashtable<Long, User>();
-    private final Hashtable<String, MultiEmitterProcessor<ExportEvent>> listeners = new Hashtable<>();
+    private final ConcurrentHashMap<Long, User> jobRequestToUser = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, MultiEmitterProcessor<ExportEvent>> listeners = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, ExportEvent> mostRecentExportEvents = new ConcurrentHashMap<>();
 
     protected ExportSpecs exportExists(ExportSpecs exportSpecs) {
         // Call DB for export history and check if it exists. Store in cache if it exists for future requests.
@@ -30,8 +33,9 @@ public class ExportStatusCreator implements ExportStatusEventCreator {
 
     public ExportEvent fireExportCompleted(long jobID, VCDataIdentifier vcdID, String format, String location, ExportSpecs exportSpecs) {
         User user = jobRequestToUser.get(jobID);
+        String key = entryKey(user, jobID);
 
-        if (!listeners.containsKey(user.getName() + jobID)) {
+        if (!listeners.containsKey(key)) {
             throw new RuntimeException("Did not find entry for user " + user.getName() + " and job id " + jobID);
         }
         TimeSpecs timeSpecs = (exportSpecs!=null)?exportSpecs.getTimeSpecs():(null);
@@ -50,8 +54,9 @@ public class ExportStatusCreator implements ExportStatusEventCreator {
         event.setHumanReadableExportData(exportSpecs != null ? exportSpecs.getHumanReadableExportData() : null);
 
         event.setHumanReadableExportData(exportSpecs.getHumanReadableExportData());
-        listeners.get(user.getName() + jobID).onNext(event);
-        listeners.get(user.getName() + jobID).complete();
+        listeners.get(key).onNext(event);
+        listeners.get(key).complete();
+        mostRecentExportEvents.put(key, event);
         removeServerExportListener(user, jobID);
         return event;
     }
@@ -61,16 +66,30 @@ public class ExportStatusCreator implements ExportStatusEventCreator {
     }
 
     public synchronized Multi<ExportEvent> getUsersExportStatus(User user, long exportJobID) throws ObjectNotFoundException {
-        if (!listeners.containsKey(user.getName() + exportJobID)) {
+        String key = entryKey(user, exportJobID);
+        if (!listeners.containsKey(key)) {
             throw new ObjectNotFoundException("Did not find entry for user " + user.getName() + " and job id " + exportJobID);
         }
-        return listeners.get(user.getName() + exportJobID).toMulti();
+        return listeners.get(key).toMulti();
+    }
+
+    public synchronized ExportEvent getMostRecentExportStatus (User user, long exportJobID) throws ObjectNotFoundException {
+        String key = entryKey(user, exportJobID);
+        if (!mostRecentExportEvents.containsKey(key)) {
+            throw new ObjectNotFoundException("Did not find entry for user " + user.getName() + " and job id " + exportJobID);
+        }
+        return mostRecentExportEvents.get(key);
     }
 
     public synchronized void addServerExportListener(User user, long exportJobID){
         MultiEmitterProcessor<ExportEvent> emitter = MultiEmitterProcessor.create();
-        listeners.put(user.getName() + exportJobID, emitter);
+        String key = entryKey(user, exportJobID);
+        listeners.put(key, emitter);
         jobRequestToUser.put(exportJobID, user);
+        ExportEvent event = new ExportEvent(
+                this, exportJobID, user, "", null, ExportEvent.EXPORT_ASSEMBLING,
+                "", "", 0.0, null, null);
+        mostRecentExportEvents.put(key, event);
     }
 
     public void fireExportAssembling(long jobID, VCDataIdentifier vcdID, String format) {
@@ -80,21 +99,25 @@ public class ExportStatusCreator implements ExportStatusEventCreator {
     }
 
     public void fireExportEvent(ExportEvent event) {
-        if (!listeners.containsKey(event.getUser().getName() + event.getJobID())){
+        String key = entryKey(event.getUser(), event.getJobID());
+        if (!listeners.containsKey(key)){
             throw new RuntimeException("Did not find entry for user " + event.getUser().getName() + " and job id " + event.getJobID());
         }
         MultiEmitterProcessor<ExportEvent> listener = listeners.get(event.getUser().getName() + event.getJobID());
         listener.onNext(event);
+        mostRecentExportEvents.put(key, event);
     }
 
     public void fireExportFailed(long jobID, VCDataIdentifier vcdID, String format, String message) {
         User user = jobRequestToUser.get(jobID);
-        if (!listeners.containsKey(user.getName() + jobID)) {
+        String key = entryKey(user, jobID);
+        if (!listeners.containsKey(key)) {
             throw new RuntimeException("Did not find entry for user " + user.getName() + " and job id " + jobID);
         }
         ExportEvent event = new ExportEvent(this, jobID, user, vcdID, ExportEvent.EXPORT_FAILURE, format, message, null, null, null);
-        listeners.get(user.getName() + jobID).onNext(event);
-        listeners.get(user.getName() + jobID).complete();
+        listeners.get(key).onNext(event);
+        listeners.get(key).complete();
+        mostRecentExportEvents.put(key, event);
         removeServerExportListener(user, jobID);
     }
 
@@ -114,5 +137,9 @@ public class ExportStatusCreator implements ExportStatusEventCreator {
 
     public synchronized void removeServerExportListener(User user, long exportJobID){
         listeners.remove(user.getName() + exportJobID);
+    }
+
+    private String entryKey(User user, long jobID){
+        return user.getName() + jobID;
     }
 }
