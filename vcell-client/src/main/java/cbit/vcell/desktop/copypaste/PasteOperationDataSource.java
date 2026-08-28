71abab561055a4e7e339aeaa32b2b07dbb86fa77
Logan Drescher <drescher@uchc.edu>
2026-08-28 14:41:20 -0400

    Formalized PasteOperationDataSource classes

--- vcell-client/src/main/java/cbit/vcell/desktop/copypaste/PasteOperationDataSource.java
+++ vcell-client/src/main/java/cbit/vcell/desktop/copypaste/PasteOperationDataSource.java
@@ -1,12 +1,15 @@
 package cbit.vcell.desktop.copypaste;
 
 import cbit.vcell.parser.ExpressionBindingException;
+import cbit.vcell.parser.ExpressionException;
 
 public interface PasteOperationDataSource<T> {
 
 	PasteOperationTableModelRow<T> createTableModelRow();
 
-	boolean isProposedChangeRedundant();
+	boolean isProposedDifferentThanOriginal();
 
-	void performChange() throws ExpressionBindingException;
+	void performChange() throws ExpressionBindingException, ExpressionException;
+
+	String[] getColumnNames();
 }
